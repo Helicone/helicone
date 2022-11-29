fd15233a550c45c20ee01ca678d2235c0a6a33fd
Justin <justintorre75@gmail.com>
2022-11-29 17:51:48 -0600

    added supabase and main logic for passing back the exact response from openAI while reducing latency to the minimum possible

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,22 +1,49 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+  SUPABASE_SERVICE_ROLE_KEY: string;
+  SUPABASE_URL: string;
+}
+
+interface SuccessResult {
+  data: string;
+  error: null;
+}
+interface ErrorResult {
+  data: null;
+  error: string;
+}
+
+type Result = SuccessResult | ErrorResult;
+
+function forwardRequestToOpenAi(
+  request: Request,
+  body: string
+): Promise<Response> {
+  let url = new URL(request.url);
+  const new_url = new URL(`https://api.openai.com${url.pathname}`);
+  return fetch(new_url.href, {
+    method: request.method,
+    headers: request.headers,
+    body,
+  });
+}
+async function logRequest(
+  dbClient: SupabaseClient,
+  request: Request,
+  body: string
+): Promise<Result> {
+  return { data: "THis is a UUID", error: null };
+}
+
+async function logResponse(
+  dbClient: SupabaseClient,
+  requestId: string,
+  response: Response
+): Promise<void> {
+  console.log("Logging id");
+  await new Promise((f) => setTimeout(f, 1500));
+  console.log("LOgged!", requestId);
 }
 
 export default {
@@ -25,6 +52,25 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World! :D ");
+    const auth = request.headers.get("Authorization");
+    if (auth === null) {
+      return new Response("Not authorization header found!", { status: 401 });
+    }
+
+    const body = await request.text();
+    const dbClient = createClient(
+      env.SUPABASE_URL,
+      env.SUPABASE_SERVICE_ROLE_KEY
+    );
+    const [requestId, response] = await Promise.all([
+      logRequest(dbClient, request, body),
+      forwardRequestToOpenAi(request, body),
+    ]);
+
+    ctx.waitUntil(logResponse(dbClient, requestId.data!, response));
+    return new Response(response.body, {
+      ...response,
+      headers: { requestId: requestId.data!, ...response.headers },
+    });
   },
 };
