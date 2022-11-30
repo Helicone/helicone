3de6d092b6a0ccb43bf2e1df2bce3c9a83a6d99b
Justin <justintorre75@gmail.com>
2022-11-30 01:39:28 -0600

    initial frontend layout. Also added the hashing algorithm to the FE

---
+++ web/lib/supabaseClient.ts
@@ -0,0 +1,33 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
+
+if (supabaseUrl === "" || supabaseAnonKey === "") {
+  throw new Error("URL or Anon ENV not set");
+}
+
+async function hash(key: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const hashedKey = await crypto.subtle.digest(
+    { name: "SHA-256" },
+    encoder.encode(key)
+  );
+  const byteArray = Array.from(new Uint8Array(hashedKey));
+  const hexCodes = byteArray.map((value) => {
+    const hexCode = value.toString(16);
+    const paddedHexCode = hexCode.padStart(2, "0");
+    return paddedHexCode;
+  });
+  return hexCodes.join("");
+}
+
+export const supabaseClient = async (auth: string) =>
+  createClient(supabaseUrl, supabaseAnonKey, {
+    auth: { autoRefreshToken: true },
+    global: {
+      headers: {
+        authhash: await hash(auth),
+      },
+    },
+  });
