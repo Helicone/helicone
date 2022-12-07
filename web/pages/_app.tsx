e48a2236443b6fba4eadda41ee132dbba5352f9f
Justin <justintorre75@gmail.com>
2022-12-07 16:47:29 -0500

    Analytics

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   if (typeof window !== "undefined") {
     document.documentElement.classList.add("dark");
   }
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
