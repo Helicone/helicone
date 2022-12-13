188a5434e854051acc6f59d34dd4836f31fa3569
BarakOshri <barakoshri@gmail.com>
2022-12-13 15:39:17 -0600

    Add probability column (#2)
    * add probabilities
    
    * dont modify request
    
    * small edits
    
    * fmt
    
    * line
    
    * fix build error
    
    * added userId header
    
    * Revert "added userId header"
    
    This reverts commit 4dcbc5af0d2795b235224ff39a8e036e24620163.
    
    Co-authored-by: Barak Oshri <barak@gmail.com>
    Co-authored-by: Justin <justintorre75@gmail.com>

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -370,6 +370,30 @@ function RequestTable({ client }: { client: SupabaseClient }) {
     fetch();
   }, [client]);
   console.log(data[0]);
+  const probabilities = data.map((d) => {
+    const choice = d.response_body?.choices
+      ? d.response_body?.choices[0]
+      : null;
+    if (!choice) {
+      return null;
+    }
+
+    var prob;
+    if (choice.logprobs !== undefined && choice.logprobs !== null) {
+      const tokenLogprobs = choice.logprobs.token_logprobs;
+      const sum = tokenLogprobs.reduce(
+        (total: any, num: any) => total + num,
+        0
+      );
+      prob = Math.pow(Math.E, sum);
+      prob = (prob * 100).toFixed(3).concat("%");
+    } else {
+      prob = "";
+    }
+
+    return prob;
+  });
+
   return (
     <div className="h-full">
       <div>
@@ -385,11 +409,12 @@ function RequestTable({ client }: { client: SupabaseClient }) {
               <th className="text-left">Response</th>
               <th className="text-left">Duration</th>
               <th className="text-left">Token Count</th>
+              <th className="text-left">Probability</th>
               <th className="text-left">Copy</th>
             </tr>
           </thead>
           <tbody>
-            {data.map((row) => (
+            {data.map((row, i) => (
               <tr className="text-slate-300" key={row.request_id}>
                 <td>{new Date(row.request_created_at).toLocaleString()}</td>
                 <td>{truncString(row.request_body.prompt, 15)}</td>
@@ -414,6 +439,7 @@ function RequestTable({ client }: { client: SupabaseClient }) {
                     ? row.response_body.usage.total_tokens
                     : "{{ no tokens found }}"}
                 </td>
+                <td>{probabilities[i]}</td>
                 <td>
                   <DocumentDuplicateIcon
                     className="h-5 w-5 text-slate-300 hover:cursor-pointer"
