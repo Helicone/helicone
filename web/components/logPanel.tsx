ce3e515a8691aaf1be98bede216cb0212465cf5b
Justin Torre <justintorre75@gmail.com>
2022-12-13 18:04:00 -0500

    Moved things to components (#8)
    * Revert "Revert "added userId header""
    
    This reverts commit 0b003b7e5a7e6d74a0ee8750f7411777c674fbcd.
    
    * added user id to view and to table
    
    * moved everthing to helpers
    and components
    
    * nits
    
    * fix

---
+++ web/components/logPanel.tsx
@@ -0,0 +1,92 @@
+import { DocumentDuplicateIcon } from "@heroicons/react/24/solid";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { middleTruncString } from "../lib/stringHelpers";
+import { Request as ValyrRequest } from "../schema/request";
+import { ValyrResponse } from "../schema/resoponse";
+
+interface Log {
+  event: "request" | "response";
+  id: string;
+  created_at: Date;
+  body: string;
+}
+export function Logs({ client }: { client: SupabaseClient }) {
+  const [logs, setLogs] = useState<Log[]>([]);
+
+  useEffect(() => {
+    const channel = client.channel("db-messages");
+    channel.on(
+      "postgres_changes",
+      { event: "INSERT", schema: "public", table: "request" },
+      (payload) => {
+        console.log("REQUEST", payload);
+        const request: ValyrRequest = payload.new as unknown as ValyrRequest;
+        setLogs((logs) =>
+          logs.concat([
+            {
+              event: "request",
+              id: request.id,
+              created_at: new Date(request.created_at),
+              body: JSON.stringify(request.body),
+            },
+          ])
+        );
+      }
+    );
+    channel.on(
+      "postgres_changes",
+      { event: "INSERT", schema: "public", table: "response" },
+      (payload) => {
+        console.log("RESPONSE", payload);
+        const response: ValyrResponse = payload.new as unknown as ValyrResponse;
+        setLogs((logs) =>
+          logs.concat([
+            {
+              event: "response",
+              id: response.id,
+              created_at: new Date(response.created_at),
+              body: JSON.stringify(response.body),
+            },
+          ])
+        );
+      }
+    );
+    channel.subscribe(async (status) => {
+      console.log("STATUS", status);
+    });
+  }, [client]);
+
+  return (
+    <div>
+      {logs.reverse().map((log) => (
+        <LogCard log={log} key={log.id} />
+      ))}
+    </div>
+  );
+}
+
+function LogCard({ log: l, key }: { log: Log; key: string }): JSX.Element {
+  return (
+    <div
+      key={key}
+      className="flex flex-row justify-between items-center border-[1px] border-slate-700 rounded-lg px-5 py-3"
+    >
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row gap-2">
+          <p className="text-slate-300">{l.event}</p>
+          <p className="text-slate-300">{l.created_at.toLocaleString()}</p>
+        </div>
+        <div className="flex flex-row gap-2">
+          <p className="text-slate-300">{middleTruncString(l.body, 50)}</p>
+        </div>
+      </div>
+      <DocumentDuplicateIcon
+        className="h-5 w-5 text-slate-300 hover:cursor-pointer"
+        onClick={() => {
+          navigator.clipboard.writeText(l.body);
+        }}
+      />
+    </div>
+  );
+}
---
+++ web/components/requestTable.tsx
@@ -0,0 +1,124 @@
+import { DocumentDuplicateIcon } from "@heroicons/react/24/solid";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { truncString } from "../lib/stringHelpers";
+
+interface ResponseAndRequest {
+  response_body: any;
+  response_id: string;
+  response_created_at: string;
+  request_id: string;
+  request_body: any;
+  request_path: string;
+  request_created_at: string;
+  request_user_id: string;
+}
+
+export function RequestTable({ client }: { client: SupabaseClient }) {
+  const [data, setData] = useState<ResponseAndRequest[]>([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const { data, error } = await client
+        .from("response_and_request")
+        .select("*")
+        .order("request_created_at", { ascending: false })
+        .limit(1000);
+      if (error) {
+        console.log(error);
+      } else {
+        setData(data);
+      }
+    };
+    fetch();
+  }, [client]);
+  console.log(data[0]);
+  const probabilities = data.map((d) => {
+    const choice = d.response_body?.choices
+      ? d.response_body?.choices[0]
+      : null;
+    if (!choice) {
+      return null;
+    }
+
+    var prob;
+    if (choice.logprobs !== undefined && choice.logprobs !== null) {
+      const tokenLogprobs = choice.logprobs.token_logprobs;
+      const sum = tokenLogprobs.reduce(
+        (total: any, num: any) => total + num,
+        0
+      );
+      prob = Math.pow(Math.E, sum);
+      prob = (prob * 100).toFixed(3).concat("%");
+    } else {
+      prob = "";
+    }
+
+    return prob;
+  });
+
+  return (
+    <div className="h-full">
+      <div>
+        <span>Showing the most recent {} </span>
+        <span className="font-thin text-xs">(max 1000)</span>
+      </div>
+      <div className="h-full overflow-y-auto mt-3">
+        <table className="w-full mt-5 table-auto ">
+          <thead>
+            <tr className="text-slate-300">
+              <th className="text-left">Time</th>
+              <th className="text-left">Request</th>
+              <th className="text-left">Response</th>
+              <th className="text-left">Duration</th>
+              <th className="text-left">Token Count</th>
+              <th className="text-left">Probability</th>
+              <th className="text-left">User Id</th>
+              <th className="text-left">Copy</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, i) => (
+              <tr className="text-slate-300" key={row.request_id}>
+                <td>{new Date(row.request_created_at).toLocaleString()}</td>
+                <td>{truncString(row.request_body.prompt, 15)}</td>
+                <td>
+                  {truncString(
+                    row.response_body.choices
+                      ? row.response_body.choices[0].text
+                      : "{{ no reponse }}",
+                    15
+                  )}
+                </td>
+                <td>
+                  {(
+                    (new Date(row.response_created_at).getTime() -
+                      new Date(row.request_created_at).getTime()) /
+                    1000
+                  ).toString()}{" "}
+                  s
+                </td>
+                <td>
+                  {row.response_body.usage
+                    ? row.response_body.usage.total_tokens
+                    : "{{ no tokens found }}"}
+                </td>
+                <td>{probabilities[i]}</td>
+                <td>
+                  {row.request_user_id && truncString(row.request_user_id, 5)}
+                </td>
+                <td>
+                  <DocumentDuplicateIcon
+                    className="h-5 w-5 text-slate-300 hover:cursor-pointer"
+                    onClick={() => {
+                      navigator.clipboard.writeText(JSON.stringify(row));
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
