196fc4cf20a602120d7860c54e45885bb8f37a46
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-01-30 15:18:52 -0800

    feat: ui updates (#36)

--- web/lib/useKeys.ts
+++ web/lib/useKeys.ts
@@ -1,38 +1,31 @@
 import { SupabaseClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Database } from "../supabase/database.types";
 
 export function useKeys(supabaseClient: SupabaseClient) {
-  const [apiKeys, setApiKeys] = useState<
-    Database["public"]["Tables"]["user_api_keys"]["Row"][]
-  >([]);
+  const [apiKeys, setApiKeys] =
+    useState<Database["public"]["Tables"]["user_api_keys"]["Row"][]>();
 
-  useEffect(() => {
-    function getKeys() {
-      supabaseClient
-        .from("user_api_keys")
-        .select("*")
-        .then((res) => {
-          if (res.error) {
-            console.log(res.error);
-          } else {
-            setApiKeys(res.data);
-          }
-        });
-    }
-    getKeys();
+  const getKeys = useCallback(async () => {
     supabaseClient
-      .channel("user_api_keys_changes")
-      .on(
-        "postgres_changes",
-        { event: "*", schema: "public", table: "user_api_keys" },
-        () => {
-          getKeys();
+      .from("user_api_keys")
+      .select("*")
+      .then((res) => {
+        if (res.error) {
+          console.log(res.error);
+        } else {
+          const keys = res.data;
+          keys.forEach((key) => {
+            key.created_at = new Date(key.created_at).toLocaleString();
+          });
+          setApiKeys(keys);
         }
-      )
-      .subscribe(async (status) => {
-        console.log("STATUS", status);
       });
   }, [supabaseClient]);
-  return apiKeys;
+
+  useEffect(() => {
+    getKeys();
+  }, [getKeys]);
+
+  return { apiKeys, getKeys };
 }
---
+++ web/pages/onboarding.tsx
@@ -0,0 +1,44 @@
+import { ArrowDownIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/solid";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import NavBar from "../components/shared/navBar";
+import OnboardingPage from "../components/templates/onboarding/onboardingPage";
+
+interface OnboardingProps {
+  origin: string;
+  step?: number;
+}
+
+const Onboarding = (props: OnboardingProps) => {
+  const { origin, step } = props;
+
+  return <OnboardingPage step={step} origin={origin} />;
+};
+
+export default Onboarding;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const supabase = createServerSupabaseClient(context);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  const { req, query } = context;
+  const { step } = query;
+
+  return {
+    props: {
+      origin: req.headers.host,
+      step: parseInt(step as string),
+    },
+  };
+}
