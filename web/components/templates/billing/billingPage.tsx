d9b79b3c827d89d62105aeac6b9a0531fa93b722
Khoa Nguyen <scottmktn@gmail.com>
2023-02-03 15:56:31 -0800

    feat: usage page

--- web/components/templates/billing/billingPage.tsx
+++ web/components/templates/billing/billingPage.tsx
@@ -1,36 +1,23 @@
 import {
-  ArrowTopRightOnSquareIcon,
-  ChevronDoubleRightIcon,
-  ChevronRightIcon,
   LightBulbIcon,
+  WrenchScrewdriverIcon,
 } from "@heroicons/react/24/outline";
-import {
-  CheckIcon,
-  InformationCircleIcon,
-  KeyIcon,
-  TrashIcon,
-} from "@heroicons/react/24/solid";
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
-import { GetServerSidePropsContext } from "next";
-import Link from "next/link";
-import { FormEvent, useEffect, useState } from "react";
-import { hydrate } from "react-dom";
-import { middleTruncString } from "../../../lib/stringHelpers";
-import { hashAuth } from "../../../lib/supabaseClient";
-import { useKeys } from "../../../lib/useKeys";
-import { Database } from "../../../supabase/database.types";
-import ThemedTable from "../../shared/themedTable";
-import { loadStripe } from "@stripe/stripe-js";
 
-import getStripe from "../../../utlis/getStripe";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
+import { Database } from "../../../supabase/database.types";
+
 import Subscriptions from "./subscriptions";
 import { subscriptionChange } from "../../../lib/subscriptionChange";
 import AuthLayout from "../../shared/layout/authLayout";
-import { useRouter } from "next/router";
 import { UserSettingsResponse } from "../../../pages/api/user_settings";
 import Stripe from "stripe";
+import { clsx } from "../../shared/clsx";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 export type Tier = "free" | "pro" | "enterprise" | "pro-pending-cancel";
+
 export async function fetchPostJSON(url: string, data?: {}) {
   try {
     // Default options are marked with *
@@ -58,62 +45,6 @@ export async function fetchPostJSON(url: string, data?: {}) {
 
 interface BillingPageProps {}
 
-const ProgressBar = ({ percent }: { percent: number }) => {
-  return (
-    <div className="bg-gray-300 h-4 rounded overflow-hidden">
-      <div
-        className="bg-blue-500 text-xs  h-full"
-        style={{ width: `${percent}%` }}
-      ></div>
-    </div>
-  );
-};
-
-const RequestProgress = ({ userLimit }: { userLimit: number }) => {
-  const client = useSupabaseClient<Database>();
-  const [requestsCount, setRequestsCount] = useState(0);
-
-  useEffect(() => {
-    const startOfThisMonth = new Date();
-    startOfThisMonth.setDate(1);
-    startOfThisMonth.setHours(0);
-    startOfThisMonth.setMinutes(0);
-    startOfThisMonth.setSeconds(0);
-    startOfThisMonth.setMilliseconds(0);
-
-    client &&
-      client
-        .from("request_rbac")
-        .select("*", { count: "exact" })
-        .gte("created_at", startOfThisMonth.toISOString())
-        .then((res) => {
-          if (res.error !== null) {
-            console.error(res.error);
-          } else {
-            setRequestsCount(res.count!);
-          }
-        });
-  }, [client]);
-
-  // This is a simple progress bar using tailwindcss
-  return (
-    <div>
-      <ProgressBar
-        percent={
-          userLimit === 0
-            ? 0
-            : Math.round((requestsCount / userLimit) * 100 * 1000) / 1000
-        }
-      />
-      <div className="text-xs text-gray-500">
-        {requestsCount} / {userLimit} requests
-      </div>
-      <div className="text-xs text-gray-500">
-        You have {userLimit - requestsCount} requests left this month.
-      </div>
-    </div>
-  );
-};
 const CurrentSubscriptionStatus = ({
   subscription,
   tier,
@@ -123,14 +54,14 @@ const CurrentSubscriptionStatus = ({
 }) => {
   if (tier === "free") {
     return (
-      <div className="text-gray-500">
+      <div className="mt-2 text-sm text-gray-700">
         You are currently on the free tier. You can upgrade to the pro tier to
         get more requests per month.
       </div>
     );
   } else if (tier === "pro") {
     return (
-      <div className="text-gray-500">
+      <div className="mt-2 text-sm text-gray-700">
         You are currently on the pro tier. You can upgrade to the enterprise
         tier to get more requests per month.
       </div>
@@ -147,7 +78,7 @@ const CurrentSubscriptionStatus = ({
         24
     );
     return (
-      <div className="text-gray-500">
+      <div className="mt-2 text-sm text-gray-700">
         You pro account is still active until {endingDate} ({daysLeft} days
         left). You can upgrade to the enterprise tier to get more requests per
         month.
@@ -155,7 +86,7 @@ const CurrentSubscriptionStatus = ({
     );
   } else if (tier === "enterprise") {
     return (
-      <div className="text-gray-500">
+      <div className="mt-2 text-sm text-gray-700">
         You are currently on the enterprise tier. You can downgrade to the pro
         tier to get less requests per month.
       </div>
@@ -165,11 +96,24 @@ const CurrentSubscriptionStatus = ({
   }
 };
 
+interface PlanProps {
+  id: number;
+  name: string;
+  tier: Tier;
+  limit: string;
+  features: string[];
+  isCurrent: boolean;
+}
+
 const BillingPage = (props: BillingPageProps) => {
+  const client = useSupabaseClient<Database>();
+
   const [userSettings, setUserSettings] = useState<UserSettingsResponse | null>(
     null
   );
+  const [requestsCount, setRequestsCount] = useState(0);
   const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     fetch("/api/user_settings")
@@ -188,9 +132,27 @@ const BillingPage = (props: BillingPageProps) => {
         console.log("LIMIT IS", data);
         setUserSettings(data);
       });
-  }, []);
 
-  const router = useRouter();
+    const startOfThisMonth = new Date();
+    startOfThisMonth.setDate(1);
+    startOfThisMonth.setHours(0);
+    startOfThisMonth.setMinutes(0);
+    startOfThisMonth.setSeconds(0);
+    startOfThisMonth.setMilliseconds(0);
+
+    client
+      .from("request_rbac")
+      .select("*", { count: "exact" })
+      .gte("created_at", startOfThisMonth.toISOString())
+      .then((res) => {
+        if (res.error !== null) {
+          console.error(res.error);
+        } else {
+          setRequestsCount(res.count!);
+        }
+      });
+  }, [client]);
+
   if (userSettings === null) {
     return (
       <AuthLayout>
@@ -205,43 +167,202 @@ const BillingPage = (props: BillingPageProps) => {
     );
   }
   const currentTier = userSettings.user_settings.tier as Tier;
+  const userLimit = userSettings.user_settings.request_limit;
+
+  const plans: PlanProps[] = [
+    {
+      id: 1,
+      name: "Personal",
+      tier: "free",
+      limit: "1000 requests",
+      features: ["Basic Support", "User Metrics"],
+      isCurrent: currentTier === "free",
+    },
+    {
+      id: 2,
+      name: "Professional",
+      tier: "pro",
+      limit: "1,000,000 requests",
+      features: [
+        "Priority Support",
+        "Advanced Insights",
+        "Early Access to New Features",
+      ],
+      isCurrent: currentTier === "pro" || currentTier === "pro-pending-cancel",
+    },
+    {
+      id: 3,
+      name: "Enterprise",
+      tier: "enterprise",
+      limit: "unlimited requests",
+      features: [
+        "Prompt Discovery",
+        "Dedicated Support",
+        "Design Consultation",
+      ],
+      isCurrent: currentTier === "enterprise",
+    },
+  ];
 
   return (
     <AuthLayout>
-      <div className="flex flex-col space-y-16 overflow-scroll">
-        <div className="flex flex-col space-y-2">
-          <div className="text-gray-500">
-            Manage your billing and subscription
+      <div className="flex flex-col space-y-12">
+        <div className="flex flex-col space-y-4">
+          <div className="text-xl font-bold">Usage this month</div>
+          <div className="flex flex-col sm:flex-row w-full gap-4 h-full justify-between items-left sm:items-center">
+            <div className="flex flex-row w-full bg-gray-300 h-4 rounded-md">
+              <div
+                className="bg-gradient-to-r from-sky-500 to-purple-500 h-full rounded-md"
+                style={{
+                  width: `${
+                    userLimit === 0
+                      ? 0
+                      : Math.round((requestsCount / userLimit) * 100 * 1000) /
+                        1000
+                  }%`,
+                }}
+              />
+            </div>
+            <div className="flex flex-row min-w-[200px] text-sm sm:text-md">
+              {requestsCount} / {userLimit} requests
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col space-y-2 ">
-          <div className="text-xl font-bold">Request Limit</div>
-          <div className="text-gray-500">
-            You can make {userSettings.user_settings.request_limit ?? 0}{" "}
-            requests per month
-          </div>
-          <RequestProgress
-            userLimit={userSettings.user_settings.request_limit ?? 0}
-          />
-          <div className="border-2 p-5 rounded-md flex gap-5 items-center text-gray-500">
-            <LightBulbIcon className="h-6 w-6 text-gray-500" />
+          <div className="border-2 p-4 text-sm rounded-md flex flex-row items-center text-gray-600 border-gray-300 w-fit gap-4">
+            <LightBulbIcon className="h-4 w-4 text-gray-600" />
             We continue logging your requests after your limit is reached, you
             will just lose access to the dashboard until you upgrade.
           </div>
         </div>
+        <div className="">
+          <div className="sm:flex sm:items-center">
+            <div className="sm:flex-auto">
+              <h1 className="text-xl font-semibold text-gray-900">Plans</h1>
+              <CurrentSubscriptionStatus
+                subscription={userSettings.subscription}
+                tier={currentTier}
+              />
+            </div>
+          </div>
+          <div className="mt-10 ring-1 ring-gray-300 sm:-mx-6 md:mx-0 rounded-lg bg-white">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                  >
+                    Plan
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Limit
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Features
+                  </th>
 
-        <div className="flex flex-col space-y-2">
-          <div className="text-xl font-bold">Subscription</div>
-          <CurrentSubscriptionStatus
-            subscription={userSettings.subscription}
-            tier={currentTier}
-          />
-          <Subscriptions
-            activeSubscription={currentTier}
-            onClick={(newTier) =>
-              subscriptionChange(newTier, currentTier, router)
-            }
-          />
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span className="sr-only">Select</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {plans.map((plan, planIdx) => (
+                  <tr key={plan.id}>
+                    <td
+                      className={clsx(
+                        planIdx === 0 ? "" : "border-t border-transparent",
+                        "relative py-4 pl-4 sm:pl-6 pr-3 text-sm"
+                      )}
+                    >
+                      <div className="font-medium text-gray-900">
+                        {plan.name}
+                        {plan.isCurrent ? (
+                          <span className="ml-1 text-sky-600">
+                            (Current Plan)
+                          </span>
+                        ) : null}
+                      </div>
+                      <div className="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
+                        <span>{plan.limit}</span>
+                        <div>
+                          {plan.features.map((feature, idx) => (
+                            <div key={idx}>{feature}</div>
+                          ))}
+                        </div>
+                        {/* <span className="hidden sm:inline">·</span> */}
+                      </div>
+                      {planIdx !== 0 ? (
+                        <div className="absolute right-0 left-6 -top-px h-px bg-gray-200" />
+                      ) : null}
+                    </td>
+                    <td
+                      className={clsx(
+                        planIdx === 0 ? "" : "border-t border-gray-200",
+                        "hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+                      )}
+                    >
+                      {plan.limit}
+                    </td>
+                    <td
+                      className={clsx(
+                        planIdx === 0 ? "" : "border-t border-gray-200",
+                        "hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+                      )}
+                    >
+                      <div className="space-y-2">
+                        {plan.features.map((feature, idx) => (
+                          <div key={idx} className="flex flex-row">
+                            <WrenchScrewdriverIcon className="h-3 w-3 mt-1 mr-1 text-gray-500" />
+                            {feature}
+                          </div>
+                        ))}
+                      </div>
+                    </td>
+                    <td
+                      className={clsx(
+                        planIdx === 0 ? "" : "border-t border-transparent",
+                        "relative py-3.5 pl-3 pr-4 sm:pr-6 text-right text-sm font-medium"
+                      )}
+                    >
+                      {plan.tier === "enterprise" ? (
+                        <button
+                          type="button"
+                          className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30"
+                          disabled={plan.isCurrent}
+                          onClick={() =>
+                            subscriptionChange(plan.tier, currentTier, router)
+                          }
+                        >
+                          Select<span className="sr-only">, {plan.name}</span>
+                        </button>
+                      ) : (
+                        <button
+                          type="button"
+                          className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30"
+                          disabled={plan.isCurrent}
+                          onClick={() =>
+                            subscriptionChange(plan.tier, currentTier, router)
+                          }
+                        >
+                          Select<span className="sr-only">, {plan.name}</span>
+                        </button>
+                      )}
+
+                      {planIdx !== 0 ? (
+                        <div className="absolute right-6 left-0 -top-px h-px bg-gray-200" />
+                      ) : null}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </AuthLayout>
