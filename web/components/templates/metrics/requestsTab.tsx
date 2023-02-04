f356d4f485e8ef0ad5153b3fdf102ad0d2d3d7ab
Khoa Nguyen <scottmktn@gmail.com>
2023-02-04 14:54:54 -0800

    css fix

--- web/components/templates/metrics/requestsTab.tsx
+++ web/components/templates/metrics/requestsTab.tsx
@@ -66,28 +66,9 @@ export default function RequestsTab() {
 
   return (
     <div className="">
-      {/* REAALL Below */}
-      {/* <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Transactions</h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A table of placeholder stock market data that does not make any
-            sense.
-          </p>
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-          >
-            Export
-          </button>
-        </div>
-      </div> */}
-      {/* UNCOMMENT BELOW */}
-      <div className="flex flex-col">
-        <div className="overflow-x-auto">
-          <div className="inline-block min-w-full py-2 align-middle">
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
             <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
               <table className="min-w-full divide-y divide-gray-300">
                 <thead className="bg-gray-50">
@@ -138,57 +119,54 @@ export default function RequestsTab() {
                       scope="col"
                       className="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6"
                     >
-                      <span className="sr-only">View</span>
+                      <span className="sr-only">Edit</span>
                     </th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {data.map((req, idx) => (
-                    <tr key={req.request_id}>
+                  {data.map((row, idx) => (
+                    <tr key={row.request_id}>
                       <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">
-                        {new Date(req.request_created_at!).toLocaleString()}
+                        {new Date(row.request_created_at!).toLocaleString()}
                       </td>
                       <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
-                        {req.request_body?.prompt
-                          ? truncString(req.request_body.prompt, 15)
+                        {row.request_body?.prompt
+                          ? truncString(row.request_body.prompt, 15)
                           : "{{no prompt }}"}
                       </td>
                       <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
                         {truncString(
-                          req.response_body!.choices
-                            ? req.response_body!.choices[0].text
+                          row.response_body!.choices
+                            ? row.response_body!.choices[0].text
                             : "{{ no reponse }}",
                           15
                         )}
                       </td>
                       <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
                         {(
-                          (new Date(req.response_created_at!).getTime() -
-                            new Date(req.request_created_at!).getTime()) /
+                          (new Date(row.response_created_at!).getTime() -
+                            new Date(row.request_created_at!).getTime()) /
                           1000
                         ).toString()}{" "}
                         s
                       </td>
                       <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                        {req.response_body!.usage
-                          ? req.response_body!.usage.total_tokens
+                        {row.response_body!.usage
+                          ? row.response_body!.usage.total_tokens
                           : "{{ no tokens found }}"}
                       </td>
                       <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
                         {probabilities[idx]}
                       </td>
                       <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-                        {req.request_user_id &&
-                          truncString(req.request_user_id, 5)}
+                        {row.request_user_id &&
+                          truncString(row.request_user_id, 5)}
                       </td>
                       <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit
-                          <span className="sr-only">, {req.request_id}</span>
-                        </a>
+                        <button className="text-sky-600 hover:text-sky-900">
+                          View
+                          <span className="sr-only">, {row.request_id}</span>
+                        </button>
                       </td>
                     </tr>
                   ))}
