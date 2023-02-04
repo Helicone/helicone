74a3a26a033b6721a4c2750e26fcf659fbb89fb7
Khoa Nguyen <scottmktn@gmail.com>
2023-02-03 16:06:53 -0800

    remove disable

--- web/components/templates/billing/billingPage.tsx
+++ web/components/templates/billing/billingPage.tsx
@@ -335,7 +335,6 @@ const BillingPage = (props: BillingPageProps) => {
                         <button
                           type="button"
                           className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30"
-                          disabled={plan.isCurrent}
                           onClick={() =>
                             subscriptionChange(plan.tier, currentTier, router)
                           }
