9374c87d0f10fdf462e99ee844428a36b7e40ac8
Khoa Nguyen <scottmktn@gmail.com>
2023-02-04 17:27:11 -0800

    now? please?

--- web/components/templates/metrics/requestsTab.tsx
+++ web/components/templates/metrics/requestsTab.tsx
@@ -554,12 +554,12 @@ export default function RequestsTab() {
     <>
       <div className="">
         <div className="mt-4 flex flex-col">
-          <div className="overflow-hidden">
-            <div className="inline-block min-w-full py-2 align-middle">
-              <div className="overflow-auto shadow ring-1 ring-black ring-opacity-5 md:rounded-lg h-[70vh] max-h-[70vh]">
-                <table className="min-w-full divide-y divide-gray-30">
-                  <thead className="bg-gray-50 sticky top-0">
-                    <tr className="z-10">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-gray-50">
+                    <tr>
                       <th
                         scope="col"
                         className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
@@ -757,7 +757,7 @@ export default function RequestsTab() {
           </div>
           <div className="mt-5 sm:mt-6 w-full justify-between gap-4 flex flex-row">
             <button
-              autoFocus={false}
+              type="button"
               className="inline-flex w-full justify-center rounded-md border border-transparent bg-gray-300 px-4 py-2 text-base font-medium text-black shadow-sm hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 sm:text-sm"
               onClick={() => {
                 // TODO: add copy to clipboard notification
@@ -767,7 +767,7 @@ export default function RequestsTab() {
               Copy
             </button>
             <button
-              autoFocus={false}
+              type="button"
               className="inline-flex w-full justify-center rounded-md border border-transparent bg-sky-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:text-sm"
               onClick={() => setOpen(false)}
             >
