566bb7f1b02194fa642fcf1c11640027bc87316b
Khoa Nguyen <scottmktn@gmail.com>
2023-02-04 17:06:21 -0800

    ???

--- web/components/templates/metrics/requestsTab.tsx
+++ web/components/templates/metrics/requestsTab.tsx
@@ -93,10 +93,10 @@ export default function RequestsTab() {
         <div className="mt-4 flex flex-col">
           <div className="">
             <div className="inline-block min-w-full py-2 align-middle">
-              <div className="overflow-auto shadow ring-1 ring-black ring-opacity-5 md:rounded-lg max-h-[70vh]">
+              <div className="overflow-y-hidden overflow-x-auto shadow ring-1 ring-black ring-opacity-5 md:rounded-lg max-h-[70vh]">
                 <table className="min-w-full divide-y divide-gray-30">
                   <thead className="bg-gray-50 sticky top-0">
-                    <tr>
+                    <tr className="z-10">
                       <th
                         scope="col"
                         className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
