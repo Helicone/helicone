aef4c611403a9df99197806f26e2b9b239f1af4a
Khoa Nguyen <scottmktn@gmail.com>
2023-02-05 12:57:02 -0800

    fix error take 2

--- web/components/templates/requests/requestsTab.tsx
+++ web/components/templates/requests/requestsTab.tsx
@@ -146,8 +146,8 @@ export default function RequestsTab() {
           </div>
         </div>
         <div className="mt-4 flex flex-col">
-          <div className="-my-0 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="block min-w-full py-2 align-middle md:px-6 lg:px-8">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-300">
                   <thead className="bg-white">
