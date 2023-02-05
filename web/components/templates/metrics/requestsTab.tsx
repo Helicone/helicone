9e75df41e720b436cd9e4e04d618960e26f51973
Khoa Nguyen <scottmktn@gmail.com>
2023-02-04 18:10:49 -0800

    info

--- web/components/templates/metrics/requestsTab.tsx
+++ web/components/templates/metrics/requestsTab.tsx
@@ -90,6 +90,21 @@ export default function RequestsTab() {
   return (
     <>
       <div className="">
+        <div className="mt-4 sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <p className="mt-2 text-sm text-gray-700">
+              Showing the latest 100 requests.
+            </p>
+          </div>
+          {/* <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+            >
+              Export
+            </button>
+          </div> */}
+        </div>
         <div className="mt-4 flex flex-col">
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
