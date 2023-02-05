e2dd3391ce1fa94685cbd51a7ac10574e7de2842
Justin Torre <justintorre75@gmail.com>
2023-02-04 23:00:58 -0800

    models page (#18)

--- web/components/templates/dashboard/metricsPanel.tsx
+++ web/components/templates/dashboard/metricsPanel.tsx
@@ -18,6 +18,35 @@ const OPENAI_FINETUNE_COSTS = {
   davinci: 0.12,
 };
 
+export function modelCost(
+  modelRow: Database["public"]["Views"]["model_metrics"]["Row"]
+): number {
+  const model = modelRow.model;
+  const tokens = modelRow.sum_tokens;
+  if (tokens === null) {
+    console.error("Tokens is null");
+    return 0;
+  }
+  if (model === null) {
+    console.error("Model is null");
+    return 0;
+  }
+  const is_finetuned_model = model.includes(":");
+
+  const model_prefix = is_finetuned_model ? model.split(":")[0] : model;
+
+  const costs = is_finetuned_model ? OPENAI_FINETUNE_COSTS : OPENAI_COSTS;
+
+  const cost = Object.entries(costs).find(([key]) =>
+    model_prefix.includes(key)
+  )?.[1];
+  if (!cost) {
+    console.error("No cost found for model", model);
+    return 0;
+  }
+  return (cost * tokens) / 1000;
+}
+
 export function MetricsPanel() {
   const client = useSupabaseClient<Database>();
   interface Metrics {
@@ -103,34 +132,7 @@ export function MetricsPanel() {
           }
           if (metrics) {
             const total_cost = metrics.reduce((acc, m) => {
-              const model = m.model;
-              const tokens = m.sum_tokens;
-              if (tokens === null) {
-                console.error("Tokens is null");
-                return 0;
-              }
-              if (model === null) {
-                console.error("Model is null");
-                return 0;
-              }
-              const is_finetuned_model = model.includes(":");
-
-              const model_prefix = is_finetuned_model
-                ? model.split(":")[0]
-                : model;
-
-              const costs = is_finetuned_model
-                ? OPENAI_FINETUNE_COSTS
-                : OPENAI_COSTS;
-
-              const cost = Object.entries(costs).find(([key]) =>
-                model_prefix.includes(key)
-              )?.[1];
-              if (!cost) {
-                console.error("No cost found for model", model);
-                return 0;
-              }
-              return acc + (cost * tokens) / 1000;
+              return acc + modelCost(m);
             }, 0);
             setData((data) => ({
               ...data,
