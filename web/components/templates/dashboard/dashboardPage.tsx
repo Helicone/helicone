db79933fb2be15c76955201b3a8740731e5f4414
Khoa Nguyen <scottmktn@gmail.com>
2023-02-06 12:09:06 -0800

    fix demo on dashboard

--- web/components/templates/dashboard/dashboardPage.tsx
+++ web/components/templates/dashboard/dashboardPage.tsx
@@ -29,32 +29,6 @@ const DashboardPage = (props: DashboardPageProps) => {
   return (
     <AuthLayout>
       <div className="space-y-16">
-        {user?.email === "valyrdemo@gmail.com" && (
-          <div className="text-sm flex flex-col items-center justify-center bg-red-800 text-white p-2 mb-5">
-            <div
-              className="hover:bg-red-900 hover:cursor-pointer text-base underline flex flex-row items-center justify-center bg-red-800 text-white"
-              onClick={() => {
-                client.auth.signOut().then(() => {
-                  router.push("/");
-                });
-              }}
-            >
-              <ExclamationCircleIcon className="h-5 w-5 mr-2" />
-              <p className="">Exit demo</p>
-            </div>
-            <div>
-              Demo data from:{" "}
-              <Link
-                href="https://demoapp.valyrai.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                AI App Ideas
-              </Link>{" "}
-            </div>
-          </div>
-        )}
         <MetricsPanel />
         {/* <div className="h-20 bg-yellow-500">
         <Logs />
