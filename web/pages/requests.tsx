ce875389d2516b312e00d22f2d0f6b68db7256a0
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-02-07 15:18:36 -0800

    feat: pagination (#25)

--- web/pages/requests.tsx
+++ web/pages/requests.tsx
@@ -1,19 +1,82 @@
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSidePropsContext } from "next";
+import { getPagination } from "../components/shared/getPagination";
 import AuthLayout from "../components/shared/layout/authLayout";
 import MetaData from "../components/shared/metaData";
 import RequestsTab from "../components/templates/requests/requestsTab";
+import { getRequests, ResponseAndRequest } from "../services/lib/requests";
+import { Database } from "../supabase/database.types";
 
-interface RequestsProps {}
+interface RequestsProps {
+  initialSession: any;
+  user: any;
+  error: string | null;
+  data: ResponseAndRequest[];
+  count: number | null;
+  page: number;
+  from: number;
+  to: number;
+}
 
 const Requests = (props: RequestsProps) => {
-  const {} = props;
+  const { data, error, count, page, from, to } = props;
 
   return (
     <MetaData title="Users">
       <AuthLayout>
-        <RequestsTab />
+        <RequestsTab
+          requests={data}
+          error={error}
+          count={count}
+          page={page}
+          from={from}
+          to={to}
+        />
       </AuthLayout>
     </MetaData>
   );
 };
 
 export default Requests;
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const supabase = createServerSupabaseClient(context);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session)
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+
+  const { page, page_size } = context.query;
+
+  let currentPage = parseInt(page as string, 10) || 1;
+  const pageSize = parseInt(page_size as string, 10) || 25;
+
+  const { data, error, count, from, to } = await getRequests(
+    supabase,
+    currentPage,
+    pageSize
+  );
+
+  return {
+    props: {
+      initialSession: session,
+      user: session.user,
+      error: error?.message || null,
+      data: (data as ResponseAndRequest[]) || [],
+      count: count,
+      page: currentPage,
+      from: from,
+      to: to,
+    },
+  };
+};
