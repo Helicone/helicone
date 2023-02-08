2386994d34af5082a8ef5ea7bc83dcd251e19b00
Justin <justintorre75@gmail.com>
2023-02-07 18:49:08 -0800

    3m added

--- web/components/templates/dashboard/timeGraph.tsx
+++ web/components/templates/dashboard/timeGraph.tsx
@@ -40,7 +40,7 @@ const RenderLineChart = ({
           type="monotone"
           dataKey="count"
           stroke="#8884d8"
-          strokeWidth={1}
+          strokeWidth={1.5}
           animationDuration={0}
         />
         <XAxis dataKey="time" />
@@ -61,30 +61,53 @@ interface TimeGraphConfig {
 const timeGraphConfig: Record<TimeInterval, TimeGraphConfig> = {
   "1h": {
     timeMap: (date) => date.toLocaleTimeString(),
-    increment: (date) => new Date(date.getTime() + 120 * 1000), // every 2 minutes
+    increment: (date) => new Date(date.getTime() + 60 * 1000 * 5), // every 5 minutes
     dbIncrement: "min",
     start: getXMinuteasAgoFloored(60),
     end: getXMinuteasAgoFloored(0),
   },
   "24h": {
-    timeMap: (date) => date.toLocaleTimeString(),
+    timeMap: (date) =>
+      date.toLocaleTimeString(undefined, {
+        hour: "2-digit",
+      }),
     increment: (date) => new Date(date.getTime() + 60 * 60 * 1000), // every 2 hours
     dbIncrement: "hour",
     start: getXHoursAgoFloored(24),
     end: getXHoursAgoFloored(0),
   },
   "7d": {
-    timeMap: (date) => date.toLocaleDateString(),
+    timeMap: (date) =>
+      date.toLocaleDateString(undefined, {
+        dateStyle: "short",
+      }),
     increment: (date) => new Date(date.getTime() + 24 * 60 * 60 * 1000), // every day
     dbIncrement: "day",
     start: getXDaysAgoFloored(7),
     end: getXDaysAgoFloored(0),
   },
   "1m": {
-    timeMap: (date) => date.toLocaleDateString(),
+    timeMap: (date) =>
+      date.toLocaleDateString(undefined, {
+        year: "2-digit",
+        month: "2-digit",
+        day: "2-digit",
+      }),
     increment: (date) => new Date(date.getTime() + 24 * 60 * 60 * 1000), // every day
     dbIncrement: "day",
     start: getXDaysAgoFloored(30),
+    end: getXHoursAgoFloored(0),
+  },
+  "3m": {
+    timeMap: (date) =>
+      date.toLocaleDateString(undefined, {
+        year: "2-digit",
+        month: "2-digit",
+        day: "2-digit",
+      }),
+    increment: (date) => new Date(date.getTime() + 24 * 60 * 60 * 7 * 1000), // every week
+    dbIncrement: "day",
+    start: getXDaysAgoFloored(30 * 3),
     end: getXHoursAgoFloored(0),
   },
 };
