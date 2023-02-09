dc44d00c1875a3f4dfa4fc3744ee8a8a268fcb96
Khoa Nguyen <scottmktn@gmail.com>
2023-02-09 14:52:58 -0800

    temporarily remove api key on dashboard

--- web/components/templates/dashboard/dashboardPage.tsx
+++ web/components/templates/dashboard/dashboardPage.tsx
@@ -20,32 +20,32 @@ const DashboardPage = (props: DashboardPageProps) => {
     <AuthLayout user={user}>
       <AuthHeader
         title={"Dashboard"}
-        actions={
-          <div className="flex flex-row items-center gap-2">
-            <label
-              htmlFor="location"
-              className="block text-sm font-medium text-gray-700 whitespace-nowrap"
-            >
-              Key Name:
-            </label>
-            <select
-              id="location"
-              name="location"
-              className="flex w-full max-w-sm rounded-md border-gray-300 py-1.5 pl-3 pr-8 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-              defaultValue={25}
-              onChange={(e) => {
-                // TODO: add key change handler
-                console.log(e.target.value);
-              }}
-            >
-              {keys.map((key) => (
-                <option key={key.api_key_hash} value={key.api_key_hash}>
-                  {key.key_name}
-                </option>
-              ))}
-            </select>
-          </div>
-        }
+        // actions={
+        //   <div className="flex flex-row items-center gap-2">
+        //     <label
+        //       htmlFor="location"
+        //       className="block text-sm font-medium text-gray-700 whitespace-nowrap"
+        //     >
+        //       Key Name:
+        //     </label>
+        //     <select
+        //       id="location"
+        //       name="location"
+        //       className="flex w-full max-w-sm rounded-md border-gray-300 py-1.5 pl-3 pr-8 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+        //       defaultValue={25}
+        //       onChange={(e) => {
+        //         // TODO: add key change handler
+        //         console.log(e.target.value);
+        //       }}
+        //     >
+        //       {keys.map((key) => (
+        //         <option key={key.api_key_hash} value={key.api_key_hash}>
+        //           {key.key_name}
+        //         </option>
+        //       ))}
+        //     </select>
+        //   </div>
+        // }
       />
       <div className="space-y-16">
         <MetricsPanel />
