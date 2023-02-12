667cd73434cfa84730d5053e9c9db53287075921
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-02-12 09:04:01 -0800

    fix truncating (#45)

--- web/lib/stringHelpers.ts
+++ web/lib/stringHelpers.ts
@@ -1,8 +1,10 @@
 export function truncString(str: string, n: number) {
+  if (!str) return "";
   return str.length > n ? str.substring(0, n - 1) + "..." : str;
 }
 
 export function middleTruncString(str: string, n: number) {
+  if (!str) return "";
   return str.length > n
     ? str.substring(0, n / 2) +
         "..." +
