d42d59ad04d961129f7c7bc1e2e4e38ea616f80a
Barak Oshri <barakoshri@gmail.com>
2023-02-15 17:12:37 -0800

    remove unused functions

--- web/components/templates/requests/requestsPage.tsx
+++ web/components/templates/requests/requestsPage.tsx
@@ -142,28 +142,6 @@ const RequestsPage = (props: RequestsPageProps) => {
     };
   });
 
-  const hasPrevious = page > 1;
-  const hasNext = to <= count!;
-
-  const makeColumn = (name: string) => {
-    return (
-      <th
-        scope="col"
-        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-      >
-        {name}
-      </th>
-    );
-  };
-
-  const makeValue = (val: string) => {
-    return (
-      <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
-        {val}
-      </td>
-    );
-  };
-
   const makeCardProperty = (name: string, val: string) => {
     return (
       <li className="w-full flex flex-row justify-between gap-4 text-sm">
