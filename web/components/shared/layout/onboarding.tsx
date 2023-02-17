330066b43e4cdf70cacc8f5c31ef046961cf3f5e
Khoa Nguyen <scottmktn@gmail.com>
2023-02-17 14:42:30 -0800

    recommend password

--- web/components/shared/layout/onboarding.tsx
+++ web/components/shared/layout/onboarding.tsx
@@ -256,7 +256,7 @@ const Onboarding = (props: OnboardingProps) => {
                       id="password"
                       name="password"
                       type="password"
-                      autoComplete="current-password"
+                      autoComplete="new-password"
                       onChange={(e) => setPassword(e.target.value)}
                       required
                       className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 text-md sm:text-lg p-2 sm:p-4 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
