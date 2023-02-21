89834d123abcf8e7e56117a5aa220b4dccf26846
Barak Oshri <barakoshri@gmail.com>
2023-02-20 22:33:29 -0800

    return weird button

--- web/components/templates/requests/requestsPage.tsx
+++ web/components/templates/requests/requestsPage.tsx
@@ -275,6 +275,13 @@ const RequestsPage = (props: RequestsPageProps) => {
             >
               <button
                 type="button"
+                className="inline-flex sm:hidden items-center justify-center rounded-md border border-transparent bg-sky-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:w-auto"
+              >
+                <ArrowDownTrayIcon className="mr-1 flex-shrink-0 h-4 w-4" />
+                Export
+              </button>
+              <button
+                type="button"
                 className="hidden sm:inline-flex items-center justify-center rounded-md border border-transparent bg-sky-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:w-auto"
               >
                 <ArrowDownTrayIcon className="mr-2 flex-shrink-0 h-4 w-4" />
