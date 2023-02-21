4ea66fb9295e4680991f1584583f1068dbb704ab
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-02-21 15:39:25 -0800

    feat: update request table (#71)

--- web/services/lib/requests.ts
+++ web/services/lib/requests.ts
@@ -35,15 +35,52 @@ export type ResponseAndRequest = Omit<
 const getRequests = async (
   client: SupabaseClient<any, "public", any>,
   currentPage: number,
-  pageSize: number
+  pageSize: number,
+  sortBy: string | null,
+  timeFilter: string | null
 ) => {
   const { from, to } = getPagination(currentPage - 1, pageSize);
 
-  const { data, error, count } = await client
+  let query = client
     .from("response_and_request_rbac")
-    .select("*", { count: "exact" })
-    .order("request_created_at", { ascending: false })
-    .range(from, to);
+    .select("*", { count: "exact" });
+
+  let date = new Date();
+
+  if (sortBy === "time_desc" || sortBy === null) {
+    query = query.order("request_created_at", { ascending: false });
+  }
+  if (sortBy === "time_asc") {
+    query = query.order("request_created_at", { ascending: true });
+  }
+  if (timeFilter?.includes("custom:")) {
+    const timeRange = timeFilter?.split("custom:")[1].split("_");
+    const startTime = timeRange[0];
+    const endTime = timeRange[1];
+
+    query = query.gte("request_created_at", startTime);
+    query = query.lte("request_created_at", endTime);
+  }
+  if (timeFilter === "day") {
+    date = new Date(date.getTime() - 24 * 60 * 60 * 1000);
+    query = query.gte("request_created_at", date.toISOString());
+  }
+  if (timeFilter === "wk") {
+    date = new Date(date.getTime() - 7 * 24 * 60 * 60 * 1000);
+    query = query.gte("request_created_at", date.toISOString());
+  }
+  if (timeFilter === "mo") {
+    date = new Date(date.getTime() - 30 * 24 * 60 * 60 * 1000);
+    query = query.gte("request_created_at", date.toISOString());
+  }
+  if (timeFilter === "3mo") {
+    date = new Date(date.getTime() - 90 * 24 * 60 * 60 * 1000);
+    query = query.gte("request_created_at", date.toISOString());
+  }
+
+  query = query.range(from, to);
+
+  const { data, error, count } = await query;
 
   return { data, error, count, from, to };
 };
