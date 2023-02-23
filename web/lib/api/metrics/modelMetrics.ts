3a8e6946a4025d709b7c34cf8f45a25c7c072d97
Justin Torre <justintorre75@gmail.com>
2023-02-22 19:10:25 -0800

    added Cache Savings to metrics and request counter (#78)

--- web/lib/api/metrics/modelMetrics.ts
+++ web/lib/api/metrics/modelMetrics.ts
@@ -5,13 +5,18 @@ export interface ModelMetrics {
   model: string;
   sum_tokens: number;
 }
-export async function getModelMetrics(filter: FilterNode, user_id: string) {
+export async function getModelMetrics(
+  filter: FilterNode,
+  user_id: string,
+  cached: boolean
+) {
   const query = `
 SELECT response.body ->> 'model'::text as model,
   sum(((response.body -> 'usage'::text) ->> 'total_tokens'::text)::bigint)::bigint AS sum_tokens
 FROM response 
   left join request on response.request = request.id
   left join user_api_keys on request.auth_hash = user_api_keys.api_key_hash
+  ${cached ? "inner join cache_hits ch ON ch.request_id = request.id" : ""}
 WHERE (
   user_api_keys.user_id = '${user_id}'
   AND (${buildFilter(filter)})
