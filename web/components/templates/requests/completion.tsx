d8a9393c7a2babea4e4062e506976be44e90da3d
Khoa Nguyen <scottmktn@gmail.com>
2023-03-03 17:28:58 -0800

    chatGPT UI

--- web/components/templates/requests/completion.tsx
+++ web/components/templates/requests/completion.tsx
@@ -9,16 +9,16 @@ export const Completion = (props: CompletionProps) => {
   const { request, response } = props;
 
   return (
-    <div className="flex flex-col sm:flex-row gap-4 text-sm w-full">
+    <div className="flex flex-col gap-2 text-xs w-full space-y-2">
       <div className="w-full flex flex-col text-left space-y-1">
-        <p>Request:</p>
-        <p className="p-2 border border-gray-300 bg-gray-100 rounded-md whitespace-pre-wrap h-[250px] max-h-[250px] overflow-auto">
+        <p className="text-gray-500 font-medium">Request</p>
+        <p className="p-2 border border-gray-300 bg-gray-100 rounded-md whitespace-pre-wrap h-full max-h-[300px] overflow-auto">
           {request}
         </p>
       </div>
       <div className="w-full flex flex-col text-left space-y-1">
-        <p>Response:</p>
-        <p className="p-2 border border-gray-300 bg-gray-100 rounded-md whitespace-pre-wrap h-[250px] max-h-[250px] overflow-auto">
+        <p className="text-gray-500 font-medium">Response</p>
+        <p className="p-2 border border-gray-300 bg-gray-100 rounded-md whitespace-pre-wrap h-full max-h-[300px] overflow-auto">
           {response}
         </p>
       </div>
