bd9bfa7ac63c4c423c1d596f80ab4434eaf1eac9
Khoa Nguyen <scottmktn@gmail.com>
2023-03-06 18:11:12 -0800

    pushing pp

--- web/components/templates/requests/chat.tsx
+++ web/components/templates/requests/chat.tsx
@@ -19,39 +19,50 @@ export const Chat = (props: ChatProps) => {
     <div className="w-full flex flex-col text-left space-y-1 text-xs">
       <p className="text-gray-500 font-medium">Messages</p>
       <div className="text-xs w-full border border-gray-300 rounded-md overflow-auto divide-y divide-gray-200 h-full max-h-[500px]">
-        {messages.map((message, index) => {
-          const isAssistant = message.role === "assistant";
-          const isSystem = message.role === "system";
-          const isUser = message.role === "user";
+        {messages.length > 0 ? (
+          messages.map((message, index) => {
+            const isAssistant = message.role === "assistant";
+            const isSystem = message.role === "system";
 
-          return (
-            <div key={index} className="">
-              <div
-                className={clsx(
-                  isAssistant || isSystem ? "bg-gray-100" : "bg-white",
-                  "items-start px-4 py-4 text-left font-semibold grid grid-cols-10 gap-2"
-                )}
-              >
-                <div className="col-span-1">
-                  {isAssistant || isSystem ? (
-                    <Image
-                      src={"/assets/chatGPT.png"}
-                      className="h-5 w-5 rounded-md"
-                      height={24}
-                      width={24}
-                      alt="ChatGPT Logo"
-                    />
-                  ) : (
-                    <UserCircleIcon className="h-5 w-5 bg-white rounded-full" />
+            return (
+              <div key={index} className="">
+                <div
+                  className={clsx(
+                    isAssistant || isSystem ? "bg-gray-100" : "bg-white",
+                    "items-start px-4 py-4 text-left font-semibold grid grid-cols-10 gap-2"
                   )}
-                </div>
-                <div className="whitespace-pre-wrap col-span-9">
-                  {message.content}
+                >
+                  <div className="col-span-1">
+                    {isAssistant || isSystem ? (
+                      <Image
+                        src={"/assets/chatGPT.png"}
+                        className="h-5 w-5 rounded-md"
+                        height={24}
+                        width={24}
+                        alt="ChatGPT Logo"
+                      />
+                    ) : (
+                      <UserCircleIcon className="h-5 w-5 bg-white rounded-full" />
+                    )}
+                  </div>
+                  <div className="whitespace-pre-wrap col-span-9">
+                    {message.content}
+                  </div>
                 </div>
               </div>
+            );
+          })
+        ) : (
+          <div className="">
+            <div
+              className={clsx(
+                "bg-gray-100 items-start px-4 py-4 text-left font-semibold grid grid-cols-10 gap-2"
+              )}
+            >
+              n/a
             </div>
-          );
-        })}
+          </div>
+        )}
       </div>
     </div>
   );
