87180e5b43b450b5c176046ce42298bea2841b65
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-03-07 16:41:14 -0800

    fix: moderations (#117)

--- web/components/templates/requests/moderation.tsx
+++ web/components/templates/requests/moderation.tsx
@@ -1,10 +1,15 @@
-interface CompletionProps {
+import ReactJson from "react-json-pretty";
+import { Json } from "../../../supabase/database.types";
+
+interface ModerationProps {
   request?: string;
-  response?: string;
+  response?: { [key: string]: Json }[];
 }
 
-export const Completion = (props: CompletionProps) => {
+const Moderation = (props: ModerationProps) => {
   const { request, response } = props;
+
+  //   const jsonResponse = JSON.parse(response[0] || "{}");
 
   return (
     <div className="flex flex-col gap-2 text-xs w-full space-y-2">
@@ -17,9 +22,11 @@ export const Completion = (props: CompletionProps) => {
       <div className="w-full flex flex-col text-left space-y-1">
         <p className="text-gray-500 font-medium">Response</p>
         <p className="p-2 border border-gray-300 bg-gray-100 rounded-md whitespace-pre-wrap h-full max-h-[300px] overflow-auto">
-          {response || "n/a"}
+          <ReactJson data={response} />
         </p>
       </div>
     </div>
   );
 };
+
+export default Moderation;
