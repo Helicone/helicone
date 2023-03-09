8afd490380bc797013d357a0954a78ff4eff2b53
Justin <justintorre75@gmail.com>
2023-03-08 23:05:00 -0800

    added metadata fetching

--- web/pages/api/request/[id]/metaData.ts
+++ web/pages/api/request/[id]/metaData.ts
@@ -2,26 +2,27 @@
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import {
-  getRequestsMetaData,
+  getRequestMetaData,
   RequestMetaData,
-} from "../../../lib/api/request/requestMetaData";
-
-import { Result } from "../../../lib/result";
+} from "../../../../lib/api/request/metadata";
+import { Result } from "../../../../lib/result";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Result<RequestMetaData[], string>>
 ) {
+  const { id: requestId } = req.query;
+
   const client = createServerSupabaseClient({ req, res });
   const user = await client.auth.getUser();
   if (!user.data || !user.data.user) {
     res.status(401).json({ error: "Unauthorized", data: null });
     return;
   }
-  const { requestIds } = req.body as {
-    requestIds: string[];
-  };
-  const metrics = await getRequestsMetaData(user.data.user.id, requestIds);
-  console.log(metrics);
+  const metrics = await getRequestMetaData(
+    user.data.user.id,
+    requestId as string
+  );
+  console.log("METRICS", metrics);
   res.status(metrics.error === null ? 200 : 500).json(metrics);
 }
