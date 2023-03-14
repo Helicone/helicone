70f3e5b31f8dae01bba5b3d6bd531b4a039c72c9
Justin Torre <justintorre75@gmail.com>
2023-03-13 23:38:43 -0700

    Update web/pages/api/hello.ts

--- web/pages/api/hello.ts
+++ web/pages/api/hello.ts
@@ -10,27 +10,4 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: "John Doe" });
-  var es = await fetch("http://127.0.0.1:8787/v1/engines/davinci/completions", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + "<API KEY>",
-    },
-    method: "POST",
-    body: JSON.stringify({
-      prompt: "HELLO write me a book",
-      temperature: 0.75,
-      top_p: 0.95,
-      max_tokens: 10,
-      stream: true,
-      stop: ["\n\n"],
-    }),
-  });
-
-  const reader = es.body?.pipeThrough(new TextDecoderStream()).getReader();
-
-  while (true) {
-    const res = await reader?.read();
-    if (res?.done) break;
-    console.log("Received", res?.value);
-  }
 }
