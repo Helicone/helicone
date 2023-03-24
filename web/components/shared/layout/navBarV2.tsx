7881032ac0f82fedc65509d76688354b94015252
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-03-24 00:56:06 -0700

    fix nav mobile (#171)

--- web/components/shared/layout/navBarV2.tsx
+++ web/components/shared/layout/navBarV2.tsx
@@ -74,10 +74,17 @@ const NavBarV2 = (props: NavBarV2Props) => {
           <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Helicone</span>
             <Image
-              className="rounded-md"
+              className="hidden sm:block rounded-md"
               src="/assets/heli-full-logo.png"
               width={150}
-              height={75}
+              height={150}
+              alt="Helicone-full-logo"
+            />
+            <Image
+              className="block sm:hidden rounded-md"
+              src="/assets/heli-logo.png"
+              width={40}
+              height={40}
               alt="Helicone-full-logo"
             />
           </Link>
@@ -125,11 +132,13 @@ const NavBarV2 = (props: NavBarV2Props) => {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-50 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center gap-x-6 justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
+              <span className="sr-only">Helicone</span>
+              <Image
+                className="rounded-md"
+                src="/assets/heli-full-logo.png"
+                width={150}
+                height={150}
+                alt="Helicone-full-logo"
               />
             </a>
 
