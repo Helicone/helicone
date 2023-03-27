72bdeeb9278c83178ddb9f930cd2e6d002da075b
Justin Torre <justintorre75@gmail.com>
2023-03-27 03:04:51 -0400

    mini ui fixes (#181)

--- web/components/shared/themed/themedTextDropDown.tsx
+++ web/components/shared/themed/themedTextDropDown.tsx
@@ -37,7 +37,7 @@ export function ThemedTextDropDown(props: ThemedTextDropDownProps) {
           onChange(v);
         }}
       >
-        <div className="relative mt-1">
+        <div className="relative">
           <div className="relative w-full cursor-default overflow-hidden rounded-md bg-white text-left border-2 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
