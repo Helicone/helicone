471e2fd472c61aa09264e9455726a1c59c25a61d
Justin Torre <justintorre75@gmail.com>
2023-03-29 14:35:30 -0400

    fixed columns from jumping (#194)

--- web/components/templates/requests/requestsPage.tsx
+++ web/components/templates/requests/requestsPage.tsx
@@ -197,18 +197,9 @@ const RequestsPage = (props: RequestsPageProps) => {
     },
   ];
 
-  const localStorageColumns =
-    typeof window !== "undefined"
-      ? localStorage.getItem("requestsColumns")
-      : null;
-
   const sessionStorageKey =
     typeof window !== "undefined" ? sessionStorage.getItem("currentKey") : null;
 
-  const parsed = JSON.parse(localStorageColumns || "[]") as Column[];
-
-  const [defaultColumns, setDefaultColumns] =
-    useState<Column[]>(initialColumns);
   const [currentPage, setCurrentPage] = useState<number>(page);
   const [currentPageSize, setCurrentPageSize] = useState<number>(pageSize);
   const [advancedFilters, setAdvancedFilters] = useLocalStorageState<
@@ -243,6 +234,8 @@ const RequestsPage = (props: RequestsPageProps) => {
     values,
     from,
     isLoading,
+    isPropertiesLoading,
+    isValuesLoading,
     properties,
     refetch,
     filterMap,
@@ -259,6 +252,11 @@ const RequestsPage = (props: RequestsPageProps) => {
     },
     sortLeaf
   );
+
+  // console.log("values", values);
+  // console.log("requests", requests);
+  // console.log("filterMap", filterMap
+  // console.log("properties", properties);
 
   const { keys, isLoading: isKeysLoading } = useGetKeys();
 
@@ -292,25 +290,13 @@ const RequestsPage = (props: RequestsPageProps) => {
     setOpen(true);
   };
 
-  // columns
-
-  const [columns, setColumns] = useState<Column[]>(defaultColumns);
+  const [columns, setColumns] = useLocalStorageState<Column[]>(
+    "requestTableColumns",
+    []
+  );
 
   useEffect(() => {
-    if (
-      columns.length > initialColumns.length ||
-      (values.length === 0 && properties.length === 0)
-    ) {
-      if (parsed) {
-        columns.forEach((column) => {
-          const match = parsed.find((c) => c.key === column.key);
-          if (match) {
-            column.active = match.active;
-          }
-        });
-      }
-      return;
-    }
+    if (isPropertiesLoading || isValuesLoading) return;
     const propertiesColumns: Column[] = properties.map((p) => {
       return {
         key: p,
@@ -351,23 +337,25 @@ const RequestsPage = (props: RequestsPageProps) => {
     });
 
     const newColumns = [
-      ...defaultColumns,
+      ...initialColumns,
       ...valuesColumns,
       ...propertiesColumns,
     ];
 
-    if (parsed) {
-      newColumns.forEach((column) => {
-        const match = parsed.find((c) => c.key === column.key);
-        if (match) {
-          column.active = match.active;
+    setColumns((prev) => {
+      return newColumns.map((c) => {
+        const prevColumn = prev.find((p) => p.key === c.key);
+        if (prevColumn) {
+          return {
+            ...prevColumn,
+            ...c,
+          };
         }
+        return c;
       });
-    }
-
-    setColumns(newColumns);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [defaultColumns, values, properties]);
+  }, [isPropertiesLoading, isValuesLoading]);
 
   const columnOrderIndex = columns.findIndex((c) => c.key === orderBy.column);
   if (columnOrderIndex > -1) {
@@ -449,19 +437,7 @@ const RequestsPage = (props: RequestsPageProps) => {
               }}
               editColumns={{
                 columns: columns,
-                onColumnCallback: (columns) => {
-                  const active = columns.map((c) => {
-                    return {
-                      key: c.key,
-                      active: c.active,
-                    };
-                  });
-                  localStorage.setItem(
-                    "requestsColumns",
-                    JSON.stringify(active)
-                  );
-                  setDefaultColumns(columns);
-                },
+                onColumnCallback: setColumns,
               }}
               timeFilter={{
                 customTimeFilter: true,
