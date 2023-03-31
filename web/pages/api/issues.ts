060cdfa986f294b361b8a91475ac90307a20a0d9
Justin <justintorre75@gmail.com>
2023-03-31 16:56:25 -0700

    fixed small things and made two sections

--- web/pages/api/issues.ts
+++ web/pages/api/issues.ts
@@ -3,29 +3,22 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { Octokit } from "@octokit/core";
 
 const GITHUB_API_KEY = process.env.GITHUB_API_KEY ?? "";
-async function typesGetHeliconeIssues() {
-  const octokit = new Octokit({
-    auth: GITHUB_API_KEY,
-  });
 
-  return await octokit.request("GET /issues");
-}
 async function getHeliconeIssues() {
   const octokit = new Octokit({
     auth: GITHUB_API_KEY,
   });
 
-  return octokit.request("GET /repos/Helicone/helicone/issues", {
-    owner: "OWNER",
-    repo: "REPO",
+  return octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner: "Helicone",
+    repo: "helicone",
     headers: {
       "X-GitHub-Api-Version": "2022-11-28",
     },
   });
 }
 type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
-
-type HeliconeIssuesResponseType = ReturnType<typeof typesGetHeliconeIssues>;
+type HeliconeIssuesResponseType = ReturnType<typeof getHeliconeIssues>;
 export type HeliconeIssuesResolvedType =
   UnwrapPromise<HeliconeIssuesResponseType>;
 
@@ -33,7 +26,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<HeliconeIssuesResolvedType>
 ) {
-  res
-    .status(200)
-    .json((await getHeliconeIssues()) as HeliconeIssuesResolvedType);
+  res.status(200).json(await getHeliconeIssues());
 }
--- web/pages/roadmap.tsx
+++ web/pages/roadmap.tsx
@@ -29,24 +29,71 @@ const Home = (props: HomeProps) => {
   });
 
   console.log(data);
+
+  function getLabel(label: HeliconeIssuesResolvedType["data"][0]["labels"][0]) {
+    if (typeof label === "string") {
+      return label;
+    }
+    return label.name;
+  }
+
+  const activeIssues = data?.data.filter((issue) =>
+    issue.labels.map((label) => getLabel(label)).includes("active")
+  );
+  const communityVotes = data?.data
+    .filter(
+      (issue) =>
+        !issue.labels.map((label) => getLabel(label)).includes("active") &&
+        issue.labels.map((label) => getLabel(label)).includes("roadmap")
+    )
+    .sort((a, b) => {
+      if (
+        a.reactions?.["+1"] === undefined ||
+        b.reactions?.["+1"] === undefined
+      ) {
+        return 0;
+      }
+      if (a.reactions?.["+1"] > b.reactions?.["+1"]) {
+        return -1;
+      }
+      if (a.reactions?.["+1"] < b.reactions?.["+1"]) {
+        return 1;
+      }
+      return 0;
+    });
   return (
     <MetaData title="Home">
       <BasePageV2>
-        {data?.data
-          .filter((issue) =>
-            issue.labels.find((label) => {
-              if (typeof label === "string") {
-                return label === "roadmap";
-              }
-              return label.name === "roadmap";
-            })
-          )
-          .map((issue) => (
-            <div key={issue.id}>
-              {issue.html_url}
-              {issue.title} {issue.reactions?.["+1"]}
-            </div>
-          ))}
+        <div>
+          <h1 className="text-3xl font-bold leading-9 text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+            Roadmap
+          </h1>
+          <div>
+            <h2 className="mt-3 text-xl leading-7 text-gray-500 sm:mt-4 sm:text-2xl sm:leading-9 md:text-3xl md:leading-9">
+              Active Issues
+            </h2>
+            {activeIssues?.map((issue) => (
+              <div key={issue.id + "_active"}>
+                {issue.html_url}
+                {issue.title} {issue.reactions?.["+1"]}
+              </div>
+            ))}
+          </div>
+          <div>
+            <h2 className="mt-3 text-xl leading-7 text-gray-500 sm:mt-4 sm:text-2xl sm:leading-9 md:text-3xl md:leading-9">
+              Community Votes {/* WE should rename this */}
+            </h2>
+          </div>
+        </div>
+        {communityVotes?.map((issue) => (
+          <div key={issue.id + "_not_active"}>
+            {issue.labels.map((label, i) => (
+              <div key={i}>{getLabel(label)}</div>
+            ))}
+            {issue.html_url}
+            {issue.title} {issue.reactions?.["+1"]}
+          </div>
+        ))}
       </BasePageV2>
     </MetaData>
   );
