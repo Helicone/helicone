43266141b420de07c752ce657018ca3a3ea27448
Justin <justintorre75@gmail.com>
2023-03-30 21:28:08 -0700

    add import

--- web/lib/supabaseClient.ts
+++ web/lib/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import crypto from "crypto";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
