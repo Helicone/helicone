9834244f6bb0d208c9d24f8051025ffd27783907
Justin <justintorre75@gmail.com>
2023-03-31 08:45:36 -0700

    fixed hashing algo

--- web/lib/supabaseClient.ts
+++ web/lib/supabaseClient.ts
@@ -1,27 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-import crypto from "crypto";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
 if (supabaseUrl === "" || supabaseAnonKey === "") {
   throw new Error("URL or Anon ENV not set");
-}
-
-export async function hashAuth(key: string): Promise<string> {
-  key = `Bearer ${key}`;
-  const encoder = new TextEncoder();
-  const hashedKey = await crypto.subtle.digest(
-    { name: "SHA-256" },
-    encoder.encode(key)
-  );
-  const byteArray = Array.from(new Uint8Array(hashedKey));
-  const hexCodes = byteArray.map((value) => {
-    const hexCode = value.toString(16);
-    const paddedHexCode = hexCode.padStart(2, "0");
-    return paddedHexCode;
-  });
-  return hexCodes.join("");
 }
 
 export const supabaseClientAuthHash = async (authhash: string) =>
