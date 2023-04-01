b27ca3e3d0e26b1682c1e8f2af08c7fc48050d49
Khoa Nguyen <scottmktn@gmail.com>
2023-03-31 19:32:09 -0700

    ui-v1

--- web/pages/roadmap.tsx
+++ web/pages/roadmap.tsx
@@ -11,6 +11,10 @@ import { redirectIfLoggedIn } from "../lib/redirectIdLoggedIn";
 import { Octokit } from "@octokit/core";
 import { HeliconeIssuesResolvedType } from "./api/issues";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+import { StarIcon } from "@heroicons/react/20/solid";
+import { getUSDate } from "../components/shared/utils/utils";
+import { CalendarDaysIcon } from "@heroicons/react/24/outline";
 
 interface HomeProps {}
 
@@ -37,9 +41,26 @@ const Home = (props: HomeProps) => {
     return label.name;
   }
 
-  const activeIssues = data?.data.filter((issue) =>
-    issue.labels.map((label) => getLabel(label)).includes("active")
-  );
+  const activeIssues = data?.data
+    .filter((issue) =>
+      issue.labels.map((label) => getLabel(label)).includes("active")
+    )
+    .sort((a, b) => {
+      if (
+        a.reactions?.["+1"] === undefined ||
+        b.reactions?.["+1"] === undefined
+      ) {
+        return 0;
+      }
+      if (a.reactions?.["+1"] > b.reactions?.["+1"]) {
+        return -1;
+      }
+      if (a.reactions?.["+1"] < b.reactions?.["+1"]) {
+        return 1;
+      }
+      return 0;
+    });
+
   const communityVotes = data?.data
     .filter(
       (issue) =>
@@ -61,39 +82,90 @@ const Home = (props: HomeProps) => {
       }
       return 0;
     });
+
   return (
     <MetaData title="Home">
       <BasePageV2>
-        <div>
-          <h1 className="text-3xl font-bold leading-9 text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            Roadmap
-          </h1>
-          <div>
-            <h2 className="mt-3 text-xl leading-7 text-gray-500 sm:mt-4 sm:text-2xl sm:leading-9 md:text-3xl md:leading-9">
-              Active Issues
-            </h2>
-            {activeIssues?.map((issue) => (
-              <div key={issue.id + "_active"}>
-                {issue.html_url}
-                {issue.title} {issue.reactions?.["+1"]}
+        <div className="mx-auto flex w-full max-w-7xl items-center justify-between p-6 md:justify-start md:space-x-10 lg:px-8">
+          <div className="py-16 max-w-3xl space-y-16">
+            <div className="flex flex-col space-y-4">
+              <p className="text-5xl font-sans">Roadmap</p>
+              <p className="text-lg font-sans">
+                We are building a community driven roadmap. View and vote on
+                issues that you would like to see implemented in Helicone.
+              </p>
+              <p className="text-lg font-sans">
+                Have an idea for a feature? Create an issue on our github or
+                discuss it on our discord.
+              </p>
+            </div>
+
+            <div className="flex flex-col space-y-16">
+              <div className="w-full flex flex-col">
+                <h2 className="mt-3 text-xl leading-7 text-gray-900 sm:mt-4 sm:text-2xl sm:leading-9 md:text-3xl md:leading-9 mb-4">
+                  Active Issues
+                </h2>
+                <div className="divide-y divide-y-gray-200">
+                  {activeIssues?.map((issue) => (
+                    <Link
+                      href={issue.html_url}
+                      key={issue.id + "_active"}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex flex-row justify-between py-3 px-4 items-center hover:bg-gray-100"
+                    >
+                      <div>
+                        <p className="text-md text-gray-900 font-semibold">
+                          {issue.title}
+                        </p>
+                        <div className="text-xs text-gray-500 flex flex-row items-center space-x-1">
+                          <p className="truncate overflow-ellipsis max-w-[16rem] sm:max-w-[24rem] md:max-w-[32rem] lg:max-w-[40rem]">
+                            {issue.body}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex flex-row items-center space-x-2 ">
+                        <StarIcon className="text-yellow-300 h-5" />
+                        {issue.reactions?.["+1"]}
+                      </div>
+                    </Link>
+                  ))}
+                </div>
               </div>
-            ))}
-          </div>
-          <div>
-            <h2 className="mt-3 text-xl leading-7 text-gray-500 sm:mt-4 sm:text-2xl sm:leading-9 md:text-3xl md:leading-9">
-              Community Votes {/* WE should rename this */}
-            </h2>
+              <div>
+                <h2 className="mt-3 text-xl leading-7 text-gray-900 sm:mt-4 sm:text-2xl sm:leading-9 md:text-3xl md:leading-9 mb-4">
+                  Backlog
+                </h2>
+                <div className="divide-y divide-y-gray-200">
+                  {communityVotes?.map((issue) => (
+                    <Link
+                      href={issue.html_url}
+                      key={issue.id + "_not_active"}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex flex-row justify-between py-3 px-4 items-center hover:bg-gray-100"
+                    >
+                      <div>
+                        <p className="text-md text-gray-900 font-semibold">
+                          {issue.title}
+                        </p>
+                        <div className="text-xs text-gray-500 flex flex-row items-center space-x-1">
+                          <p className="truncate overflow-ellipsis max-w-[16rem] sm:max-w-[24rem] md:max-w-[32rem] lg:max-w-[40rem]">
+                            {issue.body}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex flex-row items-center space-x-2">
+                        <StarIcon className="text-yellow-300 h-4" />
+                        {issue.reactions?.["+1"]}
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        {communityVotes?.map((issue) => (
-          <div key={issue.id + "_not_active"}>
-            {issue.labels.map((label, i) => (
-              <div key={i}>{getLabel(label)}</div>
-            ))}
-            {issue.html_url}
-            {issue.title} {issue.reactions?.["+1"]}
-          </div>
-        ))}
       </BasePageV2>
     </MetaData>
   );
