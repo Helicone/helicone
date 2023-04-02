664e54a686d7dbb8a180887778b1a04b922d2b67
Barak Oshri <barakoshri@gmail.com>
2023-04-02 13:07:59 -0700

    slight updates

--- web/components/templates/home/homePage.tsx
+++ web/components/templates/home/homePage.tsx
@@ -319,8 +319,13 @@ export default function HomePage() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0">
               <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Tools built for companies of the future
+                Tools built for savvy developers
               </h2>
+              <p className="mt-6 text-xl leading-7 text-gray-600">
+                Cache requests to save latency and money, proactively track
+                errors in your application, handle rate limits and reliability
+                concerns with Helicone.
+              </p>
             </div>
             <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
               <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-100 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
