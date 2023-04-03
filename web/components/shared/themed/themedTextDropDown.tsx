02dc5555a73efeb518abe25d067e874dee2905e6
Khoa Nguyen <scottmktn@gmail.com>
2023-04-03 13:55:05 -0700

    improve UX of combobox

--- web/components/shared/themed/themedTextDropDown.tsx
+++ web/components/shared/themed/themedTextDropDown.tsx
@@ -40,11 +40,14 @@ export function ThemedTextDropDown(props: ThemedTextDropDownProps) {
       >
         <div className="relative">
           <div className="relative w-full cursor-default overflow-hidden rounded-md bg-white text-left border border-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-sky-300 sm:text-sm">
-            <Combobox.Input
-              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              onChange={(event) => setQuery(event.target.value)}
-            />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <Combobox.Button
+              as="div"
+              className="right-0 flex items-center pr-2"
+            >
+              <Combobox.Input
+                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                onChange={(event) => setQuery(event.target.value)}
+              />
               <ChevronDownIcon
                 className="h-5 w-5 text-gray-400"
                 aria-hidden="true"
@@ -58,7 +61,10 @@ export function ThemedTextDropDown(props: ThemedTextDropDownProps) {
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options className="z-30 absolute mt-1 max-h-60 w-full shadow-2xl overflow-auto rounded-md bg-white py-1 text-base ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Combobox.Options
+              static
+              className="z-30 absolute mt-1 max-h-60 w-full shadow-2xl overflow-auto rounded-md bg-white py-1 text-base ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            >
               {filteredPeople.length === 0 && query !== "" ? (
                 <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                   Nothing found.
