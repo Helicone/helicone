d3dffd50fb09a190c19e89148e37f89d75ce920b
Justin <justintorre75@gmail.com>
2023-04-05 01:07:15 -0700

    added tests for token calc

--- worker/e2e_test.py
+++ worker/e2e_test.py
@@ -1,9 +1,47 @@
 # import asyncio
+import uuid
+from supabase import create_client, Client
+import os
 import time
 import openai
 import json
 # import httpx
 openai.api_base = "http://127.0.0.1:8787/v1"
+SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU"
+SUPABASE_URL = "http://localhost:54321"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+
+
+def assert_stream_and_not_stream_same_tokens(**kwargs):
+    notStreamRequestId = uuid.uuid4()
+    kwargs["temperature"] = 0
+    kwargs["headers"] = {
+        "Helicone-Request-Id": str(notStreamRequestId),
+        "OpenAI-Organization": ""
+    }
+    if kwargs.get("messages"):
+        openai.ChatCompletion.create(**kwargs)
+    else:
+        openai.Completion.create(**kwargs)
+    streamRequestId = uuid.uuid4()
+
+    kwargs["headers"] = {
+        "Helicone-Request-Id": str(streamRequestId),
+    }
+    kwargs["stream"] = True
+    if kwargs.get("messages"):
+        [resp for resp in openai.ChatCompletion.create(**kwargs)]
+    else:
+        [resp for resp in openai.Completion.create(**kwargs)]
+    time.sleep(.5)
+    notStreamedUsage = supabase.table('response').select(
+        "*").eq("request", notStreamRequestId).single().execute().data.get("body").get("usage")
+    streamedUsage = supabase.table('response').select(
+        "*").eq("request", streamRequestId).single().execute().data.get("body").get("usage")
+    print("notStreamedUsage", notStreamedUsage)
+    print("streamedUsage", streamedUsage)
+    assert notStreamedUsage == streamedUsage
 
 
 def test_streamed_response_delays():
@@ -153,3 +191,29 @@ test_cached_response()
 test_streamed_response()
 test_streamed_chat_response()
 test_streamed_response_delays()
+
+assert_stream_and_not_stream_same_tokens(prompt="ONLY RESPOND 'hi' exactly 10 times\n",
+                                         max_tokens=10,
+                                         temperature=0,
+                                         model='text-davinci-003')
+
+
+assert_stream_and_not_stream_same_tokens(model='gpt-3.5-turbo',
+                                         messages=[
+                                             {
+                                                 "role": "system",
+                                                 "content": "ONLY RESPOND 'hi' 10 times\n"
+                                             }, {
+                                                 "role": "assistant",
+                                                 "content": "ONLY RESPOND 'hi' 10 times\n"
+                                             },
+                                             {
+                                                 "role": "assistant",
+                                                 "content": "ONLY RESPOND 'hi' 10 times\n"
+                                             }, {
+                                                 "role": "assistant",
+                                                 "content": "ONLY RESPOND 'hi' 10 times\n"
+                                             }
+                                         ],
+                                         max_tokens=100,
+                                         temperature=0)
