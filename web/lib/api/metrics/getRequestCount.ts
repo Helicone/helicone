6ee02af51ec934198eb11aa75e0a626e7c72fd92
Justin Torre <justintorre75@gmail.com>
2023-04-11 12:55:27 -0700

    UI Fixes (#285)
    * removed cache as well
    
    * fixed response time

--- web/lib/api/metrics/getRequestCount.ts
+++ web/lib/api/metrics/getRequestCount.ts
@@ -9,27 +9,34 @@ import { dbExecute } from "../db/dbExecute";
 export interface Count {
   count: number;
 }
+
 export async function getRequestCount(
   filter: FilterNode,
   user_id: string,
   cached: boolean
 ): Promise<Result<number, string>> {
-  const { filter: filterString, argsAcc } = await buildFilterWithAuth(
-    user_id,
-    filter,
-    []
-  );
+  console.log("GETTING REQUEST COUNT", cached);
+  if (cached) {
+    // TEMPORARILY DISABLED
+    return { data: 0, error: null };
+  }
+
+  const { filter: filterString, argsAcc } = buildFilter(filter, []);
   const query = `
 SELECT 
   COUNT(*) AS count
  FROM request
    LEFT JOIN response ON response.request = request.id
+   LEFT JOIN user_api_keys ON user_api_keys.api_key_hash = request.auth_hash
   ${cached ? "inner join cache_hits ch ON ch.request_id = request.id" : ""}
 WHERE (
-  (${filterString})
+  user_api_keys.user_id = '${user_id}'
+  AND (${filterString})
 )
 `;
+
   const { data, error } = await dbExecute<Count>(query, argsAcc);
+
   if (error !== null) {
     return { data: null, error: error };
   }
