11b60c2031b699bd1939f139106ccb25b7478ef8
Justin <justintorre75@gmail.com>
2023-04-13 00:59:42 -0700

    migrations

--- supabase/migrations/20230412224310_helicone_user_request_column.sql
+++
@@ -1,5 +0,0 @@
-
-alter table "public"."request" add constraint "request_helicone_user_fkey" FOREIGN KEY (helicone_user) REFERENCES auth.users(id) not valid;
-
-alter table "public"."request" validate constraint "request_helicone_user_fkey";
-
