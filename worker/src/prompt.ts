d6675977c5b041fb48a4beef602074b459e22dbc
BarakOshri <barakoshri@gmail.com>
2023-04-19 20:01:56 -0700

    Request-level rate limits (#306)
    * helicone in helicone exclamation exclamation mark
    
    * add throttling
    
    * fix kv just have wrangler left
    
    * added kv namespace
    
    * right before token throttling
    
    * packages
    
    * mostly works
    
    * returning headers in response
    
    * added lint and finished headers return
    
    * weird updates
    
    * change wording from throttle to ratelimit

--- worker/src/prompt.ts
+++ worker/src/prompt.ts
@@ -1,184 +1,186 @@
 import { GenericResult, Result } from ".";
 
 export interface Prompt {
-    prompt: string;
-    values: { [key: string]: string };
+  prompt: string;
+  values: { [key: string]: string };
 }
 
 interface Message {
-    role: string;
-    content: string;
+  role: string;
+  content: string;
 }
 
 export interface ChatPrompt {
-    prompt: Message[];
-    values: { [key: string]: string };
+  prompt: Message[];
+  values: { [key: string]: string };
 }
 
 interface PromptResult {
-    request: Request,
-    body: string,
-    prompt?: Prompt | ChatPrompt,
+  request: Request;
+  body: string;
+  prompt?: Prompt | ChatPrompt;
 }
 
-function formatPrompt(prompt: Prompt): Result {
-    let formattedString = prompt.prompt;
-    const missingValues = [];
+function formatPrompt(prompt: Prompt): GenericResult<string> {
+  let formattedString = prompt.prompt;
+  const missingValues = [];
 
-    for (const key in prompt.values) {
-        const placeholder = new RegExp(`{{${key}}}`, 'g');
-        if (!formattedString.includes(`{{${key}}}`)) {
-            missingValues.push(key);
-        } else {
-            formattedString = formattedString.replace(placeholder, prompt.values[key]);
-        }
+  for (const key in prompt.values) {
+    const placeholder = new RegExp(`{{${key}}}`, "g");
+    if (!formattedString.includes(`{{${key}}}`)) {
+      missingValues.push(key);
+    } else {
+      formattedString = formattedString.replace(
+        placeholder,
+        prompt.values[key]
+      );
     }
+  }
 
-    const regex = /{{([^{}]+)}}/g;
-    let match = regex.exec(formattedString);
-    const missingPlaceholders = [];
+  const regex = /{{([^{}]+)}}/g;
+  let match = regex.exec(formattedString);
+  const missingPlaceholders = [];
 
-    while (match) {
-        if (!prompt.values.hasOwnProperty(match[1])) {
-            missingPlaceholders.push(match[1]);
-        }
-        match = regex.exec(formattedString);
+  while (match) {
+    if (!prompt.values.hasOwnProperty(match[1])) {
+      missingPlaceholders.push(match[1]);
     }
+    match = regex.exec(formattedString);
+  }
 
-    return {
-        data: formattedString,
-        error: null,
-    };
+  return {
+    data: formattedString,
+    error: null,
+  };
 }
 
 function updateContentLength(clone: Request, text: string): Request {
-    const body = new TextEncoder().encode(text);
-    const headers = new Headers(clone.headers);
-    headers.set("Content-Length", `${body.byteLength}`);
+  const body = new TextEncoder().encode(text);
+  const headers = new Headers(clone.headers);
+  headers.set("Content-Length", `${body.byteLength}`);
 
-    return new Request(clone.url, {
-        method: clone.method,
-        headers,
-        body
-    });
+  return new Request(clone.url, {
+    method: clone.method,
+    headers,
+    body,
+  });
 }
 
 export async function extractPrompt(
-    request: Request,
+  request: Request
 ): Promise<GenericResult<PromptResult>> {
-    const isPromptRegexOn = request.headers.get("Helicone-Prompt-Format") !== null;
-	
-    if (isPromptRegexOn) {
-        try {
-            const cloneRequest = request.clone();
-            const cloneBody = await cloneRequest.text();
-            const json = cloneBody ? JSON.parse(cloneBody) : {};
-            if ("messages" in json) {
-                return extractPromptMessages(cloneRequest, json);
-            }
+  const isPromptRegexOn =
+    request.headers.get("Helicone-Prompt-Format") !== null;
 
-            const prompt = JSON.parse(json["prompt"]);
-            const stringPromptResult = formatPrompt(prompt);
-            if (stringPromptResult.error !== null) {
-                return {
-                    data: null,
-                    error: stringPromptResult.error,
-                };
-            }
-            const stringPrompt = stringPromptResult.data;
-            json["prompt"] = stringPrompt;
-            const body = JSON.stringify(json);
-            const formattedRequest = updateContentLength(cloneRequest, body);
+  if (isPromptRegexOn) {
+    try {
+      const cloneRequest = request.clone();
+      const cloneBody = await cloneRequest.text();
+      const json = cloneBody ? JSON.parse(cloneBody) : {};
+      if ("messages" in json) {
+        return extractPromptMessages(cloneRequest, json);
+      }
 
-            return {
-                data: {
-                    request: formattedRequest,
-                    body: body,
-                    prompt: prompt,
-                },
-                error: null,
-            };
-        } catch (error) {
-            console.error(error);
-            return {
-                data: null,
-                error: `Error parsing prompt: ${error}`,
-            }
-        }
-    } else {
+      const prompt = JSON.parse(json["prompt"]);
+      const stringPromptResult = formatPrompt(prompt);
+      if (stringPromptResult.error !== null) {
         return {
-            data: {
-                request: request,
-                body: await request.text(),
-            },
-            error: null,
-        }
+          data: null,
+          error: stringPromptResult.error,
+        };
+      }
+      const stringPrompt = stringPromptResult.data;
+      json["prompt"] = stringPrompt;
+      const body = JSON.stringify(json);
+      const formattedRequest = updateContentLength(cloneRequest, body);
+
+      return {
+        data: {
+          request: formattedRequest,
+          body: body,
+          prompt: prompt,
+        },
+        error: null,
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        data: null,
+        error: `Error parsing prompt: ${error}`,
+      };
     }
+  } else {
+    return {
+      data: {
+        request: request,
+        body: await request.text(),
+      },
+      error: null,
+    };
+  }
 }
 
 async function extractPromptMessages(
-    cloneRequest: Request,
-    json: any,
+  cloneRequest: Request,
+  json: any
 ): Promise<GenericResult<PromptResult>> {
-    const regexPrompt = json["messages"];
-    const regexMessages = regexPrompt;
-    const regexValues = json["values"];
-    
-    // If regexValues is not defined, return an error
-    if (regexValues === undefined) {
-        return {
-            data: null,
-            error: "Missing values in the template-formatted prompt",
-        };
-    }
+  const regexPrompt = json["messages"];
+  const regexMessages = regexPrompt;
+  const regexValues = json["values"];
 
-    let formattedMessages = [];
-    for (let i = 0; i < regexMessages.length; i++) {
-        let message = regexMessages[i];
-        const content = message["content"];
-
-        const formattedContent = formatPrompt(
-            {
-                prompt: content,
-                values: regexValues,
-            }
-        )
-
-        if (formattedContent.error !== null) {
-            return {
-                data: null,
-                error: formattedContent.error,
-            };
-        }
-
-        const formattedMessage = {
-            ...message,
-            content: formattedContent.data,
-        }
-
-        formattedMessages.push(formattedMessage);
-    }
-    json["messages"] = formattedMessages;
-    delete json["values"];
-
-    const body = JSON.stringify(json);
-    const formattedRequest = updateContentLength(cloneRequest, body);
-
-    const data = {
-        request: formattedRequest,
-        prompt: regexPrompt,
-        body,
-    }
-
+  // If regexValues is not defined, return an error
+  if (regexValues === undefined) {
     return {
-        data: {
-            request: formattedRequest,
-            prompt: {
-                prompt: regexPrompt,
-                values: regexValues,
-            },
-            body,
-        },
-        error: null,
+      data: null,
+      error: "Missing values in the template-formatted prompt",
     };
+  }
+
+  const formattedMessages = [];
+  for (let i = 0; i < regexMessages.length; i++) {
+    const message = regexMessages[i];
+    const content = message["content"];
+
+    const formattedContent = formatPrompt({
+      prompt: content,
+      values: regexValues,
+    });
+
+    if (formattedContent.error !== null) {
+      return {
+        data: null,
+        error: formattedContent.error,
+      };
+    }
+
+    const formattedMessage = {
+      ...message,
+      content: formattedContent.data,
+    };
+
+    formattedMessages.push(formattedMessage);
+  }
+  json["messages"] = formattedMessages;
+  delete json["values"];
+
+  const body = JSON.stringify(json);
+  const formattedRequest = updateContentLength(cloneRequest, body);
+
+  const data = {
+    request: formattedRequest,
+    prompt: regexPrompt,
+    body,
+  };
+
+  return {
+    data: {
+      request: formattedRequest,
+      prompt: {
+        prompt: regexPrompt,
+        values: regexValues,
+      },
+      body,
+    },
+    error: null,
+  };
 }
