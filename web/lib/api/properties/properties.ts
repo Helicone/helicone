e4fccd7900f548b30459997a61d674da7177cfa4
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-04-22 11:20:58 -0700

    feat/properties (#316)
    * new properties table
    
    * remove unused sql

--- web/lib/api/properties/properties.ts
+++ web/lib/api/properties/properties.ts
@@ -11,14 +11,22 @@ export interface Property {
 export async function getProperties(
   user_id: string
 ): Promise<Result<Property[], string>> {
+  // The new query once the backfill/migration is complete
+  //   const query = `
+  // SELECT DISTINCT key AS property
+  // FROM properties p
+  // JOIN user_api_keys u ON p.auth_hash = u.api_key_hash
+  // WHERE (u.user_id = '${user_id}');`;
+
   const query = `
-SELECT DISTINCT keys AS property
-FROM request r
-JOIN user_api_keys u ON r.auth_hash = u.api_key_hash
-CROSS JOIN LATERAL jsonb_object_keys(properties) keys
-WHERE (u.user_id = '${user_id}');`;
+  SELECT DISTINCT keys AS property
+  FROM request r
+  JOIN user_api_keys u ON r.auth_hash = u.api_key_hash
+  CROSS JOIN LATERAL jsonb_object_keys(properties) keys
+  WHERE (u.user_id = '${user_id}');`;
 
   const { data, error } = await dbExecute<Property>(query, []);
+
   if (error !== null) {
     return { data: null, error: error };
   }
