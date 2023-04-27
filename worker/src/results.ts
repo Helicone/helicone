ac2dccebf52df498c4706bcbeaf33b463b528730
Justin Torre <justintorre75@gmail.com>
2023-04-27 16:30:40 -0700

    Clickhouse (#336)
    * added clickhouse local
    
    * clickhouse added to worker and running locally
    
    * clickhouse cleanup
    
    * username and password fixes
    
    * added backfill script
    
    * added dbExecute to run in paralell with the others
    
    * 😭

---
+++ worker/src/results.ts
@@ -0,0 +1,23 @@
+import { PostgrestSingleResponse } from "@supabase/supabase-js";
+
+export type Result<T, K> = SuccessResult<T> | ErrorResult<K>;
+
+interface SuccessResult<T> {
+  data: T;
+  error: null;
+}
+interface ErrorResult<T> {
+  data: null;
+  error: T;
+}
+
+export type GenericResult<T> = Result<T, string>;
+
+export function mapPostgrestErr<T>(
+  result: PostgrestSingleResponse<T>
+): Result<T, string> {
+  if (result.error === null) {
+    return { data: result.data, error: null };
+  }
+  return { data: null, error: result.error.message };
+}
