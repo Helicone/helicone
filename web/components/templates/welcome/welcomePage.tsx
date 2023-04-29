480e66cda461dad6450514e04ef34bc5b6e3b8e3
Khoa Nguyen <scottmktn@gmail.com>
2023-04-29 11:33:46 -0700

    fix

--- web/components/templates/welcome/welcomePage.tsx
+++ web/components/templates/welcome/welcomePage.tsx
@@ -211,7 +211,7 @@ const WelcomePage = (props: DashboardPageProps) => {
   ];
 
   return (
-    <div className="flex flex-col py-24 w-full max-w-2xl mx-auto min-h-max">
+    <div className="flex flex-col py-24 w-full max-w-2xl mx-auto min-h-max px-4 mx-auto">
       <h1 className="text-3xl font-semibold text-gray-900 w-full text-center mb-10">
         Get Started with Helicone
       </h1>
