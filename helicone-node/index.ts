1da05e36f66de05b0c4570c6a35e33c8923e6532
BarakOshri <barakoshri@gmail.com>
2023-04-30 12:23:12 -0400

    Node package (#332)
    * first draft
    
    * added readme
    
    * new features and change to typescript
    
    * node package almost ready
    
    * update
    
    * works for configurations
    
    * add linting and fix tests
    
    * lint
    
    * README
    
    * update readme
    
    * update
    
    * few changes

---
+++ helicone-node/index.ts
@@ -0,0 +1,92 @@
+import {
+  Configuration as OpenAIConfiguration,
+  OpenAIApi as OpenAIApiOriginal,
+} from "openai";
+
+interface HeliconeConfigurationOptions {
+  apiKey: string;
+  heliconeApiKey?: string;
+  properties?: { [key: string]: any };
+  cache?: boolean;
+  retry?: boolean | { [key: string]: any };
+  rateLimitPolicy?: string | { [key: string]: any };
+}
+
+class HeliconeConfiguration extends OpenAIConfiguration {
+  constructor(options: HeliconeConfigurationOptions) {
+    super({ apiKey: options.apiKey });
+
+    const heliconeHeaders = {
+      "Helicone-Auth": `Bearer ${options.heliconeApiKey}`,
+      ...getPropertyHeaders(options.properties),
+      ...getCacheHeaders(options.cache),
+      ...getRetryHeaders(options.retry),
+      ...getRateLimitPolicyHeaders(options.rateLimitPolicy),
+    };
+
+    this.baseOptions = {
+      ...this.baseOptions,
+      headers: {
+        ...this.baseOptions.headers,
+        ...heliconeHeaders,
+      },
+    };
+  }
+}
+
+function getPropertyHeaders(properties?: { [key: string]: any }): {
+  [key: string]: string;
+} {
+  if (!properties) return {};
+  const headers: { [key: string]: string } = {};
+  for (const key in properties) {
+    headers[`Helicone-Property-${key}`] = properties[key].toString();
+  }
+  return headers;
+}
+
+function getCacheHeaders(cache?: boolean): { [key: string]: string } {
+  return cache ? { "Helicone-Cache-Enabled": "true" } : {};
+}
+
+function getRetryHeaders(retry?: boolean | { [key: string]: any }): {
+  [key: string]: string;
+} {
+  if (!retry) return {};
+  const headers: { [key: string]: string } = {
+    "Helicone-Retry-Enabled": "true",
+  };
+  if (typeof retry === "object") {
+    if (retry.num) headers["Helicone-Retry-Num"] = retry.num.toString();
+    if (retry.factor)
+      headers["Helicone-Retry-Factor"] = retry.factor.toString();
+    if (retry.min_timeout)
+      headers["Helicone-Retry-Min-Timeout"] = retry.min_timeout.toString();
+    if (retry.max_timeout)
+      headers["Helicone-Retry-Max-Timeout"] = retry.max_timeout.toString();
+  }
+  return headers;
+}
+
+function getRateLimitPolicyHeaders(
+  rateLimitPolicy?: string | { [key: string]: any }
+): { [key: string]: string } {
+  if (!rateLimitPolicy) return {};
+  let policy = "";
+  if (typeof rateLimitPolicy === "string") {
+    policy = rateLimitPolicy;
+  } else if (typeof rateLimitPolicy === "object") {
+    policy = `${rateLimitPolicy.quota};w=${rateLimitPolicy.time_window}`;
+    if (rateLimitPolicy.segment) policy += `;s=${rateLimitPolicy.segment}`;
+  } else {
+    throw new TypeError(
+      "rate_limit_policy must be either a string or a dictionary"
+    );
+  }
+  return { "Helicone-RateLimit-Policy": policy };
+}
+
+export {
+  HeliconeConfiguration as Configuration,
+  OpenAIApiOriginal as OpenAIApi,
+};
---
+++ helicone-node/tests/helicone.test.ts
@@ -0,0 +1,84 @@
+import { Configuration, OpenAIApi } from "./../index";
+import { v4 as uuidv4 } from "uuid";
+
+const apiKey = process.env.OPENAI_API_KEY;
+const heliconeApiKey = process.env.HELICONE_API_KEY;
+
+if (!apiKey || !heliconeApiKey) {
+  throw new Error("API keys must be set as environment variables.");
+}
+
+// Test cache behavior
+test("cache", async () => {
+  const uniqueId = uuidv4();
+  const prompt = `Cache test with UUID: ${uniqueId}`;
+
+  const configuration = new Configuration({
+    apiKey,
+    heliconeApiKey,
+    cache: true,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  await openai.createCompletion({
+    model: "text-ada-001",
+    prompt,
+    max_tokens: 10,
+  });
+}, 60000);
+
+// Test rate limit policy
+test("rate limit policy", async () => {
+  const rateLimitPolicyDict = { quota: 10, time_window: 60 };
+  const rateLimitPolicyStr = "10;w=60";
+
+  let configuration = new Configuration({
+    apiKey,
+    heliconeApiKey,
+    rateLimitPolicy: rateLimitPolicyDict,
+  });
+
+  let openai = new OpenAIApi(configuration);
+
+  await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Rate limit policy test" }],
+  });
+
+  configuration = new Configuration({
+    apiKey,
+    heliconeApiKey,
+    rateLimitPolicy: rateLimitPolicyStr,
+  });
+
+  openai = new OpenAIApi(configuration);
+
+  await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Rate limit policy test" }],
+  });
+}, 60000);
+
+// Test custom properties
+test("custom properties", async () => {
+  const properties = {
+    Session: "24",
+    Conversation: "support_issue_2",
+    App: "mobile",
+  };
+
+  const configuration = new Configuration({
+    apiKey,
+    heliconeApiKey,
+    properties,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  await openai.createCompletion({
+    model: "text-ada-001",
+    prompt: "Custom properties test",
+    max_tokens: 10,
+  });
+}, 60000);
