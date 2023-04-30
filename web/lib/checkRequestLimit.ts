0bc8764f5ccc0d45b46e86fde4c1818e66df86ec
Justin Torre <justintorre75@gmail.com>
2023-04-30 00:20:37 -0700

    Check request in try (#343)
    * added all the clickhouse stuff for reads
    
    * added requestOverLimit within a try catch

--- web/lib/checkRequestLimit.ts
+++ web/lib/checkRequestLimit.ts
@@ -7,38 +7,41 @@ import {
 } from "./api/request/request";
 
 export async function requestOverLimit(client: SupabaseClient<Database>) {
-  const startOfThisMonth = new Date();
-  startOfThisMonth.setDate(1);
-  startOfThisMonth.setHours(0);
-  startOfThisMonth.setMinutes(0);
-  startOfThisMonth.setSeconds(0);
-  startOfThisMonth.setMilliseconds(0);
-  const userId = (await client.auth.getUser()).data.user?.id;
-  if (!userId) {
-    return false;
-  }
+  try {
+    const startOfThisMonth = new Date();
+    startOfThisMonth.setDate(1);
+    startOfThisMonth.setHours(0);
+    startOfThisMonth.setMinutes(0);
+    startOfThisMonth.setSeconds(0);
+    startOfThisMonth.setMilliseconds(0);
+    const userId = (await client.auth.getUser()).data.user?.id;
+    if (!userId) {
+      return false;
+    }
 
-  const [
-    { data: count, error },
-    { data: userSettings, error: userSettingsError },
-  ] = await Promise.all([
-    await getRequestCountClickhouse(userId, {
-      response_copy_v1: {
-        request_created_at: {
-          gte: startOfThisMonth.toISOString(),
+    const [
+      { data: count, error },
+      { data: userSettings, error: userSettingsError },
+    ] = await Promise.all([
+      await getRequestCountClickhouse(userId, {
+        response_copy_v1: {
+          request_created_at: {
+            gte: startOfThisMonth.toISOString(),
+          },
         },
-      },
-    }),
-    client.from("user_settings").select("*").single(),
-  ]);
-  console.log("GETSSP", { count, error, userSettings, userSettingsError });
+      }),
+      client.from("user_settings").select("*").single(),
+    ]);
 
-  if (userSettingsError !== null || userSettings === null) {
+    if (userSettingsError !== null || userSettings === null) {
+      return false;
+    }
+    if (error !== null || count === null) {
+      return false;
+    }
+
+    return count > userSettings.request_limit;
+  } catch (e) {
     return false;
   }
-  if (error !== null || count === null) {
-    return false;
-  }
-
-  return count > userSettings.request_limit;
 }
