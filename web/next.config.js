12ed20a37853becb52c8e68fa053c031106aa0df
Justin Torre <justintorre75@gmail.com>
2023-04-30 23:38:10 -0700

    used rewrites for posthog updateS (#352)

--- web/next.config.js
+++ web/next.config.js
@@ -23,6 +23,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/:path*",
+        destination: "https://app.posthog.com/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
