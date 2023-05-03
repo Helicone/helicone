fa581a6c9ce57e65225edde4a742956ba7380414
Khoa Nguyen <scottmktn@gmail.com>
2023-05-03 11:46:46 -0700

    add org hooks

---
+++ web/services/hooks/organizations.tsx
@@ -0,0 +1,64 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useQuery } from "@tanstack/react-query";
+import { Members } from "../../pages/api/organization/[id]/members";
+import { Owner } from "../../pages/api/organization/[id]/owner";
+import { Database } from "../../supabase/database.types";
+
+const useGetOrgMembers = (orgId: string) => {
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ["OrganizationsMembers", orgId],
+    queryFn: async (query) => {
+      const organizationId = query.queryKey[1];
+      return fetch(`/api/organization/${organizationId}/members`).then(
+        (res) => res.json() as Promise<Members>
+      );
+    },
+    refetchOnWindowFocus: false,
+  });
+  return {
+    data,
+    isLoading,
+    refetch,
+  };
+};
+
+const useGetOrgOwner = (orgId: string) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["OrganizationsMembersOwner", orgId],
+    queryFn: async (query) => {
+      const organizationId = query.queryKey[1];
+      return fetch(`/api/organization/${organizationId}/owner`).then(
+        (res) => res.json() as Promise<Owner>
+      );
+    },
+    refetchOnWindowFocus: false,
+  });
+  return {
+    data,
+    isLoading,
+  };
+};
+
+const useGetOrgs = () => {
+  const supabaseClient = useSupabaseClient<Database>();
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ["Organizations"],
+    queryFn: async (query) => {
+      const { data, error } = await supabaseClient
+        .from("organization")
+        .select(`*`);
+      if (error) {
+        throw error;
+      }
+      return data;
+    },
+    refetchOnWindowFocus: false,
+  });
+  return {
+    data,
+    isLoading,
+    refetch,
+  };
+};
+
+export { useGetOrgMembers, useGetOrgOwner, useGetOrgs };
