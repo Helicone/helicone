55b03cf6e63b26c334a83efab7657464d1d12279
Justin Torre <justintorre75@gmail.com>
2023-05-03 12:48:56 -0700

    fixed posthog to use www. for helicone (#363)

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -17,7 +17,7 @@ if (
   process.env.NEXT_PUBLIC_ENDPOINT
 ) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, {
-    api_host: "https://helicone.ai/ingest",
+    api_host: "https://www.helicone.ai/ingest",
   });
 }
 
