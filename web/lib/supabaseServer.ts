2abe3b9a53a3900687220464fc29d858a2c633bf
Justin Torre <justintorre75@gmail.com>
2023-05-08 10:24:54 -0700

    added back default ENV if other env not set (#376)

--- web/lib/supabaseServer.ts
+++ web/lib/supabaseServer.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../supabase/database.types";
 
-const supabaseUrl = process.env.SUPABASE_URL ?? "";
+export const supabaseUrl =
+  process.env.SUPABASE_URL ?? process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY ?? "";
 
 if (supabaseUrl === "" || supabaseServiceKey === "") {
