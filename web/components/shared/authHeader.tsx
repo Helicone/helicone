ce1e463b5008c54f330181a872e6f91313046ba8
Khoa Nguyen <scottmktn@gmail.com>
2023-05-15 11:04:41 -0700

    padding on auth header

--- web/components/shared/authHeader.tsx
+++ web/components/shared/authHeader.tsx
@@ -15,7 +15,7 @@ const AuthHeader = (props: AuthHeaderProps) => {
   const { title, breadcrumb, headerActions, actions } = props;
 
   return (
-    <div className="flex flex-row sm:items-center border-b border-gray-200 pb-2 mb-2 justify-between">
+    <div className="flex flex-row sm:items-center border-b border-gray-200 pb-2 mb-4 justify-between">
       <div className="sm:flex-auto items-center flex flex-row space-x-4">
         <div className="flex flex-row space-x-4 items-center">
           {breadcrumb && (
