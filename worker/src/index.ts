8dcd927f43df1e8ceac8ab74f9f31af31c8193ef
Barak Oshri <barakoshri@gmail.com>
2023-05-15 14:43:33 -0700

    bad merge

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -6,10 +6,6 @@ import {
   getCacheSettings,
   recordCacheHit,
   saveToCache,
-<<<<<<< HEAD
-=======
-  uncachedRequest,
->>>>>>> 849cdd5edcdec64430d0818866cc461d709d8bec
 } from "./cache";
 import { ClickhouseEnv, dbInsertClickhouse } from "./clickhouse";
 import { once } from "./helpers";
