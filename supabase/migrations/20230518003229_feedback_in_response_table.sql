7f62d4168a4ab560aefbaa560ca5bc6ddd3f00e5
Barak Oshri <barakoshri@gmail.com>
2023-05-18 12:49:56 -0700

    remove set

--- supabase/migrations/20230518003229_feedback_in_response_table.sql
+++ supabase/migrations/20230518003229_feedback_in_response_table.sql
@@ -1,7 +1,5 @@
 alter table "public"."response" add column "feedback" jsonb;
 
-set check_function_bodies = off;
-
 CREATE OR REPLACE FUNCTION public.insert_feedback_and_update_response(response_id uuid, feedback_metric_id bigint, boolean_value boolean, numerical_value double precision, string_value text, categorical_value text, created_by text, name text)
  RETURNS bigint
  LANGUAGE plpgsql
