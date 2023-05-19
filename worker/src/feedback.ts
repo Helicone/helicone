4921ff09f3c7fa0c9477a09865affcde45b23b05
BarakOshri <barakoshri@gmail.com>
2023-05-19 16:45:27 -0700

    date fix (#426)

--- worker/src/feedback.ts
+++ worker/src/feedback.ts
@@ -228,7 +228,10 @@ export async function addFeedback(
     metricId = data.id;
   } else {
     // Validate the data type before inserting the feedback
-    if (!(metricData.data_type == "categorical" && dataType == "string") && metricData.data_type !== dataType) {
+    if (
+      !(metricData.data_type == "categorical" && dataType == "string") &&
+      metricData.data_type !== dataType
+    ) {
       throw new Error(
         `Data type of this feedback request "${dataType}" does not match the data type of the created feedback metric "${metricData.data_type}".}`
       );
@@ -277,17 +280,19 @@ export async function addFeedback(
   }
 
   // Execute the transaction
-  const { data, error: insertError } = await dbClient.rpc('insert_feedback_and_update_response', {
-    response_id: feedbackData.response_id, 
-    feedback_metric_id: feedbackData.feedback_metric_id, 
-    boolean_value: feedbackData.boolean_value || null, 
-    numerical_value: feedbackData.float_value || null, 
-    categorical_value: feedbackData.categorical_value || null, 
-    string_value: feedbackData.string_value || null,
-    created_by: feedbackData.created_by,
-    name: name
-  })
-
+  const { data, error: insertError } = await dbClient.rpc(
+    "insert_feedback_and_update_response",
+    {
+      response_id: feedbackData.response_id,
+      feedback_metric_id: feedbackData.feedback_metric_id,
+      boolean_value: feedbackData.boolean_value || null,
+      numerical_value: feedbackData.float_value || null,
+      categorical_value: feedbackData.categorical_value || null,
+      string_value: feedbackData.string_value || null,
+      created_by: feedbackData.created_by,
+      name: name,
+    }
+  );
 
   // Handle error
   if (insertError) {
@@ -296,5 +301,4 @@ export async function addFeedback(
   } else {
     return data.id;
   }
-
 }
