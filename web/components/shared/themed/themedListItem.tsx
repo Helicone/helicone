9d1b82167d15f5b0ae2b14baace2dd72441a67ac
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-05-22 11:27:52 -0700

    user-totals-css (#430)

--- web/components/shared/themed/themedListItem.tsx
+++ web/components/shared/themed/themedListItem.tsx
@@ -25,11 +25,17 @@ const ThemedListItem = (props: ThemedListItemProps) => {
       onClick={onClickHandler}
     >
       <div className="flex flex-col space-y-0.5">
-        <div className="flex flex-row space-x-2.5 items-center">
-          <p className="hidden md:block text-sm text-gray-900 leading-6 font-semibold">
+        <div className="flex flex-row gap-2.5 items-center">
+          <p className="hidden xl:block text-sm text-gray-900 leading-6 font-semibold text-left">
+            {truncString(
+              "fdjalghljfdakghfdlkagdklsFGKLDSFGGDLKGHDSLKFdhslkfsdk",
+              56
+            )}
+          </p>
+          <p className="hidden md:block xl:hidden text-sm text-gray-900 leading-6 font-semibold text-left">
             {truncString(title, 36)}
           </p>
-          <p className="block md:hidden text-sm text-gray-900 leading-6 font-semibold">
+          <p className="block md:hidden text-sm text-gray-900 leading-6 font-semibold text-left">
             {truncString(title, 16)}
           </p>
           {pill && pill}
