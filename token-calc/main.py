c858a02a644bf5636e2da8c843758bb670e41ba8
Justin <justintorre75@gmail.com>
2023-05-24 21:20:39 -0700

    small fixes

--- token-calc/main.py
+++ token-calc/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 import anthropic
 
 
 class Item(BaseModel):
-    sample_str: str
+    text: str
 
 
 app = FastAPI()
@@ -12,10 +13,16 @@ app = FastAPI()
 
 @app.post("/anthropic/count_tokens")
 async def count_tokens_endpoint(item: Item):
-    num_tokens = anthropic.count_tokens(item.sample_str)
-    return {"Number of tokens": num_tokens}
+    num_tokens = anthropic.count_tokens(item.text)
+    return {"count": num_tokens}
 
 
 @app.get("/healthcheck")
 def healthcheck():
     return {"status": "healthy"}
+
+
+# Redirect to docs if no path is specified
+@app.get("/")
+def redirect_to_docs():
+    return RedirectResponse(url="https://helicone.ai/token_count")
