b402f85b7ccf44ac98ba3268b8e0ab4548eb8a97
Justin <justintorre75@gmail.com>
2023-06-15 14:36:04 -0700

    fixed bug to pass back status

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -8,6 +8,7 @@ export interface Env {
   SUPABASE_SERVICE_ROLE_KEY: string;
   SUPABASE_URL: string;
   TOKENIZER_COUNT_API: string;
+  TOKEN_COUNT_URL: string;
   RATE_LIMIT_KV: KVNamespace;
   CLICKHOUSE_HOST: string;
   CLICKHOUSE_USER: string;
