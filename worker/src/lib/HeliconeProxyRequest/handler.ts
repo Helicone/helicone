cc174e2cc433f9e2a917f533e81a917beacac471
Justin Torre <justintorre75@gmail.com>
2023-06-15 15:01:13 -0700

    make sure status is not lost on response object (#495)

--- worker/src/lib/HeliconeProxyRequest/handler.ts
+++ worker/src/lib/HeliconeProxyRequest/handler.ts
@@ -71,6 +71,7 @@ export async function handleProxyRequest(
       response: new Response(body, {
         ...response,
         headers: responseHeaders,
+        status: response.status,
       }),
     },
     error: null,
