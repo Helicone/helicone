d59f837e9563f1bc7ab198be845e4227f41e7e68
Khoa Nguyen <scottmktn@gmail.com>
2023-06-18 16:30:25 -0700

    new columns button

--- web/components/templates/requestsV2/viewColumns.tsx
+++ web/components/templates/requestsV2/viewColumns.tsx
@@ -1,7 +1,8 @@
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useRef, useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
 import {
+  AdjustmentsHorizontalIcon,
   MinusCircleIcon,
   PlusCircleIcon,
   ViewColumnsIcon,
@@ -21,9 +22,11 @@ export default function ViewColumns<T>(props: ViewColumnsProps<T>) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="items-center flex font-semibold text-gray-900 text-sm rounded-lg hover:text-sky-900 hover:bg-sky-100 px-4 py-2">
-          <ViewColumnsIcon className="h-5 w-5 mr-2 inline-block" />
-          Columns {`( ${visibleColumns} / ${columns.length} )`}
+        <Menu.Button className="border border-gray-300 rounded-lg px-2.5 py-1.5 bg-white hover:bg-sky-50 flex flex-row items-center gap-2">
+          <AdjustmentsHorizontalIcon className="h-5 w-5 text-gray-900" />
+          <p className="text-sm font-medium text-gray-900">
+            View {`( ${visibleColumns} / ${columns.length} )`}
+          </p>
         </Menu.Button>
       </div>
       <Transition
@@ -35,56 +38,48 @@ export default function ViewColumns<T>(props: ViewColumnsProps<T>) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute z-10 right-0 mt-2 w-[250px] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute z-10 right-0 mt-2 w-[200px] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50 rounded-t-lg">
             <button
               onClick={() => onSelectAll(false)}
-              className="text-xs flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100 rounded-t-lg border-b border-gray-900/5"
+              className="text-xs flex items-center justify-center gap-x-2.5 p-2.5 font-semibold text-gray-900 hover:bg-gray-100 rounded-t-lg border-b border-gray-900/5"
             >
-              <MinusCircleIcon
-                className="h-4 w-4 flex-none text-gray-400"
-                aria-hidden="true"
-              />
               Deselect All
             </button>
             <button
               onClick={() => onSelectAll(true)}
-              className="text-xs flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100 border-b border-gray-900/5"
+              className="text-xs flex items-center justify-center gap-x-2.5 p-2.5 font-semibold text-gray-900 hover:bg-gray-100 border-b border-gray-900/5"
             >
-              <PlusCircleIcon
-                className="h-4 w-4 flex-none text-gray-400"
-                aria-hidden="true"
-              />
               Select All
             </button>
           </div>
-          <div className="px-1">
+          <div className="">
             {columns.map((column, idx) => {
               const header = column.columnDef.header as string;
 
               return (
-                <div key={idx}>
-                  <div
-                    key={column.id}
-                    className={clsx(idx !== 0 && "border-t border-gray-200")}
+                <div key={idx} className="flex flex-row my-[3px]">
+                  <label
+                    key={idx}
+                    className="relative mx-1 my-[1px] px-3 py-2 rounded-lg select-none font-medium text-gray-900 w-full items-center flex hover:bg-sky-100 hover:cursor-pointer"
                   >
-                    <label
-                      key={idx}
-                      className="relative p-4 select-none font-medium text-gray-900 w-full justify-between items-center flex hover:bg-sky-100 hover:cursor-pointer"
-                    >
-                      <span className="text-sm text-gray-900 hover:text-sky-900 font-semibold">
-                        {header}
-                      </span>
-                      <input
-                        {...{
-                          type: "checkbox",
-                          checked: column.getIsVisible(),
-                          onChange: column.getToggleVisibilityHandler(),
-                        }}
-                        className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-600"
-                      />
-                    </label>
-                  </div>
+                    {column.getIsVisible() ? (
+                      <CheckIcon className="h-4 w-4 text-gray-900" />
+                    ) : (
+                      <div className="h-4 w-4" />
+                    )}
+                    <span className="text-sm text-gray-700 hover:text-sky-900 pl-2">
+                      {header}
+                    </span>
+                    <input
+                      {...{
+                        type: "checkbox",
+                        checked: column.getIsVisible(),
+                        onChange: column.getToggleVisibilityHandler(),
+                      }}
+                      className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-600 sr-only"
+                    />
+                  </label>
                 </div>
               );
             })}
