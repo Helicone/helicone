4782174d95bb8eae0b107b996973e7e5bb1e84e1
Khoa Nguyen <scottmktn@gmail.com>
2023-06-20 14:40:52 -0700

    Revert "export"
    This reverts commit c2756fc3841e3955a8ba53e27f1a8806d45e654d.

--- web/components/templates/requestsV2/exportButton.tsx
+++ web/components/templates/requestsV2/exportButton.tsx
@@ -1,9 +1,7 @@
-import { ArrowDownTrayIcon, ArrowPathIcon } from "@heroicons/react/24/outline";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Papa from "papaparse";
 import { useState } from "react";
-import { clsx } from "../../shared/clsx";
-import useNotification from "../../shared/notification/useNotification";
 import ThemedModal from "../../shared/themed/themedModal";
 import { NormalizedRequest } from "./builder/abstractRequestBuilder";
 
@@ -11,15 +9,18 @@ interface ExportButtonProps<T> {
   rows: T[];
 }
 
+const notificationMethods = [
+  { id: "filtered", title: "Only selected columns", filtered: true },
+  { id: "all", title: "All event properties", filtered: false },
+];
+
 export default function ExportButton<T>(props: ExportButtonProps<T>) {
   const { rows } = props;
 
   const [open, setOpen] = useState(false);
   const [downloadingCSV, setDownloadingCSV] = useState(false);
 
-  const { setNotification } = useNotification();
-
-  const csvDownload = () => {
+  const csvDownload = async (filtered: boolean) => {
     setDownloadingCSV(true);
 
     // Convert JSON data to CSV
@@ -34,9 +35,52 @@ export default function ExportButton<T>(props: ExportButtonProps<T>) {
     link.click();
     document.body.removeChild(link);
 
-    setDownloadingCSV(false);
-    setOpen(false);
-    setNotification("CSV downloaded successfully!", "success");
+    // fetch("/api/request", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     filter: advancedFilters,
+    //     offset: 0,
+    //     limit: 500,
+    //     sort: sortLeaf,
+    //   }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     const requests = res.data as HeliconeRequest[];
+    //     const wrappedRequests = requests.map((request) => {
+    //       const wrappedRequest = convertRequest(request, values);
+    //       const obj: any = {};
+    //       columns.forEach((col) => {
+    //         if (filtered) {
+    //           if (col.active) {
+    //             obj[col.key] = wrappedRequest[col.key];
+    //           }
+    //         } else {
+    //           obj[col.key] = wrappedRequest[col.key];
+    //         }
+    //       });
+    //       return obj;
+    //     });
+    // // Convert JSON data to CSV
+    // const csv = Papa.unparse(wrappedRequests);
+    // // Create a blob with the CSV data
+    // const blob = new Blob([csv], { type: "text/csv" });
+    // // Create a download link and click it to start the download
+    // const link = document.createElement("a");
+    // link.href = URL.createObjectURL(blob);
+    // link.download = "requests.csv";
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
+    //   })
+    //   .catch((e) => console.error(e))
+    //   .finally(() => {
+    //     setDownloadingCSV(false);
+    //     setOpenExport(false);
+    //   });
   };
 
   return (
@@ -69,6 +113,36 @@ export default function ExportButton<T>(props: ExportButtonProps<T>) {
                 .
               </p>
             </div>
+
+            <fieldset className="space-y-2">
+              <p className="text-xs text-gray-600">Properties on export</p>
+              <legend className="sr-only">Notification method</legend>
+              <div className="space-y-2">
+                {notificationMethods.map((notificationMethod) => (
+                  <div
+                    key={notificationMethod.id}
+                    className="flex items-center"
+                  >
+                    <input
+                      id={notificationMethod.id}
+                      name="notification-method"
+                      type="radio"
+                      defaultChecked={notificationMethod.id === "filtered"}
+                      className="h-4 w-4 border-gray-300 text-sky-600 focus:ring-sky-600 hover:cursor-pointer"
+                      onClick={() => {
+                        // setExportFiltered(notificationMethod.filtered);
+                      }}
+                    />
+                    <label
+                      htmlFor={notificationMethod.id}
+                      className="ml-3 block text-sm font-medium leading-6 text-gray-600"
+                    >
+                      {notificationMethod.title}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </fieldset>
             <p className="text-sm sm:text-md text-gray-600">
               Export may take a lot of time. Please do not close this modal once
               export is started.
@@ -85,9 +159,9 @@ export default function ExportButton<T>(props: ExportButtonProps<T>) {
             </button>
             <button
               className="items-center rounded-md bg-black px-4 py-2 text-md flex font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              onClick={csvDownload}
+              //   onClick={() => csvExport.onClick(exportFiltered)}
             >
-              {downloadingCSV ? (
+              {/* {csvExport.downloadingCSV ? (
                 <>
                   <ArrowPathIcon
                     className={clsx("h-5 w-5 inline animate-spin mr-2")}
@@ -96,7 +170,8 @@ export default function ExportButton<T>(props: ExportButtonProps<T>) {
                 </>
               ) : (
                 <p>Export</p>
-              )}
+              )} */}
+              Export
             </button>
           </div>
         </div>
