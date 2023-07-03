d339c8c1a2c484e52bb85f30426259433e4f9186
Justin <justintorre75@gmail.com>
2023-07-03 17:25:45 -0400

    added request/ch/count?org_id=sdlkfhasdfhjds route

---
+++ web/pages/api/request/ch/count.ts
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {
+  getRequestCount,
+  getRequestCountClickhouse,
+} from "../../../../lib/api/request/request";
+
+import {
+  HandlerWrapperOptions,
+  withAuth,
+} from "../../../../lib/api/handlerWrappers";
+import { Result } from "../../../../lib/result";
+import { FilterNode } from "../../../../services/lib/filters/filterDefs";
+
+async function handler({
+  req,
+  res,
+  supabaseClient,
+}: HandlerWrapperOptions<Result<number, string>>) {
+  const { org_id: orgId } = req.query as {
+    org_id: string;
+  };
+  const { data: org, error: orgError } = await supabaseClient
+    .getClient()
+    .from("organization")
+    .select("*")
+    .eq("id", orgId);
+
+  if (orgError !== null || !org || org.length === 0) {
+    res.status(400).json({ error: "Invalid org", data: null });
+    return;
+  }
+
+  const count = await getRequestCountClickhouse(orgId, "all");
+  res.status(count.error === null ? 200 : 500).json(count);
+}
+
+export default withAuth(handler);
