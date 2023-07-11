5f004b9756c86385738274bac55212f875315b0f
Justin <justintorre75@gmail.com>
2023-07-10 22:59:39 -0700

    sudo working with wrapping in authbind

--- mitmproxy.sh
+++ mitmproxy.sh
@@ -10,12 +10,20 @@ create_files() {
 
 # Function to start the proxy
 start_proxy() {
+  echo "Creating required directories and files..."
+  mkdir -p ~/.helicone
+  touch ~/.helicone/proxy_pid
+  touch ~/.helicone/mitmproxy.log
+  export HELICONE_API_KEY=sk-tm2og4a-7msunia-tqbcuxq-bhbkfha
+  export HELICONE_CACHE_ENABLED=true
   echo "Starting the proxy..."
 
   # Install necessary packages
   echo "Step 1: Installing necessary packages..."
   sudo apt update
+  sudo apt-get update
   sudo apt install -y curl ca-certificates mitmproxy
+  sudo apt install iptables
 
   # Add to /etc/hosts
   echo "Step 2: Adding entry to /etc/hosts..."
@@ -26,35 +34,39 @@ start_proxy() {
   echo 'import os' > add_headers.py
   echo 'def request(flow):' >> add_headers.py
   echo '    flow.request.headers["Helicone-Auth"] = "Bearer " + os.environ.get("HELICONE_API_KEY")' >> add_headers.py
-  
+
   # Start a reverse proxy and save its PID
   echo "Step 4: Starting a reverse proxy and saving its PID..."
-  nohup mitmweb --mode reverse:https://oai.hconeai.com:443 --listen-port 443 -s add_headers.py > ~/.helicone/mitmproxy.log 2>&1 &
-  echo $! > ~/.helicone/proxy_pid
-  echo "Proxy started."
-  sleep 1
+
+  sudo apt-get install authbind
+  sudo touch /etc/authbind/byport/443
+  sudo chmod 500 /etc/authbind/byport/443
+  sudo chown $USER /etc/authbind/byport/443
+
+  nohup authbind --deep mitmweb --mode reverse:https://oai.hconeai.com:443 --listen-port 443 -s add_headers.py | tee -a ~/.helicone/mitmproxy.log 2>&1 &
+  echo $! | tee -a ~/.helicone/proxy_pid
+  echo "Proxy started.... sleeping 20 seconds"
+  sleep 20
+  echo "Done sleeping."
   cat ~/.helicone/mitmproxy.log
+
   echo  "FINDING MITMPROXY CERTIFICATE"
+  find ~ -name 'mitmproxy-ca-cert.pem'
   sudo find ~ -name 'mitmproxy-ca-cert.pem'
   echo  "DONE FINDING MITMPROXY CERTIFICATE"
-  
+
+
   # Install the mitmproxy certificate
   echo "Step 5: Installing the mitmproxy certificate..."
   # Note: Run mitmproxy once if the certificate does not exist
   USER_HOME=$HOME
   sudo cp $USER_HOME/.mitmproxy/mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/mitmproxy-ca-cert.crt
   sudo update-ca-certificates
-  
+
   # Append the mitmproxy certificate to the curl certificate bundle
   echo "Step 6: Appending the mitmproxy certificate to the curl certificate bundle..."
-  sudo bash -c "cat $HOME/.mitmproxy/mitmproxy-ca-cert.pem >> /etc/ssl/certs/ca-certificates.crt"
-  echo "Setup complete. Please manually install the mitmproxy certificate in your browser."
-  # Print the command to kill mitmweb
-  echo "To stop the mitmweb process, you can run:"
-  echo "./script.sh stop"
-  # Provide instructions for setting the REQUESTS_CA_BUNDLE environment variable
-  echo "To set the REQUESTS_CA_BUNDLE environment variable, you can add the following line to your shell profile:"
-  echo 'export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt'
+  sudo bash -c "cat $USER_HOME/.mitmproxy/mitmproxy-ca-cert.pem >> /etc/ssl/certs/ca-certificates.crt"
+
 }
 
 # Function to stop the proxy
@@ -65,7 +77,7 @@ stop_proxy() {
   if ps -p $(cat ~/.helicone/proxy_pid) > /dev/null
   then
      echo "Stopping the proxy..."
-     sudo kill -9 $(cat ~/.helicone/proxy_pid)
+     kill -9 $(cat ~/.helicone/proxy_pid)
      echo "Proxy stopped."
   else
      echo "Proxy is not running."
