57a728edd48c92de320ca3d611abac3db3bd3de6
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-07-11 17:08:58 -0700

    onboarding-fix (#561)

--- web/components/templates/welcome/welcomePage.tsx
+++ web/components/templates/welcome/welcomePage.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeftIcon, ChevronLeftIcon } from "@heroicons/react/20/solid";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { User, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -69,17 +70,50 @@ const WelcomePage = (props: WelcomePageProps) => {
 
   return (
     <div className="bg-gray-200 h-screen w-screen overflow-hidden items-center justify-center align-middle flex flex-col text-gray-900 relative">
-      <button
-        onClick={() => {
-          supabaseClient.auth.signOut().then(() => {
-            router.push("/");
-          });
-        }}
-        className="absolute p-8 left-0 top-0 flex flex-row w-full gap-1 text-xs items-center underline underline-offset-2 font-semibold text-gray-900"
-      >
-        <ArrowLeftIcon className="h-3 w-3 inline" />
-        Sign Out
-      </button>
+      <div className="flex flex-row justify-between items-center w-full top-0 absolute">
+        <button
+          onClick={() => {
+            supabaseClient.auth.signOut().then(() => {
+              router.push("/");
+            });
+          }}
+          className="p-8 flex flex-row gap-1 text-xs items-center underline underline-offset-2 font-semibold text-gray-900"
+        >
+          <ArrowLeftIcon className="h-3 w-3 inline" />
+          Sign Out
+        </button>
+        <button
+          onClick={async () => {
+            const { data: userSettings, error: userSettingsError } =
+              await supabaseClient
+                .from("user_settings")
+                .select("*")
+                .eq("user", user.id)
+                .single();
+
+            if (userSettings === null) {
+              // add the user into the userSettings page
+              const { data: newUserSettings, error: newUserSettingsError } =
+                await supabaseClient
+                  .from("user_settings")
+                  .insert({
+                    user: user.id,
+                    tier: "free",
+                    request_limit: 100_000,
+                  })
+                  .select("*")
+                  .single();
+            }
+
+            router.push("/dashboard");
+          }}
+          className="p-8 flex flex-row gap-1 text-xs items-center underline underline-offset-2 font-semibold text-gray-900"
+        >
+          Skip Onboarding
+          <ArrowRightIcon className="h-3 w-3 inline" />
+        </button>
+      </div>
+
       {stepArray[step]}
       <div className="h-4 w-full mx-auto bottom-0 mb-8 absolute flex">
         <ul className="flex flex-row gap-6 items-center w-full mx-auto justify-center">
