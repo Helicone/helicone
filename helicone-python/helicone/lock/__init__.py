55f407315dc5fc2a4608750abe8936f86c15bf53
Justin <justintorre75@gmail.com>
2023-07-14 22:51:13 -0700

    ensure lockfiles are present

--- helicone-python/helicone/lock/__init__.py
+++ helicone-python/helicone/lock/__init__.py
@@ -4,18 +4,23 @@ import lockfile
 
 
 class HeliconeLockManager:
-    LOCKFILE_PATH = os.path.expanduser(
-        "~/.helicone/custom_properties.json.lock")
-    JSON_PATH = os.path.expanduser("~/.helicone/custom_properties.json")
+    DIR_PATH = os.path.expanduser("~/.helicone")
+    LOCKFILE_PATH = os.path.join(DIR_PATH, "custom_properties.json.lock")
+    JSON_PATH = os.path.join(DIR_PATH, "custom_properties.json")
+
+    @staticmethod
+    def check_files():
+        os.makedirs(HeliconeLockManager.DIR_PATH, exist_ok=True)
+        if not os.path.exists(HeliconeLockManager.JSON_PATH):
+            with open(HeliconeLockManager.JSON_PATH, 'w') as json_file:
+                json.dump({}, json_file)
 
     @staticmethod
     def write_custom_property(property_name, value):
+        HeliconeLockManager.check_files()
         with lockfile.LockFile(HeliconeLockManager.LOCKFILE_PATH):
-            if os.path.exists(HeliconeLockManager.JSON_PATH):
-                with open(HeliconeLockManager.JSON_PATH, "r") as json_file:
-                    properties = json.load(json_file)
-            else:
-                properties = {}
+            with open(HeliconeLockManager.JSON_PATH, "r") as json_file:
+                properties = json.load(json_file)
 
             properties[property_name] = value
 
@@ -24,19 +29,23 @@ class HeliconeLockManager:
 
     @staticmethod
     def clear_all_properties():
+        HeliconeLockManager.check_files()
         with lockfile.LockFile(HeliconeLockManager.LOCKFILE_PATH):
             with open(HeliconeLockManager.JSON_PATH, "w") as json_file:
                 json.dump({}, json_file)
 
     @staticmethod
     def remove_custom_property(property_name):
+        HeliconeLockManager.check_files()
         with lockfile.LockFile(HeliconeLockManager.LOCKFILE_PATH):
-            if os.path.exists(HeliconeLockManager.JSON_PATH):
-                with open(HeliconeLockManager.JSON_PATH, "r") as json_file:
-                    properties = json.load(json_file)
+            with open(HeliconeLockManager.JSON_PATH, "r") as json_file:
+                properties = json.load(json_file)
 
-                if property_name in properties:
-                    del properties[property_name]
+            if property_name in properties:
+                del properties[property_name]
 
-                    with open(HeliconeLockManager.JSON_PATH, "w") as json_file:
-                        json.dump(properties, json_file)
+                with open(HeliconeLockManager.JSON_PATH, "w") as json_file:
+                    json.dump(properties, json_file)
+
+
+HeliconeLockManager.check_files()
