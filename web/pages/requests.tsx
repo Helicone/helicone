6a421bb8253d9711e77a2c742db4bbcecaf3a66e
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-07-18 13:22:03 -0700

    request-deep-link (#597)
    * deep-link request drawer

--- web/pages/requests.tsx
+++ web/pages/requests.tsx
@@ -2,8 +2,13 @@ import { User } from "@supabase/auth-helpers-react";
 import { GetServerSidePropsContext } from "next";
 import AuthLayout from "../components/shared/layout/authLayout";
 import MetaData from "../components/shared/metaData";
+import { NormalizedRequest } from "../components/templates/requestsV2/builder/abstractRequestBuilder";
+import getRequestBuilder from "../components/templates/requestsV2/builder/requestBuilder";
 import RequestsPageV2 from "../components/templates/requestsV2/requestsPageV2";
+import { getRequests, HeliconeRequest } from "../lib/api/request/request";
+import { Result } from "../lib/result";
 import { SupabaseServerWrapper } from "../lib/wrappers/supabase";
+import { FilterNode } from "../services/lib/filters/filterDefs";
 import {
   SortDirection,
   SortLeafRequest,
@@ -18,10 +23,11 @@ interface RequestsV2Props {
     sortDirection: SortDirection | null;
     isCustomProperty: boolean;
   };
+  initialRequest: HeliconeRequest | null;
 }
 
 const RequestsV2 = (props: RequestsV2Props) => {
-  const { user, currentPage, pageSize, sort } = props;
+  const { user, currentPage, pageSize, sort, initialRequest } = props;
 
   return (
     <MetaData title={"Requests"}>
@@ -30,6 +36,7 @@ const RequestsV2 = (props: RequestsV2Props) => {
           currentPage={currentPage}
           pageSize={pageSize}
           sort={sort}
+          initialRequest={initialRequest === null ? undefined : initialRequest}
         />
       </AuthLayout>
     </MetaData>
@@ -55,11 +62,48 @@ export const getServerSideProps = async (
       },
     };
 
-  const { page, page_size, sortKey, sortDirection, isCustomProperty } =
-    context.query;
+  const {
+    page,
+    page_size,
+    sortKey,
+    sortDirection,
+    isCustomProperty,
+    requestId,
+  } = context.query;
 
   const currentPage = parseInt(page as string, 10) || 1;
   const pageSize = parseInt(page_size as string, 10) || 10;
+  let initialRequest: HeliconeRequest | null = null;
+  if (requestId) {
+    const { data, error } = await getRequests(
+      "69c2783c-c523-4522-b3d1-ff2696343609",
+      {
+        left: {
+          request: {
+            id: {
+              equals: requestId,
+            },
+          },
+        },
+        operator: "and",
+        right: "all",
+      } as FilterNode,
+      0,
+      1,
+      {}
+    );
+    if (data && data.length > 0) {
+      initialRequest = data[0];
+      if (initialRequest.response_created_at) {
+        initialRequest.response_created_at =
+          initialRequest.response_created_at.toString();
+      }
+      if (initialRequest.request_created_at) {
+        initialRequest.request_created_at =
+          initialRequest.request_created_at.toString();
+      }
+    }
+  }
 
   return {
     props: {
@@ -71,6 +115,7 @@ export const getServerSideProps = async (
         sortDirection: sortDirection ? (sortDirection as SortDirection) : null,
         isCustomProperty: isCustomProperty === "true",
       },
+      initialRequest: initialRequest,
     },
   };
 };
