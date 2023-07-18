a77038a7e513e0e5e3551920c4b58703aa52a76b
Justin <justintorre75@gmail.com>
2023-07-18 00:53:34 -0700

    wow-wow-wow

--- web/lib/supabaseServer.ts
+++ web/lib/supabaseServer.ts
@@ -1,15 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../supabase/database.types";
 
-export const supabaseUrl =
-  process.env.SUPABASE_URL ?? process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+export const supabaseServerUrl =
+  process.env.SUPABASE_URL ?? process.env.SUPABASE_URL ?? "";
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY ?? "";
 
-if (supabaseUrl === "" || supabaseServiceKey === "") {
+if (supabaseServerUrl === "" || supabaseServiceKey === "") {
   throw new Error(`URL or Anon ENV not set for Server - ${supabaseServiceKey}`);
 }
 
 export const supabaseServer = createClient<Database>(
-  supabaseUrl,
+  supabaseServerUrl,
   supabaseServiceKey
 );
--- web/lib/wrappers/supabase.ts
+++ web/lib/wrappers/supabase.ts
@@ -9,7 +9,7 @@ import {
   NextApiRequest,
   NextApiResponse,
 } from "next";
-import { supabaseUrl as serverSupabaseUrl } from "../supabaseServer";
+import { supabaseServerUrl } from "../supabaseServer";
 import { ORG_ID_COOKIE_KEY } from "../constants";
 import { Result } from "../result";
 
@@ -24,7 +24,7 @@ export class SupabaseServerWrapper<T> {
   client: SupabaseClient<Database>;
   ctx: SSRContext<T>;
   constructor(ctx: SSRContext<T>, options?: SupabaseServerWrapperOptions) {
-    const supabaseUrl = options?.supabaseUrl ?? serverSupabaseUrl ?? "";
+    const supabaseUrl = options?.supabaseUrl ?? supabaseServerUrl ?? "";
     this.ctx = ctx;
     this.client = createServerSupabaseClient<Database>(ctx, {
       supabaseUrl,
