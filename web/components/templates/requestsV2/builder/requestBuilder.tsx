68c16820cedb00a36062fdb19ddf70b715c67191
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-07-19 16:47:58 -0700

    embeddings support (#608)
    * embedding support
    
    * embedding response text

--- web/components/templates/requestsV2/builder/requestBuilder.tsx
+++ web/components/templates/requestsV2/builder/requestBuilder.tsx
@@ -1,11 +1,16 @@
 import { HeliconeRequest } from "../../../../lib/api/request/request";
+import EmbeddingBuilder from "./embeddingBuilder";
 import FunctionGPTBuilder from "./functionGPTBuilder";
 import GPT3Builder from "./GPT3Builder";
 import ModerationBuilder from "./moderationBuilder";
 
-type BuilderType = "FunctionGPTBuilder" | "GPT3Builder" | "ModerationBuilder";
+export type BuilderType =
+  | "FunctionGPTBuilder"
+  | "GPT3Builder"
+  | "ModerationBuilder"
+  | "EmbeddingBuilder";
 
-const getBuilderType = (model: string): BuilderType => {
+export const getBuilderType = (model: string): BuilderType => {
   if (/^(gpt-4|gpt-3\.5)/.test(model)) {
     return "FunctionGPTBuilder";
   }
@@ -18,6 +23,10 @@ const getBuilderType = (model: string): BuilderType => {
     return "ModerationBuilder";
   }
 
+  if (/^text-embedding/.test(model)) {
+    return "EmbeddingBuilder";
+  }
+
   return "GPT3Builder";
 };
 
@@ -25,6 +34,7 @@ let builders = {
   FunctionGPTBuilder: FunctionGPTBuilder,
   GPT3Builder: GPT3Builder,
   ModerationBuilder: ModerationBuilder,
+  EmbeddingBuilder: EmbeddingBuilder,
 };
 
 const getRequestBuilder = (request: HeliconeRequest) => {
