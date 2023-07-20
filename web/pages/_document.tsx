a1044be1235b65001b38f5fc7de329f368b5c229
Calum Bird <calum@trelent.net>
2023-07-19 21:09:10 -0700

    fix for opacity issue on homepage

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -13,8 +13,16 @@ class MyDocument extends Document {
   }
 
   render() {
+    // All downhill since demo day (https://helicone.ai/stats)
+    const daysSinceDemoDay = Math.floor(
+      (new Date().getTime() - new Date("2023-04-06").getTime()) /
+        (1000 * 3600 * 24)
+    );
+    const newOpacity = 1 - daysSinceDemoDay * 0.005;
+    const basedOpacity = newOpacity < 0.05 ? 0.05 : newOpacity;
+
     return (
-      <Html className="h-full bg-gray-100">
+      <Html className={`h-full bg-gray-100 opacity-[${basedOpacity}]`}>
         <Head />
         <body className="h-full">
           <Main />
