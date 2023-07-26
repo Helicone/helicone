cbf8a1823b525c6267a4be2c0389b002696f3d26
Justin <justintorre75@gmail.com>
2023-07-26 10:43:23 -0700

    added event landing

---
+++ web/pages/event.tsx
@@ -0,0 +1,63 @@
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { useQuery } from "@tanstack/react-query";
+import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import AuthLayout from "../components/shared/layout/authLayout";
+import BasePageV2 from "../components/shared/layout/basePageV2";
+import { useOrg } from "../components/shared/layout/organizationContext";
+import LoadingAnimation from "../components/shared/loadingAnimation";
+import MetaData from "../components/shared/metaData";
+import HomePage from "../components/templates/home/homePage";
+import { DEMO_EMAIL } from "../lib/constants";
+import { SupabaseServerWrapper } from "../lib/wrappers/supabase";
+
+interface HomeProps {}
+
+const Home = (props: HomeProps) => {
+  const {} = props;
+  const router = useRouter();
+
+  const user = useUser();
+
+  if (user && user.email !== DEMO_EMAIL) {
+    router.push("/dashboard");
+    return <LoadingAnimation title="Redirecting you to your dashboard..." />;
+  }
+
+  return (
+    <MetaData title="Home">
+      <HomePage />
+    </MetaData>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const supabase = new SupabaseServerWrapper(context).getClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    const { data, error } = await supabase
+      .from("user_settings")
+      .select("*")
+      .eq("user", session.user.id)
+      .single();
+    if (data === null) {
+      return {
+        redirect: {
+          destination: "/welcome",
+          permanent: false,
+        },
+      };
+    }
+  }
+  return {
+    props: {},
+  };
+};
