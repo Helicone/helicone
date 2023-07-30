da2288dc3ca56843eeced1fc328261890b32cfe9
colegottdank <colegottdank@gmail.com>
2023-07-29 23:34:11 -0700

    Update HashiCorpVault in worker

--- worker/src/lib/vault/HashiCorpVault.ts
+++ worker/src/lib/vault/HashiCorpVault.ts
@@ -11,41 +11,6 @@ class HashiCorpVault implements IVault {
     this.token = process.env.HASHICORP_VAULT_TOKEN ?? "myroot";
   }
 
-  async writeProviderKey(
-    orgId: string,
-    vaultKeyId: string,
-    providerKey: string
-  ): Promise<Result<null, string>> {
-    const vaultPath = this.createProviderVaultPath(orgId, vaultKeyId);
-    try {
-      const response: Response = await fetch(
-        this.endpoint + "/v1/" + vaultPath,
-        {
-          method: "POST",
-          headers: {
-            "X-Vault-Token": this.token,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            data: {
-              value: providerKey,
-            },
-          }),
-        }
-      );
-
-      if (!response.ok) throw new Error(await response.text());
-
-      return { error: null, data: null };
-    } catch (error: any) {
-      console.error("Error writing to vault", error);
-      return {
-        error: error.message || "Unexpected error while writing to vault",
-        data: null,
-      };
-    }
-  }
-
   async readProviderKey(
     orgId: string,
     vaultKeyId: string
@@ -61,7 +26,12 @@ class HashiCorpVault implements IVault {
         }
       );
 
-      if (!response.ok) throw new Error(await response.text());
+      if (!response.ok) {
+        return {
+          error: `Error reading from vault: ${response.statusText}`,
+          data: null,
+        };
+      }
 
       const data: { data?: { data?: { value?: string } } } | null =
         await response.json();
--- worker/src/lib/vault/IVault.ts
+++ worker/src/lib/vault/IVault.ts
@@ -1,12 +1,6 @@
 import { Result } from "../../results";
 
 export interface IVault {
-  writeProviderKey(
-    orgId: string,
-    vaultKeyId: string,
-    providerKey: string
-  ): Promise<Result<null, string>>;
-
   readProviderKey(
     orgId: string,
     vaultKeyId: string
