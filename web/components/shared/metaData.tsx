f56e2a12c64a5bfadaa7193be30941e5eaa866e4
Justin Torre <justintorre75@gmail.com>
2023-07-30 00:53:47 -0700

    added more meta: (#666)

--- web/components/shared/metaData.tsx
+++ web/components/shared/metaData.tsx
@@ -3,20 +3,18 @@ import Head from "next/head";
 interface MetaDataProps {
   children: React.ReactNode;
   title: string;
+  image?: string;
 }
 
 const MetaData = (props: MetaDataProps) => {
-  const { children, title } = props;
+  const { children, title, image } = props;
 
   return (
     <>
       <Head>
         <title>{`${title} | Helicone - Monitoring for Generative AI`}</title>
         <link rel="icon" href="/assets/landing/helicone-mobile.webp" />
-        <meta
-          property="og:title"
-          content="Helicone | Monitoring for Generative AI"
-        />
+        <meta property="og:title" content={title} />
         <meta
           property="og:description"
           name="description"
@@ -25,7 +23,11 @@ const MetaData = (props: MetaDataProps) => {
         />
         <meta
           property="og:image"
-          content="https://www.helicone.ai/_next/image?url=%2Fassets%2Flanding%2Fhelicone-mobile.webp&w=384&q=75"
+          content={
+            image
+              ? image
+              : "https://www.helicone.ai/_next/image?url=%2Fassets%2Flanding%2Fhelicone-mobile.webp&w=384&q=75"
+          }
         />
       </Head>
       {children}
