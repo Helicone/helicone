1db5602da3ffbfd95d6b17da1d020ae5adaf74d2
Justin <justintorre75@gmail.com>
2023-07-29 23:51:45 -0700

    formatting

--- web/pages/blog/autoGPT.tsx
+++ web/pages/blog/autoGPT.tsx
@@ -15,9 +15,9 @@ const ExampleBlog = (props: ExampleBlogProps) => {
   return (
     <MetaData title="AutoGPT x Helicone: Optimizing Evaluation Pipelines">
       <NavBarV2 />
-      <section>
-        <div className="bg-white px-6 py-6 lg:px-8">
-          <div className="mx-auto max-w-7xl text-base leading-7 text-gray-700">
+      <section className="bg-white">
+        <div className="flex flex-col items-center">
+          <div className="px-12 max-w-7xl text-base leading-7 text-gray-700">
             <img
               className="h-full w-auto mx-auto"
               src="/assets/autoGPTxHelicone.png"
@@ -27,15 +27,16 @@ const ExampleBlog = (props: ExampleBlogProps) => {
               AutoGPT {"<>"} Helicone
             </h1>
             <p className="mt-6 text-xl leading-8">
-              Building the Best Evaluation Pipeline for Agents Comparison
+              Constructing the Optimal Evaluation Pipeline for Agent Comparison
             </p>
             <div className="mt-10 max-w-7xl">
               <p>
-                AutoGPT is hard at work on their Auto-GPT-Benchmarks repository.
-                Their goal? To construct the best evaluation pipeline for
-                comparing different agents. AutoGPT is fully leveraging the
-                power of Helicone without changing a single line of code. Here
-                are the key features driving this synergy:
+                AutoGPT is diligently working on their Auto-GPT-Benchmarks
+                repository. Their objective? To build the optimal evaluation
+                pipeline for comparing different agents. AutoGPT is fully
+                leveraging the capabilities of Helicone without modifying a
+                single line of code. Here are the key features that facilitate
+                this synergy:
               </p>
               <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
                 <li className="flex gap-x-3">
@@ -47,8 +48,8 @@ const ExampleBlog = (props: ExampleBlogProps) => {
                     <strong className="font-semibold text-gray-900">
                       Proxy Integration:
                     </strong>{" "}
-                    Helicone's role as a proxy enables AutoGPT to maintain their
-                    codebase untouched.
+                    Helicone&apos;s role as a proxy allows AutoGPT to keep their
+                    codebase intact.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
@@ -60,9 +61,10 @@ const ExampleBlog = (props: ExampleBlogProps) => {
                     <strong className="font-semibold text-gray-900">
                       Caching:
                     </strong>{" "}
-                    For minor code changes that don't require re-calling the LLM
-                    for an entire CI pipeline, requests can be cached on edge
-                    servers. This feature saves AutoGPT over $10 per PR!
+                    For minor code modifications that don&apos;t necessitate
+                    re-calling the LLM for an entire CI pipeline, requests can
+                    be cached on edge servers. This feature saves AutoGPT over
+                    $10 per PR!
                   </span>
                 </li>
                 <li className="flex gap-x-3">
@@ -74,73 +76,21 @@ const ExampleBlog = (props: ExampleBlogProps) => {
                     <strong className="font-semibold text-gray-900">
                       GraphQL:
                     </strong>{" "}
-                    Our data extraction API allows AutoGPT to create custom
+                    Our data extraction API enables AutoGPT to generate custom
                     reports upon the completion of a CI job.
                   </span>
                 </li>
               </ul>
-              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-                AutoGPT's Integration Journey
-              </h2>
-              <p className="mt-6">
-                AutoGPT embarked on their integration journey with Helicone by
-                using the MITM tools that allow them to intercept all the
-                traffic going to OpenAI on their CI machine and reroute it to
-                Helicone. This strategic move allows AutoGPT to leverage all of
-                Helicone's features. The integration, facilitated by a
-                Man-In-The-Middle (MITM), captures all logs that would be sent
-                to OpenAI.
-              </p>
-              <pre className="mt-8 p-6 rounded-md bg-gray-100 text-gray-700">
-                {`bash -c "$(curl -fsSL https://raw.githubusercontent.com/Helicone/helicone/main/mitmproxy.sh)" -s start`}
-              </pre>
-              <p className="mt-6">
-                Within the benchmarks, there's a python library where specific
-                custom properties can be configured, filtered, and measured like
-                this:
-              </p>
-              <pre className="mt-8 p-6 rounded-md bg-gray-100 text-gray-700">
-                {`HeliconeLockManager.write_custom_property("job_id", "1")
-HeliconeLockManager.write_custom_property("agent", "smol-developer")`}
-              </pre>
-              <p className="mt-6">
-                To enable caching, they simply set an environment variable like
-                this:
-              </p>
-              <pre className="mt-8 p-6 rounded-md bg-gray-100 text-gray-700">
-                {`export HELICONE_CACHE_ENABLED="true"`}
-              </pre>
-              <span>
-                Ideal for minor code changes where a full LLM call isn't
-                required for an entire CI pipeline, AutoGPT can cache their
-                requests on Helicone's edge servers. This strategic move saves
-                them over 10 dollars per pull request!
-              </span>
-
-              <li className="flex gap-x-3">
-                <CheckCircleIcon
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    GraphQL:
-                  </strong>{" "}
-                  Helicone's API for data extraction provides AutoGPT with the
-                  ability to construct custom reports in their CI once a job is
-                  complete.
-                </span>
-              </li>
             </div>
             <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-              AutoGPT's Workflow with Helicone
+              AutoGPT&apos;s Workflow with Helicone
             </h2>
             <p className="mt-6">
-              AutoGPT's workflow took advantage of Helicone's Man-In-The-Middle
-              (MITM) tools. This facilitated the interception of all traffic
-              that would have been directed to OpenAI from their CI machine and
-              rerouted it to Helicone. Consequently, AutoGPT can now leverage
-              all of Helicone's features.
+              AutoGPT&apos;s workflow took advantage of Helicone&apos;s
+              Man-In-The-Middle (MITM) tools. This facilitated the interception
+              of all traffic that would have been directed to OpenAI from their
+              CI machine and rerouted it to Helicone. Consequently, AutoGPT can
+              now leverage all of Helicone&apos;s features.
             </p>
             <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900">
               Integration
@@ -190,7 +140,7 @@ HeliconeLockManager.write_custom_property("agent", "smol-developer")`}</code>
                   className="mt-0.5 h-5 w-5 flex-none text-gray-300"
                   aria-hidden="true"
                 />
-                AutoGPT's agent comparison dashboard
+                AutoGPT&apos;s agent comparison dashboard
               </figcaption>
             </figure>
             <p className="mt-6">
@@ -215,10 +165,10 @@ HeliconeLockManager.write_custom_property("agent", "smol-developer")`}</code>
               Determining Cost Savings
             </h3>
             <p className="mt-6">
-              For scenarios where testing the agent's functionality is needed
-              but calling the API is not, such as small code changes, they can
-              monitor their cache usage effortlessly through the dashboards.
-              Here's an example:
+              For scenarios where testing the agent&apos;s functionality is
+              needed but calling the API is not, such as small code changes,
+              they can monitor their cache usage effortlessly through the
+              dashboards. Here&apos;s an example:
             </p>
             <figure className="mt-10 border-l border-indigo-600 pl-9">
               <img
@@ -240,7 +190,7 @@ HeliconeLockManager.write_custom_property("agent", "smol-developer")`}</code>
             </p>
             <figure className="mt-10 border-l border-indigo-600 pl-9">
               <img
-                className="aspect-video rounded-xl bg-gray-50 object-cover"
+                className="aspect-video rounded-xl bg-gray-50 object-cover max-w-2xl"
                 src="/assets/blog/cacheRequestTable.png"
                 alt="Cache Request Table"
               />
@@ -253,11 +203,11 @@ HeliconeLockManager.write_custom_property("agent", "smol-developer")`}</code>
               </figcaption>
             </figure>
             <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-              Moving Forward
+              The Road Ahead
             </h2>
             <p className="mt-6">
-              We are currently developing a suite of GraphQL endpoints that
-              allow AutoGPT to ingest some of their data easily and add it
+              We are currently developing a suite of GraphQL endpoints that will
+              allow AutoGPT to easily ingest some of their data and add it
               directly to the reports after a run.
             </p>
             <figure className="mt-10 border-l border-indigo-600 pl-9">
@@ -279,7 +229,7 @@ HeliconeLockManager.write_custom_property("agent", "smol-developer")`}</code>
               have a tight integration between report generation and Helicone.
               Here is a preview of what a benchmark report will look like:
             </p>
-            <pre className="mt-8 p-4 bg-gray-800 text-white rounded-md">
+            <pre className="mt-8 p-4 bg-gray-800 text-white rounded-md text-xs max-w-lg">
               <code>
                 {`AutoGPT report
 Job-Id: 8AB138BF-3DEA-4E84-BC27-5FE624B956BC
@@ -328,11 +278,11 @@ Challenge send-email
               </code>
             </pre>
             <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-              Thanks for reading!
+              Thank You for Reading!
             </h2>
             <p className="mt-6">
-              Thank you so much for reading our first blog post. We are super
-              excited to be partnering with AutoGPT and enable rich logging for
+              We appreciate your time in reading our first blog post. We are
+              thrilled to be partnering with AutoGPT to enable rich logging for
               them and deliver value using Helicone. If you are interested in
               learning more about Helicone or would like to meet the team,
               please email me at justin @ helicone.ai
