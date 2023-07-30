d5d70efb0cff2e2eb60e77d431b1030ff0f06558
Khoa Nguyen <scottmktn@gmail.com>
2023-07-29 21:15:07 -0700

    add model

--- web/components/templates/requestsV2/builder/claudeBuilder.tsx
+++ web/components/templates/requestsV2/builder/claudeBuilder.tsx
@@ -28,8 +28,8 @@ class ClaudeBuilder extends AbstractRequestBuilder {
       requestText: this.response.request_body.prompt || "Invalid Prompt",
       responseText: getResponseText(),
       model:
-        this.response.request_body?.body?.model ||
-        this.response.response_body.model,
+        this.response.request_body.model ||
+        this.response.response_body.body?.model,
       render:
         this.response.response_status === 0 ||
         this.response.response_status === null ? (
