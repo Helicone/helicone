faa2cd2664623f7638a912a9d32536daf8093510
Khoa Nguyen <scottmktn@gmail.com>
2023-08-01 14:05:40 -0700

    model pill

--- web/components/templates/requestsV2/modelPill.tsx
+++ web/components/templates/requestsV2/modelPill.tsx
@@ -11,19 +11,19 @@ const ModelPill = (props: ModelPillProps) => {
   const builderType = getBuilderType(model);
 
   let modelMapping = {
-    FunctionGPTBuilder: "purple",
-    GPT3Builder: "green",
-    ModerationBuilder: "teal",
-    EmbeddingBuilder: "blue",
-    ClaudeBuilder: "orange",
+    FunctionGPTBuilder: "bg-purple-50 text-purple-700 ring-purple-200",
+    GPT3Builder: "bg-green-50 text-green-700 ring-green-200",
+    ModerationBuilder: "bg-teal-50 text-teal-700 ring-teal-200",
+    EmbeddingBuilder: "bg-blue-50 text-blue-700 ring-blue-200",
+    ClaudeBuilder: "bg-orange-50 text-orange-700 ring-orange-200",
   };
 
-  const color = modelMapping[builderType] || "gray";
+  // const color = modelMapping[builderType] || "gray";
 
   return (
     <span
       className={clsx(
-        `bg-${color}-50 text-${color}-700 ring-${color}-200`,
+        modelMapping[builderType] || "bg-gray-50 text-gray-700 ring-gray-200",
         `inline-flex items-center rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset`
       )}
     >
