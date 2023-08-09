d699f1c75785b595677249f3820ac91effb6bafa
colegottdank <colegottdank@gmail.com>
2023-08-09 10:41:14 -0700

    Limit body size (#696)
    * Limit body size
    
    * Address nit

--- web/lib/api/request/request.ts
+++ web/lib/api/request/request.ts
@@ -15,6 +15,7 @@ import {
   printRunnableQuery,
 } from "../db/dbExecute";
 export type Provider = "OPENAI" | "ANTHROPIC";
+const MAX_TOTAL_BODY_SIZE = 3900000;
 export interface HeliconeRequest {
   response_id: string;
   response_created_at: string;
@@ -94,6 +95,7 @@ export async function getRequestsCached(
     left join prompt on request.formatted_prompt_id = prompt.id
   WHERE (
     (${builtFilter.filter})
+    AND (LENGTH(response.body::text) + LENGTH(request.body::text)) <= ${MAX_TOTAL_BODY_SIZE}
   )
   ${sortSQL !== undefined ? `ORDER BY ${sortSQL}` : ""}
   LIMIT ${limit}
@@ -190,6 +192,7 @@ export async function getRequests(
     left join prompt on request.formatted_prompt_id = prompt.id
   WHERE (
     (${builtFilter.filter})
+    AND (LENGTH(response.body::text) + LENGTH(request.body::text)) <= ${MAX_TOTAL_BODY_SIZE}
   )
   ${sortSQL !== undefined ? `ORDER BY ${sortSQL}` : ""}
   LIMIT ${limit}
