95d55af6aa498d67bd31f6cc75c4e47b76e94b02
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-08-21 16:12:52 -0400

    keys-and-gql (#737)

--- web/components/shared/themed/themedTableHeader.tsx
+++ web/components/shared/themed/themedTableHeader.tsx
@@ -41,7 +41,6 @@ import { Json } from "../../../supabase/database.types";
 import ThemedModal from "./themedModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import useNotification from "../notification/useNotification";
-import { useLayouts } from "../../../services/hooks/useLayouts";
 import Link from "next/link";
 import { Result } from "../../../lib/result";
 import { ThemedPill } from "./themedPill";
@@ -83,30 +82,6 @@ interface ThemedHeaderProps {
       search: string
     ) => Promise<Result<void, string>>;
   };
-  layout?: {
-    currentLayout: {
-      columns: Json;
-      created_at: string | null;
-      filters: Json;
-      id: number;
-      name: string;
-      user_id: string;
-    } | null;
-    setLayout: (name: string) => void;
-    onCreateLayout: (layoutName: string) => void;
-    layouts:
-      | {
-          columns: Json;
-          created_at: string | null;
-          filters: Json;
-          id: number;
-          name: string;
-          user_id: string;
-        }[]
-      | null
-      | undefined;
-    clearLayout: () => void;
-  };
 }
 
 const notificationMethods = [
@@ -115,49 +90,17 @@ const notificationMethods = [
 ];
 
 export default function ThemedHeader(props: ThemedHeaderProps) {
-  const {
-    isFetching,
-    editColumns,
-    timeFilter,
-    advancedFilter,
-    layout,
-    csvExport,
-  } = props;
+  const { isFetching, editColumns, timeFilter, advancedFilter, csvExport } =
+    props;
 
   const [showAdvancedFilters, setShowAdvancedFilters] = useState(false);
   const [exportFiltered, setExportFiltered] = useState(false);
   const [openLayout, setOpenLayout] = useState(false);
   const [name, setName] = useState("");
   const [openDelete, setOpenDelete] = useState(false);
-  const [deleteLayout, setDeleteLayout] = useState<{
-    columns: Json;
-    created_at: string | null;
-    filters: Json;
-    id: number;
-    name: string;
-    user_id: string;
-  }>();
 
   const supabaseClient = useSupabaseClient();
   const { setNotification } = useNotification();
-  const { refetch: refetchLayouts } = useLayouts();
-
-  const onDeleteLayoutHandler = async (layoutId: number) => {
-    const { error } = await supabaseClient
-      .from("layout")
-      .delete()
-      .eq("id", layoutId);
-
-    if (error) {
-      setNotification("Error deleting layout", "error");
-      setOpenLayout(false);
-      return;
-    }
-    setOpenLayout(false);
-    setNotification("Layout deleted", "success");
-    refetchLayouts();
-    layout?.clearLayout();
-  };
 
   return (
     <>
@@ -381,74 +324,6 @@ export default function ThemedHeader(props: ThemedHeaderProps) {
                 ) : (
                   <p>Export</p>
                 )}
-              </button>
-            </div>
-          </div>
-        </ThemedModal>
-      )}
-      {layout && openLayout && (
-        <ThemedModal open={openLayout} setOpen={setOpenLayout}>
-          <div className="flex flex-col space-y-4 sm:space-y-8 max-w-sm">
-            <div className="flex flex-col space-y-2">
-              <p className="text-sm sm:text-md font-semibold text-gray-900">
-                Create Layout
-              </p>
-              <p className="text-sm sm:text-md text-gray-500">
-                Create layouts to easily switch between different filters,
-                column selections, and sort orders.
-              </p>
-            </div>
-
-            <input
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-              placeholder={"Layout Name..."}
-              value={name}
-              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
-            />
-            <div className="w-full flex justify-end text-sm">
-              <button
-                className="items-center rounded-md bg-black px-3 py-1.5 text-md flex font-normal text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                onClick={() => {
-                  layout.onCreateLayout(name);
-                  setOpenLayout(false);
-                }}
-              >
-                <PlusIcon className="h-4 w-4 inline" />
-                Add Layout
-              </button>
-            </div>
-          </div>
-        </ThemedModal>
-      )}
-      {layout && openDelete && (
-        <ThemedModal open={openDelete} setOpen={setOpenDelete}>
-          <div className="flex flex-col space-y-4 sm:space-y-8 min-w-[25rem]">
-            <div className="flex flex-col space-y-2">
-              <p className="text-sm sm:text-md font-semibold text-gray-900">
-                Delete Layout
-              </p>
-              <p className="text-sm sm:text-md text-gray-500">
-                {`Are you sure you want to delete layout: ${deleteLayout?.name}?`}
-              </p>
-            </div>
-
-            <div className="w-full flex justify-end text-sm space-x-2">
-              <button
-                type="button"
-                onClick={() => setOpenDelete(false)}
-                className="flex flex-row items-center rounded-md bg-white px-4 py-2 text-sm font-medium border border-gray-300 hover:bg-gray-50 text-gray-900 shadow-sm hover:text-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
-              >
-                Cancel
-              </button>
-              <button
-                className="flex flex-row items-center rounded-md bg-red-500 px-4 py-2 text-sm font-medium border border-red-500 hover:bg-red-700 text-gray-50 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500"
-                onClick={() => {
-                  deleteLayout?.id && onDeleteLayoutHandler(deleteLayout.id);
-                  setOpenDelete(false);
-                }}
-              >
-                Delete Layout
               </button>
             </div>
           </div>
