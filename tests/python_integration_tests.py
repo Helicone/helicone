473821677a4fb391fc387edb5691af1216c48ed6
colegottdank <colegottdank@gmail.com>
2023-08-21 21:48:29 -0700

    OpenAI proxy and async tests

--- tests/python_integration_tests.py
+++ tests/python_integration_tests.py
@@ -6,6 +6,8 @@ import psycopg2
 from psycopg2.extras import DictCursor
 import uuid
 import time
+from helicone.openai_async import openai, Meta
+from helicone.globals import helicone_global
 
 load_dotenv()
 
@@ -73,6 +75,50 @@ def test_openai_proxy():
     request_data = fetch_from_db(query, (requestId,))
     assert request_data, "Request data not found in the database for the given property request id"
 
+    latest_request = request_data[0]
+    assert message_content in latest_request["body"]["messages"][0]["content"], "Request not found in the database"
+
+    query = "SELECT * FROM response WHERE request = %s LIMIT 1"
+    response_data = fetch_from_db(query, (latest_request["id"],))
+    assert response_data, "Response data not found in the database for the given request ID"
+
+def test_openai_async():
+    print("Running test_openai_async...")
+    
+    # Set the API key for Helicone
+    helicone_global.api_key = helicone_api_key
+    helicone_global.base_url = helicone_async_url
+    openai.api_key = openai_api_key
+    
+    requestId = str(uuid.uuid4())
+    print("Request ID: " + requestId)
+    message_content = test_openai_async.__name__ + " - " + requestId
+    messages = [
+        {
+            "role": "system",
+            "content": message_content
+        }
+    ]
+    
+    # Using the helicone package for async logging
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=10,
+        helicone_meta=Meta(
+            custom_properties={
+                "requestId": requestId
+            }
+        )
+    )
+    assert response, "Response from OpenAI API is empty"
+    
+    time.sleep(1)  # Give some time for the async logging to complete
+    
+    query = "SELECT * FROM properties INNER JOIN request ON properties.request_id = request.id WHERE key = 'requestid' AND value = %s LIMIT 1"
+    request_data = fetch_from_db(query, (requestId,))
+    assert request_data, "Request data not found in the database for the given property request id"
+    
     latest_request = request_data[0]
     assert message_content in latest_request["body"]["messages"][0]["content"], "Request not found in the database"
 
