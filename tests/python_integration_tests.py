2a76c84965ff58e80996d14b0dad38b633e6a1d9
colegottdank <colegottdank@gmail.com>
2023-09-08 16:14:37 -0700

    Add new integration tests for vault

--- tests/python_integration_tests.py
+++ tests/python_integration_tests.py
@@ -18,6 +18,9 @@ openai_org_id = os.environ["OPENAI_ORG"]
 helicone_api_key = os.environ["HELICONE_API_KEY"]
 supabase_key = os.environ["SUPABASE_KEY"]
 supabase_url = os.environ["SUPABASE_URL"]
+org_id = '83635a30-5ba6-41a8-8cc6-fb7df941b24a'
+helicone_proxy_key = os.environ['HELICONE_PROXY_KEY']
+hashed_proxy_key = os.environ['HASHED_HELICONE_PROXY_KEY']
 
 def connect_to_db():
     return psycopg2.connect(
@@ -36,6 +39,17 @@ def fetch_from_db(query, params=None):
     cur.close()
     conn.close()
     return results
+
+def insert_into_db(query, params):
+    # Construct the query
+    conn = connect_to_db()
+    cur = conn.cursor(cursor_factory=DictCursor)
+    cur.execute(query, (params))
+    result = cur.fetchone() # Fetch the returned ID
+    conn.commit()
+    cur.close()
+    conn.close()
+    return result
 
 def fetch(base_url, endpoint, method="GET", json=None, headers=None):
     url = f"{base_url}/{endpoint}"
@@ -62,6 +76,61 @@ def test_openai_proxy():
     headers = {
         "Authorization": f"Bearer {openai_api_key}",
         "Helicone-Auth": f"Bearer {helicone_api_key}",
+        "Helicone-Property-RequestId": requestId,
+        "OpenAI-Organization": openai_org_id
+    }
+
+    response = fetch(helicone_proxy_url, "chat/completions", method="POST", json=data, headers=headers)
+    assert response, "Response from OpenAI API is empty"
+
+    time.sleep(3) # Helicone needs time to insert request into the database
+
+    query = "SELECT * FROM properties INNER JOIN request ON properties.request_id = request.id WHERE key = 'requestid' AND value = %s LIMIT 1"
+    request_data = fetch_from_db(query, (requestId,))
+    assert request_data, "Request data not found in the database for the given property request id"
+
+    latest_request = request_data[0]
+    assert message_content in latest_request["body"]["messages"][0]["content"], "Request not found in the database"
+
+    query = "SELECT * FROM response WHERE request = %s LIMIT 1"
+    response_data = fetch_from_db(query, (latest_request["id"],))
+    assert response_data, "Response data not found in the database for the given request ID"
+    print("passed")
+
+def test_helicone_proxy_key():
+    print("\n---------Running test_helicone_proxy_key---------")
+
+    query = """
+    INSERT INTO provider_keys(org_id, provider_name, provider_key_name, provider_key)
+    VALUES (%s, %s, %s, %s)
+    RETURNING id;
+    """
+
+    query2 = """
+    INSERT INTO helicone_proxy_keys(id, org_id, helicone_proxy_key_name, helicone_proxy_key, provider_key_id)
+    VALUES (%s, %s, %s, %s, %s)
+    RETURNING id;
+    """
+
+    provider_key_data = insert_into_db(query, (org_id, "OpenAI", "Team1", openai_api_key))
+    insert_into_db(query2, ('d144312e-5c65-4eaa-a1c1-f0c143080601', org_id, "Cole", hashed_proxy_key, provider_key_data[0]))
+
+    requestId = str(uuid.uuid4())
+    print("Request ID: " + requestId + "")
+    message_content = test_openai_proxy.__name__ + " - " + requestId
+    messages = [
+        {
+            "role": "user",
+            "content": message_content
+        }
+    ]
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": messages,
+        "max_tokens": 10
+    }
+    headers = {
+        "Authorization": f"Bearer {helicone_proxy_key}",
         "Helicone-Property-RequestId": requestId,
         "OpenAI-Organization": openai_org_id
     }
