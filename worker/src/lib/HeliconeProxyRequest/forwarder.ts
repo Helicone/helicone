7c42c454a8bc10c9d7902b5382db1c4fe631e3d0
Justin <justintorre75@gmail.com>
2023-09-12 00:32:48 -0700

    change queue

--- worker/src/lib/HeliconeProxyRequest/forwarder.ts
+++ worker/src/lib/HeliconeProxyRequest/forwarder.ts
@@ -123,7 +123,7 @@ export async function proxyForwarder(
         supabase: createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY),
         queue: new InsertQueue(
           createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY),
-          env.FEEDBACK_INSERT_QUEUE,
+          env.FALLBACK_QUEUE,
           env.REQUEST_AND_RESPONSE_QUEUE_KV
         ),
       },
