b1eaae4a55778480389fb623b71f3e2e38c7ffd9
Justin <justintorre75@gmail.com>
2023-09-14 11:01:27 -0700

    added ability to create Limits for different provider keys

--- web/components/templates/vault/createProxyKeyModal.tsx
+++ web/components/templates/vault/createProxyKeyModal.tsx
@@ -10,7 +10,10 @@ import { clsx } from "../../shared/clsx";
 import {
   ArrowPathIcon,
   ClipboardDocumentListIcon,
+  PlusCircleIcon,
+  TrashIcon,
 } from "@heroicons/react/24/outline";
+import { Database } from "../../../supabase/database.types";
 
 interface CreateProxyKeyModalProps {
   providerKeys: DecryptedProviderKey[];
@@ -18,9 +21,204 @@ interface CreateProxyKeyModalProps {
   setOpen: (open: boolean) => void;
   onSuccess: () => void;
 }
+interface LimitRow {
+  _limitType: "cost" | "count";
+  cost?: number;
+  count?: number;
+  timewindow_seconds?: number;
+}
+const LimitRowDiv = (props: {
+  limit: LimitRow;
+  setLimit: (limit: LimitRow) => void;
+  onDelete: () => void;
+}) => {
+  const { limit, setLimit, onDelete } = props;
+  const { _limitType, cost, count, timewindow_seconds } = limit;
+  const [timeGrain, setTimeGrain] = useState<
+    "seconds" | "minutes" | "hours" | "days"
+  >("seconds");
+  return (
+    <div className="flex flex-row gap-2 items-center">
+      <select
+        className="block w-full rounded-md border border-gray-300  shadow-sm p-2 text-sm"
+        required
+        value={_limitType}
+        onChange={(e) => {
+          console.log("e.target.value", e.target.value);
+          const newLimit = { ...limit };
+          newLimit._limitType = e.target.value as any;
+          newLimit.cost = undefined;
+          newLimit.count = undefined;
+          setLimit(newLimit);
+        }}
+      >
+        <option key={"cost-options"} value={"cost"}>
+          cost
+        </option>
+        <option key={"cost-options"} value={"count"}>
+          count
+        </option>
+      </select>
+
+      <input
+        type="number"
+        className="block w-full rounded-md border border-gray-300 shadow-sm p-2 text-sm"
+        placeholder={_limitType === "cost" ? "Cost USD" : "Count"}
+        value={_limitType === "cost" ? cost : count}
+        onChange={(e) => {
+          const newLimit = { ...limit };
+          let value = Number(e.target.value);
+          if (isNaN(value)) return;
+          if (value < 0) value = 0;
+          if (_limitType === "cost") {
+            newLimit.cost = Number(value);
+            newLimit.count = undefined;
+          } else {
+            newLimit.count = Number(value);
+            newLimit.cost = undefined;
+          }
+          setLimit(newLimit);
+        }}
+      />
+      {" For "}
+      <input
+        type="number"
+        className="block w-full rounded-md border border-gray-300 shadow-sm p-2 text-sm"
+        placeholder="Time Window"
+        value={(() => {
+          if (timeGrain === "seconds") {
+            return timewindow_seconds;
+          } else if (timeGrain === "minutes") {
+            return timewindow_seconds! / 60;
+          } else if (timeGrain === "hours") {
+            return timewindow_seconds! / 60 / 60;
+          } else if (timeGrain === "days") {
+            return timewindow_seconds! / 60 / 60 / 24;
+          }
+          return timewindow_seconds;
+        })()}
+        onChange={(e) => {
+          const newLimit = { ...limit };
+          let value = Number(e.target.value);
+          if (isNaN(value)) return;
+          if (value < 0) value = 0;
+          if (timeGrain === "seconds") {
+            newLimit.timewindow_seconds = Number(value);
+          } else if (timeGrain === "minutes") {
+            newLimit.timewindow_seconds = Number(value) * 60;
+          } else if (timeGrain === "hours") {
+            newLimit.timewindow_seconds = Number(value) * 60 * 60;
+          } else if (timeGrain === "days") {
+            newLimit.timewindow_seconds = Number(value) * 60 * 60 * 24;
+          }
+          setLimit(newLimit);
+        }}
+      />
+
+      <select
+        className="block w-full rounded-md border border-gray-300  shadow-sm p-2 text-sm"
+        required
+        value={timeGrain}
+        onChange={(e) => {
+          setTimeGrain(e.target.value as any);
+        }}
+      >
+        <option key={"seconds"} value={"seconds"}>
+          seconds
+        </option>
+        <option key={"minutes"} value={"minutes"}>
+          minutes
+        </option>
+        <option key={"hours"} value={"hours"}>
+          hours
+        </option>
+        <option key={"days"} value={"days"}>
+          days
+        </option>
+      </select>
+      <div
+        className="flex flex-row items-center rounded-md bg-white px-4 py-2 text-sm font-semibold border border-gray-300 hover:bg-gray-50 text-gray-900 shadow-sm hover:text-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+        onClick={() => {
+          onDelete();
+        }}
+      >
+        <TrashIcon className="h-5 w-5 text-gray-900 hover:cursor-pointer" />
+      </div>
+    </div>
+  );
+};
+
+const LimitsInput = (props: {
+  setLimits: (
+    limits: Database["public"]["Tables"]["helicone_proxy_key_limits"]["Insert"][]
+  ) => void;
+}) => {
+  const [limits, setLimits] = useState<LimitRow[]>([]);
+  return (
+    <div className="flex flex-col space-y-1.5 text-sm">
+      <label htmlFor="provider-key-name">Limits</label>
+      {limits.map((limit, idx) => (
+        <LimitRowDiv
+          limit={limit}
+          setLimit={(newLimit) => {
+            const newLimits = [...limits];
+            newLimits[idx] = newLimit;
+            setLimits(newLimits);
+            props.setLimits(
+              newLimits.map((limit) => ({
+                cost: limit.cost,
+                count: limit.count,
+                timewindow_seconds: limit.timewindow_seconds,
+                id: crypto.randomUUID(),
+                helicone_proxy_key: "null",
+                currency: limit.cost ? "USD" : undefined,
+              }))
+            );
+          }}
+          onDelete={() => {
+            const newLimits = [...limits];
+            newLimits.splice(idx, 1);
+            setLimits(newLimits);
+            props.setLimits(
+              newLimits.map((limit) => ({
+                cost: limit.cost,
+                count: limit.count,
+                timewindow_seconds: limit.timewindow_seconds,
+                id: crypto.randomUUID(),
+                helicone_proxy_key: "null",
+                currency: limit.cost ? "USD" : undefined,
+              }))
+            );
+          }}
+          key={`limit-${idx}`}
+        />
+      ))}
+      <div
+        className="flex flex-row gap-2 hover:cursor-pointer hover:bg-slate-300 w-fit px-2 py-1 rounded-lg"
+        onClick={() => {
+          setLimits([
+            ...limits,
+            {
+              _limitType: "cost",
+              cost: undefined,
+              count: undefined,
+              timewindow_seconds: undefined,
+            },
+          ]);
+        }}
+      >
+        <div>Add new limit</div>
+        <PlusCircleIcon className="h-5 w-5 text-gray-900" />
+      </div>
+    </div>
+  );
+};
 
 const CreateProxyKeyModal = (props: CreateProxyKeyModalProps) => {
   const { providerKeys, open, setOpen, onSuccess } = props;
+  const [limits, setLimits] = useState<
+    Database["public"]["Tables"]["helicone_proxy_key_limits"]["Insert"][]
+  >([]);
 
   const [returnedKey, setReturnedKey] =
     useState<DecryptedProviderKeyMapping | null>(null);
@@ -61,6 +259,7 @@ const CreateProxyKeyModal = (props: CreateProxyKeyModalProps) => {
       body: JSON.stringify({
         heliconeProxyKeyName: proxyKeyName.value,
         providerKeyId: providerKeyName.value,
+        limits: limits,
       }),
     })
       .then(
@@ -87,7 +286,7 @@ const CreateProxyKeyModal = (props: CreateProxyKeyModalProps) => {
           action="#"
           method="POST"
           onSubmit={handleSubmitHandler}
-          className="flex flex-col space-y-8 w-[400px]"
+          className="flex flex-col space-y-8 w-[600px]"
         >
           <h1 className="text-lg font-semibold text-gray-900">
             Create Proxy Key
@@ -120,6 +319,7 @@ const CreateProxyKeyModal = (props: CreateProxyKeyModalProps) => {
               ))}
             </select>
           </div>
+          <LimitsInput setLimits={setLimits} />
 
           <div className="flex justify-end gap-2">
             <button
