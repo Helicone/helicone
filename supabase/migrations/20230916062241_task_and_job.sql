393e68c73181dd10d1aa6136e6cecf85bc064284
Justin <justintorre75@gmail.com>
2023-09-17 17:24:35 -0700

    migrations

--- supabase/migrations/20230916062241_task_and_job.sql
+++ supabase/migrations/20230916062241_task_and_job.sql
@@ -32,11 +32,16 @@ CREATE TABLE "public"."task_parents" (
     PRIMARY KEY ("task_id", "parent_task_id")
 );
 
+CREATE TABLE "public"."request_job_task" (
+    "request_id" uuid NOT NULL,
+    "job_id" uuid NOT NULL,
+    "task_id" uuid NOT NULL,
+    PRIMARY KEY ("request_id", "job_id", "task_id")
+);
+
+
+
 alter table "public"."task" enable row level security;
-
-alter table "public"."request" add column "job_id" uuid;
-
-alter table "public"."request" add column "task_id" uuid;
 
 CREATE UNIQUE INDEX jobs_pkey ON public.job USING btree (id);
 
@@ -45,14 +50,6 @@ CREATE UNIQUE INDEX tasks_pkey ON public.task USING btree (id);
 alter table "public"."job" add constraint "jobs_pkey" PRIMARY KEY using index "jobs_pkey";
 
 alter table "public"."task" add constraint "tasks_pkey" PRIMARY KEY using index "tasks_pkey";
-
-alter table "public"."request" add constraint "request_job_id_fkey" FOREIGN KEY (job_id) REFERENCES job(id) not valid;
-
-alter table "public"."request" validate constraint "request_job_id_fkey";
-
-alter table "public"."request" add constraint "request_task_id_fkey" FOREIGN KEY (task_id) REFERENCES task(id) not valid;
-
-alter table "public"."request" validate constraint "request_task_id_fkey";
 
 alter table "public"."job" add constraint "job_org_id_fkey" FOREIGN KEY (org_id) REFERENCES organization(id) not valid;
 
@@ -73,3 +70,18 @@ ALTER TABLE "public"."task_parents" VALIDATE CONSTRAINT "task_parents_task_id_fk
 ALTER TABLE "public"."task_parents" ADD CONSTRAINT "task_parents_parent_task_id_fkey" FOREIGN KEY ("parent_task_id") REFERENCES task(id) NOT VALID;
 
 ALTER TABLE "public"."task_parents" VALIDATE CONSTRAINT "task_parents_parent_task_id_fkey";
+
+ALTER TABLE "public"."request_job_task" 
+ADD CONSTRAINT "request_job_task_request_id_fkey" FOREIGN KEY ("request_id") REFERENCES request(id) NOT VALID;
+
+ALTER TABLE "public"."request_job_task" VALIDATE CONSTRAINT "request_job_task_request_id_fkey";
+
+ALTER TABLE "public"."request_job_task" 
+ADD CONSTRAINT "request_job_task_task_id_fkey" FOREIGN KEY ("task_id") REFERENCES task(id) NOT VALID;
+
+ALTER TABLE "public"."request_job_task" VALIDATE CONSTRAINT "request_job_task_task_id_fkey";
+
+ALTER TABLE "public"."request_job_task"
+ADD CONSTRAINT "request_job_task_job_id_fkey" FOREIGN KEY ("job_id") REFERENCES job(id) NOT VALID;
+
+ALTER TABLE "public"."request_job_task" VALIDATE CONSTRAINT "request_job_task_job_id_fkey";
