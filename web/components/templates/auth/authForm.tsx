fb4ebda89a3e9ba9d1e073b68f0347bce5117b16
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-09-28 16:31:22 -0700

    sign in fix (#886)
    Co-authored-by: Scott Nguyen <scott@helicone.ai>

--- web/components/templates/auth/authForm.tsx
+++ web/components/templates/auth/authForm.tsx
@@ -158,7 +158,7 @@ const AuthForm = (props: AuthFormProps) => {
                           type="email"
                           autoComplete="email"
                           required
-                          value={mfsEmail !== null ? mfsEmail : ""}
+                          placeholder={mfsEmail !== null ? mfsEmail : ""}
                           className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 text-sm lg:text-md lg:leading-6"
                         />
                       </div>
