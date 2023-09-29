e7272391c1e7f164793ee6c1a8e5cdc955356281
Scott Nguyen <scott@helicone.ai>
2023-09-29 16:45:38 -0700

    fix mobile

--- web/components/shared/layout/navbar/navBarV2.tsx
+++ web/components/shared/layout/navbar/navBarV2.tsx
@@ -175,7 +175,7 @@ const NavBarV2 = (props: NavBarV2Props) => {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-50 px-6 py-6 sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-50 px-6 py-6 sm:ring-1 sm:ring-gray-900/10">
           <div className="flex flex-col gap-10 h-full">
             <div>
               <div className="flex items-center gap-x-6 justify-between">
