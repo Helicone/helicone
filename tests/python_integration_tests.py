f1f0edc16c262d7a8526f05e8926a29afe865894
Justin Torre <justintorre75@gmail.com>
2023-10-09 16:29:49 -0700

    fixed python package and moved some tests around to prevent the same mistake happening again (#912)

--- tests/python_integration_tests.py
+++ tests/python_integration_tests.py
@@ -1,3 +1,5 @@
+# TODO deprecate this file and move all test to helicone_python/tests
+
 import requests
 import pytest
 import os
@@ -22,6 +24,7 @@ org_id = '83635a30-5ba6-41a8-8cc6-fb7df941b24a'
 helicone_proxy_key = 'sk-helicone-proxy-7wpoayi-xm5e6cy-wfimwqy-avnannq-d144312e-5c65-4eaa-a1c1-f0c143080601'
 hashed_proxy_key = '246172676f6e32696424763d3139246d3d3236323134342c743d332c703d3124415972396d5431736832356a474546546630614371672468767537654e7879674f474c6c7633584f4a597565643162414b6f326732732f7a575a30584c4b6c716134000000000000000000000000000000000000000000000000000000000000'
 
+
 def connect_to_db():
     return psycopg2.connect(
         dbname="postgres",
@@ -30,6 +33,7 @@ def connect_to_db():
         host="localhost",
         port="54322"
     )
+
 
 def fetch_from_db(query, params=None):
     conn = connect_to_db()
@@ -40,22 +44,25 @@ def fetch_from_db(query, params=None):
     conn.close()
     return results
 
+
 def insert_into_db(query, params):
     # Construct the query
     conn = connect_to_db()
     cur = conn.cursor(cursor_factory=DictCursor)
     cur.execute(query, (params))
-    result = cur.fetchone() # Fetch the returned ID
+    result = cur.fetchone()  # Fetch the returned ID
     conn.commit()
     cur.close()
     conn.close()
     return result
+
 
 def fetch(base_url, endpoint, method="GET", json=None, headers=None):
     url = f"{base_url}/{endpoint}"
     response = requests.request(method, url, json=json, headers=headers)
     response.raise_for_status()
     return response.json()
+
 
 def test_openai_proxy():
     print("\n---------Running test_proxy---------")
@@ -80,22 +87,25 @@ def test_openai_proxy():
         "OpenAI-Organization": openai_org_id
     }
 
-    response = fetch(helicone_proxy_url, "chat/completions", method="POST", json=data, headers=headers)
+    response = fetch(helicone_proxy_url, "chat/completions",
+                     method="POST", json=data, headers=headers)
     assert response, "Response from OpenAI API is empty"
 
-    time.sleep(3) # Helicone needs time to insert request into the database
+    time.sleep(3)  # Helicone needs time to insert request into the database
 
     query = "SELECT * FROM properties INNER JOIN request ON properties.request_id = request.id WHERE key = 'requestid' AND value = %s LIMIT 1"
     request_data = fetch_from_db(query, (requestId,))
     assert request_data, "Request data not found in the database for the given property request id"
 
     latest_request = request_data[0]
-    assert message_content in latest_request["body"]["messages"][0]["content"], "Request not found in the database"
+    assert message_content in latest_request["body"]["messages"][
+        0]["content"], "Request not found in the database"
 
     query = "SELECT * FROM response WHERE request = %s LIMIT 1"
     response_data = fetch_from_db(query, (latest_request["id"],))
     assert response_data, "Response data not found in the database for the given request ID"
     print("passed")
+
 
 def test_helicone_proxy_key():
     print("\n---------Running test_helicone_proxy_key---------")
@@ -112,8 +122,10 @@ def test_helicone_proxy_key():
     RETURNING id;
     """
 
-    provider_key_data = insert_into_db(query, (org_id, "OpenAI", "Team1", openai_api_key))
-    insert_into_db(query2, ('d144312e-5c65-4eaa-a1c1-f0c143080601', org_id, "Cole", hashed_proxy_key, provider_key_data[0]))
+    provider_key_data = insert_into_db(
+        query, (org_id, "OpenAI", "Team1", openai_api_key))
+    insert_into_db(query2, ('d144312e-5c65-4eaa-a1c1-f0c143080601',
+                   org_id, "Cole", hashed_proxy_key, provider_key_data[0]))
 
     requestId = str(uuid.uuid4())
     print("Request ID: " + requestId + "")
@@ -135,32 +147,35 @@ def test_helicone_proxy_key():
         "OpenAI-Organization": openai_org_id
     }
 
-    response = fetch(helicone_proxy_url, "chat/completions", method="POST", json=data, headers=headers)
+    response = fetch(helicone_proxy_url, "chat/completions",
+                     method="POST", json=data, headers=headers)
     assert response, "Response from OpenAI API is empty"
 
-    time.sleep(3) # Helicone needs time to insert request into the database
+    time.sleep(3)  # Helicone needs time to insert request into the database
 
     query = "SELECT * FROM properties INNER JOIN request ON properties.request_id = request.id WHERE key = 'requestid' AND value = %s LIMIT 1"
     request_data = fetch_from_db(query, (requestId,))
     assert request_data, "Request data not found in the database for the given property request id"
 
     latest_request = request_data[0]
-    assert message_content in latest_request["body"]["messages"][0]["content"], "Request not found in the database"
+    assert message_content in latest_request["body"]["messages"][
+        0]["content"], "Request not found in the database"
 
     query = "SELECT * FROM response WHERE request = %s LIMIT 1"
     response_data = fetch_from_db(query, (latest_request["id"],))
     assert response_data, "Response data not found in the database for the given request ID"
     print("passed")
 
+
 def test_openai_async():
     print("---------Running test_openai_async---------")
-    
+
     # Set the API key for Helicone
     helicone_global.api_key = helicone_api_key
     helicone_global.base_url = helicone_async_url
     openai.api_key = openai_api_key
     openai.organization = openai_org_id
-    
+
     requestId = str(uuid.uuid4())
     print("Request ID: " + requestId)
     message_content = test_openai_async.__name__ + " - " + requestId
@@ -170,7 +185,7 @@ def test_openai_async():
             "content": message_content
         }
     ]
-    
+
     # Using the helicone package for async logging
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -183,15 +198,16 @@ def test_openai_async():
         )
     )
     assert response, "Response from OpenAI API is empty"
-    
+
     time.sleep(3)  # Give some time for the async logging to complete
-    
+
     query = "SELECT * FROM properties INNER JOIN request ON properties.request_id = request.id WHERE key = 'requestid' AND value = %s LIMIT 1"
     request_data = fetch_from_db(query, (requestId,))
     assert request_data, "Request data not found in the database for the given property request id"
-    
+
     latest_request = request_data[0]
-    assert message_content in latest_request["body"]["messages"][0]["content"], "Request not found in the database"
+    assert message_content in latest_request["body"]["messages"][
+        0]["content"], "Request not found in the database"
 
     query = "SELECT * FROM response WHERE request = %s LIMIT 1"
     response_data = fetch_from_db(query, (latest_request["id"],))
