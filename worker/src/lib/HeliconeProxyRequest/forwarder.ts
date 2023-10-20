0a163a3b7f8100f20e298e3539030c04423e6720
gennyflynn <109100177+gennyflynn@users.noreply.github.com>
2023-10-20 14:59:18 -0700

    Add custom properties (#969)

--- worker/src/lib/HeliconeProxyRequest/forwarder.ts
+++ worker/src/lib/HeliconeProxyRequest/forwarder.ts
@@ -125,6 +125,7 @@ export async function proxyForwarder(
         dbWrapper: new DBWrapper(env, loggable.auth()),
         queue: new InsertQueue(
           createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY),
+          new ClickhouseClientWrapper(env),
           env.FALLBACK_QUEUE,
           env.REQUEST_AND_RESPONSE_QUEUE_KV
         ),
