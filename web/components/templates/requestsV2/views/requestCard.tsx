b909c1c25a863af30520a5995f4a296e9bc15be7
Scott Nguyen <scott@helicone.ai>
2023-10-26 16:16:21 -0700

    x

--- web/components/templates/requestsV2/views/requestCard.tsx
+++ web/components/templates/requestsV2/views/requestCard.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import { getUSDate, getUSDateFromString } from "../../../shared/utils/utils";
 import { formatNumber } from "../../users/initialColumns";
 import { NormalizedRequest } from "../builder/abstractRequestBuilder";
 import ModelPill from "../modelPill";
 import StatusBadge from "../statusBadge";
+import {
+  HandThumbUpIcon as HTUp,
+  HandThumbDownIcon as HTDown,
+} from "@heroicons/react/24/solid";
+import {
+  HandThumbDownIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
+import { clsx } from "../../../shared/clsx";
+import { updateRequestFeedback } from "../../../../services/lib/requests";
+import useNotification from "../../../shared/notification/useNotification";
 
 interface RequestCardProps {
   request: NormalizedRequest;
@@ -12,17 +24,82 @@ interface RequestCardProps {
 const RequestCard = (props: RequestCardProps) => {
   const { request, properties } = props;
 
+  const [requestFeedback, setRequestFeedback] = useState<{
+    createdAt: string | null;
+    id: string | null;
+    rating: boolean | null;
+  }>(request.feedback);
+  const [isFeedbackLoading, setIsFeedbackLoading] = useState(false);
+
+  const { setNotification } = useNotification();
+
+  const updateFeedbackHandler = async (requestId: string, rating: boolean) => {
+    setIsFeedbackLoading(true);
+    updateRequestFeedback(requestId, rating)
+      .then((res) => {
+        if (res && res.status === 200) {
+          setRequestFeedback({
+            ...requestFeedback,
+            rating: rating,
+          });
+          setNotification("Feedback submitted", "success");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        setNotification("Error submitting feedback", "error");
+      })
+      .finally(() => {
+        setIsFeedbackLoading(false);
+      });
+  };
+
   return (
     <div className="rounded-lg px-4 pb-4 pt-8 flex flex-row justify-between w-full relative gap-8">
       <div className="sticky top-8 flex flex-col space-y-4 h-full w-full max-w-md">
         <div className="flex flex-row justify-between items-center w-full border-b border-gray-100 py-2">
-          <p className="font-semibold text-xl">
-            {new Date(request.createdAt).toLocaleString()}
-          </p>
-          <StatusBadge
-            statusType={request.status.statusType}
-            errorCode={request.status.code}
-          />
+          <div className="flex flex-row items-center gap-2">
+            <p className="font-semibold text-xl">
+              {new Date(request.createdAt).toLocaleString()}
+            </p>
+            <StatusBadge
+              statusType={request.status.statusType}
+              errorCode={request.status.code}
+            />
+          </div>
+
+          <div className="flex flex-row items-center space-x-4">
+            <button
+              onClick={() => {
+                if (requestFeedback.rating === true) {
+                  return;
+                }
+
+                updateFeedbackHandler(request.id, true);
+              }}
+            >
+              {requestFeedback.rating === true ? (
+                <HTUp className={clsx("h-5 w-5 text-green-500")} />
+              ) : (
+                <HandThumbUpIcon className="h-5 w-5 text-green-500" />
+              )}
+            </button>
+            <button
+              onClick={() => {
+                if (requestFeedback.rating === false) {
+                  return;
+                }
+
+                updateFeedbackHandler(request.id, false);
+              }}
+            >
+              {requestFeedback.rating === false ? (
+                <HTDown className={clsx("h-5 w-5 text-red-500")} />
+              ) : (
+                <HandThumbDownIcon className="h-5 w-5 text-red-500" />
+              )}
+            </button>
+          </div>
         </div>
 
         <div className="flex flex-wrap gap-4 items-center">
