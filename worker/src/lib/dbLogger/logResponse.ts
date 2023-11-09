69cabf06dc3246e9a0c846eeca4c930a1ecfff43
Justin Torre <justintorre75@gmail.com>
2023-11-08 16:42:07 -0800

    removed image url for none url images (#1011)

--- worker/src/lib/dbLogger/logResponse.ts
+++ worker/src/lib/dbLogger/logResponse.ts
@@ -77,6 +77,41 @@ async function getPromptId(
   }
 }
 
+// Replaces all the image_url that is not a url or not { url: url }  with
+// { unsupported_image: true }
+//
+function unsupportedImage(body: any): any {
+  if (typeof body !== "object" || body === null) {
+    return body;
+  }
+  if (Array.isArray(body)) {
+    return body.map((item) => unsupportedImage(item));
+  }
+  const notSupportMessage = {
+    helicone_message:
+      "Storing images as bytes is currently not supported within Helicone.",
+  };
+  if (body["image_url"] !== undefined) {
+    const imageUrl = body["image_url"];
+    if (typeof imageUrl === "string" && !imageUrl.startsWith("http")) {
+      body.image_url = notSupportMessage;
+    }
+    if (
+      typeof imageUrl === "object" &&
+      imageUrl.url !== undefined &&
+      typeof imageUrl.url === "string" &&
+      !imageUrl.url.startsWith("http")
+    ) {
+      body.image_url = notSupportMessage;
+    }
+  }
+  const result: any = {};
+  for (const key in body) {
+    result[key] = unsupportedImage(body[key]);
+  }
+  return result;
+}
+
 export async function logRequest(
   request: DBLoggableProps["request"],
   responseId: string,
@@ -164,7 +199,7 @@ export async function logRequest(
     const requestData = {
       id: request.requestId,
       path: request.path,
-      body: request.omitLog ? {} : requestBody,
+      body: request.omitLog ? {} : unsupportedImage(requestBody),
       auth_hash: request.providerApiKeyAuthHash,
       user_id: request.userId ?? null,
       prompt_id: request.promptId ?? null,
