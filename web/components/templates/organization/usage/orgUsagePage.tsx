b91974dad8321620d6c6f7ec9a3b94fb85d02524
Scott Nguyen <scott@helicone.ai>
2023-11-14 00:58:27 -0800

    account pages

--- web/components/templates/organization/usage/orgUsagePage.tsx
+++ web/components/templates/organization/usage/orgUsagePage.tsx
@@ -96,9 +96,9 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
   const renderInfo = () => {
     if (org.tier === "free") {
       return (
-        <div className="border-2 p-4 text-sm rounded-lg flex flex-col text-gray-600 border-gray-300 w-full gap-4">
+        <div className="border-2 p-4 text-sm rounded-lg flex flex-col text-gray-500 border-gray-300 dark:border-gray-700 w-full gap-4">
           <div className="flex flex-row gap-2 w-full h-4">
-            <div className="relative h-full w-full flex-auto bg-gray-300 rounded-md">
+            <div className="relative h-full w-full flex-auto bg-gray-300 dark:bg-gray-700 rounded-md">
               <div
                 className="aboslute h-full bg-purple-500 rounded-md"
                 style={{
@@ -109,15 +109,15 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
             <div className="flex-1 w-full whitespace-nowrap">
               <div className="flex flex-row gap-1.5 items-center text-black">
                 <span>{`${Number(count?.data).toLocaleString()}`}</span>
-                <span className="text-gray-400 text-sm">/</span>
-                <span className="text-sm text-gray-400">{`${Number(
+                <span className="text-gray-500 text-sm">/</span>
+                <span className="text-sm text-gray-500">{`${Number(
                   100_000
                 ).toLocaleString()}`}</span>
               </div>
             </div>
           </div>
-          <div className="flex flex-row items-center text-gray-600 w-fit gap-4">
-            <LightBulbIcon className="h-6 w-6 text-gray-600 hidden sm:inline" />
+          <div className="flex flex-row items-center text-gray-500 w-fit gap-4">
+            <LightBulbIcon className="h-6 w-6 text-gray-500 hidden sm:inline" />
             We continue logging your requests after your limit is reached, but
             you will lose access to the dashboard until you upgrade.
           </div>
@@ -145,7 +145,7 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
               <ChevronLeftIcon className="h-5 w-5" />
             </button>
 
-            <h1 className="text-4xl font-semibold tracking-wide">
+            <h1 className="text-4xl font-semibold tracking-wide text-black dark:text-white">
               {getMonthName(startOfMonthFormatted + 1)}
             </h1>
             {!isNextMonthDisabled && (
@@ -157,7 +157,7 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
               </button>
             )}
           </div>
-          <p className="text-md">
+          <p className="text-md text-gray-900 dark:text-gray-100">
             Below is a summary of your monthly usage and your plan. Click{" "}
             <Link href="/pricing" className="text-blue-500 underline">
               here
@@ -166,26 +166,26 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
           </p>
         </div>
         {isCountLoading ? (
-          <div className="h-24 w-full bg-gray-300 animate-pulse rounded-md"></div>
+          <div className="h-24 w-full bg-gray-300 dark:bg-gray-700 animate-pulse rounded-md"></div>
         ) : (
           renderInfo()
         )}
 
         <div className="flex flex-col sm:flex-row sm:space-x-4">
           <div className="flex flex-wrap items-baseline justify-between gap-y-2 pt-8 min-w-[200px]">
-            <dt className="text-sm font-medium leading-6 text-gray-700">
+            <dt className="text-sm font-medium leading-6 text-gray-700 dark:text-gray-300">
               Your Plan
             </dt>
-            <dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
+            <dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900 dark:text-gray-100">
               {capitalizeHelper(org.tier || "")}
             </dd>
           </div>
           {org.tier === "free" && (
             <div className="flex flex-wrap items-baseline justify-between gap-y-2 pt-8 min-w-[200px]">
-              <dt className="text-sm font-medium leading-6 text-gray-700">
+              <dt className="text-sm font-medium leading-6 text-gray-700 dark:text-gray-300">
                 Requests
               </dt>
-              <dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
+              <dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900 dark:text-gray-100">
                 {isCountLoading
                   ? "Loading..."
                   : Number(count?.data || 0).toLocaleString()}
@@ -194,11 +194,11 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
           )}
           {rateLimitTracker.request && (
             <div className="flex flex-wrap items-baseline justify-between gap-y-2 pt-8 min-w-[200px]">
-              <dt className="text-sm font-medium leading-6 text-red-700">
+              <dt className="text-sm font-medium leading-6 text-red-700 dark:text-red-300">
                 Rate Limited
               </dt>
 
-              <dd className="w-full flex-row gap-2 text-3xl font-medium leading-10 tracking-tight text-red-900">
+              <dd className="w-full flex-row gap-2 text-3xl font-medium leading-10 tracking-tight text-red-900 dark:text-red-100">
                 {isCountLoading
                   ? "Loading..."
                   : Number(
@@ -214,7 +214,7 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
         </div>
         <ul
           role="list"
-          className="mt-6 grid grid-cols-1 gap-8 border-t border-gray-200 py-6 sm:grid-cols-2"
+          className="mt-6 grid grid-cols-1 gap-8 border-t border-gray-200 dark:border-gray-800 py-6 sm:grid-cols-2"
         >
           {org.tier === "free" && (
             <li className="flow-root">
@@ -231,7 +231,7 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
                   />
                 </div>
                 <div>
-                  <h3 className="text-sm font-medium text-gray-900">
+                  <h3 className="text-sm font-medium text-gray-900 dark:text-gray-100">
                     <button
                       onClick={() => setOpen(true)}
                       className="focus:outline-none"
@@ -250,7 +250,7 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
           )}
           {org.tier !== "free" && (
             <li className="flow-root">
-              <div className="relative -m-3 flex items-center space-x-4 rounded-xl p-3 focus-within:ring-2 focus-within:ring-sky-500 hover:bg-white">
+              <div className="relative -m-3 flex items-center space-x-4 rounded-xl p-3 focus-within:ring-2 focus-within:ring-sky-500 hover:bg-white dark:hover:bg-black">
                 <div
                   className={clsx(
                     "bg-green-500",
@@ -258,12 +258,12 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
                   )}
                 >
                   <CreditCardIcon
-                    className="h-6 w-6 text-white"
+                    className="h-6 w-6 text-white dark:text-black"
                     aria-hidden="true"
                   />
                 </div>
                 <div>
-                  <h3 className="text-sm font-medium text-gray-900">
+                  <h3 className="text-sm font-medium text-gray-900 dark:text-gray-100">
                     <button
                       className="focus:outline-none"
                       onClick={async () => {
@@ -292,7 +292,7 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
           )}
           {org.tier !== "enterprise" && (
             <li className="flow-root">
-              <div className="relative -m-3 flex items-center space-x-4 rounded-xl p-3 focus-within:ring-2 focus-within:ring-sky-500 hover:bg-white">
+              <div className="relative -m-3 flex items-center space-x-4 rounded-xl p-3 focus-within:ring-2 focus-within:ring-sky-500 hover:bg-white dark:hover:bg-black">
                 <div
                   className={clsx(
                     "bg-purple-500",
@@ -305,7 +305,7 @@ const OrgUsagePage = (props: OrgUsagePageProps) => {
                   />
                 </div>
                 <div>
-                  <h3 className="text-sm font-medium text-gray-900">
+                  <h3 className="text-sm font-medium text-gray-900 dark:text-gray-100">
                     <Link
                       className="focus:outline-none"
                       href={
--- web/components/templates/vault/vaultPage.tsx
+++ web/components/templates/vault/vaultPage.tsx
@@ -98,22 +98,22 @@ const VaultPage = () => {
 
   return (
     <>
-      <div className="flex flex-col space-y-12 divide-y divide-gray-300 max-w-3xl py-4">
+      <div className="flex flex-col space-y-12 divide-y divide-gray-300 dark:divide-gray-700 py-4 max-w-3xl">
         <div className="flex flex-col space-y-4">
           <div className="flex flex-row justify-between w-full items-center">
-            <h1 className="font-semibold text-2xl text-gray-900">
+            <h1 className="font-semibold text-2xl text-gray-900 dark:text-gray-100">
               Provider Keys
             </h1>
             <button
               onClick={() => {
                 setIsProviderOpen(true);
               }}
-              className="bg-gray-900 hover:bg-gray-700 whitespace-nowrap rounded-md px-4 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500"
+              className="bg-gray-900 hover:bg-gray-700 dark:bg-gray-100 dark:hover:bg-gray-300 flex flex-row whitespace-nowrap rounded-md pl-3 pr-4 py-2 text-sm font-semibold text-white dark:text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500"
             >
               Add Provider Key
             </button>
           </div>
-          <p className="text-gray-600">
+          <p className="text-gray-500">
             These keys will be used to authenticate with your provider.
           </p>
           {isLoading ? (
@@ -121,7 +121,7 @@ const VaultPage = () => {
               {Array.from({ length: 3 }).map((_, index) => (
                 <li
                   key={index}
-                  className="h-6 flex flex-row justify-between gap-2 bg-gray-300 animate-pulse rounded-md"
+                  className="h-6 flex flex-row justify-between gap-2 bg-gray-300 dark:bg-gray-700 animate-pulse rounded-md"
                 ></li>
               ))}
             </ul>
@@ -149,10 +149,10 @@ const VaultPage = () => {
               onClick={() => {
                 setIsProviderOpen(true);
               }}
-              className="relative flex flex-col justify-center items-center w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="bg-gray-900 hover:bg-gray-700 dark:bg-gray-100 dark:hover:bg-gray-300 flex flex-row whitespace-nowrap rounded-md pl-3 pr-4 py-2 text-sm font-semibold text-white dark:text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500"
             >
               <KeyIcon className="h-8 w-8" />
-              <span className="mt-2 block text-sm font-semibold text-gray-900">
+              <span className="mt-2 block text-sm font-semibold text-gray-900 dark:text-gray-100">
                 Add a provider key
               </span>
             </button>
@@ -160,19 +160,19 @@ const VaultPage = () => {
         </div>
         <div className="flex flex-col space-y-4 pt-12">
           <div className="flex flex-row justify-between w-full items-center">
-            <h1 className="font-semibold text-2xl text-gray-900">
+            <h1 className="font-semibold text-2xl text-gray-900 dark:text-gray-100">
               Helicone Proxy Keys
             </h1>
             <button
               onClick={() => {
                 setIsProxyOpen(true);
               }}
-              className="bg-gray-900 hover:bg-gray-700 whitespace-nowrap rounded-md px-4 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500"
+              className="bg-gray-900 hover:bg-gray-700 dark:bg-gray-100 dark:hover:bg-gray-300 flex flex-row whitespace-nowrap rounded-md pl-3 pr-4 py-2 text-sm font-semibold text-white dark:text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500"
             >
               Create Proxy Key
             </button>
           </div>
-          <p className="text-gray-600">
+          <p className="text-gray-500">
             These keys will replace your provider keys in your application. This
             ensures that any usage will be logged in Helicone.
           </p>
@@ -181,7 +181,7 @@ const VaultPage = () => {
               {Array.from({ length: 3 }).map((_, index) => (
                 <li
                   key={index}
-                  className="h-6 flex flex-row justify-between gap-2 bg-gray-300 animate-pulse rounded-md"
+                  className="h-6 flex flex-row justify-between gap-2 bg-gray-300 dark:bg-gray-700 animate-pulse rounded-md"
                 ></li>
               ))}
             </ul>
@@ -203,7 +203,7 @@ const VaultPage = () => {
               className="relative flex flex-col justify-center items-center w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <KeyIcon className="h-8 w-8" />
-              <span className="mt-2 block text-sm font-semibold text-gray-900">
+              <span className="mt-2 block text-sm font-semibold text-gray-900 dark:text-gray-100">
                 Add a proxy key
               </span>
             </button>
@@ -223,8 +223,10 @@ const VaultPage = () => {
       />
       <ThemedModal open={deleteProviderOpen} setOpen={setDeleteProviderOpen}>
         <div className="flex flex-col gap-4 w-full">
-          <p className="font-semibold text-lg">Delete Provider Key</p>
-          <p className="text-gray-700 w-[400px] whitespace-pre-wrap text-sm">
+          <p className="font-semibold text-lg text-gray-900 dark:text-gray-100">
+            Delete Provider Key
+          </p>
+          <p className="text-gray-700 dark:text-gray-300 w-[400px] whitespace-pre-wrap text-sm">
             This Provider Key will be deleted from your account. All proxy keys
             that are mapped to this provider key will be deleted as well. Are
             you sure you want to delete this provider key?
@@ -233,7 +235,7 @@ const VaultPage = () => {
             <button
               onClick={() => setDeleteProviderOpen(false)}
               type="button"
-              className="flex flex-row items-center rounded-md bg-white px-4 py-2 text-sm font-semibold border border-gray-300 hover:bg-gray-50 text-gray-900 shadow-sm hover:text-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+              className="flex flex-row items-center rounded-md bg-white dark:bg-black px-4 py-2 text-sm font-semibold border border-gray-300 dark:border-gray-700 hover:bg-gray-50 text-gray-900 dark:hover:bg-gray-900 dark:text-gray-100 shadow-sm hover:text-gray-700 dark:hover:text-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
             >
               Cancel
             </button>
@@ -243,7 +245,7 @@ const VaultPage = () => {
                   await deleteProviderKey(selectedProviderKey.id!);
                 }
               }}
-              className="items-center rounded-md bg-red-600 px-4 py-2 text-sm flex font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              className="items-center rounded-md bg-red-500 px-4 py-2 text-sm flex font-semibold text-white dark:text-black shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
               Delete
             </button>
@@ -252,8 +254,10 @@ const VaultPage = () => {
       </ThemedModal>
       <ThemedModal open={deleteProxyOpen} setOpen={setDeleteProxyOpen}>
         <div className="flex flex-col gap-4 w-full">
-          <p className="font-semibold text-lg">Delete Proxy Key</p>
-          <p className="text-gray-700 w-[400px] whitespace-pre-wrap text-sm">
+          <p className="font-semibold text-lg text-gray-900 dark:text-gray-100">
+            Delete Proxy Key
+          </p>
+          <p className="text-gray-700 dark:text-gray-300 w-[400px] whitespace-pre-wrap text-sm">
             This Proxy Key will be deleted from your account. Are you sure you
             want to delete this proxy key?
           </p>
@@ -261,7 +265,7 @@ const VaultPage = () => {
             <button
               onClick={() => setDeleteProxyOpen(false)}
               type="button"
-              className="flex flex-row items-center rounded-md bg-white px-4 py-2 text-sm font-semibold border border-gray-300 hover:bg-gray-50 text-gray-900 shadow-sm hover:text-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+              className="flex flex-row items-center rounded-md bg-white dark:bg-black px-4 py-2 text-sm font-semibold border border-gray-300 dark:border-gray-700 hover:bg-gray-50 text-gray-900 dark:hover:bg-gray-900 dark:text-gray-100 shadow-sm hover:text-gray-700 dark:hover:text-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
             >
               Cancel
             </button>
@@ -271,7 +275,7 @@ const VaultPage = () => {
                   await deleteHeliconeProxyKey(selectedProxyKey.id!);
                 }
               }}
-              className="items-center rounded-md bg-red-600 px-4 py-2 text-sm flex font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              className="items-center rounded-md bg-red-500 px-4 py-2 text-sm flex font-semibold text-white dark:text-black shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
               Delete
             </button>
