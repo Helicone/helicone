262d978f381af7410bfb91541ed64147834db379
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-11-15 13:20:06 -0800

    unknown-mapping (#1051)

--- web/components/templates/requests/completion.tsx
+++ web/components/templates/requests/completion.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { removeLeadingWhitespace } from "../../shared/utils/utils";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { Tooltip } from "@mui/material";
 
 interface CompletionProps {
   request: string;
@@ -13,33 +14,51 @@ interface CompletionProps {
 
   rawRequest: any;
   rawResponse: any;
+
+  defaultMode?: "pretty" | "json";
 }
 
 export const Completion = (props: CompletionProps) => {
-  const { request, response, rawRequest, rawResponse } = props;
+  const {
+    request,
+    response,
+    rawRequest,
+    rawResponse,
+    defaultMode = "pretty",
+  } = props;
 
-  const [mode, setMode] = useState<"pretty" | "json">("pretty");
+  const [mode, setMode] = useState<"pretty" | "json">(defaultMode);
 
   return (
     <div className="w-full flex flex-col text-left space-y-2 text-sm">
       <div className="w-full border border-gray-300 dark:border-gray-700 rounded-md divide-y divide-gray-300 dark:divide-gray-700 h-full">
-        <div className="h-10 px-2 rounded-md flex flex-row items-center justify-between w-full bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-          <div className="flex flex-row items-center space-x-2"></div>
-          <button
-            onClick={() => {
-              setMode(mode === "pretty" ? "json" : "pretty");
-            }}
-            className="flex flex-row space-x-1 items-center hover:bg-gray-200 dark:hover:bg-gray-800 py-1 px-2 rounded-lg"
-          >
-            <ChevronUpDownIcon className="h-4 w-4" />
-            <p className="text-xs font-semibold text-gray-900 dark:text-gray-100">
-              {mode === "pretty" ? "JSON" : "Pretty"}
-            </p>
-          </button>
+        <div className="h-10 px-2 rounded-md flex flex-row items-center justify-end w-full bg-gray-50 text-gray-900 dark:bg-black dark:text-gray-100">
+          {defaultMode === "json" ? (
+            <Tooltip title="Model is pending mapping">
+              <button className="hover:cursor-not-allowed flex flex-row space-x-1 items-center hover:bg-gray-200 dark:hover:bg-gray-800 py-1 px-2 rounded-lg">
+                <ChevronUpDownIcon className="h-4 w-4" />
+                <p className="text-xs font-semibold text-gray-900 dark:text-gray-100">
+                  {mode === "pretty" ? "JSON" : "Pretty"}
+                </p>
+              </button>
+            </Tooltip>
+          ) : (
+            <button
+              onClick={() => {
+                setMode(mode === "pretty" ? "json" : "pretty");
+              }}
+              className="flex flex-row space-x-1 items-center hover:bg-gray-200 dark:hover:bg-gray-800 py-1 px-2 rounded-lg"
+            >
+              <ChevronUpDownIcon className="h-4 w-4" />
+              <p className="text-xs font-semibold text-gray-900 dark:text-gray-100">
+                {mode === "pretty" ? "JSON" : "Pretty"}
+              </p>
+            </button>
+          )}
         </div>
 
         {mode === "json" ? (
-          <div className="flex flex-col space-y-4 bg-gray-100 dark:bg-gray-900 relative rounded-b-md">
+          <div className="flex flex-col space-y-4 bg-gray-100 dark:bg-[#17191d] relative rounded-b-md">
             <div className="flex flex-col space-y-2 p-4">
               <p className="font-semibold text-gray-900 dark:text-gray-100 text-sm">
                 Request
@@ -58,7 +77,7 @@ export const Completion = (props: CompletionProps) => {
             </div>
           </div>
         ) : (
-          <div className="flex flex-col space-y-4 bg-gray-100 dark:bg-gray-900 relative rounded-b-md">
+          <div className="flex flex-col space-y-4 bg-gray-100 dark:bg-[#17191d] relative rounded-b-md">
             <div className="flex flex-col space-y-2 p-4">
               <p className="font-semibold text-gray-900 dark:text-gray-100 text-sm">
                 Request
