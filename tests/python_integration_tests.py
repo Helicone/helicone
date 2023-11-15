174abd4be752380a71c7f24daff6c3412744556c
Justin <justintorre75@gmail.com>
2023-11-15 11:48:27 -0800

    fixed test

--- tests/python_integration_tests.py
+++ tests/python_integration_tests.py
@@ -25,6 +25,7 @@ org_id = '83635a30-5ba6-41a8-8cc6-fb7df941b24a'
 helicone_proxy_key = 'sk-helicone-proxy-7wpoayi-xm5e6cy-wfimwqy-avnannq-d144312e-5c65-4eaa-a1c1-f0c143080601'
 hashed_proxy_key = '246172676f6e32696424763d3139246d3d3236323134342c743d332c703d3124415972396d5431736832356a474546546630614371672468767537654e7879674f474c6c7633584f4a597565643162414b6f326732732f7a575a30584c4b6c716134000000000000000000000000000000000000000000000000000000000000'
 
+
 def connect_to_db():
     return psycopg2.connect(
         dbname="postgres",
@@ -60,8 +61,14 @@ def insert_into_db(query, params):
 def fetch(base_url, endpoint, method="GET", json=None, headers=None):
     url = f"{base_url}/{endpoint}"
     response = requests.request(method, url, json=json, headers=headers)
-    response.raise_for_status()
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        print(response.text)
+        raise e
     return response.json()
+
 
 def test_gateway_api():
     print("\n---------Running test_gateway_api---------")
@@ -83,18 +90,17 @@ def test_gateway_api():
     headers = {
         "Authorization": f"Bearer {openai_api_key}",
         "Helicone-Auth": f"Bearer {helicone_api_key}",
-        "Helicone-Target-Url": "https://api.openai.com/v1/",
-        "Helicone-Target-Provider": "OPENAI",
+        "Helicone-Target-Url": "https://api.openai.com",
         "Helicone-Property-RequestId": requestId,
         "OpenAI-Organization": openai_org_id
     }
-    
+
     response = fetch(helicone_gateway_url, "v1/chat/completions",
                      method="POST", json=data, headers=headers)
     assert response, "Response from OpenAI API is empty"
 
     time.sleep(3)  # Helicone needs time to insert request into the database
-    
+
     query = "SELECT * FROM properties INNER JOIN request ON properties.request_id = request.id WHERE key = 'requestid' AND value = %s LIMIT 1"
     request_data = fetch_from_db(query, (requestId,))
     assert request_data, "Request data not found in the database for the given property request id"
@@ -107,6 +113,7 @@ def test_gateway_api():
     response_data = fetch_from_db(query, (latest_request["id"],))
     assert response_data, "Response data not found in the database for the given request ID"
     print("passed")
+
 
 def test_openai_proxy():
     print("\n---------Running test_proxy---------")
