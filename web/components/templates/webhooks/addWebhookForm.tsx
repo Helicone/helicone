a0bdac05166ba110bc9f253cf7a8df4811b97d18
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-11-16 15:40:51 -0800

    user-count (#1058)

--- web/components/templates/webhooks/addWebhookForm.tsx
+++ web/components/templates/webhooks/addWebhookForm.tsx
@@ -54,11 +54,13 @@ const AddWebhookForm = (props: AddWebhookFormProps) => {
       onSubmit={handleSubmitHandler}
       className="flex flex-col gap-4 w-full space-y-4 min-w-[30rem]"
     >
-      <p className="font-semibold text-lg">Listen to events</p>
+      <p className="font-semibold text-lg text-gray-900 dark:text-gray-100">
+        Listen to events
+      </p>
       <div className="space-y-1.5 text-sm">
         <label
           htmlFor="webhook-url"
-          className="block text-sm font-medium leading-6 text-gray-900"
+          className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-100"
         >
           Endpoint URL
         </label>
@@ -67,7 +69,7 @@ const AddWebhookForm = (props: AddWebhookFormProps) => {
           name="webhook-url"
           id="webhook-url"
           className={clsx(
-            "block w-full rounded-md border border-gray-300 shadow-sm p-2 text-sm"
+            "block w-full rounded-md border border-gray-300 dark:border-gray-700 shadow-sm p-2 text-sm"
           )}
           placeholder={"https://"}
         />
@@ -76,13 +78,13 @@ const AddWebhookForm = (props: AddWebhookFormProps) => {
         <button
           type="button"
           onClick={close}
-          className="flex flex-row items-center rounded-md bg-white px-4 py-2 text-sm font-semibold border border-gray-300 hover:bg-gray-50 text-gray-900 shadow-sm hover:text-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+          className="flex flex-row items-center rounded-md bg-white dark:bg-black px-4 py-2 text-sm font-semibold border border-gray-300 hover:bg-gray-50 text-gray-900 dark:border-gray-700 dark:hover:bg-gray-900 dark:text-gray-100 shadow-sm hover:text-gray-700 dark:hover:text-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
         >
           Cancel
         </button>
         <button
           type="submit"
-          className="items-center rounded-md bg-black px-4 py-2 text-sm flex font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          className="items-center rounded-md bg-black dark:bg-white px-4 py-2 text-sm flex font-semibold text-white dark:text-black shadow-sm hover:bg-gray-800 dark:hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
         >
           {isLoading && (
             <ArrowPathIcon className="w-4 h-4 mr-1.5 animate-spin" />
