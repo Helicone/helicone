8a7714fdfecb592544add80b033b8e33dbf1e1f5
Scott Nguyen <scott@helicone.ai>
2023-11-22 15:14:04 -0800

    slower globe

--- web/components/templates/home/globe.tsx
+++ web/components/templates/home/globe.tsx
@@ -35,7 +35,7 @@ export default function Globe() {
         // Called on every animation frame.
         // `state` will be an empty object, return updated params.
         state.phi = phi;
-        phi += 0.01;
+        phi += 0.001;
       },
     });
 
