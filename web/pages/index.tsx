445f577d726b0c1b7a11c0f3a5d8a6959ed45343
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-11-22 00:52:26 -0800

    landing page revamp

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -8,6 +8,7 @@ import MetaData from "../components/shared/metaData";
 import { DEMO_EMAIL } from "../lib/constants";
 import { SupabaseServerWrapper } from "../lib/wrappers/supabase";
 import HomePageV2 from "../components/templates/home/homePageV2";
+import Head from "next/head";
 
 interface HomeProps {}
 
@@ -27,9 +28,31 @@ const Home = (props: HomeProps) => {
   }
 
   return (
-    <MetaData title="Home">
+    <>
+      <Head>
+        <title>{`Helicone - The easiest way to monitor your LLM-application at scale`}</title>
+        <link rel="icon" href="/assets/landing/helicone-mobile.webp" />
+        <meta
+          property="og:title"
+          content={
+            "Helicone - The easiest way to monitor your LLM-application at scale"
+          }
+        />
+        <meta
+          property="og:description"
+          name="description"
+          content="Monitoring usage and costs for language models shouldn't be a hassle. With Helicone, you can focus on building your product, not building and maintaining your own analytics solution."
+          key="desc"
+        />
+        <meta
+          property="og:image"
+          content={
+            "https://www.helicone.ai/_next/image?url=%2Fassets%2Flanding%2Fhelicone-mobile.webp&w=384&q=75"
+          }
+        />
+      </Head>
       <HomePageV2 />
-    </MetaData>
+    </>
   );
 };
 
