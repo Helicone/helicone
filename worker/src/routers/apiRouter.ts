0a34e3ddb2adf479263fadd9f5d8f7723171a459
colegottdank <colegottdank@gmail.com>
2023-11-27 14:01:25 -0500

    Refactor authentication and add request property endpoint

--- worker/src/routers/apiRouter.ts
+++ worker/src/routers/apiRouter.ts
@@ -126,7 +126,6 @@ async function logAsync(
 }
 
 export const getAPIRouter = (router: BaseRouter) => {
-
   router.post(
     "/job",
     async (
@@ -338,7 +337,7 @@ export const getAPIRouter = (router: BaseRouter) => {
   );
 
   router.put(
-    '/request/:id/property',
+    "/request/:id/property",
     async (
       { params: { id } },
       requestWrapper: RequestWrapper,
@@ -346,35 +345,43 @@ export const getAPIRouter = (router: BaseRouter) => {
       _: ExecutionContext
     ) => {
       const client = await createAPIClient(env, requestWrapper);
+
       const authParams = await client.db.getAuthParams();
-      
-      interface Body {
-        key: string,
-        value: string,
-      }
       if (authParams.error !== null) {
         return client.response.unauthorized();
-      }      
+      }
+
+      interface Body {
+        key: string;
+        value: string;
+      }
+
       const property = await requestWrapper.getJson<Body>();
-      const {data, error} = await client.db.getRequestById(id);
+      const { data, error } = await client.db.getRequestById(id);
 
       if (error) {
         return client.response.newError(error, 500);
       }
-      
+
       if (!data) {
         return client.response.newError("Request not found.", 404);
       }
 
       const properties = {
-        ...(data?.properties as Record<string, any> || {}),
+        ...((data?.properties as Record<string, any>) || {}),
         [property.key]: property.value,
-      }
+      };
 
-      await client.queue.putRequestProperty(id, properties, property, authParams.data.organizationId, data)
-      return client.response.successJSON({"ok": 'true'});
+      await client.queue.putRequestProperty(
+        id,
+        properties,
+        property,
+        authParams.data.organizationId,
+        data
+      );
+      return client.response.successJSON({ ok: "true" });
     }
-  )
+  );
 
   // Proxy only + proxy forwarder
   router.all(
