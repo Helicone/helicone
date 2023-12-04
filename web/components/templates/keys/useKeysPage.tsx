72eed3701ca17dd8d912ea4fbb24ac51bf38e4ef
Scott <50155740+ScottMktn@users.noreply.github.com>
2023-12-04 12:08:07 -0800

    key-fixes (#1115)

--- web/components/templates/keys/useKeysPage.tsx
+++ web/components/templates/keys/useKeysPage.tsx
@@ -8,10 +8,21 @@ const useKeysPage = () => {
   } = useHeliconeKeys();
 
   const isLoading = isHeliconeKeysLoading;
+  heliconeKeys?.data?.sort((a, b) => {
+    const aDate = new Date(a.created_at);
+    const bDate = new Date(b.created_at);
+
+    if (aDate > bDate) {
+      return 1;
+    } else if (bDate > aDate) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
 
   return {
     isLoading,
-
     heliconeKeys,
     refetchHeliconeKeys,
   };
