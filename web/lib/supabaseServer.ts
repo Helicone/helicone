320aa71719e080a769ae83b6f5c7e26e71f18409
Umut Hope YILDIRIM <umut475@gmail.com>
2023-12-13 10:29:33 -0800

    Update supabaseServer.ts file

--- web/lib/supabaseServer.ts
+++ web/lib/supabaseServer.ts
@@ -3,8 +3,10 @@ import { Database } from "../supabase/database.types";
 
 export const supabaseUrl =
   process.env.SUPABASE_URL ?? process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY ?? process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY ?? "";
-
+const supabaseServiceKey =
+  process.env.SUPABASE_SERVICE_KEY ??
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY ??
+  "";
 if (supabaseUrl === "" || supabaseServiceKey === "") {
   throw new Error(`URL or Anon ENV not set for Server - ${supabaseServiceKey}`);
 }
