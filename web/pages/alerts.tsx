0033fcb3d0632e7a4b06dc90008bd71d9c9752d5
Umut Hope YILDIRIM <umut475@gmail.com>
2023-12-13 12:49:53 -0800

    Fix alerts page and remove unnecessary code

--- web/pages/alerts.tsx
+++ web/pages/alerts.tsx
@@ -3,28 +3,8 @@ import AuthLayout from "../components/shared/layout/authLayout";
 import AuthHeader from "../components/shared/authHeader";
 import MetaData from "../components/shared/metaData";
 import AlertsPage from "../components/templates/alerts/alertsPage";
-<<<<<<< HEAD
-import { withAuthSSR } from "../lib//api/handlerWrappers";
-
-import {
-  useAlertPage,
-  useAlertHistoryPage,
-} from "../components/templates/alerts/useAlertPage";
-
-interface AlertProps {
-  user: User;
-  orgId: string;
-}
-
-const Alert = (props: AlertProps) => {
-  const { user, orgId } = props;
-
-  const { alert, refreshAlert } = useAlertPage(orgId);
-  const { alertHistory, refreshAlertHistory } = useAlertHistoryPage(orgId);
-=======
 import { withAuthSSR } from "../lib/api/handlerWrappers";
 import { Database } from "../../supabase/database.types";
-import { supabaseServer } from "../lib/supabaseServer";
 
 interface AlertProps {
   user: User;
@@ -36,23 +16,12 @@ interface AlertProps {
 
 const Alert = (props: AlertProps) => {
   const { user } = props;
->>>>>>> d3ddd2aa (alert fixes)
 
   return (
     <MetaData title="Alerts">
       <AuthLayout user={user}>
         <AuthHeader title={"Alerts"} />
-<<<<<<< HEAD
-        <AlertsPage
-          orgId={orgId}
-          alerts={alert}
-          refreshAlert={refreshAlert}
-          alertHistory={alertHistory}
-          refreshAlertHistory={refreshAlertHistory}
-        />
-=======
         <AlertsPage user={user} />
->>>>>>> d3ddd2aa (alert fixes)
       </AuthLayout>
     </MetaData>
   );
@@ -62,21 +31,13 @@ export default Alert;
 
 export const getServerSideProps = withAuthSSR(async (options) => {
   const {
-<<<<<<< HEAD
-    userData: { user, orgId },
-=======
     userData: { user, userId, orgId },
     supabaseClient,
->>>>>>> d3ddd2aa (alert fixes)
   } = options;
 
   return {
     props: {
       user,
-<<<<<<< HEAD
-      orgId,
-=======
->>>>>>> d3ddd2aa (alert fixes)
     },
   };
 });
