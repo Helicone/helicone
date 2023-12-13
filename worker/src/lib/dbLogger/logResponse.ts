d981af35db8746c7d403a82e1242dba9530fc866
Justin <justintorre75@gmail.com>
2023-12-13 00:00:10 -0800

    killed prompt

--- worker/src/lib/dbLogger/logResponse.ts
+++ worker/src/lib/dbLogger/logResponse.ts
@@ -8,75 +8,6 @@ import { InsertQueue } from "./insertQueue";
 
 const MAX_USER_ID_LENGTH = 7000;
 
-async function getPromptId(
-  dbClient: SupabaseClient,
-  prompt: Prompt | ChatPrompt,
-  name: string | null,
-  providerAuthHash: string
-): Promise<Result<string, string>> {
-  // First, get the prompt id if there's a match in the prompt table
-  const { data, error } = await dbClient
-    .from("prompt")
-    .select("id")
-    .eq("auth_hash", providerAuthHash)
-    .eq("prompt", prompt.prompt)
-    .limit(1);
-  if (error !== null) {
-    return { data: null, error: error.message };
-  }
-  if (data !== null && data.length > 0) {
-    return { data: data[0].id, error: null };
-  } else {
-    let newPromptName;
-    if (name) {
-      newPromptName = name;
-    } else {
-      // First, query the database to find the highest prompt name suffix
-      const { data: highestSuffixData } = await dbClient
-        .from("prompt")
-        .select("name")
-        .order("name", { ascending: false })
-        .like("name", "Prompt (%)")
-        .eq("auth_hash", providerAuthHash)
-        .limit(1)
-        .single();
-
-      // Extract the highest suffix number from the highest prompt name suffix found
-      let highestSuffix = 0;
-      if (highestSuffixData) {
-        const matches = highestSuffixData.name.match(/\((\d+)\)/);
-        if (matches) {
-          highestSuffix = parseInt(matches[1]);
-        }
-      }
-
-      // Increment the highest suffix to get the new suffix for the new prompt name
-      const newSuffix = highestSuffix + 1;
-
-      // Construct the new prompt name with the new suffix
-      newPromptName = `Prompt (${newSuffix})`;
-    }
-
-    // If there's no match, insert the prompt and get the id
-    const { data, error } = await dbClient
-      .from("prompt")
-      .insert([
-        {
-          id: crypto.randomUUID(),
-          prompt: prompt.prompt,
-          name: newPromptName,
-          auth_hash: providerAuthHash,
-        },
-      ])
-      .select("id")
-      .single();
-    if (error !== null) {
-      return { data: null, error: error.message };
-    }
-    return { data: data.id, error: null };
-  }
-}
-
 // Replaces all the image_url that is not a url or not { url: url }  with
 // { unsupported_image: true }
 //
@@ -132,10 +63,6 @@ export async function logRequest(
   >
 > {
   try {
-    const prompt = request.promptFormatter?.prompt;
-
-    const prompt_values = prompt !== undefined ? prompt.values : null;
-
     if (!authParams.organizationId) {
       return { data: null, error: "Helicone organization not found" };
     }
@@ -180,7 +107,7 @@ export async function logRequest(
       prompt_id: request.promptId ?? null,
       properties: request.properties,
       formatted_prompt_id: null,
-      prompt_values: prompt_values,
+      prompt_values: null,
       helicone_user: authParams.userId ?? null,
       helicone_api_key_id: authParams.heliconeApiKeyId ?? null,
       helicone_org_id: authParams.organizationId,
