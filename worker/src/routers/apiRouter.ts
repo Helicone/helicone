a2f8bbca9dce71c0c970c565f9aad18d008ac8b4
colegottdank <colegottdank@gmail.com>
2023-12-14 17:45:43 -0500

    Alert init 2 (#1155)
    * Rewrite alerts
    
    * Add Github authentication support (#1144)
    
    * alert-history (#1147)
    
    * Add redirect options for Google and GitHub sign-in (#1151)
    
    * fixed frontend from false positive: (#1152)
    
    * fixed bug when the time map would be the same value and wrongly indexed (#1153)
    
    * fixed the company sizes to be 2-5 (#1154)
    
    * Feat users cost overtime and styling (#1119)
    
    * Add user cost over time chart
    
    * chore: fixed typo and grammer
    
    * fix: update styles and themes
    
    * feat: made commandbar theme responsive
    
    * fix: fixed more dark mode styling
    
    * chore: typo
    
    * feat: tabs added to userModal and themedTabs dark mode
    
    * Update UI styles in themedTableV5, upgradeProModal, orgPlanPage, and propertiesPage
    
    * Add valueFormatter to UserModal
    
    * Update text color in propertiesPage.tsx
    
    * Update tab colors to blue
    
    * Update imports and remove unused code
    
    * feat: optimize images (#1135)
    
    * Add status filter to alert query
    
    * Refactor AlertManager class and fix email sending
    bug
    
    * Add redirect options for Google and GitHub sign-in (#1151)
    
    * Feat users cost overtime and styling (#1119)
    
    * Add user cost over time chart
    
    * chore: fixed typo and grammer
    
    * fix: update styles and themes
    
    * feat: made commandbar theme responsive
    
    * fix: fixed more dark mode styling
    
    * chore: typo
    
    * feat: tabs added to userModal and themedTabs dark mode
    
    * Update UI styles in themedTableV5, upgradeProModal, orgPlanPage, and propertiesPage
    
    * Add valueFormatter to UserModal
    
    * Update text color in propertiesPage.tsx
    
    * Update tab colors to blue
    
    * Update imports and remove unused code
    
    * Remove Alerter class and update wrangler.toml
    
    * Remove unused import
    
    * Remove Alerter import and bindings
    
    * Refactor log method signature in DBLoggable class
    
    ---------
    
    Co-authored-by: Umut Hope YILDIRIM <umut475@gmail.com>
    Co-authored-by: Scott <50155740+ScottMktn@users.noreply.github.com>
    Co-authored-by: Justin Torre <justintorre75@gmail.com>

--- worker/src/routers/apiRouter.ts
+++ worker/src/routers/apiRouter.ts
@@ -14,7 +14,6 @@ import {
   HeliconeNode as HeliconeNode,
   validateHeliconeNode as validateHeliconeNode,
 } from "../lib/models/Tasks";
-import { Alerter } from "../db/Alerter";
 import { validateAlertCreate } from "../lib/validators/alertValidators";
 
 class InternalResponse {
@@ -452,13 +451,6 @@ export const getAPIRouter = (router: BaseRouter) => {
         return client.response.newError(alertError, 500);
       }
 
-      const alerter = new Alerter(env.ALERTER);
-      const { error: configError } = await alerter.upsertAlert(alertRow);
-
-      if (configError !== null) {
-        return client.response.newError(configError, 500);
-      }
-
       return client.response.successJSON({ ok: "true" }, true);
     }
   );
@@ -480,16 +472,6 @@ export const getAPIRouter = (router: BaseRouter) => {
 
       if (deleteErr) {
         return client.response.newError(deleteErr, 500);
-      }
-
-      const alerter = new Alerter(env.ALERTER);
-      const deleteRes = await alerter.deleteAlert(
-        id,
-        authParams.organizationId
-      );
-
-      if (deleteRes.error) {
-        return client.response.newError(deleteRes.error, 500);
       }
 
       return client.response.successJSON({ ok: "true" }, true);
