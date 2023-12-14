b6bc4d84c55ea1b09196a7fc8b17485ceb46b956
Umut Hope YILDIRIM <umut475@gmail.com>
2023-12-13 19:42:26 -0800

    Add redirect options for Google and GitHub sign-in (#1151)

--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -1,19 +1,13 @@
 import { User } from "@supabase/auth-helpers-react";
 import { init } from "commandbar";
+import { User } from "@supabase/auth-helpers-react";
+import { init } from "commandbar";
 import AuthLayout from "../components/shared/layout/authLayout";
 import MetaData from "../components/shared/metaData";
 import DashboardPage from "../components/templates/dashboard/dashboardPage";
 import { withAuthSSR } from "../lib/api/handlerWrappers";
 
-import { useEffect, useState } from "react";
-import { useOrg } from "../components/shared/layout/organizationContext";
-import { useRouter } from "next/router";
-import { useQuery } from "@tanstack/react-query";
-import { format } from "date-fns";
-import { Result } from "../lib/result";
-import { useGetAuthorized } from "../services/hooks/dashboard";
-import UpgradeProModal from "../components/shared/upgradeProModal";
-import { useTheme } from "../components/shared/theme/themeContext";
+import { useEffect } from "react";
 
 interface DashboardProps {
   user: User;
@@ -21,7 +15,6 @@ interface DashboardProps {
 
 const Dashboard = (props: DashboardProps) => {
   const { user } = props;
-  const theme = useTheme();
 
   useEffect(() => {
     if (!process.env.NEXT_PUBLIC_COMMAND_BAR_HELPHUB_0) return;
@@ -52,8 +45,18 @@ export default Dashboard;
 export const getServerSideProps = withAuthSSR(async (options) => {
   const {
     userData: { user, orgHasOnboarded },
+    userData: { user, orgHasOnboarded },
     supabaseClient,
   } = options;
+
+  if (!orgHasOnboarded) {
+    return {
+      redirect: {
+        destination: "/welcome",
+        permanent: false,
+      },
+    };
+  }
 
   if (!orgHasOnboarded) {
     return {
