9299958e7aa9eb40ebc2b6b1b3a98667a9266347
Justin Torre <justintorre75@gmail.com>
2023-12-16 01:48:31 -0800

    Aggregated stats gql count (#1167)

--- web/lib/api/graphql/client/gql.ts
+++ web/lib/api/graphql/client/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from "./graphql";
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,10 +13,8 @@ import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-  "\n  query FetchRuns($limit: Int, $offset: Int, $filters: [HeliconeJobFilter!]) {\n    heliconeJob(filters: $filters, offset: $offset, limit: $limit) {\n      id\n      name\n      description\n      status\n      created_at\n      updated_at\n      timeout_seconds\n      node_count\n      request_count\n      properties {\n        value\n        name\n      }\n    }\n  }\n":
-    types.FetchRunsDocument,
-  "\n  query FetchTasks(\n    $heliconeNodeId: String\n    $limit: Int\n    $offset: Int\n    $jobId: String\n    $filters: [HeliconeNodeFilter!]\n  ) {\n    heliconeNode(\n      id: $heliconeNodeId\n      limit: $limit\n      offset: $offset\n      job_id: $jobId\n      filters: $filters\n    ) {\n      id\n      name\n      description\n      created_at\n      updated_at\n      job_id\n      parent_node_ids\n      properties {\n        name\n        value\n      }\n    }\n  }\n":
-    types.FetchTasksDocument,
+    "\n  query FetchRuns($limit: Int, $offset: Int, $filters: [HeliconeJobFilter!]) {\n    heliconeJob(filters: $filters, offset: $offset, limit: $limit) {\n      id\n      name\n      description\n      status\n      created_at\n      updated_at\n      timeout_seconds\n      node_count\n      request_count\n      properties {\n        value\n        name\n      }\n    }\n  }\n": types.FetchRunsDocument,
+    "\n  query FetchTasks(\n    $heliconeNodeId: String\n    $limit: Int\n    $offset: Int\n    $jobId: String\n    $filters: [HeliconeNodeFilter!]\n  ) {\n    heliconeNode(\n      id: $heliconeNodeId\n      limit: $limit\n      offset: $offset\n      job_id: $jobId\n      filters: $filters\n    ) {\n      id\n      name\n      description\n      created_at\n      updated_at\n      job_id\n      parent_node_ids\n      properties {\n        name\n        value\n      }\n    }\n  }\n": types.FetchTasksDocument,
 };
 
 /**
@@ -36,19 +34,14 @@ export function gql(source: string): unknown;
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: "\n  query FetchRuns($limit: Int, $offset: Int, $filters: [HeliconeJobFilter!]) {\n    heliconeJob(filters: $filters, offset: $offset, limit: $limit) {\n      id\n      name\n      description\n      status\n      created_at\n      updated_at\n      timeout_seconds\n      node_count\n      request_count\n      properties {\n        value\n        name\n      }\n    }\n  }\n"
-): (typeof documents)["\n  query FetchRuns($limit: Int, $offset: Int, $filters: [HeliconeJobFilter!]) {\n    heliconeJob(filters: $filters, offset: $offset, limit: $limit) {\n      id\n      name\n      description\n      status\n      created_at\n      updated_at\n      timeout_seconds\n      node_count\n      request_count\n      properties {\n        value\n        name\n      }\n    }\n  }\n"];
+export function gql(source: "\n  query FetchRuns($limit: Int, $offset: Int, $filters: [HeliconeJobFilter!]) {\n    heliconeJob(filters: $filters, offset: $offset, limit: $limit) {\n      id\n      name\n      description\n      status\n      created_at\n      updated_at\n      timeout_seconds\n      node_count\n      request_count\n      properties {\n        value\n        name\n      }\n    }\n  }\n"): (typeof documents)["\n  query FetchRuns($limit: Int, $offset: Int, $filters: [HeliconeJobFilter!]) {\n    heliconeJob(filters: $filters, offset: $offset, limit: $limit) {\n      id\n      name\n      description\n      status\n      created_at\n      updated_at\n      timeout_seconds\n      node_count\n      request_count\n      properties {\n        value\n        name\n      }\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: "\n  query FetchTasks(\n    $heliconeNodeId: String\n    $limit: Int\n    $offset: Int\n    $jobId: String\n    $filters: [HeliconeNodeFilter!]\n  ) {\n    heliconeNode(\n      id: $heliconeNodeId\n      limit: $limit\n      offset: $offset\n      job_id: $jobId\n      filters: $filters\n    ) {\n      id\n      name\n      description\n      created_at\n      updated_at\n      job_id\n      parent_node_ids\n      properties {\n        name\n        value\n      }\n    }\n  }\n"
-): (typeof documents)["\n  query FetchTasks(\n    $heliconeNodeId: String\n    $limit: Int\n    $offset: Int\n    $jobId: String\n    $filters: [HeliconeNodeFilter!]\n  ) {\n    heliconeNode(\n      id: $heliconeNodeId\n      limit: $limit\n      offset: $offset\n      job_id: $jobId\n      filters: $filters\n    ) {\n      id\n      name\n      description\n      created_at\n      updated_at\n      job_id\n      parent_node_ids\n      properties {\n        name\n        value\n      }\n    }\n  }\n"];
+export function gql(source: "\n  query FetchTasks(\n    $heliconeNodeId: String\n    $limit: Int\n    $offset: Int\n    $jobId: String\n    $filters: [HeliconeNodeFilter!]\n  ) {\n    heliconeNode(\n      id: $heliconeNodeId\n      limit: $limit\n      offset: $offset\n      job_id: $jobId\n      filters: $filters\n    ) {\n      id\n      name\n      description\n      created_at\n      updated_at\n      job_id\n      parent_node_ids\n      properties {\n        name\n        value\n      }\n    }\n  }\n"): (typeof documents)["\n  query FetchTasks(\n    $heliconeNodeId: String\n    $limit: Int\n    $offset: Int\n    $jobId: String\n    $filters: [HeliconeNodeFilter!]\n  ) {\n    heliconeNode(\n      id: $heliconeNodeId\n      limit: $limit\n      offset: $offset\n      job_id: $jobId\n      filters: $filters\n    ) {\n      id\n      name\n      description\n      created_at\n      updated_at\n      job_id\n      parent_node_ids\n      properties {\n        name\n        value\n      }\n    }\n  }\n"];
 
 export function gql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
