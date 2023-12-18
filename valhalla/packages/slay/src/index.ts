95eda50a5ae79d0f767f13bbd337afcd2e7d05ed
Justin <justintorre75@gmail.com>
2023-12-18 14:37:38 -0500

    ?

--- valhalla/packages/slay/src/index.ts
+++ valhalla/packages/slay/src/index.ts
@@ -1,7 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import cors from "cors";
 import express from "express";
 import { readFileSync } from "fs";
 import http from "http";
