487a0563d02d38afb0f4c7403f84faade569156c
colegottdank <colegottdank@gmail.com>
2023-12-21 11:50:44 -0500

    Refactor alertValidators.ts to improve code readability and add additional validation checks

--- worker/src/lib/validators/alertValidators.ts
+++ worker/src/lib/validators/alertValidators.ts
@@ -23,18 +23,35 @@ export function validateAlertCreate(
   // Validating each field
   if (alert.created_at && !Date.parse(alert.created_at))
     return { data: null, error: "Invalid created_at date" };
+
   if (alert.updated_at && !Date.parse(alert.updated_at))
     return { data: null, error: "Invalid updated_at date" };
+
   if (!alert.name) return { data: null, error: "Name is required" };
+
   if (!alert.org_id) return { data: null, error: "org_id is required" };
-  if (typeof alert.threshold !== "number" || alert.threshold < 0)
+
+  if (typeof alert.threshold !== "number" || alert.threshold <= 0)
     return { data: null, error: "Invalid threshold" };
+
+  if (alert.metric === "cost" && alert.threshold < 0.01)
+    return { data: null, error: "Invalid threshold" };
+
+  if (
+    alert.metric === "response.status" &&
+    (alert.threshold > 100 || alert.threshold <= 0)
+  )
+    return { data: null, error: "Invalid threshold" };
+
   if (!isValidTimePeriod(alert.time_window))
     return { data: null, error: "Invalid time_window" };
+
   if (alert.metric !== "response.status" && alert.metric !== "cost")
     return { data: null, error: "Invalid metric" };
+
   if (!isValidEmailArray(alert.emails))
     return { data: null, error: "Invalid emails" };
+
   if (alert.status !== "resolved")
     return { data: null, error: "Invalid status" };
 
