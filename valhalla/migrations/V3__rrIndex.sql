2e956f1ed8c2fb4c610280249ce58983b2fa74ec
Justin Torre <justintorre75@gmail.com>
2023-12-20 21:14:14 -0800

    added index (#1188)

---
+++ valhalla/migrations/V3__rrIndex.sql
@@ -0,0 +1,2 @@
+create index if not exists response_id_idx on public.response using btree (id) tablespace pg_default;
+create index if not exists request_id_idx on public.request using btree (id) tablespace pg_default;
