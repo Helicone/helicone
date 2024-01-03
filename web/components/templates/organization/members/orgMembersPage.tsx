e55047af04d71b7303a758f4007f682aa422f55e
Scott Nguyen <scott@helicone.ai>
2024-01-03 12:06:34 -0500

    fix

--- web/components/templates/organization/members/orgMembersPage.tsx
+++ web/components/templates/organization/members/orgMembersPage.tsx
@@ -44,10 +44,10 @@ const OrgMembersPage = (props: OrgMembersPageProps) => {
     ? data?.data
         .filter((d) => {
           // if the org is a customer org, remove all "owner" roles UNLESS the user is the owner
-          if (
-            orgContext?.currentOrg.owner !== user?.id &&
-            orgContext?.currentOrg.organization_type === "customer"
-          ) {
+          if (orgContext?.currentOrg.owner === user?.id) {
+            return true;
+          }
+          if (orgContext?.currentOrg.organization_type === "customer") {
             return d.org_role !== "owner";
           }
         })
