512352d04ba9571851c5d28fa97152ba6541939f
Justin Torre <justintorre75@gmail.com>
2024-01-05 15:17:58 -0800

    Customer portal changes 3 (#1233)

--- web/components/shared/layout/authLayout.tsx
+++ web/components/shared/layout/authLayout.tsx
@@ -393,28 +393,26 @@ const AuthLayout = (props: AuthLayoutProps) => {
                   )}
                 >
                   {(org?.currentOrg?.organization_type === "reseller" ||
-                    org?.currentOrg?.organization_type === "customer") &&
-                    org?.currentOrg?.owner === user.id && (
-                      <div className="flex w-full">
-                        <button
-                          onClick={() => {
-                            router.push("/enterprise/portal");
-                            if (
-                              org.currentOrg?.organization_type ===
-                                "customer" &&
-                              org.currentOrg?.reseller_id
-                            ) {
-                              org.setCurrentOrg(org.currentOrg.reseller_id);
-                            }
-                          }}
-                          className="border border-gray-300 dark:border-gray-700 dark:text-white w-full flex text-black px-4 py-1 text-sm font-medium items-center text-center justify-center mx-4 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800 rounded-md"
-                        >
-                          {org.currentOrg?.organization_type === "customer"
-                            ? "Edit Customer"
-                            : "Customer Portal"}
-                        </button>
-                      </div>
-                    )}
+                    org?.isResellerOfCurrentCustomerOrg) && (
+                    <div className="flex w-full">
+                      <button
+                        onClick={() => {
+                          router.push("/enterprise/portal");
+                          if (
+                            org.currentOrg?.organization_type === "customer" &&
+                            org.currentOrg?.reseller_id
+                          ) {
+                            org.setCurrentOrg(org.currentOrg.reseller_id);
+                          }
+                        }}
+                        className="border border-gray-300 dark:border-gray-700 dark:text-white w-full flex text-black px-4 py-1 text-sm font-medium items-center text-center justify-center mx-4 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800 rounded-md"
+                      >
+                        {org.currentOrg?.organization_type === "customer"
+                          ? "Edit Customer"
+                          : "Customer Portal"}
+                      </button>
+                    </div>
+                  )}
                   <nav className="flex-1 space-y-6 px-2 pb-4 pt-2">
                     <div className="flex flex-col space-y-1">
                       {navigation.map((item, idx) => {
