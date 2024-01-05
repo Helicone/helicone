9b86234331432ff43dff0f9e6f397173382de2e1
Scott <50155740+ScottMktn@users.noreply.github.com>
2024-01-05 11:40:22 -0800

    onboarding-ensure (#1227)

--- web/services/hooks/organizations.tsx
+++ web/services/hooks/organizations.tsx
@@ -78,7 +78,7 @@ const useGetOrgs = () => {
         return [];
       }
       if (!data.find((d) => d.is_personal)) {
-        // await supabaseClient.rpc("ensure_personal");
+        await supabaseClient.rpc("ensure_personal");
         // console.warn("Created personal org");
         // just a shim that will only execute once for the entire life time of a user
         return (await supabaseClient.from("organization").select(`*`)).data!;
