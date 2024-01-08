487a1b9eade74bbd1c0ec36e54309f0da5e1edab
Scott Nguyen <scott@helicone.ai>
2024-01-08 12:01:34 -0500

    org-seats

---
+++ web/components/templates/home/redirectingScreen.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { clsx } from "../../shared/clsx";
+import LoadingAnimation from "../../shared/loadingAnimation";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import Link from "next/link";
+
+interface RedirectingScreenProps {}
+
+const RedirectingScreen = (props: RedirectingScreenProps) => {
+  const {} = props;
+
+  const [loaded, setLoaded] = useState(false);
+
+  const client = useSupabaseClient();
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 2000); // delay of 2s
+    return () => clearTimeout(timer); // this will clear Timeout
+    // when component unmount like in willComponentUnmount
+  }, []);
+
+  return (
+    <div className="h-screen flex flex-col items-center gap-4 justify-center">
+      <LoadingAnimation title="Redirecting you to your dashboard..." />
+      <div
+        className={clsx(
+          `transition-all duration-700 ease-in-out ${
+            loaded ? "opacity-100" : "opacity-0"
+          }`,
+          "flex flex-col items-center text-center w-full px-2"
+        )}
+      >
+        <p className="text-xl md:text-3xl font-semibold mt-8">
+          Having trouble? Try signing out and signing back in.
+        </p>
+        <p className="text-md md:text-lg text-gray-700 font-light mt-4">
+          Or, get in contact with us via our{" "}
+          <Link href="https://discord.gg/2jZ6V7Yx" className="text-blue-500">
+            Discord
+          </Link>{" "}
+          or email us at{" "}
+          <Link href="mailto:support@helicone.ai" className="text-blue-500">
+            support@helicone.ai
+          </Link>
+        </p>
+        <button
+          onClick={() => {
+            client.auth.signOut();
+          }}
+          className="px-28 py-3 bg-gray-900 hover:bg-gray-700 font-medium text-white rounded-xl mt-8"
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RedirectingScreen;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -7,47 +7,16 @@ import { DEMO_EMAIL } from "../lib/constants";
 import { SupabaseServerWrapper } from "../lib/wrappers/supabase";
 import HomePageV2 from "../components/templates/home/homePageV2";
 import Head from "next/head";
+import RedirectingScreen from "../components/templates/home/redirectingScreen";
 
 export const Home = () => {
   const router = useRouter();
 
   const user = useUser();
-  const client = useSupabaseClient();
+
   if (user && user.email !== DEMO_EMAIL) {
     router.push("/dashboard");
-    return (
-      <div className="h-screen justify-center items-center flex">
-        <div className="flex flex-col items-center gap-5">
-          <LoadingAnimation title="Redirecting you to your dashboard..." />
-          <div>Stuck here? Try signing out and back in</div>
-          <button
-            onClick={() => {
-              client.auth.signOut();
-            }}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32"
-          >
-            Sign out
-          </button>
-
-          <div>
-            Still stuck here?{" "}
-            <a
-              href="https://discord.gg/2jZ6V7Yx"
-              className="text-blue-500 hover:text-blue-700"
-            >
-              Join our Discord
-            </a>{" "}
-            or email us at{" "}
-            <a
-              href="mailto:support@helicone.ai"
-              className="text-blue-500 hover:text-blue-700"
-            >
-              support@helicone.ai
-            </a>
-          </div>
-        </div>
-      </div>
-    );
+    return <RedirectingScreen />;
   }
 
   return (
