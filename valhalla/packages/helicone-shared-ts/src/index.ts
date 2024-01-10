1ac0a2b92a6badc66619960654afc4cc02e73c5a
Justin Torre <justintorre75@gmail.com>
2024-01-10 12:05:46 -0800

    Valhalla read fixed (#1259)

--- valhalla/packages/helicone-shared-ts/src/index.ts
+++ valhalla/packages/helicone-shared-ts/src/index.ts
@@ -2,3 +2,5 @@ export { createValhallaClient } from "./db/valhalla";
 export { withDB } from "./routers/withDB";
 export { withAuth } from "./routers/withAuth";
 export { IRouterWrapperDB, IRouterWrapper } from "./routers/iRouterWrapper";
+export { Result, ok, err } from "./modules/result";
+export * from "./shared/request/request";
