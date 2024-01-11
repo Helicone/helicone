9cd29b975ad6cb7490d670da9add6503cc35ed2f
Scott <50155740+ScottMktn@users.noreply.github.com>
2024-01-11 14:43:09 -0800

    playground-persistence (#1263)

--- web/services/hooks/requests.tsx
+++ web/services/hooks/requests.tsx
@@ -29,6 +29,7 @@ const useGetRequest = (requestId: string) => {
         }),
       }).then((res) => res.json() as Promise<Result<HeliconeRequest, string>>);
     },
+    refetchOnWindowFocus: false,
   });
   return {
     request: data?.data,
