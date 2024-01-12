f0678578d0ccb5226be0ca78038c7fc9c43980c3
colegottdank <colegottdank@gmail.com>
2024-01-12 11:52:27 -0500

    Add index on request_id in cache_hits table

--- supabase/migrations/20240111205509_cache_hits_update.sql
+++ supabase/migrations/20240111205509_cache_hits_update.sql
@@ -1,5 +1,4 @@
 -- Create an index on request_id if it doesn't exist
-CREATE INDEX CONCURRENTLY IF NOT EXISTS cache_hits_created_at_desc_idx ON public.cache_hits USING btree (created_at DESC);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS cache_hits_request_id_idx ON public.cache_hits USING btree (request_id);
 ALTER TABLE IF EXISTS public.cache_hits
 ADD COLUMN IF NOT EXISTS organization_id uuid;
