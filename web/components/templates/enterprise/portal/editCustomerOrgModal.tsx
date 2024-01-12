d2b9376b1d1225726b702ab7f156a419b8a6481f
Justin Torre <justintorre75@gmail.com>
2024-01-12 13:13:10 -0800

    allow org deletes and added name check for confirmation (#1238)

---
+++ web/components/templates/enterprise/portal/editCustomerOrgModal.tsx
@@ -0,0 +1,40 @@
+import ThemedDrawer from "../../../shared/themed/themedDrawer";
+import CreateOrgForm, { OrgLimits } from "../../organization/createOrgForm";
+
+interface EditCustomerOrgModalProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  onSuccess: () => void;
+  initialValues?: {
+    id: string;
+    name: string;
+    color: string | null;
+    icon: string | null;
+    providerKey: string | null;
+    limits?: OrgLimits;
+  };
+}
+
+const EditCustomerOrgModal = (props: EditCustomerOrgModalProps) => {
+  const { open, setOpen, onSuccess, initialValues } = props;
+
+  return (
+    <ThemedDrawer open={open} setOpen={setOpen}>
+      <div className="flex flex-col space-y-4">
+        <p className="text-2xl font-semibold text-black dark:text-white border-b border-gray-300 dark:border-gray-700 py-4">
+          Edit Customer
+        </p>
+        <CreateOrgForm
+          variant="reseller"
+          onSuccess={() => {
+            setOpen(false);
+            onSuccess();
+          }}
+          initialValues={initialValues}
+        />
+      </div>
+    </ThemedDrawer>
+  );
+};
+
+export default EditCustomerOrgModal;
