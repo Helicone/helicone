ff32b167c4d6b97c8a09093675e4df5e0a2866a9
Justin <justintorre75@gmail.com>
2024-01-13 14:48:26 -0800

    yay

---
+++ worker/src/api/lib/apiClient.ts
@@ -0,0 +1,84 @@
+import { createClient } from "@supabase/supabase-js";
+import { Env } from "../..";
+import { Database, Json } from "../../../supabase/database.types";
+import { DBWrapper, HeliconeAuth } from "../../db/DBWrapper";
+import { RequestWrapper } from "../../lib/RequestWrapper";
+import { ClickhouseClientWrapper } from "../../lib/db/clickhouse";
+import { Valhalla } from "../../lib/db/valhalla";
+import { InsertQueue } from "../../lib/dbLogger/insertQueue";
+import { Job, isValidStatus, validateRun } from "../../lib/models/Runs";
+import { HeliconeNode, validateHeliconeNode } from "../../lib/models/Tasks";
+import { validateAlertCreate } from "../../lib/validators/alertValidators";
+
+import { OpenAPIRouterType } from "@cloudflare/itty-router-openapi";
+import { Route } from "itty-router";
+import { CustomersGet } from "../../api/routes/customer-portal/customers/get";
+import { logAsync } from "../../api/helpers/logAsync";
+
+class InternalResponse {
+  constructor(private client: APIClient) {}
+
+  newError(message: string, status: number): Response {
+    console.error(`Response Error: `, message);
+    return new Response(JSON.stringify({ error: message }), { status });
+  }
+
+  successJSON(data: unknown, enableCors = false): Response {
+    if (enableCors) {
+      return new Response(JSON.stringify(data), {
+        status: 200,
+        headers: {
+          "content-type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "PUT",
+          "Access-Control-Allow-Headers":
+            "Content-Type, helicone-jwt, helicone-org-id",
+        },
+      });
+    }
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    });
+  }
+
+  unauthorized(): Response {
+    return this.newError("Unauthorized", 401);
+  }
+}
+
+class APIClient {
+  public queue: InsertQueue;
+  public response: InternalResponse;
+  db: DBWrapper;
+  private heliconeApiKeyRow?: Database["public"]["Tables"]["helicone_api_keys"]["Row"];
+
+  constructor(
+    private env: Env,
+    private requestWrapper: RequestWrapper,
+    auth: HeliconeAuth
+  ) {
+    this.response = new InternalResponse(this);
+    this.db = new DBWrapper(env, auth);
+    this.queue = new InsertQueue(
+      createClient<Database>(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY),
+      new Valhalla(env.VALHALLA_URL, auth),
+      new ClickhouseClientWrapper(env),
+      env.FALLBACK_QUEUE,
+      env.REQUEST_AND_RESPONSE_QUEUE_KV
+    );
+  }
+}
+
+export async function createAPIClient(
+  env: Env,
+  requestWrapper: RequestWrapper
+) {
+  const auth = await requestWrapper.auth();
+  if (auth.error !== null) {
+    throw new Error(auth.error);
+  }
+  return new APIClient(env, requestWrapper, auth.data);
+}
---
+++ worker/src/api/routes/customer-portal/customers/get.ts
@@ -0,0 +1,73 @@
+import { validateAlertCreate } from "../../../../lib/validators/alertValidators";
+
+import { z } from "zod";
+
+import {
+  OpenAPIRoute,
+  Path,
+  Str,
+  DateOnly,
+  OpenAPIRouterType,
+  Obj,
+} from "@cloudflare/itty-router-openapi";
+import { IRequest, Route, RouteHandler } from "itty-router";
+import { RequestWrapper } from "../../../../lib/RequestWrapper";
+import { Env, Provider } from "../../../..";
+
+const BodyOpenAPI = z.object({
+  name: z.string(),
+  description: z.string().or(z.string().array()),
+  steps: z.string().array(),
+});
+
+type BodyType = z.infer<typeof BodyOpenAPI>;
+
+export class CustomersGet extends OpenAPIRoute<
+  IRequest,
+  [RequestWrapper, Env, ExecutionContext, Provider]
+> {
+  static schema = {
+    tags: ["Tasks"],
+    summary: "Get a single Task by slug",
+    requestBody: BodyOpenAPI,
+    parameters: {
+      taskSlug: Path(Str, {
+        description: "Task slug",
+      }),
+    },
+    responses: {
+      "200": {
+        description: "Task fetched successfully",
+        schema: {
+          metaData: {},
+          task: BodyOpenAPI,
+        },
+      },
+    },
+  };
+
+  async handle(
+    _: unknown,
+    requestWrapper: RequestWrapper,
+    env: Env,
+    _ctx: ExecutionContext
+  ) {
+    type User = z.infer<typeof BodyOpenAPI>;
+
+    // Retrieve the validated slug
+    // const { taskSlug } = requestWrapper.getJson<BodyType>();
+
+    // Actually fetch a task using the taskSlug
+
+    return {
+      metaData: { meta: "data" },
+      task: {
+        name: "my task",
+        // slug: taskSlug,
+        description: "this needs to be done",
+        completed: false,
+        // due_date: new Date().toISOString().slice(0, 10),
+      },
+    };
+  }
+}
