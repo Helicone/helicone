7cd7d41c8a691abb57dd18460b25359dc31921bc
Justin Torre <justintorre75@gmail.com>
2024-01-16 00:56:43 -0800

    Customer portal deletes and creates (#1285)

---
+++ web/lib/api/organization/hasAccess.ts
@@ -0,0 +1,45 @@
+import { supabaseServer } from "../../supabaseServer";
+import { dbExecute } from "../db/dbExecute";
+
+async function _checkAccessToOrg(
+  orgId: string,
+  userId: string
+): Promise<boolean> {
+  const query = `
+  select * from organization_member om
+  where om.organization = $1 and (om.member = $2) and (om.org_role = 'admin' or om.org_role = 'owner')
+`;
+
+  const { data, error } = await dbExecute<{
+    email: string;
+    member: string;
+    org_role: string;
+  }>(query, [orgId, userId]);
+
+  return error === null && data?.length > 0;
+}
+
+export async function checkAccessToMutateOrg(
+  orgId: string,
+  userId: string
+): Promise<boolean> {
+  const orgToCheck = await supabaseServer
+    .from("organization")
+    .select("*")
+    .eq("id", orgId)
+    .single();
+
+  if (!orgToCheck.data || orgToCheck.error !== null) {
+    return false;
+  }
+  if (await _checkAccessToOrg(orgId as string, userId)) {
+    return true;
+  } else if (
+    orgToCheck.data.reseller_id &&
+    (await _checkAccessToOrg(orgToCheck.data.reseller_id as string, userId))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
