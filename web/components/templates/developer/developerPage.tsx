4815bfc18a79dfc0a8123f393f3f911be3f07196
Scott <50155740+ScottMktn@users.noreply.github.com>
2024-01-16 14:57:17 -0800

    dev conditionals (#1294)
    Co-authored-by: Scott Nguyen <scott@helicone.ai>

--- web/components/templates/developer/developerPage.tsx
+++ web/components/templates/developer/developerPage.tsx
@@ -12,6 +12,7 @@ import WebhooksPage from "../webhooks/webhooksPage";
 import { useUser } from "@supabase/auth-helpers-react";
 import VaultPage from "../vault/vaultPage";
 import { useFeatureFlags } from "../../../services/hooks/featureFlags";
+import Link from "next/link";
 
 interface DeveloperPageProps {}
 
@@ -31,43 +32,6 @@ const DeveloperPage = (props: DeveloperPageProps) => {
 
   const isPaidPlan = tier === "pro" || tier === "enterprise";
 
-  const renderConditionalTabs = () => {
-    if (hasFlag && isPaidPlan) {
-      return (
-        <>
-          <TabPanel>
-            <div className="p-4">
-              <WebhooksPage user={user!} />
-            </div>
-          </TabPanel>{" "}
-          <TabPanel>
-            <VaultPage />
-          </TabPanel>
-        </>
-      );
-    }
-    if (hasFlag) {
-      return (
-        <>
-          <TabPanel>
-            <div className="p-4">
-              <WebhooksPage user={user!} />
-            </div>
-          </TabPanel>
-        </>
-      );
-    }
-    if (isPaidPlan) {
-      return (
-        <>
-          <TabPanel>
-            <VaultPage />
-          </TabPanel>
-        </>
-      );
-    }
-  };
-
   return (
     <div className="flex flex-col space-y-4">
       <div className="flex flex-row items-center justify-between">
@@ -79,8 +43,8 @@ const DeveloperPage = (props: DeveloperPageProps) => {
         <TabList className="font-semibold" variant="line">
           <Tab icon={KeyIcon}>Keys</Tab>
           <Tab icon={CodeBracketSquareIcon}>GraphQL</Tab>
-          {hasFlag ? <Tab icon={GlobeAltIcon}>Webhooks</Tab> : <></>}
-          {isPaidPlan ? <Tab icon={LockClosedIcon}>Vault</Tab> : <></>}
+          <Tab icon={GlobeAltIcon}>Webhooks</Tab>
+          <Tab icon={LockClosedIcon}>Vault</Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
@@ -91,7 +55,56 @@ const DeveloperPage = (props: DeveloperPageProps) => {
               <GraphQLPage />
             </div>
           </TabPanel>
-          {renderConditionalTabs()}
+          <TabPanel>
+            <div className="p-4">
+              {hasFlag ? (
+                <WebhooksPage user={user!} />
+              ) : (
+                <div className="flex flex-col w-full h-96 justify-center items-center">
+                  <div className="flex flex-col w-2/5">
+                    <GlobeAltIcon className="h-12 w-12 text-black dark:text-white border border-gray-300 dark:border-gray-700 bg-white dark:bg-black p-2 rounded-lg" />
+                    <p className="text-xl text-black dark:text-white font-semibold mt-8">
+                      Webhooks are an enterprise feature
+                    </p>
+                    <p className="text-sm text-gray-500 max-w-sm mt-2">
+                      Please reach out to us at{" "}
+                      <Link
+                        href="mailto:sales@helicone.ai"
+                        className="underline text-blue-500"
+                      >
+                        sales@helicone.ai
+                      </Link>{" "}
+                      to get access to this feature.
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            {isPaidPlan ? (
+              <VaultPage />
+            ) : (
+              <div className="flex flex-col w-full h-96 justify-center items-center">
+                <div className="flex flex-col w-2/5">
+                  <GlobeAltIcon className="h-12 w-12 text-black dark:text-white border border-gray-300 dark:border-gray-700 bg-white dark:bg-black p-2 rounded-lg" />
+                  <p className="text-xl text-black dark:text-white font-semibold mt-8">
+                    Vault is an enterprise feature
+                  </p>
+                  <p className="text-sm text-gray-500 max-w-sm mt-2">
+                    Please reach out to us at{" "}
+                    <Link
+                      href="mailto:sales@helicone.ai"
+                      className="underline text-blue-500"
+                    >
+                      sales@helicone.ai
+                    </Link>{" "}
+                    to get access to this feature.
+                  </p>
+                </div>
+              </div>
+            )}
+          </TabPanel>
         </TabPanels>
       </TabGroup>
     </div>
