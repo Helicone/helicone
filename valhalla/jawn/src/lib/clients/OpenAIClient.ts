b1593fffd98440bd435b6487c8193587dfb4825d
Justin Torre <justintorre75@gmail.com>
2024-01-15 17:15:49 -0800

    Fine tune that jawn (#1281)

---
+++ valhalla/jawn/src/lib/clients/OpenAIClient.ts
@@ -0,0 +1,39 @@
+// Remove the import statement for "fs"
+import { OpenAI } from "openai";
+import { Result, ok } from "../shared/result";
+import { FileObject } from "openai/resources";
+import { FineTuningJob } from "openai/resources/fine-tuning/jobs";
+import fs from "fs";
+
+export class OpenAIClient {
+  private openai: OpenAI;
+  constructor(apiKey: string) {
+    this.openai = new OpenAI({
+      apiKey: apiKey,
+    });
+  }
+
+  async createFineTuneJob(
+    fileId: string,
+    model: string
+  ): Promise<Result<FineTuningJob, string>> {
+    const fineTune = await this.openai.fineTuning.jobs.create({
+      training_file: fileId,
+      model: model,
+    });
+
+    return ok(fineTune);
+  }
+
+  // TODO: Limit to 1 gb
+  async uploadFineTuneFile(
+    trainingFilePath: string
+  ): Promise<Result<FileObject, string>> {
+    const file = await this.openai.files.create({
+      file: fs.createReadStream(trainingFilePath),
+      purpose: "fine-tune",
+    });
+
+    return ok(file);
+  }
+}
