75af78fe0ac2551f2c0a75b4699da1d3c10e9521
Justin Torre <justintorre75@gmail.com>
2024-01-18 14:45:17 -0800

    added finetuning logs with Posthog (#1307)

--- worker/src/lib/db/valhalla.ts
+++ worker/src/lib/db/valhalla.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HeliconeAuth } from "../../db/DBWrapper";
-import { Result, err, ok } from "../../results";
+import { Result } from "../../results";
 import { paths } from "./valhalla.types";
 
 type PostMethods<T> = {
