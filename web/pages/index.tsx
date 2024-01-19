3e16bf4d72d24fe1673bc3a049694d08e9e3b3d4
Justin Torre <justintorre75@gmail.com>
2024-01-19 13:34:28 -0800

    added redirect and getting logo paths from customer domains (#1315)

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -7,6 +7,7 @@ import { SupabaseServerWrapper } from "../lib/wrappers/supabase";
 import HomePageV2 from "../components/templates/home/homePageV2";
 import Head from "next/head";
 import RedirectingScreen from "../components/templates/home/redirectingScreen";
+import { isCustomerDomain } from "../lib/customerPortalHelpers";
 
 export const Home = () => {
   const router = useRouter();
@@ -52,6 +53,15 @@ export default Home;
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
+  if (isCustomerDomain(context.req.headers.host ?? "")) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
   const supabase = new SupabaseServerWrapper(context).getClient();
   const {
     data: { session },
