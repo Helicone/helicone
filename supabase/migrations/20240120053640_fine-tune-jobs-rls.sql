fa81115fdd399d611ec038bede2d977da7805a2b
Justin <justintorre75@gmail.com>
2024-01-19 21:36:56 -0800

    rls migrationsg

---
+++ supabase/migrations/20240120053640_fine-tune-jobs-rls.sql
@@ -0,0 +1,33 @@
+drop index if exists "public"."finetune_dataset_data_oir_idx";
+
+create policy "org_members_can_select_finetune_dataset"
+on "public"."finetune_dataset"
+as permissive
+for select
+to authenticated
+using ((EXISTS ( SELECT 1
+   FROM organization
+  WHERE (organization.id = finetune_dataset.organization_id))));
+
+
+create policy "org_members_can_finetune_dataset_data"
+on "public"."finetune_dataset_data"
+as permissive
+for select
+to authenticated
+using ((EXISTS ( SELECT 1
+   FROM organization
+  WHERE (organization.id = finetune_dataset_data.organization_id))));
+
+
+create policy "org_members_can_select_fine_tune_job"
+on "public"."finetune_job"
+as permissive
+for select
+to authenticated
+using ((EXISTS ( SELECT 1
+   FROM organization
+  WHERE (organization.id = finetune_job.organization_id))));
+
+
+
