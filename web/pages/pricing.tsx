f684b6d05c61050cedfa7f1b3356c95607052dd2
Scott <50155740+ScottMktn@users.noreply.github.com>
2024-01-26 01:54:16 -0800

    seo-2 (#1356)

--- web/pages/pricing.tsx
+++ web/pages/pricing.tsx
@@ -1,11 +1,36 @@
-import MetaData from "../components/shared/metaData";
+import Head from "next/head";
 import PricingPage from "../components/templates/pricing/pricingPage";
 
 const Pricing = () => {
   return (
-    <MetaData title="Pricing">
+    <>
+      <Head>
+        <title>{`Helicone - The easiest way to monitor your LLM-application at scale`}</title>
+        <link rel="icon" href="/static/helicone-logo.png" />
+        <meta property="og:title" content={"Helicone"} />
+        <meta content="https://helicone.ai" property="og:url" />
+        <meta
+          name="description"
+          content="Pricing as simple as our code integration."
+        />
+        <meta
+          property="og:description"
+          content="Pricing as simple as our code integration."
+        />
+        <meta
+          property="og:image"
+          content={"https://www.helicone.ai/static/helicone-pricing.png"}
+        />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          content="https://www.helicone.ai/static/helicone-pricing.png"
+        />
+      </Head>
       <PricingPage />
-    </MetaData>
+    </>
   );
 };
 
