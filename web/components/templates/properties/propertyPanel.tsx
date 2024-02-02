de3281e214018f1594eebb3dd5a40b20a2b429f1
Scott Nguyen <scott@helicone.ai>
2024-02-02 14:22:01 -0800

    loading states

--- web/components/templates/properties/propertyPanel.tsx
+++ web/components/templates/properties/propertyPanel.tsx
@@ -32,6 +32,7 @@ import {
 } from "../../../lib/timeCalculations/time";
 import ExportButton from "../../shared/themed/table/exportButton";
 import useSearchParams from "../../shared/utils/useSearchParams";
+import LoadingAnimation from "../../shared/loadingAnimation";
 
 interface PropertyPanelProps {
   property: string;
@@ -64,11 +65,12 @@ const PropertyPanel = (props: PropertyPanelProps) => {
     end: new Date(),
   });
 
-  const { keyMetrics, valueMetrics, refetch, isRefetching } = usePropertyCard({
-    timeFilter,
-    property,
-    limit: showMore ? 100 : 11,
-  });
+  const { keyMetrics, valueMetrics, refetch, isRefetching, isAnyLoading } =
+    usePropertyCard({
+      timeFilter,
+      property,
+      limit: showMore ? 100 : 11,
+    });
 
   const {
     properties,
@@ -164,69 +166,78 @@ const PropertyPanel = (props: PropertyPanelProps) => {
               <CurrencyDollarIcon className="h-6 w-6 text-sky-500" />
               <div className="flex flex-col">
                 <dt className="text-gray-500 text-sm">Cost</dt>
-
-                <dd className="text-gray-900 dark:text-gray-100 text-xl font-semibold">
-                  {keyMetrics.totalCost.data?.data
-                    ? `$${keyMetrics.totalCost.data?.data.toFixed(5)}`
-                    : "$0.00"}
-                </dd>
+                {isAnyLoading ? (
+                  <div className="animate-pulse h-7 w-24 bg-gray-200 dark:bg-gray-800 rounded-lg" />
+                ) : (
+                  <dd className="text-gray-900 dark:text-gray-100 text-xl font-semibold">
+                    {keyMetrics.totalCost.data?.data
+                      ? `$${keyMetrics.totalCost.data?.data.toFixed(5)}`
+                      : "$0.00"}
+                  </dd>
+                )}
               </div>
             </li>
             <li className="w-full border border-gray-300 bg-white p-4 flex flex-row rounded-lg items-center gap-4">
               <TableCellsIcon className="h-6 w-6 text-pink-500" />
               <div className="flex flex-col">
                 <dt className="text-gray-500 text-sm">Requests</dt>
-
-                <dd className="text-gray-900 dark:text-gray-100 text-xl font-semibold">
-                  {+(keyMetrics.totalRequests?.data?.data?.toFixed(2) ?? 0)}
-                </dd>
+                {isAnyLoading ? (
+                  <div className="animate-pulse h-7 w-24 bg-gray-200 dark:bg-gray-800 rounded-lg" />
+                ) : (
+                  <dd className="text-gray-900 dark:text-gray-100 text-xl font-semibold">
+                    {+(keyMetrics.totalRequests?.data?.data?.toFixed(2) ?? 0)}
+                  </dd>
+                )}
               </div>
             </li>{" "}
             <li className="w-full border border-gray-300 bg-white p-4 flex flex-row rounded-lg items-center gap-4">
               <ClockIcon className="h-6 w-6 text-violet-500" />
               <div className="flex flex-col">
                 <dt className="text-gray-500 text-sm">Average Latency / Req</dt>
-
-                <dd className="text-gray-900 dark:text-gray-100 text-xl font-semibold">
-                  {keyMetrics.averageLatency.data?.data
-                    ? (keyMetrics.averageLatency.data.data / 1000).toFixed(2)
-                    : "n/a"}
-                </dd>
+                {isAnyLoading ? (
+                  <div className="animate-pulse h-7 w-24 bg-gray-200 dark:bg-gray-800 rounded-lg" />
+                ) : (
+                  <dd className="text-gray-900 dark:text-gray-100 text-xl font-semibold">
+                    {keyMetrics.averageLatency.data?.data
+                      ? (keyMetrics.averageLatency.data.data / 1000).toFixed(2)
+                      : "n/a"}
+                  </dd>
+                )}
               </div>
             </li>
           </ul>
 
           <Card className="py-1 px-2">
-            <Table className="overflow-auto">
-              <TableHead className="border-b border-gray-300 dark:border-gray-700">
-                <TableRow>
-                  <TableHeaderCell className="text-black dark:text-white w-[200px]">
-                    {/* the fine-tune job id  */}
-                    Value
-                  </TableHeaderCell>
-                  <TableHeaderCell className="text-black dark:text-white">
-                    Requests
-                  </TableHeaderCell>
-                  <TableHeaderCell className="text-black dark:text-white">
-                    Cost
-                  </TableHeaderCell>
-                  <TableHeaderCell className="text-black dark:text-white">
-                    Avg Comp Tokens
-                  </TableHeaderCell>
-                  <TableHeaderCell className="text-black dark:text-white">
-                    Avg Latency
-                  </TableHeaderCell>
-                  <TableHeaderCell className="text-black dark:text-white">
-                    Avg Cost
-                  </TableHeaderCell>
-                  <TableHeaderCell />
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {isPropertiesLoading ? (
-                  <div>Loading...</div>
-                ) : (
-                  cleanedValueData.map((propertyValue, i) => (
+            {isAnyLoading ? (
+              <LoadingAnimation title="Loading Values..." />
+            ) : (
+              <Table className="overflow-auto">
+                <TableHead className="border-b border-gray-300 dark:border-gray-700">
+                  <TableRow>
+                    <TableHeaderCell className="text-black dark:text-white w-[200px]">
+                      {/* the fine-tune job id  */}
+                      Value
+                    </TableHeaderCell>
+                    <TableHeaderCell className="text-black dark:text-white">
+                      Requests
+                    </TableHeaderCell>
+                    <TableHeaderCell className="text-black dark:text-white">
+                      Cost
+                    </TableHeaderCell>
+                    <TableHeaderCell className="text-black dark:text-white">
+                      Avg Comp Tokens
+                    </TableHeaderCell>
+                    <TableHeaderCell className="text-black dark:text-white">
+                      Avg Latency
+                    </TableHeaderCell>
+                    <TableHeaderCell className="text-black dark:text-white">
+                      Avg Cost
+                    </TableHeaderCell>
+                    <TableHeaderCell />
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {cleanedValueData.map((propertyValue, i) => (
                     <TableRow
                       key={i}
                       onClick={() => {
@@ -261,9 +272,8 @@ const PropertyPanel = (props: PropertyPanelProps) => {
                       className="text-black dark:text-white border-b border-gray-300 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-900 hover:cursor-pointer"
                     >
                       <>
-                        <TableCell className="font-semibold max-w-[200px] 2xl:max-w-full truncate">
-                          {propertyValue.property_value} fsdjlf jklfjdkls
-                          jfkldsajlkfdkl jfklasdjklfjklsadfjkl
+                        <TableCell className="font-semibold max-w-[200px] 2xl:max-w-[400px] truncate">
+                          {propertyValue.property_value}
                         </TableCell>
                         <TableCell>{propertyValue.total_requests}</TableCell>
                         <TableCell>
@@ -290,10 +300,11 @@ const PropertyPanel = (props: PropertyPanelProps) => {
                         </TableCell>
                       </>
                     </TableRow>
-                  ))
-                )}
-              </TableBody>
-            </Table>
+                  ))}
+                </TableBody>
+              </Table>
+            )}
+
             {!showMore && propertyValueData.length > 10 && (
               <div className="w-full items-center flex justify-center p-2">
                 <button
