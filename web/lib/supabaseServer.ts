d65040221ea13a2b2e3475c109634d40b5e0e680
Justin Torre <justintorre75@gmail.com>
2024-02-02 01:01:56 -0800

    Improved compose (#1381)

--- web/lib/supabaseServer.ts
+++ web/lib/supabaseServer.ts
@@ -3,10 +3,8 @@ import { Database } from "../supabase/database.types";
 
 export const supabaseUrl =
   process.env.SUPABASE_URL ?? process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const supabaseServiceKey =
-  process.env.SUPABASE_SERVICE_KEY ??
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY ??
-  "";
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY ?? "n/a";
+
 if (supabaseUrl === "" || supabaseServiceKey === "") {
   throw new Error(`URL or Anon ENV not set for Server - ${supabaseServiceKey}`);
 }
