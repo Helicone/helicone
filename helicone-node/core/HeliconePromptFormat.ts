8f06e43d59dc04c0102b6ed09f5a3e35151bbeb3
Justin <justintorre75@gmail.com>
2024-02-14 23:33:21 -0500

    improved hprompt to include hpromptc

--- helicone-node/core/HeliconePromptFormat.ts
+++ helicone-node/core/HeliconePromptFormat.ts
@@ -38,23 +38,49 @@ export function prompt(
   return { heliconeTemplate, inputs, builtString };
 }
 
-export function hprompt(
+type StringFormatter = (
   strings: TemplateStringsArray,
   ...values: any[]
-): string {
-  // TODO handle the case where you just have `${input}` as a string and not `{{input}}`
-  return strings.reduce((acc, string, i) => {
-    const val = values[i];
-    if (val != null) {
-      const key = Object.keys(val)[0];
-      const value = Object.values(val)[0];
-      return (
-        acc +
-        string +
-        `<helicone-prompt-input key="${key}" >${value}</helicone-prompt-input>`
-      );
-    } else {
-      return acc + string;
+) => string;
+
+export const hpromptc =
+  (type: "raw" | "template", ...otherFormaters: StringFormatter[]) =>
+  (strings: TemplateStringsArray, ...values: any[]): string => {
+    const rawParts: string[] = [];
+    const templateParts: string[] = [];
+
+    for (let i = 0; i < strings.length; i++) {
+      rawParts.push(strings[i]);
+      templateParts.push(strings[i]);
+      if (values[i] != null) {
+        const value =
+          typeof values[i] === "object"
+            ? Object.values(values[i])[0]
+            : values[i];
+        rawParts.push(value as string);
+        if (typeof values[i] === "object") {
+          templateParts.push(
+            `<helicone-prompt-input key="${Object.keys(values[i])[0]}" >${
+              value as string
+            }</helicone-prompt-input>`
+          );
+        } else {
+          templateParts.push(value as string);
+        }
+      }
     }
-  }, "");
-}
+
+    const returnedString =
+      type === "template" ? templateParts.join("") : rawParts.join("");
+
+    if (otherFormaters.length === 0) {
+      return returnedString;
+    }
+
+    return otherFormaters.reduce(
+      (acc, formatter) => formatter`${acc}`,
+      returnedString
+    );
+  };
+
+export const hprompt = hpromptc("template");
