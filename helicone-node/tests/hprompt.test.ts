d72a7a155465add8511e4d9cfbb694c8aa632b76
Nishant Shukla <554656+BinRoot@users.noreply.github.com>
2024-02-15 08:03:45 -0800

    Apply suggestions from code review
    Co-authored-by: Justin Torre <justintorre75@gmail.com>

--- helicone-node/tests/hprompt.test.ts
+++ helicone-node/tests/hprompt.test.ts
@@ -1,10 +1,10 @@
-import { hprompt } from "../core/HeliconePromptFormat";
+import { hpromptc, hprompt } from "../core/HeliconePromptFormat";
 
 describe("hprompt", () => {
   it("should handle 1 string variable", () => {
     const firstName = "Bob";
-    const raw = hprompt({ format: "raw" })`Hello ${{ firstName }}`;
-    const template = hprompt()`Hello ${{ firstName }}`;
+    const raw = hpromptc({ format: "raw" })`Hello ${{ firstName }}`;
+    const template = hprompt`Hello ${{ firstName }}`;
     expect(raw).toBe("Hello Bob");
     expect(template).toBe(
       'Hello <helicone-prompt-input key="firstName">Bob</helicone-prompt-input>'
@@ -13,8 +13,8 @@ describe("hprompt", () => {
 
   it("should handle 1 number variable", () => {
     const count = 10;
-    const raw = hprompt({ format: "raw" })`You're #${{ count }}!`;
-    const template = hprompt()`You're #${{ count }}!`;
+    const raw = hpromptc({ format: "raw" })`You're #${{ count }}!`;
+    const template = hprompt`You're #${{ count }}!`;
     expect(raw).toBe(`You're #10!`);
     expect(template).toBe(
       `You're #<helicone-prompt-input key="count">10</helicone-prompt-input>!`
@@ -23,8 +23,8 @@ describe("hprompt", () => {
 
   it("should handle 1 boolean variable", () => {
     const isTrue = true;
-    const raw = hprompt({ format: "raw" })`Is it ${{ isTrue }}?`;
-    const template = hprompt()`Is it ${{ isTrue }}?`;
+    const raw = hpromptc({ format: "raw" })`Is it ${{ isTrue }}?`;
+    const template = hprompt`Is it ${{ isTrue }}?`;
     expect(raw).toBe(`Is it true?`);
     expect(template).toBe(
       `Is it <helicone-prompt-input key="isTrue">true</helicone-prompt-input>?`
@@ -39,7 +39,7 @@ describe("hprompt", () => {
       count,
     }}!
 Is it ${{ isTrue }}?`;
-    const template = hprompt()`Hello ${{
+    const template = hprompt`Hello ${{
       firstName,
     }}, you're #${{ count }}!
 Is it ${{ isTrue }}?`;
@@ -52,8 +52,8 @@ Is it <helicone-prompt-input key="isTrue">true</helicone-prompt-input>?`
 
   it("should handle non-object variables", () => {
     const firstName = "Bob";
-    const raw = hprompt({ format: "raw" })`Hello ${firstName}`;
-    const template = hprompt()`Hello ${firstName}`;
+    const raw = hprompt`Hello ${firstName}`;
+    const template = hprompt`Hello ${firstName}`;
     expect(raw).toBe("Hello Bob");
     expect(template).toBe("Hello Bob");
   });
