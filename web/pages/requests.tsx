154d6af258195d17f5502ab8d308cf3bc3f39917
Justin <justintorre75@gmail.com>
2024-02-21 20:23:23 -0800

    ewwwww code

--- web/pages/requests.tsx
+++ web/pages/requests.tsx
@@ -4,7 +4,34 @@ import AuthLayout from "../components/layout/authLayout";
 import RequestsPageV2 from "../components/templates/requestsV2/requestsPageV2";
 import { SupabaseServerWrapper } from "../lib/wrappers/supabase";
 import { SortDirection } from "../services/lib/sorts/requests/sorts";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
+
+// Got this ugly hack from https://stackoverflow.com/questions/21926083/failed-to-execute-removechild-on-node
+const jsToRun = `
+if (typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function(child) {
+    if (child.parentNode !== this) {
+      if (console) {
+        console.error('Cannot remove a child from a different parent', child, this);
+      }
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments);
+  }
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function(newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (console) {
+        console.error('Cannot insert before a reference node from a different parent', referenceNode, this);
+      }
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments);
+  }
+}
+`;
 
 interface RequestsV2Props {
   user: User;
@@ -20,6 +47,23 @@ interface RequestsV2Props {
 
 const RequestsV2 = (props: RequestsV2Props) => {
   const { user, currentPage, pageSize, sort, initialRequestId } = props;
+
+  useEffect(() => {
+    var observer = new MutationObserver(function (event) {
+      if (document.documentElement.className.match("translated")) {
+        eval(jsToRun);
+      } else {
+        console.log("Page untranslate");
+      }
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+      childList: false,
+      characterData: false,
+    });
+  }, []);
 
   return (
     <RequestsPageV2
