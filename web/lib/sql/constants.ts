ab5a75a1822b99ac5c63a2cc6c29cafa3b1c53e3
Justin Torre <justintorre75@gmail.com>
2024-02-24 11:28:01 -0800

    added price fixes (#1460)
    :

--- web/lib/sql/constants.ts
+++ web/lib/sql/constants.ts
@@ -13,16 +13,9 @@ sum(
     WHEN (${table}.model LIKE '%gpt-4-1106-preview-vision%') THEN 0.01 * ${table}.prompt_tokens + 0.03 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%gpt-3.5-turbo-0613%') THEN 0.0015 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%gpt-3.5-turbo-16k-0613%') THEN 0.003 * ${table}.prompt_tokens + 0.004 * ${table}.completion_tokens
-    WHEN (${table}.model LIKE '%gpt-35-turbo%') THEN 0.0015 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%gpt-35-turbo-16k%') THEN 0.003 * ${table}.prompt_tokens + 0.004 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%text-embedding-ada-002%') THEN 0.0001 * ${table}.prompt_tokens + 0.0001 * coalesce(${table}.completion_tokens, 0)
     WHEN (${table}.model LIKE '%text-embedding-ada-002-v2%') THEN 0.0001 * ${table}.prompt_tokens + 0.0001 * coalesce(${table}.completion_tokens, 0)
-    WHEN (${table}.model LIKE '%ada%') THEN 0.0004 * ${table}.prompt_tokens + 0.0004 * ${table}.completion_tokens
-    WHEN (${table}.model LIKE '%babbage%') THEN 0.0005 * ${table}.prompt_tokens + 0.0005 * ${table}.completion_tokens
-    WHEN (${table}.model LIKE '%curie%') THEN 0.002 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
-    WHEN (${table}.model LIKE '%davinci%') THEN 0.02 * ${table}.prompt_tokens + 0.02 * ${table}.completion_tokens
-    WHEN (${table}.model LIKE '%gpt-3.5-turbo%') THEN 0.002 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
-    WHEN (${table}.model LIKE '%gpt-4%') THEN 0.03 * ${table}.prompt_tokens + 0.06 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%claude-v1%') THEN 0.0163 * ${table}.prompt_tokens + 0.0551 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%claude-instant-v1%') THEN 0.01102 * ${table}.prompt_tokens + 0.03268 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%claude-2%') THEN 0.01102 * ${table}.prompt_tokens + 0.03268 * ${table}.completion_tokens
@@ -37,6 +30,16 @@ sum(
     WHEN (${table}.model LIKE '%gpt-3.5-turbo-1106%') THEN 0.001 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%gemini-pro%') THEN 0.00025 * ${table}.prompt_tokens + 0.0005 * ${table}.completion_tokens
     WHEN (${table}.model LIKE '%gemini-pro-vision%') THEN 0.00025 * ${table}.prompt_tokens + 0.0005 * ${table}.completion_tokens
+
+
+    -- Default
+    WHEN (${table}.model LIKE '%gpt-4%') THEN 0.03 * ${table}.prompt_tokens + 0.06 * ${table}.completion_tokens
+    WHEN (${table}.model LIKE '%gpt-35-turbo%') THEN 0.0015 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
+    WHEN (${table}.model LIKE '%ada%') THEN 0.0004 * ${table}.prompt_tokens + 0.0004 * ${table}.completion_tokens
+    WHEN (${table}.model LIKE '%babbage%') THEN 0.0005 * ${table}.prompt_tokens + 0.0005 * ${table}.completion_tokens
+    WHEN (${table}.model LIKE '%curie%') THEN 0.002 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
+    WHEN (${table}.model LIKE '%davinci%') THEN 0.02 * ${table}.prompt_tokens + 0.02 * ${table}.completion_tokens
+    WHEN (${table}.model LIKE '%gpt-3.5-turbo%') THEN 0.002 * ${table}.prompt_tokens + 0.002 * ${table}.completion_tokens
     ELSE 0
   END
   ) / 1000
