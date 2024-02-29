4b82589c3fbce9ad3c822ac1f6a98101e4ed9112
colegottdank <colegottdank@gmail.com>
2024-02-29 14:49:35 -0800

    why

--- worker/src/lib/HeliconeProxyRequest/forwarder.ts
+++ worker/src/lib/HeliconeProxyRequest/forwarder.ts
@@ -63,6 +63,9 @@ export async function proxyForwarder(
     }
   }
 
+  if (!env.PROMPTARMOR_API_KEY) {
+    console.error("PROMPTARMOR_API_KEY not set", env.PROMPTARMOR_API_KEY);
+  }
   if (
     proxyRequest.requestWrapper.heliconeHeaders.promptSecurityEnabled &&
     provider === "OPENAI" &&
