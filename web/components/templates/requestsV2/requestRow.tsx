1cd914426f057a52c44a820fc89f0ddbfbc0463f
Justin Torre <justintorre75@gmail.com>
2024-02-29 16:25:14 -0800

    Update web/components/templates/requestsV2/requestRow.tsx

--- web/components/templates/requestsV2/requestRow.tsx
+++ web/components/templates/requestsV2/requestRow.tsx
@@ -240,7 +240,7 @@ const RequestRow = (props: {
           {request.temperature && (
             <li className="flex flex-row justify-between items-center py-2">
               <p className="font-semibold text-gray-900 dark:text-gray-100">
-                LLM Temperature
+               Temperature
               </p>
               <p className="text-gray-700 dark:text-gray-300 truncate">
                 {request.temperature.toFixed(1)}
