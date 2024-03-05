034a085d18b8cf65c7f4887d0304b562c6b6a6ff
colegottdank <colegottdank@gmail.com>
2024-03-04 16:39:05 -0800

    Store target url (#1490)
    * store target url
    
    * init
    
    * fix targetUrl in db
    
    * revert some build target url stuff
    
    * remove unused method

--- worker/src/lib/dbLogger/logResponse.ts
+++ worker/src/lib/dbLogger/logResponse.ts
@@ -136,6 +136,7 @@ export async function logRequest(
       model_override: request.modelOverride ?? null,
       created_at: createdAt.toISOString(),
       threat: request.threat ?? null,
+      target_url: request.targetUrl,
     };
 
     const customPropertyRows = Object.entries(request.properties).map(
