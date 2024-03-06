f0517a8e43427e6590c7a2471863c4d74f79655e
Justin Torre <justintorre75@gmail.com>
2024-03-06 09:02:22 -0800

    added limit as prop (#1505)

--- web/services/hooks/models.tsx
+++ web/services/hooks/models.tsx
@@ -3,7 +3,7 @@ import { TimeFilter } from "../../components/templates/dashboard/dashboardPage";
 import { Result } from "../../lib/result";
 import { ModelMetric } from "../../lib/api/models/models";
 
-const useModels = (timeFilter: TimeFilter) => {
+const useModels = (timeFilter: TimeFilter, limit: number) => {
   const { data: models, isLoading } = useQuery({
     queryKey: ["modelMetrics", timeFilter],
     queryFn: async (query) => {
@@ -15,7 +15,7 @@ const useModels = (timeFilter: TimeFilter) => {
         body: JSON.stringify({
           filter: "all",
           offset: 0,
-          limit: 5,
+          limit,
           timeFilter,
         }),
       }).then((res) => res.json() as Promise<Result<ModelMetric[], string>>);
