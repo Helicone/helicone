2e9be3deef5b70cb29a519daf03764245c03eb5d
colegottdank <colegottdank@gmail.com>
2024-03-08 13:11:18 -0800

    Prompt armor docs fix (#1522)
    * Rename Prompt Security to LLM Security
    
    * da headers
    
    * fix db types
    
    * stuff & things

--- worker/src/lib/HeliconeHeaders.ts
+++ worker/src/lib/HeliconeHeaders.ts
@@ -203,7 +203,9 @@ export class HeliconeHeaders implements IHeliconeHeaders {
       fallBacks: this.getFallBacks(),
       modelOverride: this.headers.get("Helicone-Model-Override") ?? null,
       promptSecurityEnabled:
-        this.headers.get("Helicone-Prompt-Security-Enabled") ?? null,
+        this.headers.get("Helicone-LLM-Security-Enabled") ??
+        this.headers.get("Helicone-Prompt-Security-Enabled") ??
+        null,
     };
   }
 
