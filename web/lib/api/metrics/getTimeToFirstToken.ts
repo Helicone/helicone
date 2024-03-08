c822b8bc8f0aa1e85f89dff366f2048d75349386
maamalama <bokarevstefan475@gmail.com>
2024-03-07 16:37:31 -0800

    time to first token dashboard

---
+++ web/lib/api/metrics/getTimeToFirstToken.ts
@@ -0,0 +1,18 @@
+import { TimeToFirstToken } from "../../../pages/api/metrics/timeToFirstToken";
+import { Result, resultMap } from "../../result";
+import { getXOverTime } from "./getXOverTime";
+import { DataOverTimeRequest } from "./timeDataHandlerWrapper";
+
+export async function getTimeToFirstToken(
+  data: DataOverTimeRequest
+): Promise<Result<TimeToFirstToken[], string>> {
+  const res = await getXOverTime<{
+    ttft: number;
+  }>(data, "avg(DISTINCT request_response_log.time_to_first_token) AS ttft");
+  return resultMap(res, (resData) =>
+    resData.map((d) => ({
+      time: new Date(new Date(d.created_at_trunc).getTime()),
+      ttft: Number(d.ttft),
+    }))
+  );
+}
---
+++ web/pages/api/metrics/averageTimeToFirstToken.ts
@@ -0,0 +1,28 @@
+import {
+  HandlerWrapperOptions,
+  withAuth,
+} from "../../../lib/api/handlerWrappers";
+import { getAverageTimeToFirstToken } from "../../../lib/api/metrics/averageTimeToFirstToken";
+import { Result, resultsAll } from "../../../lib/result";
+
+async function handler(options: HandlerWrapperOptions<Result<number, string>>) {
+  const { req, res, userData } = options;
+  const { data: filterData, error: filterError } = resultsAll([
+    options.body.getFilter(),
+    options.body.getTimeFilter(),
+  ]);
+  if (filterError !== null) {
+    res.status(400).json({ error: filterError, data: null });
+    return;
+  }
+  const [filter, timeFilter] = filterData;
+
+  const metrics = await getAverageTimeToFirstToken(
+    filter,
+    timeFilter,
+    userData.orgId
+  );
+  res.status(200).json(metrics);
+}
+
+export default withAuth(handler);
