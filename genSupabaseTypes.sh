416e34f1ec12ded5022ca0fba874b754ff964788
Justin Torre <justintorre75@gmail.com>
2024-03-08 16:08:12 -0800

    Update genSupabaseTypes.sh

--- genSupabaseTypes.sh
+++ genSupabaseTypes.sh
@@ -1,4 +1,6 @@
 supabase gen types typescript --local > web/supabase/database.types.ts
 supabase gen types typescript --local > worker/supabase/database.types.ts
 supabase gen types typescript --local > valhalla/jawn/src/lib/db/database.types.ts
-
+git add web/supabase/database.types.ts
+git add worker/supabase/database.types.ts
+git add valhalla/jawn/src/lib/db/database.types.ts
