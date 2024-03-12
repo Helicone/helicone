b658a54d7e7d8f7b1601ce5073c83f341ab9dfc5
colegottdank <colegottdank@gmail.com>
2024-03-11 21:49:15 -0700

    Seperate out method

--- worker/src/db/SupabaseWrapper.ts
+++ worker/src/db/SupabaseWrapper.ts
@@ -35,6 +35,7 @@ export class SupabaseWrapper {
     return this.instance;
   }
 
+  // seperate out the ctx.waitUntil to make it easier to test
   async withTiming<T>(
     promise: PostgrestBuilder<T>,
     { queryName, percentLogging = 1 }: WithTimingParams
@@ -53,40 +54,46 @@ export class SupabaseWrapper {
     // Log based on the percentage
     if (randomNumber < percentLogging) {
       SupabaseWrapper.ctx.waitUntil(
-        new Promise(async () => {
-          const distribution = {
-            series: [
-              {
-                metric: "postgres.query.execution_time",
-                points: [[timestamp, [end - start]]],
-                host: "cloudflare_worker",
-                // Query name should be `<operation>_<entity>_<action>`, e.g. `select_user_by_id`
-                tags: ["query_name:" + queryName],
-              },
-            ],
-          };
-
-          const requestInit = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Content-Encoding": "string",
-              "DD-API-KEY": SupabaseWrapper.dataDogConfig.apiKey,
-            },
-            body: JSON.stringify(distribution),
-          };
-
-          const response = await fetch(
-            `${SupabaseWrapper.dataDogConfig.endpoint}/v1/distribution_points`,
-            requestInit
-          );
-
-          return response;
-        })
+        this.logDistributionMetric(timestamp, end - start, queryName)
       );
     }
 
     return result;
+  }
+
+  async logDistributionMetric(
+    timestamp: number,
+    executionTimeMs: number,
+    queryName: string
+  ): Promise<Response> {
+    const distribution = {
+      series: [
+        {
+          metric: "postgres.query.execution_time",
+          points: [[timestamp, [executionTimeMs]]],
+          host: "cloudflare_worker",
+          // Query name should be `<operation>_<entity>_<action>`, e.g. `select_user_by_id`
+          tags: ["query_name:" + queryName],
+        },
+      ],
+    };
+
+    const requestInit = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Encoding": "string",
+        "DD-API-KEY": SupabaseWrapper.dataDogConfig.apiKey,
+      },
+      body: JSON.stringify(distribution),
+    };
+
+    const response = await fetch(
+      `${SupabaseWrapper.dataDogConfig.endpoint}/v1/distribution_points`,
+      requestInit
+    );
+
+    return response;
   }
 }
 
