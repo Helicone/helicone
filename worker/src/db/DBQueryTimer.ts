91b86635f26a88fd3cfc55e65e530fffb39b3a89
colegottdank <colegottdank@gmail.com>
2024-03-12 17:28:33 -0700

    justin happy

--- worker/src/db/DBQueryTimer.ts
+++ worker/src/db/DBQueryTimer.ts
@@ -50,33 +50,40 @@ export class DBQueryTimer {
     executionTimeMs: number,
     queryName: string
   ): Promise<Response> {
-    const distribution = {
-      series: [
-        {
-          metric: "postgres.query.execution_time",
-          points: [[timestamp, [executionTimeMs]]],
-          host: "cloudflare_worker",
-          // Query name should be `<operation>_<entity>_<action>`, e.g. `select_user_by_id`
-          tags: ["query_name:" + queryName],
+    try {
+      const distribution = {
+        series: [
+          {
+            metric: "postgres.query.execution_time",
+            points: [[timestamp, [executionTimeMs]]],
+            host: "cloudflare_worker",
+            // Query name should be `<operation>_<entity>_<action>`, e.g. `select_user_by_id`
+            tags: ["query_name:" + queryName],
+          },
+        ],
+      };
+
+      const requestInit = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Encoding": "string",
+          "DD-API-KEY": this.dataDogConfig.apiKey,
         },
-      ],
-    };
+        body: JSON.stringify(distribution),
+      };
 
-    const requestInit = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Content-Encoding": "string",
-        "DD-API-KEY": this.dataDogConfig.apiKey,
-      },
-      body: JSON.stringify(distribution),
-    };
+      const response = await fetch(
+        `${this.dataDogConfig.endpoint}/v1/distribution_points`,
+        requestInit
+      );
 
-    const response = await fetch(
-      `${this.dataDogConfig.endpoint}/v1/distribution_points`,
-      requestInit
-    );
-
-    return response;
+      return response;
+    } catch (e) {
+      console.error("Error logging distribution metric", e);
+      return new Response("Error logging distribution metric", {
+        status: 500,
+      });
+    }
   }
 }
