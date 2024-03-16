2b618b95848c30d7ac93a5209da61eec9d312693
colegottdank <colegottdank@gmail.com>
2024-03-16 09:52:13 -0700

    5 second rate limits (#1547)
    * 5 second rate limits
    
    * round up

--- worker/src/db/RateLimiter.ts
+++ worker/src/db/RateLimiter.ts
@@ -16,17 +16,41 @@ export class RateLimiter {
         maxCount: number;
       }
     > = {
+      // pro: {
+      //   windowSizeSeconds: 60,
+      //   maxCount: 10_000,
+      // },
+      // enterprise: {
+      //   windowSizeSeconds: 60,
+      //   maxCount: 100_000,
+      // },
+      // free: {
+      //   windowSizeSeconds: 60,
+      //   maxCount: 1_000,
+      // },
+      // Previously, the pro was 10,000 requests per minute = 166 requests per second
+      // There are 12 5-second intervals in a minute
+      // 10k / 12 = 833.3333333333334
+      // Which is 166 requests per second
       pro: {
-        windowSizeSeconds: 60,
-        maxCount: 10_000,
+        windowSizeSeconds: 5,
+        maxCount: 834,
       },
+      // Previously, the enterprise was 100,000 requests per minute = 1666.6666666666667 requests per second
+      // There are 12 5-second intervals in a minute
+      // 100k / 12 = 8333.333333333334
+      // Which is 1666.6666666666667 requests per second
       enterprise: {
-        windowSizeSeconds: 60,
-        maxCount: 100_000,
+        windowSizeSeconds: 5,
+        maxCount: 8334,
       },
+      // Previously, the free was 1,000 requests per minute = 16.666666666666668 requests per second
+      // There are 12 5-second intervals in a minute
+      // 1k / 12 = 83.33333333333334
+      // Which is 16.666666666666668 requests per second
       free: {
-        windowSizeSeconds: 60,
-        maxCount: 1_000,
+        windowSizeSeconds: 5,
+        maxCount: 84,
       },
     };
     tier = tier?.toLowerCase() in rateLimitParams ? tier.toLowerCase() : "free";
