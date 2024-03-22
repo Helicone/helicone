500c555d1dc7102dc5b4b621d3448db223efdf77
Scott Nguyen <scott@helicone.ai>
2024-03-22 14:51:14 -0700

    revert dbExecute

--- web/components/templates/requestsV2/viewColumns.tsx
+++ web/components/templates/requestsV2/viewColumns.tsx
@@ -1,6 +1,5 @@
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
-import { CheckIcon } from "@heroicons/react/20/solid";
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import { Column } from "@tanstack/react-table";
 import { clsx } from "../../shared/clsx";
