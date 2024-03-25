0cd4e9388615f59c3423dc30070164972f827364
colegottdank <colegottdank@gmail.com>
2024-03-25 14:56:59 -0700

    S3 PART 3 - Retrieve objects in web! (#1580)
    * Write to s3 part 1
    
    * Minio hcone
    
    * Returned signed url from jawn
    
    * return to fe
    
    * Update readme
    
    * Try catch storing in S3
    
    * Update read me
    
    * remove rosetta
    
    * Revert "remove rosetta"
    
    This reverts commit 3eb8e0e43fadef09e50395333085c2193673e7ab.
    
    * compression
    
    * remove s3 stuff from wrangler.toml
    
    * w/e
    
    * add rosetta back
    
    * it is an array
    
    * remove defaults and fail instead
    
    * add s3 implementation date
    
    * yarn update
    
    * fix yarn build
    
    * new s3 client in request
    
    * new s3 up in request
    
    * add providers
    
    * updates for default values
    
    * update s3 implementation date

--- valhalla/jawn/src/lib/routers/iRouterWrapper.ts
+++ valhalla/jawn/src/lib/routers/iRouterWrapper.ts
@@ -2,7 +2,6 @@ import { Response as ExpressResponse } from "express";
 import { Database } from "../db/database.types";
 import { AuthParams, SupabaseConnector } from "../db/supabase";
 import { RequestWrapper } from "../requestWrapper";
-import { S3Client } from "../shared/db/s3Client";
 
 export interface IRouterWrapper<T> {
   request: RequestWrapper<T>;
@@ -11,7 +10,6 @@ export interface IRouterWrapper<T> {
 
 export interface IRouterWrapperAuth<T> extends IRouterWrapper<T> {
   supabaseClient: SupabaseConnector;
-  s3Client: S3Client;
   authParams: AuthParams;
   org?: Database["public"]["Tables"]["organization"]["Row"];
 }
