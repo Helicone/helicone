bacaeae0e17e9579e4b842076e5b518866dd4176
Justin <justintorre75@gmail.com>
2024-03-25 16:34:31 -0700

    fix anthropic URL :

--- costs/src/providers/mappings.ts
+++ costs/src/providers/mappings.ts
@@ -1,5 +1,5 @@
 const openAiPattern = /^https:\/\/api\.openai\.com\/v\d+\/?$/;
-const anthropicPattern = /^https:\/\/api\.anthropic\.com\/v\d+\/?$/;
+const anthropicPattern = /^https:\/\/api\.anthropic\.com/;
 const azurePattern =
   /^(https?:\/\/)?([^.]*\.)?(openai\.azure\.com|azure-api\.net)(\/.*)?$/;
 const localProxyPattern = /^http:\/\/127\.0\.0\.1:\d+\/v\d+\/?$/;
