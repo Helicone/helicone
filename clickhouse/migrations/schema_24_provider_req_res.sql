c1bffd1b6954f9a204f2d3328c0825849de875f8
Justin <justintorre75@gmail.com>
2024-03-25 10:52:13 -0700

    Merged

--- clickhouse/migrations/schema_24_provider_req_res.sql
+++ clickhouse/migrations/schema_24_provider_req_res.sql
@@ -1,7 +1,3 @@
 ALTER TABLE request_response_log
-<<<<<<< HEAD
-ADD COLUMN IF NOT EXISTS target_url Nullable(String)
-=======
 ADD COLUMN IF NOT EXISTS provider Nullable(String)
->>>>>>> main
 AFTER time_to_first_token;
