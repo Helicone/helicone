3ed4a1c5f7782b781296ff3f8a912d5528860d4c
colegottdank <colegottdank@gmail.com>
2024-03-25 12:38:48 -0700

    fix yarn build

--- web/pages/api/request/index.ts
+++ web/pages/api/request/index.ts
@@ -11,7 +11,7 @@ import {
 import { Result } from "../../../lib/result";
 import { FilterNode } from "../../../services/lib/filters/filterDefs";
 import { SortLeafRequest } from "../../../services/lib/sorts/requests/sorts";
-import { supabaseServer } from "../../../lib/supabaseServer";
+import { S3Client } from "../../../lib/api/db/s3Client";
 
 async function handler({
   req,
@@ -26,16 +26,16 @@ async function handler({
     isCached: boolean;
   };
 
+  const s3Client = new S3Client(
+    process.env.S3_ACCESS_KEY ?? "",
+    process.env.S3_SECRET_KEY ?? "",
+    process.env.S3_ENDPOINT ?? "",
+    process.env.S3_BUCKET_NAME ?? ""
+  );
+
   const metrics = isCached
-    ? await getRequestsCached(
-        orgId,
-        filter,
-        offset,
-        limit,
-        sort,
-        supabaseServer
-      )
-    : await getRequests(orgId, filter, offset, limit, sort, supabaseServer);
+    ? await getRequestsCached(orgId, filter, offset, limit, sort, s3Client)
+    : await getRequests(orgId, filter, offset, limit, sort, s3Client);
   res.status(metrics.error === null ? 200 : 500).json(metrics);
 }
 
