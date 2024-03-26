c653e7ca1338ac71ec2984cb99f3786af34f8439
maamalama <bokarevstefan475@gmail.com>
2024-03-26 16:03:59 -0700

    themed modal fixes + cost unsupported link

--- web/components/templates/requestsV2/costPill.tsx
+++ web/components/templates/requestsV2/costPill.tsx
@@ -1,24 +1,14 @@
-import { useState } from "react";
-import ThemedModal from "../../shared/themed/themedModal";
 import Link from "next/link";
 import { clsx } from "../../shared/clsx";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 
 const CostPill = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpenModal = (
-    event: React.MouseEvent<HTMLSpanElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-    event.stopPropagation();
-    setIsOpen(true);
-  };
-
   return (
     <>
-      <button
-        onClick={handleOpenModal}
+      <Link
+        href={"https://github.com/Helicone/helicone/tree/main/costs"}
+        target="_blank"
+        rel="noopener noreferrer"
         className={clsx(
           "bg-gray-50 text-gray-700 ring-gray-200 dark:bg-gray-900 dark:text-gray-300 dark:ring-gray-800",
           "w-max inline-flex items-center rounded-lg px-2 py-1 -my-1 text-xs font-medium ring-1 ring-inset cursor-pointer"
@@ -26,46 +16,7 @@ const CostPill = () => {
       >
         Cost Unsupported{" "}
         <ArrowTopRightOnSquareIcon className="ml-1 h-4 w-4 text-orange-500 inline-block" />
-      </button>
-
-      <ThemedModal open={isOpen} setOpen={() => setIsOpen(false)}>
-        <div className="flex flex-col gap-8 inset-0 bg-opacity-50 w-full sm:w-[450px] max-w-[450px] h-full rounded-3xl">
-          <h1 className="col-span-4 font-semibold text-xl text-gray-900 dark:text-gray-100">
-            Cost Unsupported
-          </h1>
-
-          <p className="text-sm text-gray-900 dark:text-gray-100">
-            Currently, we do not support the cost of this model. Please create
-            an issue in Github{" "}
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Helicone/helicone"
-              className="underline text-blue-500"
-            >
-              https://github.com/Helicone/helicone
-            </Link>{" "}
-            or reach out to us at Discord{" "}
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://discord.gg/zsSTcH2qhG"
-              className="underline text-blue-500"
-            >
-              https://discord.gg/zsSTcH2qhG
-            </Link>{" "}
-          </p>
-          <div className="col-span-4 flex justify-end gap-2 pt-4">
-            <button
-              type="button"
-              onClick={() => setIsOpen(false)}
-              className="items-center rounded-md bg-black dark:bg-white px-4 py-2 text-sm flex font-semibold text-white dark:text-black shadow-sm hover:bg-gray-800 dark:hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </ThemedModal>
+      </Link>
     </>
   );
 };
