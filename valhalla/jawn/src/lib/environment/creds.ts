82e1c9da8eaf1a187f6a7c3a89c33d38fe29102e
colegottdank <colegottdank@gmail.com>
2024-03-27 16:48:27 -0700

    S3 Feature Flag (#1617)
    * s3 feature flag
    
    * more feature flags
    
    * s3 enabled ff improved
    
    * default s3 client true
    
    * s3 default true
    
    * case

--- valhalla/jawn/src/lib/environment/creds.ts
+++ valhalla/jawn/src/lib/environment/creds.ts
@@ -1,21 +1,22 @@
 // Pull all creds in at global state, if does not exist, throw error:
-if (!process.env.S3_ACCESS_KEY) {
-  throw new Error("No S3 access key");
-}
+if (process.env.S3_ENABLED ?? "true" === "true") {
+  if (!process.env.S3_ACCESS_KEY) {
+    throw new Error("No S3 access key");
+  }
 
-if (!process.env.S3_SECRET_KEY) {
-  throw new Error("No S3 secret key");
-}
+  if (!process.env.S3_SECRET_KEY) {
+    throw new Error("No S3 secret key");
+  }
 
-if (!process.env.S3_ENDPOINT) {
-  // This is acceptable as it is optional.
-  // If no endpoint is provided, the SDK will use the default endpoint.
-}
+  if (!process.env.S3_ENDPOINT) {
+    // This is acceptable as it is optional.
+    // If no endpoint is provided, the SDK will use the default endpoint.
+  }
 
-if (!process.env.S3_BUCKET_NAME) {
-  throw new Error("No S3 bucket name");
+  if (!process.env.S3_BUCKET_NAME) {
+    throw new Error("No S3 bucket name");
+  }
 }
-
 if (
   !process.env.SUPABASE_CREDS ||
   (process.env.SUPABASE_URL && !process.env.SUPABASE_SERVICE_ROLE_KEY)
