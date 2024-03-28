2c8539ce55747480eb633d07fd57d91f5fe658b8
Stefan Bokarev <38397684+maamalama@users.noreply.github.com>
2024-03-28 15:41:27 -0700

    Request ip null by default (#1626)
    * Request ip null
    
    * tests fix

--- worker/src/lib/dbLogger/clickhouseLog.ts
+++ worker/src/lib/dbLogger/clickhouseLog.ts
@@ -71,7 +71,7 @@ export async function logInClickhouse(
         threat: request.threat ?? null,
         time_to_first_token: response.time_to_first_token ?? null,
         target_url: request.target_url ?? null,
-        request_ip: request.request_ip ?? null,
+        request_ip: null,
         provider: request.provider ?? null,
       },
     ]),
