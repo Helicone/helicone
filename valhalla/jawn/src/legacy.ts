f0af485de7ae1a51e8e34521bc613793aa9f0007
Justin <justintorre75@gmail.com>
2024-03-30 22:44:13 -0700

    fix

--- valhalla/jawn/src/legacy.ts
+++ valhalla/jawn/src/legacy.ts
@@ -52,31 +52,6 @@ legacyRouter.use(express.json({ limit: "50mb" }));
 legacyRouter.use(express.urlencoded({ limit: "50mb" }));
 
 // for logs
-legacyRouter.use(
-  morgan(function (tokens, req, res) {
-    // Check if the request is for the specific route
-    if (req.url === "/v1/tokens/anthropic" && req.method === "POST") {
-      // Skip logging and return null
-      return null;
-    }
-
-    if (req.url === "/v1/tokens/gpt3" && req.method === "POST") {
-      // Skip logging and return null
-      return null;
-    }
-
-    // Default Morgan combined format
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-    ].join(" ");
-  })
-);
 
 legacyRouter.use(errorHandler);
 const allowedOriginsEnv = {
