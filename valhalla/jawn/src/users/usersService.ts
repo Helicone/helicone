a71e55249165fccd31f458f9a5e84b24e72c834d
Justin Torre <justintorre75@gmail.com>
2024-03-30 19:03:24 -0700

    TSOA Basic implementation (#1635)

---
+++ valhalla/jawn/src/users/usersService.ts
@@ -0,0 +1,25 @@
+// src/users/usersService.ts
+import { User } from "./user";
+
+// A post request should not contain an id.
+export type UserCreationParams = Pick<User, "email" | "name" | "phoneNumbers">;
+
+export class UsersService {
+  public get(id: number, name?: string): User {
+    return {
+      id,
+      email: "jane@doe.com",
+      name: name ?? "Jane Doe",
+      status: "Happy",
+      phoneNumbers: [],
+    };
+  }
+
+  public create(userCreationParams: UserCreationParams): User {
+    return {
+      id: Math.floor(Math.random() * 10000), // Random
+      status: "Happy",
+      ...userCreationParams,
+    };
+  }
+}
