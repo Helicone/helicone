0344818322c197c56844086928ae59306a30aeda
Justin Torre <justintorre75@gmail.com>
2024-03-30 19:03:24 -0700

    TSOA Basic implementation (#1635)

---
+++ valhalla/jawn/src/users/usersController.ts
@@ -0,0 +1,36 @@
+// src/users/usersController.ts
+import {
+  Body,
+  Controller,
+  Get,
+  Path,
+  Post,
+  Query,
+  Route,
+  SuccessResponse,
+  Tags,
+} from "tsoa";
+import { User } from "./user";
+import { UsersService, UserCreationParams } from "./usersService";
+
+@Route("v1/users")
+@Tags("User")
+export class UsersController extends Controller {
+  @Get("{userId}")
+  public async getUser(
+    @Path() userId: number,
+    @Query() name?: string
+  ): Promise<User> {
+    return new UsersService().get(userId, name);
+  }
+
+  @SuccessResponse("201", "Created") // Custom success response
+  @Post()
+  public async createUser(
+    @Body() requestBody: UserCreationParams
+  ): Promise<void> {
+    this.setStatus(201); // set return status 201
+    new UsersService().create(requestBody);
+    return;
+  }
+}
