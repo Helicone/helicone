bba37cc3b23a27125a46a0409eb0458b476c8d11
colegottdank <colegottdank@gmail.com>
2024-04-01 23:27:35 -0700

    update logic

--- web/pages/api/stripe/event_webhook/index.ts
+++ web/pages/api/stripe/event_webhook/index.ts
@@ -27,60 +27,43 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return;
     }
 
+    if (event.type === "customer.subscription.created") {
+      const subscription = event.data.object as Stripe.Subscription;
+      const subscriptionId = subscription.id;
+      const subscriptionItemId = subscription?.items.data[0].id;
+
+      // Assuming you passed the organization's ID in the `metadata` when creating the checkout session:
+      const orgId = subscription.metadata?.orgId;
+      const { data, error } = await supabaseServer
+        .from("organization")
+        .update({
+          subscription_status: "active",
+          stripe_subscription_id: subscriptionId, // this is the ID of the subscription created by the checkout
+          stripe_subscription_item_id: subscriptionItemId,
+          tier: "growth",
+        })
+        .eq("id", orgId || "");
+    } else if (event.type === "checkout.session.completed") {
+      const checkoutCompleted = event.data.object as Stripe.Checkout.Session;
+
+      const session = await stripe.checkout.sessions.retrieve(
+        checkoutCompleted.id
+      );
+
+      // Assuming you passed the organization's ID in the `metadata` when creating the checkout session:
+      const orgId = session.metadata?.orgId;
+      const { data, error } = await supabaseServer
+        .from("organization")
+        .update({
+          subscription_status: "active",
+          stripe_subscription_id: checkoutCompleted.subscription?.toString(), // this is the ID of the subscription created by the checkout
+          tier: "pro",
+        })
+        .eq("id", orgId || "");
+    }
+
     // Handle the event
     switch (event.type) {
-      case "customer.subscription.created":
-      case "checkout.session.completed":
-        // The user has completed the checkout process and has been charged.
-
-        const checkoutCompleted = event.data.object as Stripe.Checkout.Session;
-
-        let subscriptionId = checkoutCompleted.subscription?.toString();
-
-        console.log(`Checkout Completed: ${JSON.stringify(checkoutCompleted)}`);
-
-        if (!subscriptionId) {
-          console.log("Retrieving session to get subscription ID");
-          const session = await stripe.checkout.sessions.retrieve(
-            checkoutCompleted.id
-          );
-          subscriptionId = session.subscription?.toString();
-
-          console.log(`Session: ${JSON.stringify(session)}`);
-        }
-
-        if (!subscriptionId) {
-          console.error(
-            "Subscription ID is missing after retrieving the session."
-          );
-          // Handle the error appropriately
-          break;
-        }
-
-        const subscription = await stripe.subscriptions.retrieve(
-          subscriptionId
-        );
-
-        console.log(`Subscription: ${JSON.stringify(subscription)}`);
-        const subscriptionItemId = subscription?.items.data[0].id;
-
-        // Assuming you passed the organization's ID in the `metadata` when creating the checkout session:
-        const orgId = checkoutCompleted.metadata?.orgId;
-
-        const tier =
-          event.type === "checkout.session.completed" ? "pro" : "growth";
-        const { data, error } = await supabaseServer
-          .from("organization")
-          .update({
-            subscription_status: "active",
-            stripe_subscription_id: checkoutCompleted.subscription?.toString(), // this is the ID of the subscription created by the checkout
-            stripe_subscription_item_id: subscriptionItemId,
-            tier: tier,
-          })
-          .eq("id", orgId || "");
-
-        break;
-
       case "customer.subscription.updated":
         // Subscription details, like billing details or status, have changed.
         const subscriptionUpdated = event.data.object as Stripe.Subscription;
