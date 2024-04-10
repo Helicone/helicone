4567be11e17f7f8bb27fe2ea253f0e4d369b4193
Stefan Bokarev <38397684+maamalama@users.noreply.github.com>
2024-04-10 12:53:43 -0700

    Fixed image support for claude models + code style fixes (#1670)
    * abstract image parser
    
    * tests + claude fixes
    
    * tests yaml + pr fixes
    
    * lint fixes
    
    * pr fixes
    
    * pr fix
    
    * test fixes
    
    * yaml tests fix
    
    * yml fix
    
    * s3 env
    
    * s3 env to wrangler
    
    * script fix
    
    * anthropic proxy and docker compose fix
    
    * pr fix
    
    * pr fix 2
    
    * oops
    
    * fix?

---
+++ worker/src/lib/dbLogger/imageParsers/parserMapper.ts
@@ -0,0 +1,19 @@
+import { ClaudeImageParser } from "./claudeImageParser";
+import { GptVisionImageParser } from "./gptVisionImageParser";
+import { ImageModelParser } from "./imageModelParser";
+
+export function getImageModelParser(
+  modelName: string
+): ImageModelParser | null {
+  switch (modelName) {
+    case "gpt-4-vision-preview":
+    case "gpt-4-1106-vision-preview":
+      return new GptVisionImageParser(modelName);
+    case "claude-3-opus-20240229":
+    case "claude-3-sonnet-20240229":
+    case "claude-3-haiku-20240307":
+      return new ClaudeImageParser(modelName);
+    default:
+      return null;
+  }
+}
