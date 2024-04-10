39f01c4eddc72ee8c5c1026890a57b361f1bc804
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2024-04-10 13:42:45 -0600

    Update constants.ts

--- worker/src/lib/util/constants.ts
+++ worker/src/lib/util/constants.ts
@@ -6,6 +6,8 @@ export const isImageModel = (modelName: string): boolean => {
   const models = new Set<string>([
     "gpt-4-vision-preview",
     "gpt-4-1106-vision-preview",
+    "gpt-4-turbo-2024-04-09",
+    "gpt-4-turbo"
   ]);
   return models.has(modelName);
 };
