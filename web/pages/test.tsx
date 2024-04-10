4a3aa4e9f62285e0ad0717537a63978c6018d7c8
colegottdank <colegottdank@gmail.com>
2024-04-09 17:36:54 -0700

    add test growth sub button

--- web/pages/test.tsx
+++ web/pages/test.tsx
@@ -4,14 +4,50 @@ import { ReactElement } from "react";
 import { NextPageWithLayout } from "./_app";
 import AuthLayout from "../components/layout/authLayout";
 import { SupabaseServerWrapper } from "../lib/wrappers/supabase";
+import getStripe from "../utlis/getStripe";
+import { useOrg } from "../components/layout/organizationContext";
+import { useUser } from "@supabase/auth-helpers-react";
 
 const Test: NextPageWithLayout<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = (props) => {
+  const orgContext = useOrg();
+  const user = useUser();
+  async function handleGrowthCheckout() {
+    const stripe = await getStripe();
+
+    if (!stripe) {
+      console.error("Stripe failed to initialize.");
+      return;
+    }
+
+    const res = await fetch("/api/stripe/create_growth_subscription", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        orgId: orgContext?.currentOrg?.id,
+        userEmail: user?.email,
+      }),
+    });
+    const { sessionId } = await res.json();
+    const result = await stripe.redirectToCheckout({ sessionId });
+    if (result.error) {
+      console.error(result.error.message);
+    }
+  }
+
   return (
     <div className="h-screen w-full flex flex-col">
       <div className="bg-green-500 w-full p-4">
         <p className="text-white text-2xl"> THIS IS A TEST PAGE</p>
+        <button
+          onClick={() => handleGrowthCheckout()}
+          className="items-center rounded-md bg-black dark:bg-white px-4 py-2 text-sm flex font-semibold text-white dark:text-black shadow-sm hover:bg-gray-800 dark:hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+        >
+          Upgrade to Growth
+        </button>
       </div>
     </div>
   );
