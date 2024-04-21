36229a479770a21d2c66ba6eb481a8499e83d33d
Justin Torre <justintorre75@gmail.com>
2024-04-21 15:07:12 -0700

    added query timer for managers (#1735)

--- valhalla/jawn/src/managers/BaseManager.ts
+++ valhalla/jawn/src/managers/BaseManager.ts
@@ -1,5 +1,13 @@
+import { DBQueryTimer } from "../lib/db/DBQueryTimer";
 import { AuthParams } from "../lib/db/supabase";
 
 export class BaseManager {
-  constructor(protected authParams: AuthParams) {}
+  protected queryTimer: DBQueryTimer;
+  constructor(protected authParams: AuthParams) {
+    this.queryTimer = new DBQueryTimer({
+      enabled: true,
+      apiKey: process.env.DATADOG_API_KEY ?? "",
+      endpoint: process.env.DATADOG_ENDPOINT ?? "",
+    });
+  }
 }
