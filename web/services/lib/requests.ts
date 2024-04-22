94516741859c5dead0730e4fc4b42e9f63a1dbe8
Justin Torre <justintorre75@gmail.com>
2024-04-21 18:49:37 -0700

    Kill properties (#1737)

--- web/services/lib/requests.ts
+++ web/services/lib/requests.ts
@@ -1,10 +1,4 @@
-import Cookies from "js-cookie";
-import { SUPABASE_AUTH_TOKEN } from "../../lib/constants";
 import { getJawnClient } from "../../lib/clients/jawn";
-
-const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
-const API_BASE_PATH = process.env.NEXT_PUBLIC_API_BASE_PATH || "";
 
 export const updateRequestFeedback = async (
   requestId: string,
@@ -30,25 +24,18 @@ export const addRequestLabel = async (
   key: string,
   value: string
 ) => {
-  const authFromCookie = Cookies.get(SUPABASE_AUTH_TOKEN);
-  if (!authFromCookie) {
-    console.error("No auth token found in cookie");
-    return;
-  }
-  const decodedCookie = decodeURIComponent(authFromCookie);
-  const parsedCookie = JSON.parse(decodedCookie);
-  const jwtToken = parsedCookie[0];
-
-  return fetch(`${API_BASE_PATH}/request/${requestId}/property`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "helicone-jwt": jwtToken,
-      "helicone-org-id": orgId,
-    },
-    body: JSON.stringify({
-      key,
-      value,
-    }),
-  });
+  const jawn = getJawnClient(orgId);
+  return (
+    await jawn.PUT("/v1/request/{requestId}/property", {
+      body: {
+        key,
+        value,
+      },
+      params: {
+        path: {
+          requestId,
+        },
+      },
+    })
+  ).response;
 };
