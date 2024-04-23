5a1cd08e78eca63d9200f7531ea9bd62be2f8f91
Scott Nguyen <scott@helicone.ai>
2024-04-23 16:38:30 -0700

    analytics

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -16,6 +16,7 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { ThemeContextProvider } from "../components/shared/theme/themeContext";
 import { NextPage } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 if (
   typeof window !== "undefined" &&
@@ -52,6 +53,8 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
+  const trackingEnabled = process.env.NEXT_PUBLIC_TRACKING_ENABLED || false;
+
   return (
     <>
       <SessionContextProvider
@@ -73,6 +76,7 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           </QueryClientProvider>
         </ApolloProvider>
       </SessionContextProvider>
+      {trackingEnabled && <Analytics />}
     </>
   );
 }
