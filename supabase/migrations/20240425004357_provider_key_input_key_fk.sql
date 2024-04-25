74bfb6a97b6ecfbcd2b2606c9c002f84146cd021
Justin <justintorre75@gmail.com>
2024-04-24 19:42:52 -0700

    oops

--- supabase/migrations/20240425004357_provider_key_input_key_fk.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."experiment_v2_hypothesis_run" add column "result_request_id" uuid not null;
-
-alter table "public"."experiment_v2_hypothesis_run" add constraint "public_experiment_v2_hypothesis_run_result_request_id_fkey" FOREIGN KEY (result_request_id) REFERENCES request(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
-
-alter table "public"."experiment_v2_hypothesis_run" validate constraint "public_experiment_v2_hypothesis_run_result_request_id_fkey";
-
-
