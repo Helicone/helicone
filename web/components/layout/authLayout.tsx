ef985b45ceb164c1749fe727276c2294989a9ef6
Scott Nguyen <scott@helicone.ai>
2024-04-25 10:27:00 -0700

    remove banner

--- web/components/layout/authLayout.tsx
+++ web/components/layout/authLayout.tsx
@@ -554,17 +554,6 @@ const AuthLayout = (props: AuthLayoutProps) => {
             </div>
           </div>
           <main className="flex-1">
-            <div className="pointer-events-none flex sm:justify-center">
-              <div className="w-full pointer-events-auto flex items-center justify-center gap-x-6 bg-red-500 shadow-md py-2.5 px-6 sm:py-3 sm:pr-3.5 sm:pl-4">
-                <ExclamationCircleIcon className="h-6 w-6 text-white" />
-                <p className="text-white font-semibold">
-                  We are currently experiencing issues with our servers. We
-                  apologize for the inconvenience - we are still logging
-                  requests and are working to resolve the issue as soon as
-                  possible.
-                </p>
-              </div>
-            </div>
             <div
               className={clsx(
                 "mx-auto px-4 sm:px-8 bg-gray-100 dark:bg-[#17191d] h-full min-h-screen"
