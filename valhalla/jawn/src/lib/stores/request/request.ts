b523664308389ac2794b2783905a4e463f48fd37
Justin Torre <justintorre75@gmail.com>
2024-04-24 17:36:24 -0700

    Experiments backend (#1762)

--- valhalla/jawn/src/lib/stores/request/request.ts
+++ valhalla/jawn/src/lib/stores/request/request.ts
@@ -91,6 +91,17 @@ export async function getRequests(
     argsAcc: [],
   });
   const sortSQL = buildRequestSort(sort);
+
+  let joinQuery = "";
+
+  if (
+    JSON.stringify(filter).includes("prompts_versions") ||
+    JSON.stringify(filter).includes("prompt_input_record")
+  ) {
+    joinQuery = `left join prompt_input_record on request.id = prompt_input_record.source_request
+    left join prompts_versions on prompts_versions.id = prompt_input_record.prompt_version`;
+  }
+
   const query = `
   SELECT response.id AS response_id,
     response.created_at as response_created_at,
@@ -122,7 +133,6 @@ export async function getRequests(
     (response.prompt_tokens + response.completion_tokens) as total_tokens,
     response.completion_tokens as completion_tokens,
     response.prompt_tokens as prompt_tokens,
-    job_node_request.node_id as node_id,
     feedback.created_at AS feedback_created_at,
     feedback.id AS feedback_id,
     feedback.rating AS feedback_rating,
@@ -134,7 +144,7 @@ export async function getRequests(
   FROM request
     left join response on request.id = response.request
     left join feedback on response.id = feedback.response_id
-    left join job_node_request on request.id = job_node_request.request_id
+    ${joinQuery}
   WHERE (
     (${builtFilter.filter})
   )
