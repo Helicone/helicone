e08af4d839e40988f80a2ed39bf129150a9f2710
Justin <justintorre75@gmail.com>
2024-04-25 23:37:14 -0700

    dev web

--- docker/push_new_docker.py
+++ docker/push_new_docker.py
@@ -4,11 +4,13 @@ import datetime
 import requests
 import os
 
+
 def run_or_echo(test_mode, command):
     if test_mode:
         print(' '.join(command))
     else:
         subprocess.run(command, check=True)
+
 
 def main(test_mode):
     # Can comment out next line if you don't want to prune all images
@@ -21,6 +23,7 @@ def main(test_mode):
     docker_image_context = [
         {"image": "helicone/worker", "context": "../worker"},
         {"image": "helicone/web", "context": "../web"},
+        {"image": "helicone/web-dev", "context": "../web"},
         {"image": "helicone/supabase-migration-runner", "context": "../supabase"},
         {"image": "helicone/clickhouse-migration-runner", "context": "../clickhouse"},
         {"image": "helicone/jawn", "context": "../valhalla"}
@@ -29,8 +32,9 @@ def main(test_mode):
     for dic in docker_image_context:
         image = dic["image"]
         context = dic["context"]
-        
-        response = requests.get(f"https://hub.docker.com/v2/repositories/{image}/tags/?page_size=100")
+
+        response = requests.get(
+            f"https://hub.docker.com/v2/repositories/{image}/tags/?page_size=100")
         tags = [tag['name'] for tag in response.json()['results']]
 
         tag = version_tag
@@ -40,23 +44,29 @@ def main(test_mode):
             counter += 1
 
         dockerfile_name = os.path.basename(image).replace('-', '_')
-        
+
         # Adjust the Dockerfile path according to the special case for "jawn"
         if dockerfile_name == "jawn":
-            dockerfile_path = "../valhalla/dockerfile"  # Assuming the Dockerfile is directly in the context directory for "jawn"
+            # Assuming the Dockerfile is directly in the context directory for "jawn"
+            dockerfile_path = "../valhalla/dockerfile"
         else:
             dockerfile_path = f"dockerfiles/dockerfile_{dockerfile_name}"
 
-        build_command = ["docker", "build", "--platform", "linux/amd64", "-t", f"{image}:{tag}", "-f", dockerfile_path, context]
+        build_command = ["docker", "build", "--platform", "linux/amd64",
+                         "-t", f"{image}:{tag}", "-f", dockerfile_path, context]
 
         run_or_echo(test_mode, build_command)
         run_or_echo(test_mode, ["docker", "push", f"{image}:{tag}"])
-        run_or_echo(test_mode, ["docker", "tag", f"{image}:{tag}", f"{image}:latest"])
+        run_or_echo(test_mode, ["docker", "tag",
+                    f"{image}:{tag}", f"{image}:latest"])
         run_or_echo(test_mode, ["docker", "push", f"{image}:latest"])
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Build and push Docker images with custom contexts.")
-    parser.add_argument("-t", "--test", action="store_true", help="Enable test mode (commands will only be printed)")
+    parser = argparse.ArgumentParser(
+        description="Build and push Docker images with custom contexts.")
+    parser.add_argument("-t", "--test", action="store_true",
+                        help="Enable test mode (commands will only be printed)")
     args = parser.parse_args()
 
     main(test_mode=args.test)
