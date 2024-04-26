75de93f9fcc397affda31074b5c49e7b65bd57bb
Justin <justintorre75@gmail.com>
2024-04-25 23:49:03 -0700

    fix type checking

--- web/components/shared/markdownEditor.tsx
+++ web/components/shared/markdownEditor.tsx
@@ -18,7 +18,11 @@ const MarkdownEditor = (props: MarkdownEditorProps) => {
     <Editor
       value={text}
       onValueChange={setText}
-      highlight={(code) => highlight(code, languages.markdown, "markdown")}
+      highlight={(code) => {
+        if (!code) return;
+        if (typeof code !== "string") return;
+        highlight(code, languages.markdown, "markdown");
+      }}
       padding={10}
       className="text-sm border border-gray-300 dark:border-gray-700 rounded-lg"
       // mono font
