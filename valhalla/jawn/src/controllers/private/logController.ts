09d1ab1a190590673bd6c490adaa1927e69b43ce
colegottdank <colegottdank@gmail.com>
2024-05-06 16:56:19 -0700

    chore: Add log controller for handling log requests (#1826)

---
+++ valhalla/jawn/src/controllers/private/logController.ts
@@ -0,0 +1,28 @@
+// src/users/usersController.ts
+import { Body, Controller, Post, Request, Route, Security, Tags } from "tsoa";
+import { FilterLeafSubset } from "../../lib/shared/filters/filterDefs";
+import { JawnAuthenticatedRequest } from "../../types/request";
+import { Message } from "../../lib/handlers/HandlerContext";
+import { LogManager } from "../../managers/LogManager";
+
+@Route("v1/log")
+@Tags("Log")
+@Security("api_key")
+export class RequestController extends Controller {
+  /**
+   * @param logMessage Log message to log
+   */
+  @Post("/request")
+  public async getRequests(
+    @Body()
+    logMessage: Message,
+    @Request() request: JawnAuthenticatedRequest
+  ): Promise<void> {
+    const logManager = new LogManager();
+    await logManager.processLogEntry(logMessage);
+
+    // If there is a failure, not much we can do here
+    this.setStatus(200);
+    return;
+  }
+}
