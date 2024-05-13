256216df4ed2715e6c0d6a957433cd389336449e
colegottdank <colegottdank@gmail.com>
2024-05-12 17:46:49 -0700

    fix tests

--- supabase/migrations/20240513001658_response-org-id.sql
+++ supabase/migrations/20240513001658_response-org-id.sql
@@ -1,2 +1,6 @@
 ALTER TABLE response
-ADD COLUMN helicone_org_id uuid null
+ADD COLUMN helicone_org_id uuid null;
+ALTER TABLE response
+ADD CONSTRAINT unique_request_helicone_org_id UNIQUE (request, helicone_org_id);
+ALTER TABLE request
+ADD CONSTRAINT unique_id_helicone_org_id UNIQUE (id, helicone_org_id);
