cf855f5207710d2d0970a4cb929c4655aea0b859
Justin <justintorre75@gmail.com>
2024-05-20 09:32:14 -0700

    moving kafka to worker threads

--- valhalla/jawn/src/workers/kafkaConsumer.ts
+++ valhalla/jawn/src/workers/kafkaConsumer.ts
@@ -1,9 +1,12 @@
 import { parentPort } from "worker_threads";
-import { consume } from "./../lib/clients/KafkaConsumer";
+import { consume, consumeDlq } from "./../lib/clients/KafkaConsumer";
 
 parentPort?.once("message", (message) => {
   if (message === "start") {
     console.log("Kafka consumer thread started!");
     consume();
+  } else if (message === "start-dlq") {
+    console.log("Kafka DLQ consumer thread started!");
+    consumeDlq();
   }
 });
