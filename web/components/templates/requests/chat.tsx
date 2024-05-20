2bca62a0092a75eaa8efa2a7d4c042e437d7031c
Scott Nguyen <scott@helicone.ai>
2024-05-20 11:05:47 -0700

    add claude chat

--- web/components/templates/requests/chat.tsx
+++ web/components/templates/requests/chat.tsx
@@ -549,7 +549,6 @@ export const Chat = (props: ChatProps) => {
               {!(
                 model === "gpt-4-vision-preview" ||
                 model === "gpt-4-1106-vision-preview" ||
-                /^claude/.test(model) ||
                 requestId === ""
               ) && (
                 <button
