fe4eeec3cdf36ff62f8b714dce4fb8ccad01710e
Justin <justintorre75@gmail.com>
2024-05-21 10:58:21 -0700

    kafka consumer to function and remove all global variables

---
+++ valhalla/jawn/src/lib/clients/kafkaConsumers/constant.ts
@@ -0,0 +1,6 @@
+export const AVG_MESSAGE_SIZE = 2_000; // 2kB
+export const ESTIMATED_MINI_BATCH_COUNT = 3; // 3
+export const MESSAGES_PER_MINI_BATCH = 300;
+
+// DLQ
+export const DLQ_MESSAGES_PER_MINI_BATCH = 1; // 1
