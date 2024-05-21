e1642e067704b71e15262d939b9d165f30a9e7d9
Scott <50155740+ScottMktn@users.noreply.github.com>
2024-05-21 12:04:02 -0700

    admin-panel (#1975)

---
+++ web/components/templates/admin/adminPage.tsx
@@ -0,0 +1,24 @@
+import AlertBanners from "./panels/alertBanners";
+import OrgMember from "./panels/orgMember";
+
+interface AdminPageProps {}
+
+const AdminPage = (props: AdminPageProps) => {
+  const {} = props;
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <h1 className="text-2xl font-semibold">Admin Dashboard</h1>
+      <ul className="flex flex-col space-y-8 max-w-4xl">
+        <li className="w-full h-full rounded-lg flex flex-col bg-gray-500 p-4 space-y-4">
+          <AlertBanners />
+        </li>
+        <li className="w-full h-full rounded-lg flex flex-col bg-gray-500 p-4 space-y-4">
+          <OrgMember />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AdminPage;
