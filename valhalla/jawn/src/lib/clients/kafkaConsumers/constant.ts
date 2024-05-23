40d18ac54376a3d88ae3ccebc9c358112a76b58b
Justin Torre <justintorre75@gmail.com>
2024-05-23 08:52:40 -0700

    decrease dlq (#2001)

--- valhalla/jawn/src/lib/clients/kafkaConsumers/constant.ts
+++ valhalla/jawn/src/lib/clients/kafkaConsumers/constant.ts
@@ -3,7 +3,7 @@ export const ESTIMATED_MINI_BATCH_COUNT = 3; // 3
 export const MESSAGES_PER_MINI_BATCH = 300;
 
 // DLQ
-export const DLQ_ESTIMATED_MINI_BATCH_COUNT = 100;
+export const DLQ_ESTIMATED_MINI_BATCH_COUNT = 10;
 export const DLQ_MESSAGES_PER_MINI_BATCH = +(
   process.env.DLQ_MESSAGES_PER_MINI_BATCH ?? 1
 ); // 1
