2b39b919e25f51fd40dad2df73a74940688b38db
colegottdank <colegottdank@gmail.com>
2024-05-23 23:14:48 -0700

    make id a pk (#2020)

--- supabase/migrations/20240523055042_helicone-settings.sql
+++ supabase/migrations/20240523055042_helicone-settings.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS public.helicone_settings (
-    id uuid not null default gen_random_uuid(),
+    id uuid primary key not null default gen_random_uuid(),
     name text not null,
     settings jsonb not null,
     created_at timestamp with time zone not null default now()
