a4295181c9c632752f9cf48804745931d5cda52d
Justin <justintorre75@gmail.com>
2024-05-28 16:29:19 -0700

    users view fix

--- supabase/migrations/20240528214813_users_view.sql
+++ supabase/migrations/20240528214813_users_view.sql
@@ -1,8 +1,8 @@
-create view public.users as 
+create view public.users_view as 
 select 
     id,
     email, 
     last_sign_in_at,
     created_at
 from auth.users;
-revoke all on public.users from anon, authenticated;
+revoke all on public.users_view from anon, authenticated;
