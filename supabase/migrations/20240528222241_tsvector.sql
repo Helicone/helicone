4999a1d9cd5f3599f09477155f9bfe9797d88301
Justin <justintorre75@gmail.com>
2024-06-03 11:39:58 -0700

    yay

--- supabase/migrations/20240528222241_tsvector.sql
+++ supabase/migrations/20240528222241_tsvector.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.request_response_search (
-    id uuid not null default gen_random_uuid(),
+    id uuid primary key not null default gen_random_uuid(),
     request_id uuid not null,
     organization_id uuid not null,
     request_body_vector tsvector,
@@ -7,14 +7,7 @@ CREATE TABLE public.request_response_search (
     created_at timestamp with time zone not null default now()
 );
 
-ALTER TABLE IF EXISTS public.request_response_search
-    ENABLE ROW LEVEL SECURITY;
-
-revoke all on public.request_response_search from anon, authenticated;
-
 CREATE UNIQUE INDEX request_response_search_unique ON public.request_response_search USING BTREE (request_id, organization_id);
-
-ALTER TABLE "public"."request_response_search" ADD CONSTRAINT "request_response_search_layout_pkey" PRIMARY KEY USING INDEX "request_response_search_pkey";
 
 ALTER TABLE "public"."request_response_search" ADD CONSTRAINT "request_response_search_request_id_fkey" FOREIGN KEY (request_id) REFERENCES request(id) ON UPDATE cascade ON DELETE CASCADE NOT valid;
 
@@ -23,6 +16,13 @@ ALTER TABLE "public"."request_response_search" ADD CONSTRAINT "request_response_
 ALTER TABLE "public"."request_response_search" validate CONSTRAINT "request_response_search_request_id_fkey";
 
 ALTER TABLE "public"."request_response_search" validate CONSTRAINT "request_response_search_organization_id_fkey";
+
+
+ALTER TABLE IF EXISTS public.request_response_search
+    ENABLE ROW LEVEL SECURITY;
+
+revoke all on public.request_response_search from anon, authenticated;
+
 
 CREATE TEXT SEARCH DICTIONARY helicone_stopwords (
     TEMPLATE = snowball,
