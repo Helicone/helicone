e0629229ada67d58a2ff42794dbda5831b498f46
Stefan Bokarev <38397684+maamalama@users.noreply.github.com>
2024-06-03 11:33:35 -0700

    tsvector search table writes (#2049)

---
+++ supabase/migrations/20240528222241_tsvector.sql
@@ -0,0 +1,32 @@
+CREATE TABLE request_response_search (
+    id uuid not null default gen_random_uuid(),
+    request_id uuid not null,
+    organization_id uuid not null,
+    request_body_vector tsvector,
+    response_body_vector tsvector,
+    created_at timestamp with time zone not null default now()
+);
+
+CREATE UNIQUE INDEX request_response_search_unique ON public.request_response_search USING BTREE (request_id, organization_id);
+
+ALTER TABLE "public"."request_response_search" ADD CONSTRAINT "request_response_search_layout_pkey" PRIMARY KEY USING INDEX "request_response_search_pkey";
+
+ALTER TABLE "public"."request_response_search" ADD CONSTRAINT "request_response_search_request_id_fkey" FOREIGN KEY (request_id) REFERENCES request(id) ON UPDATE cascade ON DELETE CASCADE NOT valid;
+
+ALTER TABLE "public"."request_response_search" ADD CONSTRAINT "request_response_search_organization_id_fkey" FOREIGN KEY (organization_id) references organization(id) ON UPDATE cascade ON DELETE CASCADE NOT valid;
+
+ALTER TABLE "public"."request_response_search" validate CONSTRAINT "request_response_search_request_id_fkey";
+
+ALTER TABLE "public"."request_response_search" validate CONSTRAINT "request_response_search_organization_id_fkey";
+
+CREATE TEXT SEARCH DICTIONARY helicone_stopwords (
+    TEMPLATE = snowball,
+    Language = english,
+    StopWords = english
+);
+
+CREATE TEXT SEARCH CONFIGURATION helicone_search_config (COPY = pg_catalog.english);
+
+ALTER TEXT SEARCH CONFIGURATION helicone_search_config
+    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart
+    WITH helicone_stopwords;
--- valhalla/jawn/src/lib/db/database.types.ts
+++ valhalla/jawn/src/lib/db/database.types.ts
@@ -2075,6 +2075,48 @@ export type Database = {
           },
         ]
       }
+      request_response_search: {
+        Row: {
+          created_at: string
+          id: string
+          organization_id: string
+          request_body_vector: unknown | null
+          request_id: string
+          response_body_vector: unknown | null
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          organization_id: string
+          request_body_vector?: unknown | null
+          request_id: string
+          response_body_vector?: unknown | null
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          organization_id?: string
+          request_body_vector?: unknown | null
+          request_id?: string
+          response_body_vector?: unknown | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "request_response_search_organization_id_fkey"
+            columns: ["organization_id"]
+            isOneToOne: false
+            referencedRelation: "organization"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "request_response_search_request_id_fkey"
+            columns: ["request_id"]
+            isOneToOne: false
+            referencedRelation: "request"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       response: {
         Row: {
           body: Json
@@ -2702,101 +2744,6 @@ export type Database = {
           },
         ]
       }
-      s3_multipart_uploads: {
-        Row: {
-          bucket_id: string
-          created_at: string
-          id: string
-          in_progress_size: number
-          key: string
-          owner_id: string | null
-          upload_signature: string
-          version: string
-        }
-        Insert: {
-          bucket_id: string
-          created_at?: string
-          id: string
-          in_progress_size?: number
-          key: string
-          owner_id?: string | null
-          upload_signature: string
-          version: string
-        }
-        Update: {
-          bucket_id?: string
-          created_at?: string
-          id?: string
-          in_progress_size?: number
-          key?: string
-          owner_id?: string | null
-          upload_signature?: string
-          version?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "s3_multipart_uploads_bucket_id_fkey"
-            columns: ["bucket_id"]
-            isOneToOne: false
-            referencedRelation: "buckets"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
-      s3_multipart_uploads_parts: {
-        Row: {
-          bucket_id: string
-          created_at: string
-          etag: string
-          id: string
-          key: string
-          owner_id: string | null
-          part_number: number
-          size: number
-          upload_id: string
-          version: string
-        }
-        Insert: {
-          bucket_id: string
-          created_at?: string
-          etag: string
-          id?: string
-          key: string
-          owner_id?: string | null
-          part_number: number
-          size?: number
-          upload_id: string
-          version: string
-        }
-        Update: {
-          bucket_id?: string
-          created_at?: string
-          etag?: string
-          id?: string
-          key?: string
-          owner_id?: string | null
-          part_number?: number
-          size?: number
-          upload_id?: string
-          version?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "s3_multipart_uploads_parts_bucket_id_fkey"
-            columns: ["bucket_id"]
-            isOneToOne: false
-            referencedRelation: "buckets"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "s3_multipart_uploads_parts_upload_id_fkey"
-            columns: ["upload_id"]
-            isOneToOne: false
-            referencedRelation: "s3_multipart_uploads"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
     }
     Views: {
       [_ in never]: never
@@ -2834,37 +2781,6 @@ export type Database = {
         Returns: {
           size: number
           bucket_id: string
-        }[]
-      }
-      list_multipart_uploads_with_delimiter: {
-        Args: {
-          bucket_id: string
-          prefix_param: string
-          delimiter_param: string
-          max_keys?: number
-          next_key_token?: string
-          next_upload_token?: string
-        }
-        Returns: {
-          key: string
-          id: string
-          created_at: string
-        }[]
-      }
-      list_objects_with_delimiter: {
-        Args: {
-          bucket_id: string
-          prefix_param: string
-          delimiter_param: string
-          max_keys?: number
-          start_after?: string
-          next_token?: string
-        }
-        Returns: {
-          name: string
-          id: string
-          metadata: Json
-          updated_at: string
         }[]
       }
       search: {
