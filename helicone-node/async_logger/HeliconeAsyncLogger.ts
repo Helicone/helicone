20b3c8759241b71ef30e9813f02ed63dd63bd9db
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-06-06 12:14:11 -0700

    feat: revamp async logger using traceloop

--- helicone-node/async_logger/HeliconeAsyncLogger.ts
+++ helicone-node/async_logger/HeliconeAsyncLogger.ts
@@ -31,12 +31,16 @@ type IHeliconeAsyncLoggerOptions = {
     },
     chromadb?: typeof chromadb,
   },
+<<<<<<< HEAD
   headers?: Partial<Record<string, unknown>>
+=======
+>>>>>>> ebfbf5e2 (Replaced async logger with traceloop. Testing left)
 };
 
 export class HeliconeAsyncLogger {
   private apiKey: string;
   private baseUrl: string;
+<<<<<<< HEAD
   private openAI?: typeof OpenAI;
   private anthropic?: typeof anthropic;
   private azureOpenAI?: typeof azureOpenAI;
@@ -49,10 +53,24 @@ export class HeliconeAsyncLogger {
   private toolsModule?: typeof ToolsModule;
   private chromadb?: typeof chromadb;
   private headers?: Partial<Record<string, unknown>>;
+=======
+  private openAI: typeof OpenAI;
+  private anthropic: typeof anthropic;
+  private azureOpenAI: typeof azureOpenAI;
+  private cohere: typeof cohere;
+  private bedrock: typeof bedrock;
+  private google_aiplatform: typeof google_aiplatform;
+  private pinecone: typeof pinecone;
+  private chainsModule: typeof ChainsModule;
+  private agentsModule: typeof AgentsModule;
+  private toolsModule: typeof ToolsModule;
+  private chromadb: typeof chromadb;
+>>>>>>> ebfbf5e2 (Replaced async logger with traceloop. Testing left)
 
   constructor(opts: IHeliconeAsyncLoggerOptions) {
     this.apiKey = opts.apiKey;
     this.baseUrl = opts.baseUrl;
+<<<<<<< HEAD
     this.openAI = opts.providers?.openAI ?? undefined;
     this.anthropic = opts.providers?.anthropic ?? undefined;
     this.azureOpenAI = opts.providers?.azureOpenAI ?? undefined;
@@ -60,16 +78,29 @@ export class HeliconeAsyncLogger {
     this.bedrock = opts.providers?.bedrock ?? undefined;
     this.google_aiplatform = opts.providers?.google_aiplatform ?? undefined;
     this.pinecone = opts.providers?.pinecone ?? undefined;
+=======
+    this.openAI = opts.providers.openAI ?? undefined;
+    this.anthropic = opts.providers.anthropic ?? undefined;
+    this.azureOpenAI = opts.providers.azureOpenAI ?? undefined;
+    this.cohere = opts.providers.cohere ?? undefined;
+    this.bedrock = opts.providers.bedrock ?? undefined;
+    this.google_aiplatform = opts.providers?.google_aiplatform ?? undefined;
+    this.pinecone = opts.providers.pinecone ?? undefined;
+>>>>>>> ebfbf5e2 (Replaced async logger with traceloop. Testing left)
     this.chainsModule = opts.providers?.langchain?.chainsModule ?? undefined;
     this.agentsModule = opts.providers?.langchain?.agentsModule ?? undefined;
     this.toolsModule = opts.providers?.langchain?.toolsModule ?? undefined;
     this.chromadb = opts.providers?.chromadb ?? undefined;
+<<<<<<< HEAD
     this.headers = opts.headers;
+=======
+>>>>>>> ebfbf5e2 (Replaced async logger with traceloop. Testing left)
   }
 
   init() {
     traceloop.initialize({
       apiKey: this.apiKey,
+<<<<<<< HEAD
       disableBatch: true,
       exporter: new OTLPTraceExporter({
         url: this.baseUrl,
@@ -91,6 +122,27 @@ export class HeliconeAsyncLogger {
           agentsModule: this.agentsModule ?? undefined,
           toolsModule: this.toolsModule ?? undefined,
         },
+=======
+      baseUrl: this.baseUrl,
+      disableBatch: true,
+      exporter: new OTLPTraceExporter({
+        url: this.baseUrl,
+        headers: { Authorization: `Bearer ${this.apiKey}` },
+      }),
+      instrumentModules: {
+        openAI: this.openAI,
+        anthropic: this.anthropic ?? undefined,
+        azureOpenAI: this.azureOpenAI ?? undefined,
+        cohere: this.cohere ?? undefined,
+        bedrock: this.bedrock ?? undefined,
+        google_aiplatform: this.google_aiplatform ?? undefined,
+        pinecone: this.pinecone ?? undefined,
+        langchain: {
+          chainsModule: this.chainsModule ?? undefined,
+          agentsModule: this.agentsModule ?? undefined,
+          toolsModule: this.toolsModule ?? undefined,
+        },
+>>>>>>> ebfbf5e2 (Replaced async logger with traceloop. Testing left)
         chromadb: this?.chromadb ?? undefined,
       },
     });
