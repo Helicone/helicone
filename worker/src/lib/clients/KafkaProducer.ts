3ef90fcef0bc997ef3e3601ebce88b098dc0ab14
colegottdank <colegottdank@gmail.com>
2024-06-06 10:58:13 -0700

    fix local

--- worker/src/lib/clients/KafkaProducer.ts
+++ worker/src/lib/clients/KafkaProducer.ts
@@ -109,6 +109,7 @@ export class KafkaProducer {
 
   async sendMessageHttp(msg: KafkaMessage) {
     try {
+      console.log(`Sending message via REST: ${JSON.stringify(msg)}`);
       const result = await fetch(`${this.VALHALLA_URL}/v1/log/request`, {
         method: "POST",
         headers: {
