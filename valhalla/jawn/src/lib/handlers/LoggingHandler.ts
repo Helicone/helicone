d88f7b5a047923b054d00f174c1de51b0660a55b
Stefan Bokarev <38397684+maamalama@users.noreply.github.com>
2024-06-11 10:58:40 -0700

    Cut 100 more kb for tsvector buffer (#2142)
    * cut 100 more kb
    
    * 500kb

--- valhalla/jawn/src/lib/handlers/LoggingHandler.ts
+++ valhalla/jawn/src/lib/handlers/LoggingHandler.ts
@@ -520,7 +520,7 @@ export class LoggingHandler extends AbstractLogHandler {
   }
 
   private ensureMaxVectorLength = (text: string): string => {
-    const maxBytes = 848000; // ~300k less than 1MB for buffer
+    const maxBytes = 512000; // ~500k less than 1MB for buffer
     text = text.replace(/[^\x00-\x7F]/g, "");
     text = text.trim();
 
