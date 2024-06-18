52e7628f61f90584725a6a4c3647981c227dee13
Lina Lam <tt3lam@uwaterloo.ca>
2024-06-18 16:56:24 -0700

    updated community projects details + thumbnails

--- bifrost/app/community/projects.tsx
+++ bifrost/app/community/projects.tsx
@@ -11,11 +11,28 @@ const TAGS: Record<string, ProjectTag> = {
     name: "Learning",
     href: "",
   },
+  tech: {
+    name: "Tech",
+    href: "",
+  },
+  marketing: {
+    name: "Digital Marketing",
+    href: "",
+  },
+  education: {
+    name: "Education",
+    href: "",
+  },
+  healthcare: {
+    name: "Healthcare",
+    href: "",
+  },
 };
 
 interface Project {
   title: string;
   description: string;
+  usage: string;
   creators: {
     name: string;
     href: string;
@@ -24,55 +41,231 @@ interface Project {
   tags: ProjectTag[];
   href: string;
   isMonthlySpotlight?: true;
+  isOpenSourced?: true;
 }
 
 const projects: Project[] = [
   {
-    title: "Deap Learning",
+    title: "Haema",
     description:
-      "A group of students at Duke university are creating an online Youtube channel to teach deep learning concepts.",
+      "An AI mobile app to help people with diabetes learn how food and exercise affects their blood sugar.",
+    usage: 
+      "We use Helicone to log all of the requests to our AI and we're using the logged data to directly improve our product.", 
     creators: [
       {
-        name: "Anish",
-        href: "",
+        name: "Pranav Ahluwalia",
+        href: "https://x.com/haema_co",
+      },
+      {
+        name: "Pranav",
+        href: "https://x.com/PranavAhl",
+      }
+    ],
+    imageHref: "/static/community/projects/haema.webp",
+    tags: [TAGS.Healthcare],
+    href: "https://www.haema.co/",
+  },
+  {
+    title: "DemoFox",
+    description:
+      "Instantly translates jargon and articulates business value, making inter-department communication hassle-free.",
+    usage: 
+      "We use Helicone to understand why a prompt fails to return valid data.", 
+    creators: [
+      {
+        name: "Tim Elam",
+        href: "https://linkedin.com/company/demofox",
       },
     ],
-    imageHref: "/static/deep-learning.jpg",
-    tags: [TAGS.learning],
+    imageHref: "/static/community/projects/demofox.webp",
+    tags: [TAGS.Tech],
+    href: "https://www.demofox.com",
+  },
+  {
+    title: "CodeCrafters",
+    description:
+      "Practice writing complex software.",
+    usage: 
+      "We use Helicone for monitoring costs on our LLM features in production. In development it also helps with inspecting the final prompts we're generating and allows quickly tweaking and experimenting using the Playground.",
+    creators: [
+      {
+        name: "Paul Kuruvilla",
+        href: "https://x.com/rohitpaulk",
+      },
+      {
+        name: "Sarup Banskota", 
+        href: "https://x.com/sarupbanskota"
+      }
+    ],
+    imageHref: "/static/community/projects/codecrafters.webp",
+    tags: [TAGS.Education],
+    isOpenSourced: true,
+    href: "https://codecrafters.io/",
+  },
+  {
+    title: "Reworkd",
+    description:
+      "Either AgentGPT or some new stuff we're working on!",
+    usage: 
+      "API logging and cost analysis.",
+    creators: [
+      {
+        name: "Asim Shrestha",
+        href: "https://www.linkedin.com/company/reworkd/",
+      },
+    ],
+    imageHref: "/static/community/projects/reworkd.webp",
+    tags: [TAGS.Tech],
+    isOpenSourced: true,
+    href: "https://github.com/reworkd/",
+  },
+  {
+    title: "Jsonify",
+    description:
+      "Use AI to turn websites and documents into useful structured data. ",
+    usage: 
+      "Keep track of OpenAI metrics -- cost, latency, failures, etc.",
+    creators: [
+      {
+        name: "Paul Hunkin",
+        href: "https://www.linkedin.com/in/phunkin/",
+      },
+      {
+        name: "Nick Linkevich",
+        href: "https://www.linkedin.com/in/nicklink/",
+      },
+    ],
+    imageHref: "/static/community/projects/jsonify.webp",
+    tags: [TAGS.Tech],
+    href: "https://jsonify.com",
+  },
+  {
+    title: "assistant-ui",
+    description:
+      "React components for AI chat.",
+    usage: 
+      "Get a detailed token cost breakdown per user.",
+    creators: [
+      {
+        name: "Simon Farshid",
+        href: "http://linkedin.com/in/simon-farshid",
+      },
+    ],
+    imageHref: "/static/community/projects/assistant-ui.webp",
+    tags: [TAGS.Tech],
+    isOpenSourced: true,
+    href: "https://github.com/Yonom/assistant-ui",
+  },
+  {
+    title: "PitchGhost",
+    description:
+      "Help companies automatically find customers on social media by scanning and scraping Twitter, Reddit, LinkedIn, etc for posts and content that would make perfect sense for your brand or business to engage with.",
+    usage: 
+      "Our platform involves tons of calls to LLMs to read and digest all of the social media posts we scan through. Helicone has been invaluable to us in checking in and monitoring these systems and especially debugging LLM calls through the playground.",
+    creators: [
+      {
+        name: "Marc Frankel",
+        href: "https://twitter.com/pitchghost",
+      },
+    ],
+    imageHref: "/static/community/projects/pitchghost.webp",
+    tags: [TAGS.marketing],
     isMonthlySpotlight: true,
-    href: "",
+    href: "https://pitchghost.com",
+  },
+  {
+    title: "mangosqueezy",
+    description:
+      "An affiliate marketing platform where businesses connect with affiliates to grow their sales or audience. It's the first crypto-based affiliate marketing platform, which facilitates cross-border payments, real-time settlement in seconds, and zero processing fees.",
+    usage: 
+      "We are using Helicone to monitor our LLM usage so we know how many tokens we have used and which countries the usage is coming from. Additionally, we plan to implement a user rate limiting feature soon.",
+    creators: [
+      {
+        name: "Amit Mirgal",
+        href: "https://x.com/amit_mirgal",
+      },
+      {
+        name: "Devika Mahajan",
+        href: "https://www.linkedin.com/in/devika--mahajan/",
+      },
+    ],
+    imageHref: "/static/community/projects/mangosqueezy.webp",
+    tags: [TAGS.Marketing],
+    isOpenSourced: true,
+    href: "https://github.com/mangosqueezy/mangosqueezy",
+  },
+  {
+    title: "Greptile",
+    description:
+      "AI expert on any codebase, as an API.",
+    usage: 
+      "Tracing and logging LLM calls to debug, track usage, manage costs, and prepare datasets for finetuning.",
+    creators: [
+      {
+        name: "Daksh Gupta",
+        href: "https://twitter.com/dakshgup",
+      },
+      {
+        name: "Soohoon Choi",
+        href: "https://twitter.com/soohoonchoi",
+      },
+      {
+        name: "Vaishant Kameswaran",
+        href: "https://twitter.com/vaishaaant",
+      },
+    ],
+    imageHref: "/static/community/projects/greptile.webp",
+    isMonthlySpotlight: true,
+    tags: [TAGS.Tech],
+    href: "https://greptile.com",
+  },
+  {
+    title: "LinkedInFy",
+    description:
+      "Automate your LinkedIn, there is no need to waste hrs on your LinkedIn to think about what to post AI will do it for you.",
+    usage: 
+      "To monitor different LLM model results and see prompts result.",
+    creators: [
+      {
+        name: "Abhishek",
+        href: "https://x.com/abhishk_084",
+      },
+    ],
+    imageHref: "/static/community/projects/linkedinfy.webp",
+    tags: [TAGS.tech],
+    href: "https://www.linkedinfy.com/",
   },
 ];
 
 export function Projects() {
-  return (
-    <div>
-      <div className="md:gap-2 md:flex-row md:justify-start sm:items-start flex flex-col justify-start items-start  bg-[#F0F9FF] border-[#0CA5E9] border border-opacity-20 rounded-lg mx-[12px] p-[24px] mt-[24px]">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={"/static/community/stars_icon.svg"}
-          alt="Deep Learning"
-          className="h-[24px]"
-        />
-        <div className="mt-[24px] text-[#0CA5E9] font-bold sm:mt-0 sm:whitespace-nowrap">
-          Using Helicone?{" "}
-        </div>
-        <div className="text-[#0CA5E9] sm:whitespace-nowrap">
-          We want to know what you are building!{" "}
-        </div>
+  // return (
+  //   <div>
+  //     <div className="md:gap-2 md:flex-row md:justify-start sm:items-start flex flex-col justify-start items-start  bg-[#F0F9FF] border-[#0CA5E9] border border-opacity-20 rounded-lg mx-[12px] p-[24px] mt-[24px]">
+  //       {/* eslint-disable-next-line @next/next/no-img-element */}
+  //       <img
+  //         src={"/static/community/stars_icon.svg"}
+  //         alt="Deep Learning"
+  //         className="h-[24px]"
+  //       />
+  //       <div className="mt-[24px] text-[#0CA5E9] font-bold sm:mt-0 sm:whitespace-nowrap">
+  //         Using Helicone?{" "}
+  //       </div>
+  //       <div className="text-[#0CA5E9] sm:whitespace-nowrap">
+  //         We want to know what you are building!{" "}
+  //       </div>
 
-        <Link
-          href={"https://forms.gle/WpTEEE6vVdQccprD9"}
-          className="text-[#0CA5E9] sm:whitespace-nowrap"
-        >
-          <u className=" hover:text-[#0CA5E9] font-semibold">
-            Fill out this form
-          </u>{" "}
-          to be featured.
-        </Link>
-      </div>
-    </div>
-  );
+  //       <Link
+  //         href={"https://forms.gle/WpTEEE6vVdQccprD9"}
+  //         className="text-[#0CA5E9] sm:whitespace-nowrap"
+  //       >
+  //         <u className=" hover:text-[#0CA5E9] font-semibold">
+  //           Fill out this form
+  //         </u>{" "}
+  //         to be featured.
+  //       </Link>
+  //     </div>
+  //   </div>
+  // );
   return (
     <div className="grid grid-cols-2 space-y-8">
       {projects.map((project, i) => {
