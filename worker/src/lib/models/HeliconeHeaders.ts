b03a5549349bbe50ec94bc61852cc5001f48d53c
Justin Torre <justintorre75@gmail.com>
2024-06-21 09:12:04 +0800

    lytix integration (#2157)

--- worker/src/lib/models/HeliconeHeaders.ts
+++ worker/src/lib/models/HeliconeHeaders.ts
@@ -51,6 +51,7 @@ export interface IHeliconeHeaders {
   promptSecurityEnabled: Nullable<string>;
   moderationsEnabled: boolean;
   posthogKey: Nullable<string>;
+  lytixKey: Nullable<string>;
   posthogHost: Nullable<string>;
   webhookEnabled: boolean;
 }
@@ -92,6 +93,7 @@ export class HeliconeHeaders implements IHeliconeHeaders {
   posthogKey: Nullable<string>;
   posthogHost: Nullable<string>;
   webhookEnabled: boolean;
+  lytixKey: Nullable<string>;
 
   constructor(private headers: Headers) {
     const heliconeHeaders = this.getHeliconeHeaders();
@@ -119,6 +121,7 @@ export class HeliconeHeaders implements IHeliconeHeaders {
     this.modelOverride = heliconeHeaders.modelOverride;
     this.promptSecurityEnabled = heliconeHeaders.promptSecurityEnabled;
     this.moderationsEnabled = heliconeHeaders.moderationsEnabled;
+    this.lytixKey = heliconeHeaders.lytixKey;
     this.posthogKey = heliconeHeaders.posthogKey;
     this.posthogHost = heliconeHeaders.posthogHost;
     this.webhookEnabled = heliconeHeaders.webhookEnabled;
@@ -244,6 +247,7 @@ export class HeliconeHeaders implements IHeliconeHeaders {
           ? true
           : false,
       posthogKey: this.headers.get("Helicone-Posthog-Key") ?? null,
+      lytixKey: this.headers.get("Helicone-Lytix-Key") ?? null,
       posthogHost: this.headers.get("Helicone-Posthog-Host") ?? null,
       webhookEnabled:
         this.headers.get("Helicone-Webhook-Enabled") == "true" ? true : false,
