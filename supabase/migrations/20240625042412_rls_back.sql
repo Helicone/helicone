ad98a15848f57d876ab29ad8fc66c285940ef4bb
maamalama <bokarevstefan475@gmail.com>
2024-06-25 10:31:37 -0700

    insert trigger

--- supabase/migrations/20240625042412_rls_back.sql
+++ supabase/migrations/20240625042412_rls_back.sql
@@ -23,3 +23,14 @@ SELECT TO PUBLIC USING (
             )
         )
     );
+
+-- Function to add owner as member
+CREATE OR REPLACE FUNCTION organization_insert() RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER AS $$ BEGIN
+INSERT INTO public.organization_member (created_at, member, organization, org_role)
+VALUES (NEW.created_at, NEW.owner, NEW.id, 'owner');
+RETURN NEW;
+END $$;
+-- Trigger to add owner as member
+CREATE TRIGGER organization_insert_trigger
+AFTER
+INSERT ON public.organization FOR EACH ROW EXECUTE FUNCTION organization_insert();
