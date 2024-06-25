4c22fe2f936d1119f444b4615c2dc73ffa68203f
colegottdank <colegottdank@gmail.com>
2024-06-25 09:49:15 -0700

    more fixes

--- supabase/migrations/20240625162826_organization_rls_fix.sql
+++ supabase/migrations/20240625162826_organization_rls_fix.sql
@@ -18,3 +18,46 @@ SELECT TO PUBLIC USING (
     );
 create policy "Enable read access for all users" on "public"."organization_member" as permissive for
 select to public using ((member = auth.uid()));
+create policy "Enable only owner can update" on "public"."organization" as permissive for
+update to public using (
+        (
+            (owner = auth.uid())
+            OR (
+                auth.uid() IN (
+                    SELECT om.member
+                    FROM organization_member om
+                    WHERE (om.organization = organization.id)
+                )
+            )
+        )
+    ) with check (
+        (
+            (owner = auth.uid())
+            OR (
+                auth.uid() IN (
+                    SELECT om.member
+                    FROM organization_member om
+                    WHERE (om.organization = organization.id)
+                )
+            )
+        )
+    );
+CREATE OR REPLACE FUNCTION public.check_personal_soft_deleted_constraint() RETURNS trigger LANGUAGE plpgsql AS $function$ BEGIN IF NEW.is_personal = TRUE
+    AND NEW.soft_delete = TRUE THEN RAISE EXCEPTION 'is_personal and soft_delete cannot be both true for an organization';
+END IF;
+RETURN NEW;
+END;
+$function$;
+CREATE TRIGGER check_personal_soft_deleted_constraint_trigger BEFORE
+INSERT
+    OR
+UPDATE ON public.organization FOR EACH ROW EXECUTE FUNCTION check_personal_soft_deleted_constraint();
+CREATE OR REPLACE FUNCTION organization_insert() RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER AS $$ BEGIN
+INSERT INTO public.organization_member (created_at, member, organization, org_role)
+VALUES (NEW.created_at, NEW.owner, NEW.id, 'owner');
+RETURN NEW;
+END $$;
+-- Trigger to add owner as member
+CREATE TRIGGER organization_insert_trigger
+AFTER
+INSERT ON public.organization FOR EACH ROW EXECUTE FUNCTION organization_insert();
