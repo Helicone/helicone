99d5dbb82fadfb774205320cce410e02fc8ebcda
Justin <justintorre75@gmail.com>
2024-06-27 15:01:52 -0700

    org create pass

--- valhalla/jawn/src/lib/stores/OrganizationStore.ts
+++ valhalla/jawn/src/lib/stores/OrganizationStore.ts
@@ -25,14 +25,16 @@ export class OrganizationStore extends BaseStore {
       return err(error.message ?? "Failed to create organization");
     }
 
-    const { data: memberInsert, error: memberError } = await supabaseServer.client
-      .from("organization_member")
-      .insert({
-        created_at: new Date().toISOString(),
-        member: createOrgParams.owner,
-        organization: insert.id,
-        org_role: "owner",
-      });
+    const { data: memberInsert, error: memberError } =
+      await supabaseServer.client
+        .from("organization_member")
+        .insert({
+          created_at: new Date().toISOString(),
+          member: createOrgParams.owner,
+          organization: insert.id,
+          org_role: "owner",
+        })
+        .select("*");
 
     if (memberError || !memberInsert) {
       return err(memberError?.message ?? "Failed to create organization");
