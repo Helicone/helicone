7bbb83ea5ca5b4e543be30fe30ceb4d7795a6ef6
Justin <justintorre75@gmail.com>
2024-06-29 10:46:44 -0700

    local storage use org id

--- web/services/hooks/localStorage.tsx
+++ web/services/hooks/localStorage.tsx
@@ -1,10 +1,15 @@
 import { useState, useEffect, useCallback } from "react";
+import { useOrg } from "../../components/layout/organizationContext";
 
 export function useLocalStorage<T>(
   key: string,
   initialValue: T,
   onNothingStored?: (setStored: (t: T) => void) => void
 ): [T, (t: T) => void] {
+  const org = useOrg();
+
+  const orgId = org?.currentOrg?.id ?? "";
+  const orgKey = `${orgId}_${key}`; // Updated key to include orgId
   const [storedValue, setStoredValue] = useState<T>(initialValue);
 
   const setValue = useCallback(
@@ -14,19 +19,19 @@ export function useLocalStorage<T>(
           value instanceof Function ? value(storedValue) : value;
         setStoredValue(valueToStore);
         if (typeof window !== "undefined") {
-          window.localStorage.setItem(key, JSON.stringify(valueToStore));
+          window.localStorage.setItem(orgKey, JSON.stringify(valueToStore)); // Use orgKey
         }
       } catch (error) {
         console.error(error);
       }
     },
-    [key, storedValue]
+    [orgKey, storedValue] // Updated dependency to orgKey
   );
 
   useEffect(() => {
     try {
       const item =
-        typeof window !== "undefined" && window.localStorage.getItem(key);
+        typeof window !== "undefined" && window.localStorage.getItem(orgKey); // Use orgKey
       if (!item) {
         throw new Error("No item stored");
       }
@@ -38,7 +43,7 @@ export function useLocalStorage<T>(
       onNothingStored && onNothingStored(setValue);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [key, onNothingStored, setValue]);
+  }, [orgKey, onNothingStored, setValue]); // Updated dependency to orgKey
 
   return [storedValue, setValue];
 }
