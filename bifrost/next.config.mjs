940b3f9b264c3c40038964fe5def04e492e7a8c5
Justin <justintorre75@gmail.com>
2024-07-10 13:21:47 -0700

    redirect broken links

--- bifrost/next.config.mjs
+++ bifrost/next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/onboarding",
+        destination: "https://us.helicone.ai/",
+        permanent: true,
+      },
+      {
+        source: "/video",
+        destination: "https://www.youtube.com/@helicone",
+        permanent: true,
+      },
+      {
+        source: "/roadmap",
+        destination: "https://us.helicone.ai/roadmap",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
