3ae4bce026fc2a6ed758b006f9f76737bb31c262
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-07-12 13:56:35 -0700

    feat: display session stats on top of table

--- web/components/templates/sessions/sessionDetails.tsx
+++ web/components/templates/sessions/sessionDetails.tsx
@@ -9,12 +9,26 @@ import { SortDirection } from "../../../services/lib/sorts/users/sorts";
 import { Col } from "../../layout/common/col";
 import ThemedTable from "../../shared/themed/table/themedTable";
 import { INITIAL_COLUMNS } from "./initialColumns";
+import { getTimeAgo } from "../../../lib/sql/timeHelpers";
+import { useEffect, useState } from "react";
+import { updateLocale } from "moment";
+
+type TSessions = {
+  created_at: string;
+  latest_request_created_at: string;
+  session: string;
+  total_cost: number;
+  total_requests: number;
+  prompt_tokens: number;
+  completion_tokens: number;
+  total_tokens: number;
+};
 
 interface SessionDetailsProps {
   selectedName: string;
   sessionIdSearch: string;
   setSessionIdSearch: (value: string) => void;
-  sessions: any[];
+  sessions: TSessions[];
   isLoading: boolean;
   sort: {
     sortKey: string | null;
@@ -40,7 +54,33 @@ const SessionDetails = ({
   setTimeFilter,
   setInterval,
 }: SessionDetailsProps) => {
+
   const router = useRouter();
+  const [lastUsedSession, setLastUsedSession] = useState<TSessions>();
+  const [oldestSession, setOldestSession] = useState<TSessions>();
+  const [totalCost, setTotalCost] = useState("0.0");
+
+  function calculateMetadata() {
+    if (sessions.length === 0) return;
+
+    setLastUsedSession(sessions.reduce((latestSession, currentSession) => {
+      return new Date(currentSession.latest_request_created_at) > new Date(latestSession.latest_request_created_at)
+        ? currentSession
+        : latestSession;
+    }))
+
+    setTotalCost(sessions.reduce((acc, session) => acc + session.total_cost, 0).toFixed(3));
+
+    setOldestSession(sessions.reduce((latestSession, currentSession) => {
+      return new Date(currentSession.latest_request_created_at) > new Date(latestSession.latest_request_created_at)
+        ? currentSession
+        : latestSession;
+    }))
+  };
+
+  useEffect(() => {
+    calculateMetadata();
+  }, [sessions]);
 
   return (
     <Col className="space-y-4 min-w-0">
@@ -48,6 +88,12 @@ const SessionDetails = ({
         <div className="text-xl font-semibold">
           {selectedName ? selectedName : "No Name"}
         </div>
+        <ul className="text-xs mt-1 text-gray-500 flex flex-row gap-5 list-disc">
+          <p>Last used <span className="font-semibold text-sky-500">{getTimeAgo(new Date(lastUsedSession?.latest_request_created_at ?? Date.now()))}</span></p>
+          <li className="font-semibold">{sessions.length} sessions</li>
+          <li>Total cost: <span className="font-semibold">${totalCost}</span></li>
+          <li>Created on: {(new Date(oldestSession?.created_at ?? Date.now())).toDateString().slice(4,)}</li>
+        </ul>
       </div>
       <TextInput
         icon={MagnifyingGlassIcon}
