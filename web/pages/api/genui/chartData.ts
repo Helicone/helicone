d1dc04ee5b8a95b75f76294fa7d3b465afa69cb9
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-07-18 15:25:52 -0700

    feat: api route

---
+++ web/pages/api/genui/chartData.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Result } from "../../../lib/result";
+
+import util from "util";
+
+export type TRes = {
+  chartType: "line" | "bar" | "donut",
+  data: any
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Result<TRes, string>>
+) {
+  const { prompt } = req.body as { prompt: string };
+
+  try {
+    const chartData = await fetch("http://127.0.0.1:8585/v1/genUi/chart", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + process.env.HELICONE_LOCAL_KEY,
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+
+    if (!chartData.ok) {
+      res.status(500).json({ error: "Internal server error", data: null });
+      return;
+    }
+
+    const result = (await chartData.json());
+    console.log(util.inspect(result, false, null, true));
+
+    if (result.error) {
+      res.status(500).json({ error: result.error, data: null });
+      return;
+    }
+
+    res.status(200).json({ error: null, data: { chartType: result.data.chartType, data: result.data.data } });
+    return;
+
+  } catch (err) {
+    res.status(500).json({ error: `${err}`, data: null });
+    return;
+  }
+}
