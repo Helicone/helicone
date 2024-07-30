ae35bfc6109be3db78b559e7dbe0c98444515d0b
maamalama <bokarevstefan475@gmail.com>
2024-07-29 19:00:24 -0700

    reques-response body

--- clickhouse/migrations/schema_38_request_response_versioned_bodies.sql
+++ clickhouse/migrations/schema_38_request_response_versioned_bodies.sql
@@ -1,2 +1,4 @@
 ALTER TABLE default.request_response_versioned
-ADD COLUMN `request_body` String;
+ADD COLUMN request_body Nullable(String),
+ADD COLUMN response_body Nullable(String)
+AFTER assets;
