e6be0516464725a60e5613268545b12305fc00ae
Justin <justintorre75@gmail.com>
2024-07-29 20:03:06 -0400

    fix site map

--- bifrost/next-sitemap.config.js
+++ bifrost/next-sitemap.config.js
@@ -1,4 +1,11 @@
 const SITE_URL = "https://www.helicone.ai";
+const fs = require("fs");
+const path = require("path");
+
+const getBlogPosts = () => {
+  const blogDir = path.join(process.cwd(), "app", "blog", "blogs");
+  return fs.readdirSync(blogDir).map((file) => `/blog/${file}`);
+};
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
@@ -14,26 +21,13 @@ module.exports = {
     "/signin",
     "/signup",
   ],
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://docs.helicone.ai/sitemap.xml"],
-    policies: [
-      { userAgent: "Twitterbot", allow: "/" },
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/*?",
-          "/*%",
-          "/v1*",
-          "/v2*",
-          "/help/",
-          "/oss/",
-          "/api/",
-          "/tags/",
-          "/devops/",
-          "/icon.ico",
-        ],
-      },
-    ],
+  additionalPaths: async (config) => {
+    const blogPosts = getBlogPosts();
+    return blogPosts.map((path) => ({
+      loc: path,
+      changefreq: "weekly",
+      priority: 0.7,
+    }));
   },
+  // ... rest of the existing configuration
 };
