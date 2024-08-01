ff6fd38314220afb2389c1936b90be2b185520c6
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-08-01 19:09:08 +0000

    style: prettier lint

--- web/components/templates/requestsV2/useRequestsPageV2.tsx
+++ web/components/templates/requestsV2/useRequestsPageV2.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 
 import { useGetRequests } from "../../../services/hooks/requests";
 import { FilterNode } from "../../../services/lib/filters/filterDefs";
