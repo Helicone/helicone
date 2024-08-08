2658680a5da097be848e5b9911510a19ce80e079
yash karthik <52369876+YashKarthik@users.noreply.github.com>
2024-08-08 12:09:36 -0700

    fix: dont display helicone_message while loading (#2405)
    * fix: dont display helicone_message while loading
    
    * style: prettier lint

--- web/components/shared/themed/table/themedTable.tsx
+++ web/components/shared/themed/table/themedTable.tsx
@@ -260,22 +260,26 @@ export default function ThemedTable<T>(props: ThemedTableV5Props<T>) {
                           },
                         }}
                       >
-                        <span
-                          className={clsx(
-                            "w-full flex flex-grow",
-                            (cell.column.id == "requestText" ||
-                              cell.column.id == "responseText") &&
-                              dataLoading
-                              ? "animate-pulse bg-gray-200 rounded-md"
-                              : "hidden"
-                          )}
-                        >
-                          &nbsp;
-                        </span>
-
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
+                        {dataLoading &&
+                        (cell.column.id == "requestText" ||
+                          cell.column.id == "responseText") ? (
+                          <span
+                            className={clsx(
+                              "w-full flex flex-grow",
+                              (cell.column.id == "requestText" ||
+                                cell.column.id == "responseText") &&
+                                dataLoading
+                                ? "animate-pulse bg-gray-200 rounded-md"
+                                : "hidden"
+                            )}
+                          >
+                            &nbsp;
+                          </span>
+                        ) : (
+                          flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )
                         )}
                       </td>
                     ))}
