514bc252462eae74fd1e098f2864a07039b9c8f1
Justin Torre <justintorre75@gmail.com>
2024-08-10 11:21:43 -0700

    fixed sessions and build (#2416)

--- web/pages/api/graphql/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<{}>
-): Promise<void> {
-  res.status(404).json({ error: "Not Found", data: null });
-  return;
-}
