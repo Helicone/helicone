ea69bccb3a66cf466062c3ad17667b735884bfb5
Justin <justintorre75@gmail.com>
2024-08-14 21:44:35 -0700

    made datasets open directly if you check little box

--- web/components/templates/datasets/NewDataset.tsx
+++ web/components/templates/datasets/NewDataset.tsx
@@ -6,6 +6,9 @@ import { useJawnClient } from "../../../lib/clients/jawnHook";
 import useNotification from "../../shared/notification/useNotification";
 import { NormalizedRequest } from "../requestsV2/builder/abstractRequestBuilder";
 import { useGetHeliconeDatasets } from "../../../services/hooks/dataset/heliconeDataset";
+import { Checkbox } from "@mui/material";
+import { useLocalStorage } from "../../../services/hooks/localStorage";
+import { useRouter } from "next/router";
 
 interface NewDatasetProps {
   requests: NormalizedRequest[];
@@ -22,6 +25,12 @@ export default function NewDataset({ requests, onComplete }: NewDatasetProps) {
 
   const [addingRequests, setAddingRequests] = useState(false);
 
+  const [openDatasetOnAdd, setOpenDatasetOnAdd] = useLocalStorage(
+    "openDatasetOnAdd",
+    false
+  );
+
+  const router = useRouter();
   return (
     <Col className="h-[500px] w-[500px] justify-between">
       <Col className="gap-5">
@@ -93,49 +102,74 @@ export default function NewDataset({ requests, onComplete }: NewDatasetProps) {
               }" dataset.`
             : `Please select a dataset or create a new one.`}
         </div>
-        <Row className="justify-end gap-5">
-          <button
-            className="bg-white dark:bg-black border border-gray-300 dark:border-gray-700 rounded-lg px-2.5 py-1.5 hover:bg-sky-50 dark:hover:bg-sky-900"
-            onClick={onComplete}
-          >
-            Cancel
-          </button>
-          {selectedDataset && (
-            <button
-              className="bg-white dark:bg-black border border-gray-300 dark:border-gray-700 rounded-lg px-2.5 py-1.5 hover:bg-sky-50 dark:hover:bg-sky-900"
-              disabled={addingRequests}
-              onClick={async () => {
-                setAddingRequests(true);
-                const res = await jawn.POST(
-                  "/v1/helicone-dataset/{datasetId}/mutate",
-                  {
-                    params: {
-                      path: {
-                        datasetId: selectedDataset,
-                      },
-                    },
-                    body: {
-                      addRequests: requests.map((r) => r.id),
-                      removeRequests: [],
-                    },
-                  }
-                );
+        <Row className="justify-end">
+          <Col className="items-end gap-3">
+            <Row className="justify-end gap-5">
+              <button
+                className="bg-white dark:bg-black border border-gray-300 dark:border-gray-700 rounded-lg px-2.5 py-1.5 hover:bg-sky-50 dark:hover:bg-sky-900"
+                onClick={onComplete}
+              >
+                Cancel
+              </button>
+              {selectedDataset && (
+                <button
+                  className="bg-white dark:bg-black border border-gray-300 dark:border-gray-700 rounded-lg px-2.5 py-1.5 hover:bg-sky-50 dark:hover:bg-sky-900"
+                  disabled={addingRequests}
+                  onClick={async () => {
+                    setAddingRequests(true);
+                    const res = await jawn.POST(
+                      "/v1/helicone-dataset/{datasetId}/mutate",
+                      {
+                        params: {
+                          path: {
+                            datasetId: selectedDataset,
+                          },
+                        },
+                        body: {
+                          addRequests: requests.map((r) => r.id),
+                          removeRequests: [],
+                        },
+                      }
+                    );
 
-                if (res.data && !res.data.error) {
-                  setNotification("Requests added to dataset", "success");
+                    if (res.data && !res.data.error) {
+                      setNotification("Requests added to dataset", "success");
 
-                  onComplete();
-                } else {
-                  setNotification("Failed to add requests to dataset", "error");
-                }
-                setAddingRequests(false);
-              }}
-            >
-              {addingRequests
-                ? "Adding..."
-                : "Add " + requests.length + " requests"}
-            </button>
-          )}
+                      if (openDatasetOnAdd) {
+                        router.push(`/datasets/${selectedDataset}`);
+                      }
+                      onComplete();
+                    } else {
+                      setNotification(
+                        "Failed to add requests to dataset",
+                        "error"
+                      );
+                    }
+                    setAddingRequests(false);
+                  }}
+                >
+                  {addingRequests
+                    ? "Adding..."
+                    : "Add " + requests.length + " requests"}
+                </button>
+              )}
+            </Row>
+            {selectedDataset && (
+              <Row>
+                <button
+                  className="flex items-center gap-2 text-xs text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300"
+                  onClick={() => setOpenDatasetOnAdd(!openDatasetOnAdd)}
+                >
+                  <i>open dataset</i>
+                  <Checkbox
+                    checked={openDatasetOnAdd}
+                    onChange={(e) => setOpenDatasetOnAdd(e.target.checked)}
+                    size="small"
+                  />
+                </button>
+              </Row>
+            )}
+          </Col>
         </Row>
       </Col>
     </Col>
