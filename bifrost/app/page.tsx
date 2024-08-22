7e7bfdaeb15f23dc139e7c05348cf38302c6830a
maamalama <bokarevstefan475@gmail.com>
2024-08-22 00:06:18 -0700

    forcing

--- bifrost/app/page.tsx
+++ bifrost/app/page.tsx
@@ -20,12 +20,7 @@ export default function Home() {
     <>
       <main className="flex min-h-screen flex-col items-center justify-between text-black">
         <header className="text-center flex flex-col space-y-4 pb-10 md:pb-32 max-w-6xl mx-auto">
-          {
-            new Date().getDate() == PhDate.getDate() ?
-              <PhHeader />
-              :
-              <PhNotifHeader />
-          }
+          <PhHeader />
           <Link
             href="https://www.ycombinator.com/launches/I73-helicone-open-source-observability-platform-for-generative-ai"
             target="_blank"
@@ -104,35 +99,33 @@ export default function Home() {
             The open-source platform for logging, monitoring, and debugging.
           </p>
 
-          {
-            PhDate.getDate() != new Date().getDate() ? (
-              <div className="pt-4 w-fit md:mx-auto self-start md:self-center">
-                <Link
-                  href="https://us.helicone.ai/signup?demo=true"
-                  className="bg-sky-500 hover:bg-sky-600 ease-in-out duration-500 text-white border-[3px] border-sky-700 rounded-lg pl-6 pr-4 py-2 ml-6 md:ml-0 font-bold shadow-lg flex flex-row w-fit items-center gap-1 self-start md:self-center"
-                >
-                  Try Demo
-                  <ChevronRightIcon className="w-5 h-5 inline text-white" />
-                </Link>
-              </div>
-            ) : (
-              <div className="flex gap-4 pt-4 w-full justify-center flex-col md:flex-row items-start md:items-center pl-6">
-                <Link
-                  href="https://us.helicone.ai/signup?demo=true"
-                  className="hidden md:flex bg-white hover:bg-gray-100 ease-in-out duration-500 text-black border-[3px] border-gray-300 rounded-lg px-6 py-2 font-bold shadow-lg items-center gap-1 w-fit"
-                >
-                  Get a demo
-                </Link>
-                <Link
-                  href="https://us.helicone.ai/signup"
-                  className="bg-sky-500 hover:bg-sky-600 ease-in-out duration-500 text-white border-[3px] border-sky-700 rounded-lg pl-6 pr-4 py-2 font-bold shadow-lg flex w-fit items-center gap-1"
-                >
-                  Start Building
-                  <ChevronRightIcon className="w-5 h-5 inline text-white" />
-                </Link>
-              </div>
-            )
-          }
+          {PhDate.getDate() != new Date().getDate() ? (
+            <div className="pt-4 w-fit md:mx-auto self-start md:self-center">
+              <Link
+                href="https://us.helicone.ai/signup?demo=true"
+                className="bg-sky-500 hover:bg-sky-600 ease-in-out duration-500 text-white border-[3px] border-sky-700 rounded-lg pl-6 pr-4 py-2 ml-6 md:ml-0 font-bold shadow-lg flex flex-row w-fit items-center gap-1 self-start md:self-center"
+              >
+                Try Demo
+                <ChevronRightIcon className="w-5 h-5 inline text-white" />
+              </Link>
+            </div>
+          ) : (
+            <div className="flex gap-4 pt-4 w-full justify-center flex-col md:flex-row items-start md:items-center pl-6">
+              <Link
+                href="https://us.helicone.ai/signup?demo=true"
+                className="hidden md:flex bg-white hover:bg-gray-100 ease-in-out duration-500 text-black border-[3px] border-gray-300 rounded-lg px-6 py-2 font-bold shadow-lg items-center gap-1 w-fit"
+              >
+                Get a demo
+              </Link>
+              <Link
+                href="https://us.helicone.ai/signup"
+                className="bg-sky-500 hover:bg-sky-600 ease-in-out duration-500 text-white border-[3px] border-sky-700 rounded-lg pl-6 pr-4 py-2 font-bold shadow-lg flex w-fit items-center gap-1"
+              >
+                Start Building
+                <ChevronRightIcon className="w-5 h-5 inline text-white" />
+              </Link>
+            </div>
+          )}
 
           <ul className="self-center md:flex-row md:gap-16 md:justify-center px-4 pt-16 text-sm hidden md:flex w-fit">
             <li className="flex items-center space-x-2">
@@ -191,11 +184,8 @@ export default function Home() {
             <span className="text-gray-600 border-l-2 border-sky-500 pl-2 py-1 font-bold text-2xl">
               99.99%
             </span>
-            <span className="text-gray-600 pl-3 font-light">
-              Uptime
-            </span>
+            <span className="text-gray-600 pl-3 font-light">Uptime</span>
           </li>
-
         </ul>
 
         <section
@@ -312,33 +302,40 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="flex flex-col space-y-4 my-16 pb-2 md:w-1/2 w-full items-center" >
+        <section className="flex flex-col space-y-4 my-16 pb-2 md:w-1/2 w-full items-center">
           <Features />
         </section>
-        
+
         <section className="flex flex-col space-y-4 pb-2 w-full items-center">
           <OpenSource />
         </section>
 
         <section className="flex flex-col py-2 items-center mt-32">
           <div className="flex flex-row pb-2 justify-between gap-2 w-fit items-center border-2 border-gray-200 rounded-full p-1.5 py-1">
-            <Image src="/static/greptile-clear.svg" alt="Greptile Logo" width={30} height={30} />
+            <Image
+              src="/static/greptile-clear.svg"
+              alt="Greptile Logo"
+              width={30}
+              height={30}
+            />
             <XMarkIcon className="w-4 h-4" />
-            <Image src="/static/logo-clear.svg" alt="Helicone Logo" width={30} height={30} />
+            <Image
+              src="/static/logo-clear.svg"
+              alt="Helicone Logo"
+              width={30}
+              height={30}
+            />
           </div>
 
           <p className="text-gray-500 italic md:w-1/3 w-5/6 mx-auto mt-8 text-lg">
-            {'"'}We&apos;re spending the weekend combing through logs to improve our core system and slowly realizing just how unbelievably powerful Helicone is.
-
-            Without it, this would take 10-12X longer and be much more draining. It&apos;s so, so good.{'"'}
+            {'"'}We&apos;re spending the weekend combing through logs to improve
+            our core system and slowly realizing just how unbelievably powerful
+            Helicone is. Without it, this would take 10-12X longer and be much
+            more draining. It&apos;s so, so good.{'"'}
           </p>
           <div className="w-1/3 mt-6">
-            <p className="text-black font-bold">
-              Daksh Gupta
-            </p>
-            <p className="text-gray-500 text-gray-500">
-              Founder, Greptile
-            </p>
+            <p className="text-black font-bold">Daksh Gupta</p>
+            <p className="text-gray-500 text-gray-500">Founder, Greptile</p>
           </div>
         </section>
 
