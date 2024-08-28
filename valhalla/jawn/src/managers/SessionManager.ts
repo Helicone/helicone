34ea956726d395e0134453ec1f570cd9824c202c
Justin Torre <justintorre75@gmail.com>
2024-08-27 22:47:02 -0700

    metrics for sessions (#2425)

--- valhalla/jawn/src/managers/SessionManager.ts
+++ valhalla/jawn/src/managers/SessionManager.ts
@@ -6,7 +6,7 @@ import { clickhouseDb } from "../lib/db/ClickhouseWrapper";
 import { AuthParams } from "../lib/db/supabase";
 import { filterListToTree, FilterNode } from "../lib/shared/filters/filterDefs";
 import { buildFilterWithAuthClickHouse } from "../lib/shared/filters/filters";
-import { err, Result, resultMap } from "../lib/shared/result";
+import { err, ok, Result, resultMap } from "../lib/shared/result";
 import { clickhousePriceCalc } from "../packages/cost";
 import { isValidTimeZoneDifference } from "../utils/helpers";
 
@@ -30,8 +30,178 @@ export interface SessionNameResult {
   session_count: number;
 }
 
+export interface HistogramRow {
+  range_start: string;
+  range_end: string;
+  value: number;
+}
+
+export interface SessionMetrics {
+  session_count: HistogramRow[];
+  session_duration: HistogramRow[];
+  session_cost: HistogramRow[];
+}
+
 export class SessionManager {
   constructor(private authParams: AuthParams) {}
+
+  async getMetrics(
+    requestBody: SessionNameQueryParams
+  ): Promise<Result<SessionMetrics, string>> {
+    const { nameContains, timezoneDifference, useInterquartile, pSize } =
+      requestBody;
+
+    if (!isValidTimeZoneDifference(timezoneDifference)) {
+      return err("Invalid timezone difference");
+    }
+
+    const builtFilter = await buildFilterWithAuthClickHouse({
+      org_id: this.authParams.organizationId,
+      filter: nameContains
+        ? {
+            request_response_rmt: {
+              properties: {
+                "Helicone-Session-Name": {
+                  ilike: `%${nameContains}%`,
+                },
+              },
+            },
+          }
+        : "all",
+      argsAcc: [],
+    });
+
+    const SIZES_TO_PERCENTILES = {
+      p50: "0.5",
+      p75: "0.75",
+      p95: "0.95",
+      p99: "0.99",
+      "p99.9": "0.999",
+    };
+
+    const upperPercentile = SIZES_TO_PERCENTILES[pSize ?? "p75"];
+    const lowerPercentile = useInterquartile
+      ? (1 - parseFloat(upperPercentile)).toString()
+      : upperPercentile;
+
+    const buildPercentileClause = (metric: string) =>
+      useInterquartile
+        ? `quantile(${lowerPercentile})(${metric}) AS lower_bound,
+         quantile(${upperPercentile})(${metric}) AS upper_bound`
+        : `quantile(${upperPercentile})(${metric}) AS p_value`;
+
+    const buildWhereClause = (metric: string) =>
+      useInterquartile
+        ? `${metric} BETWEEN lower_bound AND upper_bound`
+        : `${metric} <= p_value`;
+
+    const sessionsCountQuery = `
+WITH session_counts AS (
+  SELECT
+    properties['Helicone-Session-Name'] AS session_name,
+    properties['Helicone-Session-Id'] AS session_id,
+    count(*) AS row_count
+  FROM request_response_rmt
+  WHERE ${builtFilter.filter}
+  GROUP BY session_name, session_id
+),
+percentiles AS (
+  SELECT
+    ${buildPercentileClause("row_count")}
+  FROM session_counts
+)
+SELECT
+  arrayJoin(histogram(10)(row_count)) AS hist
+FROM session_counts, percentiles
+WHERE ${buildWhereClause("row_count")}
+`;
+
+    const sessionCount = await clickhouseDb.dbQuery<{
+      hist: [number, number, number];
+    }>(sessionsCountQuery, builtFilter.argsAcc);
+
+    if (!sessionCount?.data) {
+      return err("No session count found");
+    }
+
+    const histogramData: HistogramRow[] = sessionCount.data.map((row) => ({
+      range_start: row.hist[0].toString(),
+      range_end: row.hist[1].toString(),
+      value: row.hist[2],
+    }));
+
+    const sessionDurationQuery = `
+WITH session_durations AS (
+  SELECT
+    properties['Helicone-Session-Id'] AS session_id,
+    dateDiff('second', min(request_response_rmt.request_created_at), max(request_response_rmt.request_created_at)) AS duration
+  FROM request_response_rmt
+  WHERE ${builtFilter.filter}
+  GROUP BY session_id
+),
+percentiles AS (
+  SELECT
+    ${buildPercentileClause("duration")}
+  FROM session_durations
+)
+SELECT
+  arrayJoin(histogram(10)(duration)) AS hist
+FROM session_durations, percentiles
+WHERE ${buildWhereClause("duration")}
+`;
+
+    const sessionDuration = await clickhouseDb.dbQuery<{
+      hist: [number, number, number];
+    }>(sessionDurationQuery, builtFilter.argsAcc);
+
+    if (!sessionDuration?.data) {
+      return err("No session duration found");
+    }
+
+    const sessionCostQuery = `
+    WITH session_costs AS (
+      SELECT
+        properties['Helicone-Session-Id'] AS session_id,
+        ${clickhousePriceCalc("request_response_rmt")} AS cost
+      FROM request_response_rmt
+      WHERE ${builtFilter.filter}
+      GROUP BY session_id
+    ),
+    percentiles AS (
+      SELECT
+        ${buildPercentileClause("cost")}
+      FROM session_costs
+    )
+    SELECT
+      arrayJoin(histogram(10)(cost)) AS hist
+    FROM session_costs, percentiles
+    WHERE ${buildWhereClause("cost")}
+    `;
+
+    const sessionCost = await clickhouseDb.dbQuery<{
+      hist: [number, number, number];
+    }>(sessionCostQuery, builtFilter.argsAcc);
+
+    if (!sessionCost?.data) {
+      return err("No session cost found");
+    }
+
+    const sessionCostData: HistogramRow[] = sessionCost.data.map((row) => ({
+      range_start: row.hist[0].toString(),
+      range_end: row.hist[1].toString(),
+      value: row.hist[2],
+    }));
+
+    return ok({
+      session_count: histogramData,
+      session_duration: sessionDuration.data.map((row) => ({
+        range_start: row.hist[0].toString(),
+        range_end: row.hist[1].toString(),
+        value: row.hist[2],
+      })),
+      session_cost: sessionCostData,
+    });
+  }
 
   async getSessionNames(
     requestBody: SessionNameQueryParams
