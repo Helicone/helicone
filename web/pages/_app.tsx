c66fe54e8a53358fd99f84ea3367a0b1cf85df41
colegottdank <colegottdank@gmail.com>
2024-09-11 16:42:15 -0700

    add pylon (#2600)

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -88,6 +88,14 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           }}
         />
       )}
+      {trackingEnabled && (
+        <Script
+          id="pylon-snippet"
+          dangerouslySetInnerHTML={{
+            __html: `(function(){var e=window;var t=document;var n=function(){n.e(arguments)};n.q=[];n.e=function(e){n.q.push(e)};e.Pylon=n;var r=function(){var e=t.createElement("script");e.setAttribute("type","text/javascript");e.setAttribute("async","true");e.setAttribute("src","https://widget.usepylon.com/widget/APP_ID");var n=t.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};if(t.readyState==="complete"){r()}else if(e.addEventListener){e.addEventListener("load",r,false)}})();`,
+          }}
+        />
+      )}
     </>
   );
 }
