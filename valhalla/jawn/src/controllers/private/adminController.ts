dd50ae9a6cfb1bfe7dd7696d4e134caa3c86944a
colegottdank <colegottdank@gmail.com>
2024-09-11 18:09:29 -0700

    filter out helicone users (#2601)

--- valhalla/jawn/src/controllers/private/adminController.ts
+++ valhalla/jawn/src/controllers/private/adminController.ts
@@ -807,8 +807,10 @@ export class AdminController extends Controller {
         count(*) as count,
         date_trunc('${body.groupBy}', created_at) AS day
       FROM organization
+      INNER JOIN auth.users u ON organization.owner = u.id
       WHERE
         created_at > now() - INTERVAL '${body.timeFilter}'
+        AND u.email NOT LIKE '%helicone.ai%'
       GROUP BY day
       ORDER BY day ASC
 
@@ -827,6 +829,7 @@ export class AdminController extends Controller {
       FROM auth.users
       WHERE
         created_at > now() - INTERVAL '${body.timeFilter}'
+        AND email NOT LIKE '%helicone.ai%'
       GROUP BY day
       ORDER BY day ASC
     `,
