0dd0963249372182815606136d468710a5ff426d
Justin Torre <justintorre75@gmail.com>
2024-09-24 08:01:00 -0700

    sessions request id in url (#2683)
    * sessions request id in url
    
    * fix build
    
    * fix build

--- web/components/templates/dashboard/dashboardPage.tsx
+++ web/components/templates/dashboard/dashboardPage.tsx
@@ -21,6 +21,7 @@ import {
 } from "../../../lib/timeCalculations/time";
 import { useGetUnauthorized } from "../../../services/hooks/dashboard";
 import { useDebounce } from "../../../services/hooks/debounce";
+import { useLocalStorage } from "../../../services/hooks/localStorage";
 import { useOrganizationLayout } from "../../../services/hooks/organization_layout";
 import {
   filterUITreeToFilterNode,
@@ -42,10 +43,10 @@ import {
   MetricsPanel,
   MetricsPanelProps,
 } from "../../shared/metrics/metricsPanel";
-import useNotification from "../../shared/notification/useNotification";
 import ThemedTableHeader from "../../shared/themed/themedHeader";
+import { ThemedSwitch } from "../../shared/themed/themedSwitch";
 import UpgradeProModal from "../../shared/upgradeProModal";
-import ReportsModal from "./reportsModal";
+import { formatLargeNumber } from "../../shared/utils/numberFormat";
 import useSearchParams from "../../shared/utils/useSearchParams";
 import { INITIAL_LAYOUT, SMALL_LAYOUT } from "./gridLayouts";
 import CountryPanel from "./panels/countryPanel";
@@ -53,9 +54,6 @@ import { QuantilesGraph } from "./quantilesGraph";
 import StyledAreaChart from "./styledAreaChart";
 import SuggestionModal from "./suggestionsModal";
 import { useDashboardPage } from "./useDashboardPage";
-import { formatLargeNumber } from "../../shared/utils/numberFormat";
-import { ThemedSwitch } from "../../shared/themed/themedSwitch";
-import { useLocalStorage } from "../../../services/hooks/localStorage";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
