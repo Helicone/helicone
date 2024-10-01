219aeb41635f15bf9815b3dadd1f567475c2b542
Kavin Valli <mail@kavin.me>
2024-10-01 16:57:54 -0700

    add give me a suggestion panel in add experiments drawer

--- web/components/templates/prompts/experiments/table/AddColumnHeader.tsx
+++ web/components/templates/prompts/experiments/table/AddColumnHeader.tsx
@@ -12,6 +12,17 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import PromptPlayground from "../../id/promptPlayground";
+import { Wand2Icon, XIcon } from "lucide-react";
+import { Card } from "@/components/layout/common";
+import { CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
 
 interface AddColumnHeaderProps {
   promptVersionId: string;
@@ -19,6 +30,14 @@ interface AddColumnHeaderProps {
   selectedProviderKey: string | null;
   refetchData: () => void; // Add this line
 }
+
+const SCORES = [
+  "Sentiment",
+  "Accuracy",
+  "Contain words",
+  "Shorter than 50 characters",
+  "Is English",
+];
 
 const AddColumnHeader: React.FC<AddColumnHeaderProps> = ({
   promptVersionId,
@@ -37,6 +56,14 @@ const AddColumnHeader: React.FC<AddColumnHeaderProps> = ({
       });
     },
   });
+
+  const [showSuggestionPanel, setShowSuggestionPanel] = useState(false);
+  const [scoreCriterias, setScoreCriterias] = useState<
+    {
+      scoreType?: (typeof SCORES)[number];
+      criteria?: string;
+    }[]
+  >([]);
 
   console.log(
     "promptVersion",
@@ -61,6 +88,124 @@ const AddColumnHeader: React.FC<AddColumnHeaderProps> = ({
           <SheetHeader>
             <SheetTitle>Add New Experiment</SheetTitle>
             <SheetDescription>
+              {showSuggestionPanel && (
+                <Card className="bg-gray-50 border border-slate-200 py-2 px-4 rounded-md text-slate-900 mb-2">
+                  <CardHeader className="flex flex-row items-center justify-between p-0">
+                    <div className="flex gap-2 items-center text-slate-900 font-medium">
+                      <Wand2Icon className="w-4 h-4" />
+                      Give me a suggestion
+                    </div>
+                    <Button variant="ghost" size="icon">
+                      <XIcon
+                        className="w-4 h-4"
+                        onClick={() => setShowSuggestionPanel(false)}
+                      />
+                    </Button>
+                  </CardHeader>
+                  <CardContent className="flex flex-col gap-2">
+                    {scoreCriterias.map((criteria, index) => (
+                      <div className="flex gap-2 items-center" key={index}>
+                        <Select
+                          value={criteria.scoreType}
+                          onValueChange={(value) => {
+                            setScoreCriterias(
+                              scoreCriterias.map((c) =>
+                                c.scoreType === criteria.scoreType
+                                  ? { ...c, scoreType: value }
+                                  : c
+                              )
+                            );
+                          }}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder="Type" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {SCORES.filter(
+                              (score) =>
+                                !scoreCriterias.some(
+                                  (c) => c.scoreType === score
+                                ) || criteria.scoreType === score
+                            ).map((score) => (
+                              <SelectItem value={score}>{score}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <p className="text-xs font-semibold text-gray-400">
+                          IS
+                        </p>
+                        <Input
+                          placeholder="Type"
+                          className="w-24"
+                          value={criteria.criteria}
+                          onChange={(e) => {
+                            setScoreCriterias(
+                              scoreCriterias.map((c) =>
+                                c.scoreType === criteria.scoreType
+                                  ? { ...c, criteria: e.target.value }
+                                  : c
+                              )
+                            );
+                          }}
+                        />
+                        <Button
+                          variant="ghost"
+                          size="icon"
+                          className="border border-slate-200 aspect-square"
+                        >
+                          <XIcon
+                            className="w-4 h-4"
+                            onClick={() => {
+                              setScoreCriterias(
+                                scoreCriterias.filter(
+                                  (c) => c.scoreType !== criteria.scoreType
+                                )
+                              );
+                            }}
+                          />
+                        </Button>
+                      </div>
+                    ))}
+                    <Button
+                      variant="ghost"
+                      className="mt-2 border border-slate-200 self-start"
+                      onClick={() => {
+                        setScoreCriterias([
+                          ...scoreCriterias,
+                          { scoreType: undefined, criteria: "" },
+                        ]);
+                      }}
+                    >
+                      <PlusIcon className="w-4 h-4" /> Add criteria
+                    </Button>
+                  </CardContent>
+                  {scoreCriterias.length > 0 &&
+                    scoreCriterias.every((c) => c.scoreType && c.criteria) && (
+                      <CardFooter>
+                        <Button className="flex items-center gap-2 w-full">
+                          <Wand2Icon className="w-4 h-4" />
+                          Suggest a prompt
+                        </Button>
+                      </CardFooter>
+                    )}
+                </Card>
+                // <div className="bg-gray-50 border border-slate-200 py-2 px-4 rounded-md">
+
+                //   {/* {scoreCriterias.map((criteria) => (
+                //     <div key={criteria}>{criteria}</div>
+                //   ))} */}
+                // </div>
+              )}
+              {!showSuggestionPanel && (
+                <Button
+                  variant="ghost"
+                  onClick={() => setShowSuggestionPanel(true)}
+                  className="flex items-center gap-2 bg-gray-50 text-slate-900 mb-4"
+                >
+                  <Wand2Icon className="w-4 h-4" />
+                  Give me a suggestion
+                </Button>
+              )}
               <PromptPlayground
                 defaultEditMode={true}
                 prompt={promptVersion.data?.data?.data?.helicone_template ?? ""}
