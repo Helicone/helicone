cf7730bbe873f76a21601ba7864a4daec70cda8d
Justin <justintorre75@gmail.com>
2024-09-30 20:11:07 -0700

    fix sql

--- valhalla/jawn/src/lib/stores/request/request.ts
+++ valhalla/jawn/src/lib/stores/request/request.ts
@@ -236,7 +236,7 @@ export async function getRequestsClickhouse(
       country_code,
       scores,
       properties,
-wi
+      assets as asset_ids,
       target_url,
     FROM request_response_rmt FINAL
     WHERE (
