bbb7bc9171a452c2955f3234c6c1e2726de5d81d
Kavin Valli <mail@kavin.me>
2024-10-03 09:27:11 -0700

    update experiments waitlist api url

--- bifrost/app/experiments/page.tsx
+++ bifrost/app/experiments/page.tsx
@@ -368,7 +368,7 @@ export default function ExperimentsWaitlist() {
     }
 
     const response = await fetch(
-      "http://localhost:8585/v1/public/waitlist/experiments",
+      "https://api.helicone.ai/v1/public/waitlist/experiments",
       {
         method: "POST",
         headers: {
