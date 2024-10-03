14e83eed23e9cdcb536b69c5651cafca3500a742
Kavin Valli <mail@kavin.me>
2024-10-03 10:28:27 -0700

    merge supabase migrations

--- supabase/migrations/20241003163624_experiments-waitlist.sql
+++ supabase/migrations/20241003163624_experiments-waitlist.sql
@@ -51,4 +51,6 @@ grant truncate on table "public"."experiments_waitlist" to "service_role";
 
 grant update on table "public"."experiments_waitlist" to "service_role";
 
+CREATE UNIQUE INDEX experiments_waitlist_email_key ON public.experiments_waitlist USING btree (email);
 
+alter table "public"."experiments_waitlist" add constraint "experiments_waitlist_email_key" UNIQUE using index "experiments_waitlist_email_key";
--- supabase/migrations/20241003164653_experiments-waitlist-email-unique.sql
+++
@@ -1,5 +0,0 @@
-CREATE UNIQUE INDEX experiments_waitlist_email_key ON public.experiments_waitlist USING btree (email);
-
-alter table "public"."experiments_waitlist" add constraint "experiments_waitlist_email_key" UNIQUE using index "experiments_waitlist_email_key";
-
-
