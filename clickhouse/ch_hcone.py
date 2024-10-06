817f440c9575cf0ed8e61cb219ae7ff603dd775d
colegottdank <colegottdank@gmail.com>
2024-10-05 22:53:43 -0700

    undo

--- clickhouse/ch_hcone.py
+++ clickhouse/ch_hcone.py
@@ -30,29 +30,23 @@ def get_host(host: str):
 
 
 def run_curl_command(query, host, port, user=None, password=None, migration_file=None):
-    if not query:
-        if user and password:
-            curl_cmd = (
-                f'cat "{migration_file}" | curl --user "{user}:{password}" '
-                f'"{get_host(host)}:{port}/" --data-binary @-'
-            )
-        else:
-            curl_cmd = (
-                f'cat "{migration_file}" | curl "{get_host(host)}:{port}/" --data-binary @-'
-            )
-    else:
-        if user and password:
-            curl_cmd = (
-                f'echo "{query}" | curl --user "{user}:{password}" '
-                f'"{get_host(host)}:{port}/" --data-binary @-'
-            )
-        else:
-            curl_cmd = (
-                f'echo "{query}" | curl "{get_host(host)}:{port}/" --data-binary @-'
-            )
 
-    result = subprocess.run(curl_cmd, shell=True, capture_output=True, text=True)
-    if result.returncode != 0:
+    if not query:
+        curl_cmd = f"cat \"{migration_file}\" | curl '{
+            get_host(host)}:{port}/' --data-binary @-"
+        if user and password:
+            curl_cmd = f"cat \"{migration_file}\" | curl --user '{user}:{
+                password}' '{get_host(host)}:{port}/' --data-binary @-"
+    else:
+        curl_cmd = f"echo \"{query}\" | curl '{
+            get_host(host)}:{port}/' --data-binary @-"
+        if user and password:
+            curl_cmd = f"echo \"{
+                query}\" | curl --user '{user}:{password}' '{get_host(host)}:{port}/' --data-binary @-"
+
+    result = subprocess.run(curl_cmd, shell=True,
+                            capture_output=True, text=True)
+    if (result.returncode != 0):
         print("Error running query")
         print("STDOUT:", result.stdout)
         print("STDERR:", result.stderr)
