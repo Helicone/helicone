4dc885d904a1a437183d73bdb706e2948cb61094
maamalama <bokarevstefan475@gmail.com>
2024-10-10 22:59:33 -0700

    fix

--- web/components/templates/prompts/experiments/table/ExperimentTable.tsx
+++ web/components/templates/prompts/experiments/table/ExperimentTable.tsx
@@ -23,7 +23,6 @@ import {
 import {
   PlusIcon,
   ChevronDownIcon,
-  ArrowDownTrayIcon,
   FunnelIcon,
 } from "@heroicons/react/24/outline";
 import ExperimentInputSelector from "../experimentInputSelector";
