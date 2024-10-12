4000e56c1fbcdcb0086f9b28f2dce62107acb3bb
Kavin Valli <mail@kavin.me>
2024-10-11 18:12:30 -0700

    improve dashboard ui

--- web/components/shared/metrics/metricsPanel.tsx
+++ web/components/shared/metrics/metricsPanel.tsx
@@ -21,16 +21,16 @@ export function MetricsPanel(props: MetricsPanelProps) {
   const { metric } = props;
 
   return (
-    <Card className="h-full flex flex-col">
-      <dd className="text-black dark:text-white flex flex-col flex-grow">
+    <Card className="border border-slate-200 bg-white text-slate-950 !shadow-sm dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50 rounded-lg h-full flex flex-col ring-0">
+      <dd className="text-slate-900 dark:text-slate-50 flex flex-col flex-grow">
         <div className="flex w-full items-center justify-between">
-          <div className="text-gray-500 text-xs">{metric.label}</div>
-          {metric.icon && <metric.icon className="w-6 h-6" />}
+          <div className="text-slate-500 text-xs">{metric.label}</div>
+          {metric.icon && <metric.icon className="w-6 h-6 text-slate-500" />}
         </div>
         {metric.isLoading ? (
-          <div className="bg-gray-200 dark:bg-gray-800 animate-pulse h-6 w-16 rounded-md mt-1" />
+          <div className="bg-slate-200 dark:bg-slate-800 animate-pulse h-6 w-16 rounded-md mt-1" />
         ) : (
-          <div className="text-xl font-semibold text-black dark:text-white mt-auto">
+          <div className="text-xl font-semibold text-slate-900 dark:text-slate-50 mt-auto">
             {metric.value} {metric.labelUnits}
           </div>
         )}
