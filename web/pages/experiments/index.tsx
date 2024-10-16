74fc4a9cf9cc5e1238a36a96d46ff1ae8374a884
maamalama <bokarevstefan475@gmail.com>
2024-10-16 09:59:52 -0700

    fix

--- web/pages/experiments/index.tsx
+++ web/pages/experiments/index.tsx
@@ -3,14 +3,14 @@ import { User } from "@supabase/auth-helpers-react";
 import { ReactElement } from "react";
 import AuthLayout from "../../components/layout/auth/authLayout";
 import { SupabaseServerWrapper } from "../../lib/wrappers/supabase";
-import ExperimentTablePageEmpty from "../../components/templates/prompts/experiments/table/experimentTablePageEmpty";
+import ExperimentsPage from "../../components/templates/prompts/experiments/table/experimentsPage";
 
 interface ExperimentPage {
   user: User;
 }
 
 const Experiments = (props: ExperimentPage) => {
-  return <ExperimentTablePageEmpty />;
+  return <ExperimentsPage />;
 };
 
 export default Experiments;
