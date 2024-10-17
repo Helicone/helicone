34f5b2a7c343af82419e0430eab8ce24d3c5b897
maamalama <bokarevstefan475@gmail.com>
2024-10-17 16:37:05 -0700

    experiments ff

--- web/pages/experiments/index.tsx
+++ web/pages/experiments/index.tsx
@@ -4,13 +4,29 @@ import { ReactElement } from "react";
 import AuthLayout from "../../components/layout/auth/authLayout";
 import { SupabaseServerWrapper } from "../../lib/wrappers/supabase";
 import ExperimentsPage from "../../components/templates/prompts/experiments/table/experimentsPage";
+import { useOrg } from "../../components/layout/organizationContext";
+import { useFeatureFlags } from "../../services/hooks/featureFlags";
+import { ContactUsSection } from "../developer";
+import { IslandContainer } from "../../components/ui/islandContainer";
 
 interface ExperimentPage {
   user: User;
 }
 
 const Experiments = (props: ExperimentPage) => {
-  return <ExperimentsPage />;
+  const orgContext = useOrg();
+
+  const { hasFlag } = useFeatureFlags(
+    "experiments",
+    orgContext?.currentOrg?.id || ""
+  );
+  return hasFlag ? (
+    <ExperimentsPage />
+  ) : (
+    <IslandContainer>
+      <ContactUsSection feature="experiments" />
+    </IslandContainer>
+  );
 };
 
 export default Experiments;
