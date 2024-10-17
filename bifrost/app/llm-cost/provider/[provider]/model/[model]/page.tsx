27edf959abc0ae049091893e43bfd762f1e56158
colegottdank <colegottdank@gmail.com>
2024-10-17 13:11:50 -0700

    sitemap update (#2820)

--- bifrost/app/llm-cost/provider/[provider]/model/[model]/page.tsx
+++ bifrost/app/llm-cost/provider/[provider]/model/[model]/page.tsx
@@ -23,29 +23,13 @@ export default async function Home({
 }
 
 export async function generateStaticParams() {
-  function formatProviderName(provider: string): string {
-    const formattingMap: { [key: string]: string } = {
-      OPENAI: "OpenAI",
-      ANTHROPIC: "Anthropic",
-      AZURE: "Azure",
-      TOGETHER: "Together AI",
-      FIREWORKS: "Fireworks",
-      OPENROUTER: "OpenRouter",
-      GROQ: "Groq",
-      QSTASH: "Qstash",
-      MISTRAL: "Mistral",
-    };
-
-    return formattingMap[provider.toUpperCase()] || provider.toUpperCase();
-  }
-
   const paths = [];
 
   for (const provider of providers) {
     for (const cost of provider.costs || []) {
       paths.push({
-        provider: encodeURIComponent(provider.provider),
-        model: encodeURIComponent(formatProviderName(cost.model.value)),
+        provider: encodeURIComponent(provider.provider.toLowerCase()),
+        model: encodeURIComponent(cost.model.value),
       });
     }
   }
