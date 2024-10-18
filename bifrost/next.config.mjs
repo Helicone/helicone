a086c3652a7d89bb0be988aa88e2d337acc6218f
Kavin Valli <mail@kavin.me>
2024-10-17 20:24:09 -0400

    add cors headers to changelog (#2830)

--- bifrost/next.config.mjs
+++ bifrost/next.config.mjs
@@ -22,6 +22,27 @@ const nextConfig = {
   images: {
     domains: ["api.producthunt.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/rss/changelog.xml",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
