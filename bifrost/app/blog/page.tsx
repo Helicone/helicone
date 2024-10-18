979a01fe76d06a07d20e3bcbb34e9d3dbf26791d
LinaLam <65078897+LinaLam@users.noreply.github.com>
2024-10-17 18:53:20 -0700

    added a Sessions blog - PENDING APPROVAL until vector DB is done (#2228)
    * added a Sessions blog
    
    * fixed merge conflict, made content better
    
    * period
    
    * changed
    
    * updated content
    
    * improved tone
    
    * small update
    
    ---------
    
    Co-authored-by: colegottdank <colegottdank@gmail.com>

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -192,6 +192,11 @@ export type BlogStructure =
 const blogContent: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "debugging-chatbots-and-ai-agents-with-sessions",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "braintrust-alternatives",
     },
   },
