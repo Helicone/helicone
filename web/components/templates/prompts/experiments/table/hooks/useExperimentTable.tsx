e2057b0700779c170100174e85ae9f873e77aceb
maamalama <bokarevstefan475@gmail.com>
2024-11-01 16:24:13 -0700

    dont need this

--- web/components/templates/prompts/experiments/table/hooks/useExperimentTable.tsx
+++ web/components/templates/prompts/experiments/table/hooks/useExperimentTable.tsx
@@ -47,13 +47,6 @@ export type TableRow = {
 type ColumnType = "input" | "output" | "experiment";
 type CellStatus = "initialized" | "success" | "running";
 
-//TODO: Move to metadata and remove this
-export function isUUID(value: string): boolean {
-  const uuidRegex =
-    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  return uuidRegex.test(value);
-}
-
 export const getRequestDataByIds = async (
   orgId: string,
   requestIds: string[]
