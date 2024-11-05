99b42bb60924653c622223a1ddf29f97b24f1730
Stefan Bokarev <38397684+maamalama@users.noreply.github.com>
2024-11-04 16:31:54 -0800

    drawer temp fix (#2921)

--- web/components/templates/prompts/experiments/table/AddColumnHeader.tsx
+++ web/components/templates/prompts/experiments/table/AddColumnHeader.tsx
@@ -3,13 +3,10 @@ import { PlusIcon } from "@heroicons/react/24/outline";
 import { useJawnClient } from "@/lib/clients/jawnHook";
 import { Button } from "@/components/ui/button";
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import PromptPlayground from "../../id/promptPlayground";
 import { Wand2Icon, XIcon } from "lucide-react";
 import { Card } from "@/components/layout/common";
@@ -29,7 +26,6 @@ interface AddColumnHeaderProps {
   experimentId: string;
   promptVersionTemplate: any;
   selectedProviderKey: string | null;
-  refetchData: () => Promise<void>; // Add this line
   handleAddColumn: (
     columnName: string,
     columnType: "experiment" | "input" | "output",
@@ -51,7 +47,6 @@ const AddColumnHeader: React.FC<AddColumnHeaderProps> = ({
   promptVersionTemplate,
   experimentId,
   selectedProviderKey,
-  refetchData, // Add this line
   handleAddColumn,
 }) => {
   const [open, setOpen] = useState(false);
@@ -66,223 +61,206 @@ const AddColumnHeader: React.FC<AddColumnHeaderProps> = ({
   >([]);
 
   return (
-    <div>
-      <div className="flex items-center justify-center w-full h-full">
-        <button
-          className="p-1 text-gray-500 hover:text-gray-700 flex flex-row items-center space-x-2"
-          onClick={() => setOpen(true)}
-        >
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <button className="p-1 text-gray-500 hover:text-gray-700 flex flex-row items-center space-x-2">
           <PlusIcon className="w-5 h-5 text-slate-700" />
           <span className="text-sm font-medium text-slate-700">
             Add Experiment
           </span>
         </button>
-      </div>
-      <Sheet open={open} onOpenChange={setOpen}>
-        <SheetContent size="large">
-          <ScrollArea className="flex flex-col overflow-y-auto max-h-full">
-            <SheetHeader>
-              <SheetTitle>Add New Experiment</SheetTitle>
-              <SheetDescription>
-                {showSuggestionPanel && (
-                  <Card className="bg-gray-50 border border-slate-200 py-2 px-4 rounded-md text-slate-900 mb-2">
-                    <CardHeader className="flex flex-row items-center justify-between p-0">
-                      <div className="flex gap-2 items-center text-slate-900 font-medium">
-                        <Wand2Icon className="w-4 h-4" />
-                        Give me a suggestion
-                      </div>
-                      <Button variant="ghost" size="icon">
-                        <XIcon
-                          className="w-4 h-4"
-                          onClick={() => setShowSuggestionPanel(false)}
-                        />
-                      </Button>
-                    </CardHeader>
-                    <CardContent className="flex flex-col gap-2">
-                      {scoreCriterias.map((criteria, index) => (
-                        <div className="flex gap-2 items-center" key={index}>
-                          <Select
-                            value={criteria.scoreType}
-                            onValueChange={(value) => {
-                              setScoreCriterias(
-                                scoreCriterias.map((c) =>
-                                  c.scoreType === criteria.scoreType
-                                    ? { ...c, scoreType: value }
-                                    : c
-                                )
-                              );
-                            }}
-                          >
-                            <SelectTrigger>
-                              <SelectValue placeholder="Type" />
-                            </SelectTrigger>
-                            <SelectContent>
-                              {SCORES.filter(
-                                (score) =>
-                                  !scoreCriterias.some(
-                                    (c) => c.scoreType === score
-                                  ) || criteria.scoreType === score
-                              ).map((score) => (
-                                <SelectItem key={score} value={score}>
-                                  {score}
-                                </SelectItem>
-                              ))}
-                            </SelectContent>
-                          </Select>
-                          <p className="text-xs font-semibold text-gray-400">
-                            IS
-                          </p>
-                          <Input
-                            placeholder="Type"
-                            className="w-24"
-                            value={criteria.criteria}
-                            onChange={(e) => {
-                              setScoreCriterias(
-                                scoreCriterias.map((c) =>
-                                  c.scoreType === criteria.scoreType
-                                    ? { ...c, criteria: e.target.value }
-                                    : c
-                                )
-                              );
-                            }}
-                          />
-                          <Button
-                            variant="ghost"
-                            size="icon"
-                            className="border border-slate-200 aspect-square"
-                          >
-                            <XIcon
-                              className="w-4 h-4"
-                              onClick={() => {
-                                setScoreCriterias(
-                                  scoreCriterias.filter(
-                                    (c) => c.scoreType !== criteria.scoreType
-                                  )
-                                );
-                              }}
-                            />
-                          </Button>
-                        </div>
-                      ))}
-                      <Button
-                        variant="ghost"
-                        className="mt-2 border border-slate-200 self-start"
-                        onClick={() => {
-                          setScoreCriterias([
-                            ...scoreCriterias,
-                            { scoreType: undefined, criteria: "" },
-                          ]);
-                        }}
-                      >
-                        <PlusIcon className="w-4 h-4" /> Add criteria
-                      </Button>
-                    </CardContent>
-                    {scoreCriterias.length > 0 &&
-                      scoreCriterias.every(
-                        (c) => c.scoreType && c.criteria
-                      ) && (
-                        <CardFooter>
-                          <Button className="flex items-center gap-2 w-full">
-                            <Wand2Icon className="w-4 h-4" />
-                            Suggest a prompt
-                          </Button>
-                        </CardFooter>
-                      )}
-                  </Card>
-                  // <div className="bg-gray-50 border border-slate-200 py-2 px-4 rounded-md">
-
-                  //   {/* {scoreCriterias.map((criteria) => (
-                  //     <div key={criteria}>{criteria}</div>
-                  //   ))} */}
-                  // </div>
-                )}
-                {!showSuggestionPanel && (
-                  <Button
-                    variant="ghost"
-                    onClick={() => setShowSuggestionPanel(true)}
-                    className="flex items-center gap-2 bg-gray-50 text-slate-900 mb-4"
-                  >
+      </PopoverTrigger>
+      <PopoverContent className="w-[700px] p-4 bg-white" align="end">
+        <ScrollArea className="flex flex-col overflow-y-auto max-h-[700px]">
+          <div className="space-y-4">
+            <h3 className="font-semibold">Add New Experiment</h3>
+            {showSuggestionPanel && (
+              <Card className="bg-gray-50 border border-slate-200 py-2 px-4 rounded-md text-slate-900 mb-2">
+                <CardHeader className="flex flex-row items-center justify-between p-0">
+                  <div className="flex gap-2 items-center text-slate-900 font-medium">
                     <Wand2Icon className="w-4 h-4" />
                     Give me a suggestion
+                  </div>
+                  <Button variant="ghost" size="icon">
+                    <XIcon
+                      className="w-4 h-4"
+                      onClick={() => setShowSuggestionPanel(false)}
+                    />
                   </Button>
-                )}
-                <PromptPlayground
-                  defaultEditMode={true}
-                  prompt={promptVersionTemplate?.helicone_template ?? ""}
-                  selectedInput={undefined}
-                  onSubmit={async (history, model) => {
-                    const promptData = {
+                </CardHeader>
+                <CardContent className="flex flex-col gap-2">
+                  {scoreCriterias.map((criteria, index) => (
+                    <div className="flex gap-2 items-center" key={index}>
+                      <Select
+                        value={criteria.scoreType}
+                        onValueChange={(value) => {
+                          setScoreCriterias(
+                            scoreCriterias.map((c) =>
+                              c.scoreType === criteria.scoreType
+                                ? { ...c, scoreType: value }
+                                : c
+                            )
+                          );
+                        }}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Type" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {SCORES.filter(
+                            (score) =>
+                              !scoreCriterias.some(
+                                (c) => c.scoreType === score
+                              ) || criteria.scoreType === score
+                          ).map((score) => (
+                            <SelectItem key={score} value={score}>
+                              {score}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                      <p className="text-xs font-semibold text-gray-400">IS</p>
+                      <Input
+                        placeholder="Type"
+                        className="w-24"
+                        value={criteria.criteria}
+                        onChange={(e) => {
+                          setScoreCriterias(
+                            scoreCriterias.map((c) =>
+                              c.scoreType === criteria.scoreType
+                                ? { ...c, criteria: e.target.value }
+                                : c
+                            )
+                          );
+                        }}
+                      />
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="border border-slate-200 aspect-square"
+                      >
+                        <XIcon
+                          className="w-4 h-4"
+                          onClick={() => {
+                            setScoreCriterias(
+                              scoreCriterias.filter(
+                                (c) => c.scoreType !== criteria.scoreType
+                              )
+                            );
+                          }}
+                        />
+                      </Button>
+                    </div>
+                  ))}
+                  <Button
+                    variant="ghost"
+                    className="mt-2 border border-slate-200 self-start"
+                    onClick={() => {
+                      setScoreCriterias([
+                        ...scoreCriterias,
+                        { scoreType: undefined, criteria: "" },
+                      ]);
+                    }}
+                  >
+                    <PlusIcon className="w-4 h-4" /> Add criteria
+                  </Button>
+                </CardContent>
+                {scoreCriterias.length > 0 &&
+                  scoreCriterias.every((c) => c.scoreType && c.criteria) && (
+                    <CardFooter>
+                      <Button className="flex items-center gap-2 w-full">
+                        <Wand2Icon className="w-4 h-4" />
+                        Suggest a prompt
+                      </Button>
+                    </CardFooter>
+                  )}
+              </Card>
+            )}
+            {!showSuggestionPanel && (
+              <Button
+                variant="ghost"
+                onClick={() => setShowSuggestionPanel(true)}
+                className="flex items-center gap-2 bg-gray-50 text-slate-900 mb-4"
+              >
+                <Wand2Icon className="w-4 h-4" />
+                Give me a suggestion
+              </Button>
+            )}
+            <PromptPlayground
+              defaultEditMode={true}
+              prompt={promptVersionTemplate?.helicone_template ?? ""}
+              selectedInput={undefined}
+              onSubmit={async (history, model) => {
+                const promptData = {
+                  model: model,
+                  messages: history.map((msg) => ({
+                    role: msg.role,
+                    content: [
+                      {
+                        text: msg.content,
+                        type: "text",
+                      },
+                    ],
+                  })),
+                };
+
+                const result = await jawn.POST(
+                  "/v1/prompt/version/{promptVersionId}/subversion",
+                  {
+                    params: {
+                      path: {
+                        promptVersionId: promptVersionId,
+                      },
+                    },
+                    body: {
+                      newHeliconeTemplate: JSON.stringify(promptData),
+                      isMajorVersion: false,
+                    },
+                  }
+                );
+
+                if (result.error || !result.data) {
+                  console.error(result);
+                  return;
+                }
+
+                const hypothesisResult = await jawn.POST(
+                  "/v1/experiment/hypothesis",
+                  {
+                    body: {
+                      experimentId: experimentId,
                       model: model,
-                      messages: history.map((msg) => ({
-                        role: msg.role,
-                        content: [
-                          {
-                            text: msg.content,
-                            type: "text",
-                          },
-                        ],
-                      })),
-                    };
+                      promptVersion: result.data.data?.id ?? "",
+                      providerKeyId: "NOKEY",
+                      status: "RUNNING",
+                    },
+                  }
+                );
+                if (hypothesisResult.error || !hypothesisResult.data) {
+                  console.error(hypothesisResult);
+                  return;
+                }
 
-                    const result = await jawn.POST(
-                      "/v1/prompt/version/{promptVersionId}/subversion",
-                      {
-                        params: {
-                          path: {
-                            promptVersionId: promptVersionId,
-                          },
-                        },
-                        body: {
-                          newHeliconeTemplate: JSON.stringify(promptData),
-                          isMajorVersion: false,
-                        },
-                      }
-                    );
+                await handleAddColumn(
+                  "Experiment",
+                  "experiment",
+                  hypothesisResult.data.data?.hypothesisId,
+                  result.data.data?.id
+                );
 
-                    if (result.error || !result.data) {
-                      console.error(result);
-                      return;
-                    }
-
-                    const hypothesisResult = await jawn.POST(
-                      "/v1/experiment/hypothesis",
-                      {
-                        body: {
-                          experimentId: experimentId,
-                          model: model,
-                          promptVersion: result.data.data?.id ?? "",
-                          providerKeyId: "NOKEY",
-                          status: "RUNNING",
-                        },
-                      }
-                    );
-                    if (hypothesisResult.error || !hypothesisResult.data) {
-                      console.error(hypothesisResult);
-                      return;
-                    }
-
-                    await handleAddColumn(
-                      "Experiment",
-                      "experiment",
-                      hypothesisResult.data.data?.hypothesisId,
-                      result.data.data?.id
-                    );
-
-                    setOpen(false); // Close the drawer after adding the column
-                  }}
-                  submitText="Test"
-                  initialModel={"gpt-4o"}
-                  editMode={false}
-                />
-              </SheetDescription>
-            </SheetHeader>
-            <SheetFooter className="pt-4">
+                setOpen(false); // Close the drawer after adding the column
+              }}
+              submitText="Test"
+              initialModel={"gpt-4o"}
+              editMode={false}
+            />
+            <div className="flex justify-end pt-4">
               <Button onClick={() => setOpen(false)}>Close</Button>
-            </SheetFooter>
-          </ScrollArea>
-        </SheetContent>
-      </Sheet>
-    </div>
+            </div>
+          </div>
+        </ScrollArea>
+      </PopoverContent>
+    </Popover>
   );
 };
 
