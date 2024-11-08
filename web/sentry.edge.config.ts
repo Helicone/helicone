8e9f9f75e267041b4a9702ac3560487a6bff156c
Justin <justintorre75@gmail.com>
2024-11-08 08:47:50 -0800

    removed sentry from eu

--- web/sentry.edge.config.ts
+++ web/sentry.edge.config.ts
@@ -5,12 +5,16 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://b0bf4af8adaecf90a2efaff9fb4ed029@o4506562130280448.ingest.us.sentry.io/4508259500687360",
+const REGION = process.env.REGION || "us";
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+if (REGION === "us") {
+  Sentry.init({
+    dsn: "https://b0bf4af8adaecf90a2efaff9fb4ed029@o4506562130280448.ingest.us.sentry.io/4508259500687360",
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
