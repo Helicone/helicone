eb0f03b68265f61846726d497b27a3fc28197556
LinaLam <tt3lam@uwaterloo.ca>
2024-11-11 12:44:12 -0800

    added all content for this blog

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -216,6 +216,11 @@ export type BlogStructure =
 const blogContent: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "prompt-evaluation-for-llms",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "pdf-chatbot-tutorial",
     },
   },
