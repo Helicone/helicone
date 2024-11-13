77364a36013faf047b8fed834a05b0bedb23b3e2
LinaLam <65078897+LinaLam@users.noreply.github.com>
2024-11-12 16:27:10 -0800

    Crewai vs autogen (#2938)

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -216,6 +216,11 @@ export type BlogStructure =
 const blogContent: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "crewai-vs-autogen",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "prompt-evaluation-for-llms",
     },
   },
