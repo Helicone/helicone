c7500b3daf727560381a7dc85557295b91153730
Kavin Valli <mail@kavin.me>
2024-11-18 16:00:51 -0500

    Landing Page (#2970)

---
+++ bifrost/components/home/Hero.tsx
@@ -0,0 +1,93 @@
+import { ISLAND_WIDTH } from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import { ChevronRightIcon } from "lucide-react";
+import LogoBox from "./LogoBox";
+
+const Hero = () => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col justify-center h-auto lg:h-[75vh] relative",
+        ISLAND_WIDTH
+      )}
+    >
+      <div className="flex flex-wrap gap-x-12 gap-y-4 items-center mt-12 lg:mt-0 mb-12">
+        <div className="flex items-center gap-2 text-sm font-medium whitespace-nowrap">
+          <p>Backed by</p>
+          <img
+            src="/static/home/yc-logo.webp"
+            alt="Y Combinator"
+            className="w-24 h-auto"
+          />
+        </div>
+        <img
+          src="/static/home/productoftheday.webp"
+          alt="Product of the Day"
+          className="w-32 h-auto"
+        />
+      </div>
+      <h1 className="text-5xl sm:text-7xl md:text-[84px] font-semibold mb-3 w-full max-w-3xl text-wrap text-black z-[10]">
+        Ship your AI app with <span className="text-brand">confidence</span>
+      </h1>
+      <p className="text-lg sm:text-xl 2xl:text-2xl text-landing-secondary font-light mb-6 lg:mb-12 z-[10]">
+        The all-in-one platform to monitor, debug and improve <br />
+        production-ready LLM applications.
+      </p>
+      <button className="bg-brand py-3 px-6 lg:py-[18px] lg:px-9 text-base lg:text-[22px] 2xl:text-[26px] font-normal flex gap-3 rounded-lg text-white self-start items-center z-[10]">
+        Get started for free
+        <ChevronRightIcon
+          className="w-5 sm:w-7 h-5 sm:h-7"
+          strokeWidth={2.33}
+        />
+      </button>
+
+      <LogoBox
+        imgSrc="/static/home/gemini.webp"
+        className="w-24 h-24 2xl:w-28 2xl:h-28 absolute top-20 right-1/3 2xl:translate-x-[-200px] translate-x-[-100px] rotate-[-15deg]"
+        innerClassName="bg-white"
+      />
+
+      <LogoBox
+        imgSrc="/static/home/logo2.webp"
+        className="w-[140px] h-[140px] 2xl:w-[180px] 2xl:h-[180px] absolute top-40 right-1/3 2xl:translate-x-[50px] translate-x-[80px] rotate-[13deg]"
+      />
+
+      <LogoBox
+        imgSrc="/static/home/chatgpt.webp"
+        className="w-[140px] h-[140px] 2xl:w-[180px] 2xl:h-[180px] absolute top-16 right-1/4 2xl:translate-x-[150px] translate-x-[150px] rotate-[15deg]"
+        innerClassName="bg-[#0FA37F] rounded-3xl"
+      />
+      <LogoBox
+        imgSrc="/static/home/togetherai.webp"
+        className="w-[120px] h-[120px] 2xl:w-[160px] 2xl:h-[160px] absolute top-12 right-0 2xl:-translate-x-[100px] -translate-x-[40px] rotate-[6deg]"
+        innerClassName="rounded-3xl"
+      />
+      <LogoBox
+        imgSrc="/static/home/anthropic.webp"
+        className="w-[150px] h-[150px] 2xl:w-[190px] 2xl:h-[190px] absolute bottom-20 right-1/3 2xl:translate-x-[50px] translate-x-[50px] rotate-[13deg]"
+        innerClassName="bg-white p-4"
+      />
+      <LogoBox
+        imgSrc="/static/home/mistral.webp"
+        className="w-24 h-24 2xl:w-28 2xl:h-28 absolute bottom-1/3 right-1/4 2xl:translate-x-[100px] translate-x-[100px] -rotate-[15deg]"
+        innerClassName="bg-white p-2"
+      />
+      <LogoBox
+        imgSrc="/static/home/groq.svg"
+        className="w-[120px] h-[120px] 2xl:w-[160px] 2xl:h-[160px] absolute top-1/2 2xl:-translate-y-[100px] -translate-y-[50px] right-0 2xl:-translate-x-[150px] -translate-x-[80px] rotate-[27deg]"
+        innerClassName="bg-white p-2"
+      />
+      <LogoBox
+        imgSrc="/static/home/logo3.webp"
+        className="w-28 h-28 2xl:w-32 2xl:h-32 absolute bottom-0 right-1/4 2xl:translate-x-[100px] translate-x-[180px] rotate-[-32deg]"
+        innerClassName="bg-white p-2"
+      />
+      <LogoBox
+        imgSrc="/static/home/logo4.webp"
+        className="w-20 h-20 2xl:w-24 2xl:h-24 absolute bottom-24 right-0 2xl:-translate-x-[150px] -translate-x-[60px] rotate-[-15deg]"
+      />
+    </div>
+  );
+};
+
+export default Hero;
--- bifrost/components/layout/navbar.tsx
+++ bifrost/components/layout/navbar.tsx
@@ -10,8 +10,11 @@ import {
   UsersIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import { Button } from "../ui/button";
 
-interface NavBarProps {}
+interface NavBarProps {
+  stars?: number;
+}
 
 const MobileHeader = (props: {
   menuDispatch: [boolean, (menuOpen: boolean) => void];
@@ -90,16 +93,16 @@ const NavLinks = () => {
       label: "Changelog",
     },
     {
-      href: "https://us.helicone.ai/open-stats",
-      label: "Stats",
-    },
-    {
       href: "/community",
       label: "Community",
     },
     {
       href: "/blog",
       label: "Blog",
+    },
+    {
+      href: "https://us.helicone.ai/open-stats",
+      label: "Stats",
     },
   ];
   return (
@@ -108,11 +111,11 @@ const NavLinks = () => {
         <Link
           href={link.href}
           className={
-            "flex flex-row items-center font-medium hover:text-black rounded-md px-3 py-1.5 focus:outline-none " +
+            "flex flex-row items-center font-regular hover:text-black rounded-md px-3 py-1.5 focus:outline-none " +
             " " +
             (path === link.href
               ? "text-black font-bold"
-              : "text-gray-600 opacity-75")
+              : "text-landing-description opacity-75")
           }
           key={`${link}-${i}`}
         >
@@ -201,14 +204,11 @@ const MobileNav = () => {
 };
 
 const NavBar = (props: NavBarProps) => {
-  const {} = props;
-  const path = usePathname();
-
   return (
-    <div className="bg-inherit top-0 sticky z-30 border-b border-gray-200">
+    <div className="bg-white top-0 sticky z-30 border-b border-gray-200">
       <MobileNav />
       <nav
-        className="gap-x-3 mx-auto md:flex max-w-6xl items-center py-3 hidden px-3 justify-between"
+        className="gap-x-3 mx-auto md:flex sm:px-16 md:px-24 2xl:px-40 max-w-[2000px] items-center py-3 hidden justify-between"
         aria-label="Global"
       >
         <div className="flex items-center md:col-span-1 order-1">
@@ -238,23 +238,49 @@ const NavBar = (props: NavBarProps) => {
         </div>
         <div className="w-full mt-4 md:mt-0 flex gap-x-1 items-center text-sm col-span-8 md:col-span-6 order-3 md:order-2 justify-between">
           <NavLinks />
-          <div className="flex items-center gap-x-2">
-            <NavIcons />
+          <div className="flex items-center gap-x-3">
+            <a
+              href="https://github.com/helicone/helicone"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button variant="outline" className="gap-x-2 rounded-lg">
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <p className="text-sm text-[#64748B]">
+                  {props.stars
+                    ? props.stars.toLocaleString("en-US", {
+                        notation: "compact",
+                        compactDisplay: "short",
+                      })
+                    : "0"}
+                </p>
+              </Button>
+            </a>
+            <Link href="/contact">
+              <Button
+                variant="secondary"
+                className="text-sm text-landing-description rounded-lg"
+              >
+                Contact us
+              </Button>
+            </Link>
+            <Link href="https://us.helicone.ai/signin">
+              <Button className="text-sm text-white rounded-lg bg-brand">
+                Log In
+              </Button>
+            </Link>
+            {/* <NavIcons /> */}
           </div>
-        </div>
-
-        <div className="flex items-center justify-end gap-x-2 col-span-1 order-2 md:order-3">
-          <Link
-            href="https://us.helicone.ai/signin"
-            className={
-              (path === "/"
-                ? "text-gray-500"
-                : "bg-sky-500 text-white hover:bg-sky-600 border-2 border-sky-700 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 px-4 py-1.5") +
-              " whitespace-nowrap rounded-md text-sm font-semibold  focus-visible:outline-sky-500"
-            }
-          >
-            Log In
-          </Link>
         </div>
       </nav>
     </div>
