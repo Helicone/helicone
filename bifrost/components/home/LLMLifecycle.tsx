86646ce99e48d21bf0d1d08a6e629f2c7721b967
Kavin Valli <mail@kavin.me>
2024-11-18 10:29:29 -0800

    fix order of llm lifecycle for mobile

--- bifrost/components/home/LLMLifecycle.tsx
+++ bifrost/components/home/LLMLifecycle.tsx
@@ -9,7 +9,7 @@ const SVG = () => (
   <svg
     // width="754"
     // height="525"
-    className="w-full h-full"
+    className="w-full h-full order-2 md:order-1"
     viewBox="0 0 754 525"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
