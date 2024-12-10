f1a30dfc5639460367b41071c6b6881fd8249c8b
LinaLam <tt3lam@uwaterloo.ca>
2024-12-09 17:35:30 -0800

    updated pricing page, landing page banner and changelog

--- bifrost/components/home/Banner.tsx
+++ bifrost/components/home/Banner.tsx
@@ -5,12 +5,12 @@ const Banner = () => {
     <div
       className={cn("flex flex-col justify-center w-full h-auto pb-4 relative")}
     >
-      <div className="bg-slate-100 text-sky-700 text-center text-sm md:text-base py-2 w-full">
+      <div className="bg-slate-100 text-slate-500 text-center text-sm md:text-base py-3 w-full">
         <p>
-          Our holiday gift to you:{" "}
+          🎁 Our holiday gift to you:{" "}
           <a
             href="https:/docs.helicone.ai/experiments"
-            className="font-semibold"
+            className="font-semibold underline hover:text-slate-600"
           >
             Experiments
           </a>{" "}
