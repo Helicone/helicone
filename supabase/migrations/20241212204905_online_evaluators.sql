78043bafcac38ca900b7ed00c7d1d608cc518860
Kavin Valli <mail@kavin.me>
2024-12-16 17:51:58 -0800

    fix migrations

--- supabase/migrations/20241212204905_online_evaluators.sql
+++ supabase/migrations/20241212204905_online_evaluators.sql
@@ -24,3 +24,8 @@ alter table "public"."online_evaluators" add constraint "public_online_evaluator
 
 alter table "public"."online_evaluators" validate constraint "public_online_evaluators_organization_fkey";
 
+alter table "public"."score_attribute" add column "evaluator_id" uuid;
+
+alter table "public"."score_attribute" add constraint "public_score_attribute_evaluator_id_fkey" FOREIGN KEY (evaluator_id) REFERENCES evaluator(id) not valid;
+
+alter table "public"."score_attribute" validate constraint "public_score_attribute_evaluator_id_fkey";
