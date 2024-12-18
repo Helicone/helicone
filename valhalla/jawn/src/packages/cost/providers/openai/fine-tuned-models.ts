91bab48a55412600299b8118f65c629a0b77f84f
Justin <justintorre75@gmail.com>
2024-12-18 14:30:47 -0800

    fix cost build

--- valhalla/jawn/src/packages/cost/providers/openai/fine-tuned-models.ts
+++ valhalla/jawn/src/packages/cost/providers/openai/fine-tuned-models.ts
@@ -16,4 +16,24 @@ export const costs: ModelRow[] = [
       completion_token: 0.000006,
     },
   },
+  {
+    model: {
+      operator: "startsWith",
+      value: "ft:gpt-4o-mini-2024-07-18:",
+    },
+    cost: {
+      prompt_token: 0.0000003,
+      completion_token: 0.0000012
+    }
+  },
+  {
+    model: {
+      operator: "startsWith",
+      value: "ft:gpt-4o-2024-08-06:",
+    },
+    cost: {
+      prompt_token: 0.00000375,
+      completion_token: 0.000015
+    }
+  }
 ];
