733feb1b4f52b5edfa9698b0d6fb9228c57847a6
LinaLam <65078897+LinaLam@users.noreply.github.com>
2024-12-22 00:49:12 -0500

    openai o3 draft (#3091)

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -216,6 +216,11 @@ export type BlogStructure =
 const blogContent: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "openai-o3",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "claude-3.5-sonnet-vs-openai-o1",
     },
   },
