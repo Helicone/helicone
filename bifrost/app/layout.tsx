e80bc389d5788c97310ad2627a619bdc34d239bd
colegottdank <colegottdank@gmail.com>
2024-12-30 21:24:17 -0500

    Add reb2b tracking script to layout and app components (#3109)

--- bifrost/app/layout.tsx
+++ bifrost/app/layout.tsx
@@ -80,6 +80,12 @@ export default async function RootLayout({
             }}
           />
           <Script
+            id="rb2b-snippet"
+            dangerouslySetInnerHTML={{
+              __html: `!function(){var reb2b=window.reb2b=window.reb2b||[];if(reb2b.invoked)return;reb2b.invoked=true;reb2b.methods=["identify","collect"];reb2b.factory=function(method){return function(){var args=Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;}};for(var i=0;i<reb2b.methods.length;i++){var key=reb2b.methods[i];reb2b[key]=reb2b.factory(key);}reb2b.load=function(key){var script=document.createElement("script");script.type="text/javascript";script.async=true;script.src="https://s3-us-west-2.amazonaws.com/b2bjsstore/b/"+key+"/LNKLDHM4VMOJ.js.gz";var first=document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script,first);};reb2b.SNIPPET_VERSION="1.0.1";reb2b.load("LNKLDHM4VMOJ");}();`,
+            }}
+          />
+          <Script
             id="google-analytics"
             strategy="afterInteractive"
             src="https://www.googletagmanager.com/gtag/js?id=G-WGDEGPP49F"
