8893ea03c7000a3cf63a6bace22bd0050ce1efe8
LinaLam <65078897+LinaLam@users.noreply.github.com>
2025-01-15 14:53:11 -0800

    landing page chanegs (#3132)

--- bifrost/components/home/Quote3.tsx
+++ bifrost/components/home/Quote3.tsx
@@ -6,28 +6,29 @@ const Quote3 = () => {
     <div className="bg-white sm:bg-gradient-to-b sm:from-white sm:to-[#F2F9FC] pt-10 pb-20 sm:pb-40">
       <div className={cn(ISLAND_WIDTH)}>
         <div className="flex flex-col gap-y-9 lg:flex-row justify-between items-start lg:items-end">
-          <h2 className="text-2xl md:text-[40px] tracking-tight leading-normal md:leading-[52px] font-semibold text-[#ACB3BA] lg:max-w-[780px] text-wrap mr-20">
-            {" "}
+          <h2 className="text-2xl md:text-[40px] tracking-tight leading-normal md:leading-[52px] font-semibold text-[#ACB3BA] w-full lg:max-w-[780px] text-wrap mr-20">
             Thank you for an{" "}
             <span className="text-black">
               excellent observability platform!
             </span>{" "}
-            . I pretty much use it for all my AI apps now.
+            I pretty much use it for all my AI apps now.
           </h2>
-          <div className="flex flex-col gap-3">
-            <img src="/static/togetherai.webp" alt="qawolf" className="w-36" />
-            <div className="flex items-center gap-6">
+          <div className="flex items-end gap-6">
+            <img
+              src="/static/home/hassan.webp"
+              alt="Hassan El Mghari"
+              className="w-12 h-12"
+            />
+            <div className="flex flex-col gap-2">
               <img
-                src="/static/home/hassan.webp"
-                alt="Hassan El Mghari"
-                className="w-12 h-12"
+                src="/static/togetherai.webp"
+                alt="qawolf"
+                className="w-28 pb-2"
               />
-              <div className="flex flex-col gap-1">
-                <h4 className="text-[13px] sm:text-xl font-medium">
-                  Hassan El Mghari
-                </h4>
-                <p className="text-[11px] sm:text-lg">Devrel Lead</p>
-              </div>
+              <h4 className="text-[17px] sm:text-xl font-medium whitespace-nowrap">
+                Hassan El Mghari
+              </h4>
+              <p className="text-[15px] sm:text-lg w-auto">DevRel Lead</p>
             </div>
           </div>
         </div>
