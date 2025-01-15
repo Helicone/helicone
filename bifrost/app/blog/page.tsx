191d36994524a191a37d8941a304e3e0967dfef8
LinaLam <65078897+LinaLam@users.noreply.github.com>
2025-01-15 15:26:23 -0800

    Blog mini vs 3.5 (#3129)

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -216,6 +216,11 @@ export type BlogStructure =
 const blogContent: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "gpt-4o-mini-vs-claude-3.5-sonnet",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "tree-of-thought-prompting",
     },
   },
