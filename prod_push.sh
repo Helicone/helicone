374b25c464625f1209fbc606bfafaca95b126255
Justin Torre <justintorre75@gmail.com>
2025-01-29 12:23:25 -0800

    Update prod_push.sh

--- prod_push.sh
+++ prod_push.sh
@@ -5,6 +5,7 @@ project_ref=${project_ref_input:-$default_project_ref}
 echo "Pushing to project ref: $project_ref"
 supabase link --project-ref $project_ref
 supabase db push
+Dakjdhlsadhfsadf
 
 jhkfdhdf
 salgkjsdfsda
