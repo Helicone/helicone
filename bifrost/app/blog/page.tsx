8579b650fb5e6fdc5ca466f4a0e2073b8310378b
LinaLam <tt3lam@uwaterloo.ca>
2025-02-17 14:59:56 -0800

    added images

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -216,6 +216,11 @@ export type BlogStructure =
 const blogContent: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "helicone-vs-traceloop",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "deepseek-janus-pro",
     },
   },
@@ -462,11 +467,6 @@ const blogContent: BlogStructure[] = [
   {
     dynmaicEntry: {
       folderName: "llm-observability",
-    },
-  },
-  {
-    dynmaicEntry: {
-      folderName: "open-webui-alts",
     },
   },
   {
