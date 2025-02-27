1d486540387b47fa653a7bb37c1d4b393dd2c553
Justin Torre <justintorre75@gmail.com>
2025-02-27 11:30:06 -0800

    Revert "Added a better view for Function calls and added a JSON Rendering function" (#3343)
    * Revert "docs (#3342)"
    
    This reverts commit c75d117c499925fdf6dc6048e86b34fcaae5de78.
    
    * keep this

--- web/components/shared/themed/themedDiv.tsx
+++ web/components/shared/themed/themedDiv.tsx
@@ -21,10 +21,7 @@ const ThemedDiv: React.FC<ThemedDivProps> = ({
 
   return (
     <>
-      <div className="flex-1 overflow-y-auto bg-white dark:bg-black px-5 pt-5">
-        {children}
-      </div>
-      <div className="px-2 py-1 sticky top-0 bg-gray-50 dark:bg-black z-10 border-t border-gray-200 dark:border-gray-800">
+      <div className="px-2 py-1 sticky top-0 bg-white dark:bg-black z-10 border-b border-gray-200 dark:border-gray-800">
         <div className="flex items-center justify-between">
           <Button
             size={"icon"}
@@ -35,6 +32,9 @@ const ThemedDiv: React.FC<ThemedDivProps> = ({
           </Button>
           <div className="ml-3 flex h-7 items-center">{actions}</div>
         </div>
+      </div>
+      <div className="flex-1 overflow-y-auto bg-white dark:bg-black px-5 pt-5">
+        {children}
       </div>
     </>
   );
