d914d4d20a6f7a8ed883c0b12c9940d2f7c88486
Tino Marques <59707674+tinomarques@users.noreply.github.com>
2025-03-04 13:34:12 -0500

    New: Helicone Realtime API - Phase 2 (#3352)
    * WIP: readover later
    
    * Misc: Removed _type
    
    * Fix: Example now resamples to correct sample rate
    
    * New: Realtime mapper and UI now correctly display audio data
    
    * Fix: Also mapping input_text type messages as text
    
    * WIP: stopping point
    
    * Fix: Tokens now logging for realtime
    
    * Update: Various tweaks and improvements to realtime messages UX
    
    * New: Realtime session highlighter
    
    * Fix: build
    
    * Misc: Moved location of mapper test
    
    * Misc: default
    
    * New: On Prem OpenRouter key backup for llm api
    
    * Update: from openrouter to openai for onPrem

--- web/components/templates/sessions/sessionId/Tree/Tree.tsx
+++ web/components/templates/sessions/sessionId/Tree/Tree.tsx
@@ -1,17 +1,9 @@
 import { useEffect, useState } from "react";
+import { HeliconeRequest } from "../../../../../lib/api/request/request";
 import { Trace, TreeNodeData } from "../../../../../lib/sessions/sessionTypes";
 import { clsx } from "../../../../shared/clsx";
 import { PathNode } from "./PathNode";
 import { RequestNode } from "./RequestNode";
-import { Col, Row } from "@/components/layout/common";
-import { SidebarCloseIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 
 export interface TreeNodeProps {
   node: TreeNodeData;
@@ -22,6 +14,11 @@ export interface TreeNodeProps {
   setShowDrawer: (x: boolean) => void;
   isRequestSingleChild?: boolean;
   onBoardingRequestTrace?: Trace;
+  realtimeData?: {
+    isRealtime: boolean;
+    effectiveRequests: HeliconeRequest[];
+    originalRequest: HeliconeRequest | null;
+  };
 }
 
 const TreeNode: React.FC<TreeNodeProps> = ({
@@ -33,9 +30,11 @@ const TreeNode: React.FC<TreeNodeProps> = ({
   setShowDrawer,
   isRequestSingleChild,
   onBoardingRequestTrace,
+  realtimeData,
 }) => {
   const [closeChildren, setCloseChildren] = useState(collapseAll ?? false);
   const [selectedRequestId, setSelectedRequestId] = selectedRequestIdDispatch;
+  const isRealtime = realtimeData?.isRealtime || false;
 
   useEffect(() => {
     setCloseChildren(collapseAll ?? false);
@@ -49,145 +48,130 @@ const TreeNode: React.FC<TreeNodeProps> = ({
       )}
       key={`${node.name}-${node.trace?.request_id}`}
     >
-      {!node.trace &&
-      node.children &&
-      (node.children.filter((c) => !c.trace).length ||
-        node.children.filter((c) => c.trace).length === 0 ||
-        node.children.filter((c) => c.trace).length > 1) ? (
-        <Col className="overflow-x-auto overflow-y-hidden">
-          <Row className="w-full group">
-            {new Array(level).fill(null).map((_, index) => (
-              <div
-                key={index}
-                className="h-9 bg-slate-50 dark:bg-slate-950 w-[24px] relative shrink-0 group-hover:bg-slate-100 dark:group-hover:bg-slate-900 group-hover:cursor-pointer"
-                onClick={() =>
-                  node.children
-                    ? setCloseChildren(!closeChildren)
-                    : setSelectedRequestId(node.trace?.request_id ?? "")
-                }
-              >
-                <div className="absolute top-0 right-[0px] w-[1px] h-9 bg-slate-200 dark:bg-slate-700 z-[2]" />
-              </div>
-            ))}
-            <PathNode
-              node={node}
-              setCloseChildren={setCloseChildren}
-              closeChildren={closeChildren}
-              setSelectedRequestId={setSelectedRequestId}
-              level={level}
-            />
-          </Row>
-          {!closeChildren &&
-            node.children.map((child, index) => (
-              <TreeNode
-                onBoardingRequestTrace={onBoardingRequestTrace}
-                key={index}
-                node={child}
-                selectedRequestIdDispatch={selectedRequestIdDispatch}
-                isLastChild={
-                  node.children?.length
-                    ? index === node.children?.length - 1
-                    : false
-                }
-                level={level + 1}
-                setShowDrawer={setShowDrawer}
-                isRequestSingleChild={node?.children?.length === 1}
-              />
-            ))}
-        </Col>
-      ) : (
-        <Row className="w-full group">
+      {level > 0 && (
+        <div
+          className={clsx(
+            "absolute h-full w-4 flex flex-col items-center",
+            {
+              "h-1/2 top-0": isLastChild && !node.children?.length,
+              "top-0": !(!node.children?.length && isLastChild),
+            },
+            "-left-5"
+          )}
+        >
           <div
             className={clsx(
-              "h-[42px] w-[24px]  shrink-0 group-hover:cursor-pointer sticky top-1/2 left-0 z-[2]",
-              selectedRequestId ===
-                (node.children
-                  ? node.children[0].trace?.request_id
-                  : node.trace?.request_id)
-                ? "bg-sky-100 dark:bg-slate-900"
-                : "bg-white dark:bg-slate-950 group-hover:bg-sky-50 dark:group-hover:bg-slate-800"
+              "w-[1px] bg-slate-200 dark:bg-slate-800 flex-1",
+              level === 1 ? "ml-4" : ""
             )}
-            onClick={() =>
-              node.children
-                ? setSelectedRequestId(node.children[0].trace?.request_id ?? "")
-                : setSelectedRequestId(node.trace?.request_id ?? "")
-            }
-          >
-            <div className="absolute top-0 right-[0px] w-[1px] h-[42px] bg-slate-200 dark:bg-slate-700 z-[2]" />
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger>
-                  <Button
-                    variant="ghost"
-                    className={clsx(
-                      "p-1 m-0 items-center hidden group-hover:flex z-[20]"
-                    )}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setSelectedRequestId(
-                        node.children
-                          ? node.children[0].trace?.request_id ?? ""
-                          : node.trace?.request_id ?? ""
-                      );
-                      setShowDrawer(true);
-                    }}
-                  >
-                    <SidebarCloseIcon className="w-4 h-4 text-slate-500" />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent side="right">
-                  <p>View request</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </div>
-
-          {level > 0 &&
-            new Array(level - 1).fill(null).map((_, index) => (
-              <div
-                key={index}
-                className={clsx(
-                  "h-[42px] w-[24px] relative shrink-0 group-hover:cursor-pointer",
-                  selectedRequestId ===
-                    (node.children
-                      ? node.children[0].trace?.request_id
-                      : node.trace?.request_id)
-                    ? "bg-sky-100 dark:bg-slate-900"
-                    : "bg-white dark:bg-slate-950 group-hover:bg-sky-50 dark:group-hover:bg-slate-900"
-                )}
-                onClick={() =>
-                  node.children
-                    ? setSelectedRequestId(
-                        node.children[0].trace?.request_id ?? ""
-                      )
-                    : setSelectedRequestId(node.trace?.request_id ?? "")
-                }
-              >
-                <div className="absolute top-0 right-[0px] w-[1px] h-[42px] bg-slate-200 dark:bg-slate-700 z-[2]" />
-              </div>
-            ))}
-
-          <RequestNode
-            isOnboardingRequest={
-              node.children
-                ? node.children[0].trace === onBoardingRequestTrace
-                : node.trace === onBoardingRequestTrace
-            }
-            selectedRequestId={selectedRequestId}
-            node={node.children ? node.children[0] : node}
-            setCloseChildren={setCloseChildren}
-            closeChildren={closeChildren}
-            setSelectedRequestId={setSelectedRequestId}
-            level={level}
-            setShowDrawer={setShowDrawer}
-            isRequestSingleChild={isRequestSingleChild ?? false}
-            label={node.children ? node.name : undefined}
-          />
-        </Row>
+          ></div>
+        </div>
+      )}
+      {/* Horizontal line for every node */}
+      {level > 0 && (
+        <div
+          className={clsx(
+            "absolute top-6 -left-5 w-4 h-[1px] bg-slate-200 dark:bg-slate-800"
+          )}
+        ></div>
+      )}
+      {level === 0 &&
+      (node.children?.length === 1 ||
+        (node.children?.length === 0 && node.trace?.request_id)) ? (
+        <div className="ml-7 tree-child-container">
+          {node.children?.length === 1 ? (
+            <TreeNode
+              node={node.children[0]}
+              selectedRequestIdDispatch={selectedRequestIdDispatch}
+              isLastChild={true}
+              level={level + 1}
+              key={0}
+              collapseAll={collapseAll}
+              setShowDrawer={setShowDrawer}
+              isRequestSingleChild={true}
+              onBoardingRequestTrace={onBoardingRequestTrace}
+              realtimeData={realtimeData}
+            />
+          ) : (
+            // Show node if its a request
+            node.trace?.request_id && (
+              <RequestNode
+                requestId={node.trace.request_id}
+                path={node.name}
+                selectedRequestIdDispatch={selectedRequestIdDispatch}
+                className="mb-2"
+                onBoardingRequestTrace={onBoardingRequestTrace}
+                isRequestSingleChild={true}
+                realtimeData={realtimeData}
+              />
+            )
+          )}
+        </div>
+      ) : (
+        // This is shown for every node unless its a request
+        <div className="w-full">
+          {node.trace?.request_id ? (
+            <RequestNode
+              requestId={node.trace.request_id}
+              path={node.name}
+              selectedRequestIdDispatch={selectedRequestIdDispatch}
+              className="mb-2"
+              onBoardingRequestTrace={onBoardingRequestTrace}
+              isRequestSingleChild={isRequestSingleChild}
+              realtimeData={realtimeData}
+            />
+          ) : (
+            // path node is for intermediate nodes - ones that aren't already request nodes
+            <PathNode
+              name={node.name ?? ""}
+              count={
+                countAllLeavesInNode(node) === 0
+                  ? node.children?.length ?? 0
+                  : countAllLeavesInNode(node)
+              }
+              onClose={() => setCloseChildren(!closeChildren)}
+              isClosed={closeChildren}
+            />
+          )}
+          {!closeChildren && node.children && (
+            // Show all children
+            <div
+              className={clsx(
+                "ml-7 tree-child-container",
+                level === 0 ? "border-0" : ""
+              )}
+            >
+              {node.children.map((child, i) => (
+                <TreeNode
+                  node={child}
+                  selectedRequestIdDispatch={selectedRequestIdDispatch}
+                  isLastChild={i === node.children!.length - 1}
+                  level={level + 1}
+                  key={i}
+                  collapseAll={collapseAll}
+                  setShowDrawer={setShowDrawer}
+                  onBoardingRequestTrace={onBoardingRequestTrace}
+                  realtimeData={realtimeData}
+                />
+              ))}
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
 };
+
+function countAllLeavesInNode(node: TreeNodeData): number {
+  if (!node.children || node.children.length === 0) {
+    return node.trace?.request_id ? 1 : 0;
+  }
+  return node.children.reduce(
+    (acc, child) => acc + countAllLeavesInNode(child),
+    0
+  );
+}
+
 interface TreeProps {
   data: TreeNodeData;
   className?: string;
@@ -196,6 +180,12 @@ interface TreeProps {
   setShowDrawer: (x: boolean) => void;
   onBoardingRequestTrace?: Trace;
   sessionId: string;
+  isRealtime?: boolean;
+  realtimeData?: {
+    isRealtime: boolean;
+    effectiveRequests: HeliconeRequest[];
+    originalRequest: HeliconeRequest | null;
+  };
 }
 
 export const Tree: React.FC<TreeProps> = ({
@@ -206,27 +196,27 @@ export const Tree: React.FC<TreeProps> = ({
   setShowDrawer,
   onBoardingRequestTrace,
   sessionId,
+  isRealtime = false,
+  realtimeData,
 }) => {
   return (
-    <div
-      className={clsx(
-        "font-sans bg-slate-50 dark:bg-black border-t border-slate-200 dark:border-slate-700",
-        className
-      )}
-    >
-      {data.children &&
-        data.children.map((child, index) => (
-          <TreeNode
-            key={index}
-            node={child}
-            selectedRequestIdDispatch={selectedRequestIdDispatch}
-            isLastChild={!!data.children && index === data.children.length - 1}
-            onBoardingRequestTrace={onBoardingRequestTrace}
-            level={0}
-            collapseAll={collapseAll}
-            setShowDrawer={setShowDrawer}
-          />
-        ))}
+    <div className={clsx("tree-container", className)}>
+      <TreeNode
+        node={data}
+        selectedRequestIdDispatch={selectedRequestIdDispatch}
+        isLastChild={true}
+        level={0}
+        collapseAll={collapseAll}
+        setShowDrawer={setShowDrawer}
+        onBoardingRequestTrace={onBoardingRequestTrace}
+        realtimeData={
+          realtimeData || {
+            isRealtime,
+            effectiveRequests: [],
+            originalRequest: null,
+          }
+        }
+      />
     </div>
   );
 };
