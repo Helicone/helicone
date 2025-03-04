1fc38763ab889e476e2b648e83bf0a6903d5eeb8
tinomarques <tinoamarques@gmail.com>
2025-03-04 20:40:13 +0000

    Revert "Realtime sessions revert (#3372)"
    This reverts commit 3b4f39f3a82d9981a11f2089be727a518909f386.

--- valhalla/jawn/src/lib/handlers/ResponseBodyHandler.ts
+++ valhalla/jawn/src/lib/handlers/ResponseBodyHandler.ts
@@ -97,7 +97,10 @@ export class ResponseBodyHandler extends AbstractLogHandler {
       context.processedLog.model = model;
 
       // Set usage
-      const usage = processedResponseBody.data?.usage ?? {};
+      const usage =
+        processedResponseBody.data?.usage ??
+        processedResponseBody.data?.processedBody?.usage ??
+        {};
       context.usage.completionTokens = usage.completionTokens;
       context.usage.promptTokens = usage.promptTokens;
       context.usage.totalTokens = usage.totalTokens;
