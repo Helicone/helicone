5bdd464b3627e1e34cb754cf32b9d08b11c68fc2
Justin <justintorre75@gmail.com>
2025-03-12 18:42:40 -0700

    rm index

--- web/filterAST/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as SearchableSelect } from "./SearchableSelect";
-export type { SearchableSelectOption } from "./SearchableSelect";
-
-export { default as SearchableInput } from "./SearchableInput";
-export type { SearchableInputOption } from "./SearchableInput";
