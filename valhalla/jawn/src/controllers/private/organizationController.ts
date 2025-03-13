62e32e58caeb378efc208713cce4bd237522d1d4
Justin <justintorre75@gmail.com>
2025-03-12 18:32:16 -0700

    revert old org controller

--- valhalla/jawn/src/controllers/private/organizationController.ts
+++ valhalla/jawn/src/controllers/private/organizationController.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  Patch,
   Path,
   Post,
   Query,
@@ -201,6 +200,58 @@ export class OrganizationController extends Controller {
     }
   }
 
+  @Post("/{organizationId}/create_filter")
+  public async createOrganizationFilter(
+    @Body()
+    requestBody: {
+      filters: OrganizationFilter[];
+      filterType: "dashboard" | "requests";
+    },
+    @Path() organizationId: string,
+    @Request() request: JawnAuthenticatedRequest
+  ): Promise<Result<null, string>> {
+    const organizationManager = new OrganizationManager(request.authParams);
+
+    const result = await organizationManager.createFilter(
+      organizationId,
+      requestBody.filters,
+      requestBody.filterType
+    );
+    if (result.error || !result.data) {
+      this.setStatus(500);
+      return err(result.error ?? "Error creating organization filter");
+    } else {
+      this.setStatus(201);
+      return ok(null);
+    }
+  }
+
+  @Post("/{organizationId}/update_filter")
+  public async updateOrganizationFilter(
+    @Body()
+    requestBody: {
+      filters: OrganizationFilter[];
+      filterType: "dashboard" | "requests";
+    },
+    @Path() organizationId: string,
+    @Request() request: JawnAuthenticatedRequest
+  ): Promise<Result<null, string>> {
+    const organizationManager = new OrganizationManager(request.authParams);
+
+    const result = await organizationManager.updateFilter(
+      organizationId,
+      requestBody.filterType,
+      requestBody.filters
+    );
+    if (result.error || !result.data) {
+      this.setStatus(500);
+      return err(result.error ?? "Error updating organization filter");
+    } else {
+      this.setStatus(201);
+      return ok(null);
+    }
+  }
+
   @Delete("/delete")
   public async deleteOrganization(
     @Request() request: JawnAuthenticatedRequest
@@ -214,6 +265,27 @@ export class OrganizationController extends Controller {
     } else {
       this.setStatus(201);
       return ok(null);
+    }
+  }
+
+  @Get("/{organizationId}/layout")
+  public async getOrganizationLayout(
+    @Path() organizationId: string,
+    @Query() filterType: string,
+    @Request() request: JawnAuthenticatedRequest
+  ): Promise<Result<OrganizationLayout, string>> {
+    const organizationManager = new OrganizationManager(request.authParams);
+
+    const result = await organizationManager.getOrganizationLayout(
+      organizationId,
+      filterType
+    );
+    if (result.error || !result.data) {
+      this.setStatus(500);
+      return err(result.error ?? "Error getting organization layout");
+    } else {
+      this.setStatus(200);
+      return ok(result.data);
     }
   }
 
