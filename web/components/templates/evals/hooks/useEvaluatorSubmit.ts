0d7c953c10003ba5777ef8e0ade4ff5dd149a614
Yusu-f <44467706+Yusu-f@users.noreply.github.com>
2025-03-15 01:36:01 +0100

    Add Mcp (#3430)
    * Add prompting-thinking-models page
    
    * add switch-to-deepseek
    
    * test
    
    * added cover image to when to switch to deepseek
    
    * added image
    
    * updated content
    
    * edit prompt-thinking-models
    
    * edit switch-to-deepseek
    
    * add open-webui-alts page
    
    * edit thinking models and delete open-webui-alts
    
    * add mcp
    
    * added image + fixed callout
    
    * Revert changes to evaluator files and themedTable.tsx
    
    * updated structure
    
    * Revert changes to themedTable.tsx and PythonEvaluatorConfigForm.tsx
    
    * reverted file
    
    * one more change
    
    ---------
    
    Co-authored-by: LinaLam <tt3lam@uwaterloo.ca>
    Co-authored-by: LinaLam <65078897+LinaLam@users.noreply.github.com>

--- web/components/templates/evals/hooks/useEvaluatorSubmit.ts
+++ web/components/templates/evals/hooks/useEvaluatorSubmit.ts
@@ -72,8 +72,8 @@ export const useLLMEvaluatorSubmit = (onSuccess: () => void) => {
         setIsSubmitting(false);
       }
     },
-    onSuccess: async () => {
-      await invalidateEvaluators.invalidate();
+    onSuccess: () => {
+      invalidateEvaluators.invalidate();
       onSuccess();
     },
   });
@@ -148,8 +148,8 @@ export const usePythonEvaluatorSubmit = (onSuccess: () => void) => {
         setIsSubmitting(false);
       }
     },
-    onSuccess: async () => {
-      await invalidateEvaluators.invalidate();
+    onSuccess: () => {
+      invalidateEvaluators.invalidate();
       onSuccess();
     },
   });
@@ -224,8 +224,8 @@ export const useLastMileEvaluatorSubmit = (onSuccess: () => void) => {
         setIsSubmitting(false);
       }
     },
-    onSuccess: async () => {
-      await invalidateEvaluators.invalidate();
+    onSuccess: () => {
+      invalidateEvaluators.invalidate();
       onSuccess();
     },
   });
