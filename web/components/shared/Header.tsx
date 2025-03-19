2a05bf4f08debe41f33df76d55d6c2c9e377e660
tinomarques <tinoamarques@gmail.com>
2025-03-19 16:34:38 +0000

    Misc: gap tweak

--- web/components/shared/Header.tsx
+++ web/components/shared/Header.tsx
@@ -10,12 +10,12 @@ export default function Header(props: HeaderProps) {
   const { title, leftActions, rightActions } = props;
   return (
     <header className="h-16 shrink-0 bg-slate-100 dark:bg-slate-900 flex flex-row justify-between items-center w-full border-b border-border px-4">
-      <div className="h-full flex flex-row items-center gap-4">
+      <div className="h-full flex flex-row items-center gap-2">
         <P className="text-muted-foreground font-semibold">{title}</P>
         {leftActions}
       </div>
 
-      <div className="h-full flex flex-row items-center gap-4">
+      <div className="h-full flex flex-row items-center gap-2">
         {rightActions}
       </div>
     </header>
