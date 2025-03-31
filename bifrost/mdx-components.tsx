7961ebac636f667f4fc18ac8b12a4a4a315ec0b1
colegottdank <colegottdank@gmail.com>
2025-03-31 15:30:41 -0700

    fix table responsiveness (#3516)

--- bifrost/mdx-components.tsx
+++ bifrost/mdx-components.tsx
@@ -3,6 +3,11 @@ import { CallToAction } from "@/components/blog/CallToAction";
 import { BottomLine } from "@/components/blog/BottomLine";
 import { Questions } from "@/components/blog/Questions";
 import { FAQ } from "./components/blog/FAQ";
+import { ReactNode } from "react";
+
+const ResponsiveTable = ({ children }: { children: ReactNode }) => {
+  return <div className="overflow-x-auto w-full">{children}</div>;
+};
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -11,5 +16,10 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     BottomLine: BottomLine,
     Questions: Questions,
     FAQ: FAQ,
+    table: (props) => (
+      <ResponsiveTable>
+        <table {...props} />
+      </ResponsiveTable>
+    ),
   };
 }
