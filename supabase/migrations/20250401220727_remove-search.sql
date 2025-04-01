ca02596f12026d625fed24a5bb2a42fa8ac2178e
Justin <justintorre75@gmail.com>
2025-04-01 15:25:47 -0700

    drop if exists

--- supabase/migrations/20250401220727_remove-search.sql
+++ supabase/migrations/20250401220727_remove-search.sql
@@ -1,7 +1,7 @@
 drop table "public"."request_response_search";
-alter table "public"."response" drop column "body";
+alter table "public"."response" drop column IF EXISTS "body";
 -- Drop the dependent policy first
-DROP POLICY "Enable select access for all users" ON public.response;
+DROP POLICY IF EXISTS "Enable select access for all users" ON public.response;
 -- Drop the policy on the request table
-DROP POLICY "Enable read access for all users" ON public.request;
-alter table "public"."request" drop column "body";
+DROP POLICY IF EXISTS "Enable read access for all users" ON public.request;
+alter table "public"."request" drop column IF EXISTS "body";
