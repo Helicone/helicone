33f7a5e47cfef7f9c6720a142823ccf60438a792
colegottdank <colegottdank@gmail.com>
2025-04-01 10:41:05 -0700

    remove warmly and koala

--- bifrost/app/layout.tsx
+++ bifrost/app/layout.tsx
@@ -74,12 +74,6 @@ export default async function RootLayout({
           <Analytics />
           <SpeedInsights />
           <Script
-            id="koala-snippet"
-            dangerouslySetInnerHTML={{
-              __html: `!function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_3d24ae9e69e18decfcb68b9d7b668c4501b5/sdk.js"),(document.body || document.head).appendChild(n)}();`,
-            }}
-          />
-          <Script
             id="rb2b-snippet"
             dangerouslySetInnerHTML={{
               __html: `!function(){var reb2b=window.reb2b=window.reb2b||[];if(reb2b.invoked)return;reb2b.invoked=true;reb2b.methods=["identify","collect"];reb2b.factory=function(method){return function(){var args=Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;}};for(var i=0;i<reb2b.methods.length;i++){var key=reb2b.methods[i];reb2b[key]=reb2b.factory(key);}reb2b.load=function(key){var script=document.createElement("script");script.type="text/javascript";script.async=true;script.src="https://s3-us-west-2.amazonaws.com/b2bjsstore/b/"+key+"/LNKLDHM4VMOJ.js.gz";var first=document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script,first);};reb2b.SNIPPET_VERSION="1.0.1";reb2b.load("LNKLDHM4VMOJ");}();`,
@@ -101,11 +95,6 @@ export default async function RootLayout({
               gtag('config', 'G-WGDEGPP49F');
             `,
             }}
-          />
-          <Script
-            id="warmly-script-loader"
-            src="https://opps-widget.getwarmly.com/warmly.js?clientId=489345224dd85359988138e0fb912d04"
-            defer
           />
         </body>
       </PHProvider>
