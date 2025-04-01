6e7a819eb6208ee1b90a85e2b4a8c565299b6879
colegottdank <colegottdank@gmail.com>
2025-04-01 11:10:18 -0700

    Fix middleware (#3522)

--- bifrost/middleware.ts
+++ bifrost/middleware.ts
@@ -167,6 +167,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
     "/career",
     "/developer",
     "/features/customer-portal",
