e04fbe4cfe57c0a266f50b343020294daa8e6db4
tinomarques <tinoamarques@gmail.com>
2025-04-04 21:58:56 +0100

    Misc: build

--- web/lib/api/metrics/modelMetrics.ts
+++ web/lib/api/metrics/modelMetrics.ts
@@ -4,6 +4,8 @@ export interface ModelMetrics {
   sum_prompt_tokens: number;
   prompt_cache_write_tokens: number;
   prompt_cache_read_tokens: number;
+  prompt_audio_tokens: number;
+  completion_audio_tokens: number;
   sum_completion_tokens: number;
   sum_tokens: number;
 }
