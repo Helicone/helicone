56f8ad5aa9c5a6f33a8620732592d904b580b8ff
Justin Torre <justintorre75@gmail.com>
2025-04-07 12:00:48 -0700

    frontend-rewrite/ backend routes for frontend rewrite (#3560)

---
+++ valhalla/jawn/src/controllers/private/userFeedbackController.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Post, Request, Route, Security, Tags } from "tsoa";
+import { dbExecute } from "../../lib/shared/db/dbExecute";
+import { JawnAuthenticatedRequest } from "../../types/request";
+
+@Route("v1/user-feedback")
+@Tags("User Feedback")
+@Security("api_key")
+export class UserFeedbackController extends Controller {
+  @Post("/")
+  public async postUserFeedback(
+    @Request() request: JawnAuthenticatedRequest,
+    @Body()
+    body: {
+      feedback: string;
+      tag: string;
+    }
+  ) {
+    const result = await dbExecute(
+      `INSERT INTO user_feedback (feedback, organization_id, tag) VALUES ($1, $2, $3)`,
+      [body.feedback, request.authParams.organizationId, body.tag]
+    );
+    if (result.error) {
+      this.setStatus(500);
+      return {
+        error: result.error,
+      };
+    }
+    return {
+      success: true,
+    };
+  }
+}
