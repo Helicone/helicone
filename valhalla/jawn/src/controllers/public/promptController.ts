3495907eb31d959d30b062b92664c1129c85318f
Justin <justintorre75@gmail.com>
2025-04-08 19:30:04 -0700

    rm lo

--- valhalla/jawn/src/controllers/public/promptController.ts
+++ valhalla/jawn/src/controllers/public/promptController.ts
@@ -537,7 +537,6 @@ export class PromptController extends Controller {
     @Request() request: JawnAuthenticatedRequest,
     @Path() user_defined_id: string
   ): Promise<Result<PromptVersionResultFilled, string>> {
-    console.log("requestBody", requestBody);
     const promptManager = new PromptManager(request.authParams);
     const result = await promptManager.getPormptVersionsTemplates(
       {
