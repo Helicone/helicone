ca3584ee83bd4028c89eb8787bac64d322443bc1
tinomarques <tinoamarques@gmail.com>
2025-04-16 22:53:02 +0100

    Refactor: Replace react-beautiful-dnd with react-dnd

--- web/components/shared/themed/table/columns/viewColumns.tsx
+++ web/components/shared/themed/table/columns/viewColumns.tsx
@@ -13,6 +13,8 @@ import {
 } from "@/components/ui/tooltip";
 import { Column } from "@tanstack/react-table";
 import { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { LuColumns3, LuInfo } from "react-icons/lu";
 import { Col } from "../../../../layout/common/col";
 import { Row } from "../../../../layout/common/row";
@@ -72,99 +74,101 @@ export default function ViewColumns<T>(props: ViewColumnsProps<T>) {
           align="center"
           sideOffset={8}
         >
-          <Row className="h-full">
-            <Col className="relative flex-1 h-full p-4">
-              <div className="flex flex-row items-center justify-start space-x-2 mb-4">
-                <h3 className="text-xs text-foreground font-medium">
-                  Column Reorder
-                </h3>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <button
-                      type="button"
-                      className="cursor-default"
-                      onClick={(e) => e.stopPropagation()}
-                    >
-                      <LuInfo className="h-5 w-5 text-muted-foreground" />
-                    </button>
-                  </TooltipTrigger>
-                  <TooltipContent side="left">
-                    <p>The ordering only affects your Requests table</p>
-                  </TooltipContent>
-                </Tooltip>
-              </div>
-              <i className="text-xs text-slate-500">
-                Note: If something is not shown, or your columns are in an
-                unexpected state, please click{" "}
-                <span className="font-bold">Back to preset</span> on the bottom
-                left of the right panel
-              </i>
+          <DndProvider backend={HTML5Backend}>
+            <Row className="h-full">
+              <Col className="relative flex-1 h-full p-4">
+                <div className="flex flex-row items-center justify-start space-x-2 mb-4">
+                  <h3 className="text-xs text-foreground font-medium">
+                    Column Reorder
+                  </h3>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <button
+                        type="button"
+                        className="cursor-default"
+                        onClick={(e) => e.stopPropagation()}
+                      >
+                        <LuInfo className="h-5 w-5 text-muted-foreground" />
+                      </button>
+                    </TooltipTrigger>
+                    <TooltipContent side="left">
+                      <p>The ordering only affects your Requests table</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+                <i className="text-xs text-slate-500">
+                  Note: If something is not shown, or your columns are in an
+                  unexpected state, please click{" "}
+                  <span className="font-bold">Back to preset</span> on the
+                  bottom left of the right panel
+                </i>
 
-              <div className="overflow-y-auto h-[calc(100%-6rem)]">
-                <DragList items={activeColumns} setItems={setActiveColumns} />
-              </div>
-              <div className="absolute bottom-0 left-0 right-0 h-6 bg-gradient-to-t from-white dark:from-black to-transparent pointer-events-none"></div>
-            </Col>
-            <Separator
-              className="dark:border-slate-800"
-              orientation="vertical"
-            />
-            <Col className="flex-1 flex flex-col h-full">
-              <div className="flex-grow overflow-hidden p-4">
-                <ColumnOptions
-                  categories={categories}
-                  selectedCategory={selectedCategory}
-                  setSelectedCategory={setSelectedCategory}
-                  columns={columns}
-                  activeColumns={activeColumns}
-                  setActiveColumns={setActiveColumns}
-                />
-              </div>
-              <div className="border-t border-border p-4">
-                <Row className="flex justify-between items-center gap-2">
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    onClick={() => {
-                      setActiveColumns(columnDefsToDragColumnItems(columns));
-                    }}
-                    className="text-xs"
-                  >
-                    Back to preset
-                  </Button>
-                  <Row>
+                <div className="overflow-y-auto h-[calc(100%-6rem)]">
+                  <DragList items={activeColumns} setItems={setActiveColumns} />
+                </div>
+                <div className="absolute bottom-0 left-0 right-0 h-6 bg-gradient-to-t from-white dark:from-black to-transparent pointer-events-none"></div>
+              </Col>
+              <Separator
+                className="dark:border-slate-800"
+                orientation="vertical"
+              />
+              <Col className="flex-1 flex flex-col h-full">
+                <div className="flex-grow overflow-hidden p-4">
+                  <ColumnOptions
+                    categories={categories}
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                    columns={columns}
+                    activeColumns={activeColumns}
+                    setActiveColumns={setActiveColumns}
+                  />
+                </div>
+                <div className="border-t border-border p-4">
+                  <Row className="flex justify-between items-center gap-2">
                     <Button
                       variant="ghost"
                       size="sm"
                       onClick={() => {
-                        setActiveColumns(
-                          activeColumns.map((c) => ({
-                            ...c,
-                            shown: false,
-                          }))
-                        );
+                        setActiveColumns(columnDefsToDragColumnItems(columns));
                       }}
                       className="text-xs"
                     >
-                      Deselect All
+                      Back to preset
                     </Button>
-                    <Button
-                      variant="ghost"
-                      size="sm"
-                      onClick={() => {
-                        setActiveColumns(
-                          activeColumns.map((c) => ({ ...c, shown: true }))
-                        );
-                      }}
-                      className="text-xs"
-                    >
-                      Select All
-                    </Button>
+                    <Row>
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        onClick={() => {
+                          setActiveColumns(
+                            activeColumns.map((c) => ({
+                              ...c,
+                              shown: false,
+                            }))
+                          );
+                        }}
+                        className="text-xs"
+                      >
+                        Deselect All
+                      </Button>
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        onClick={() => {
+                          setActiveColumns(
+                            activeColumns.map((c) => ({ ...c, shown: true }))
+                          );
+                        }}
+                        className="text-xs"
+                      >
+                        Select All
+                      </Button>
+                    </Row>
                   </Row>
-                </Row>
-              </div>
-            </Col>
-          </Row>
+                </div>
+              </Col>
+            </Row>
+          </DndProvider>
         </DropdownMenuContent>
       </DropdownMenu>
     </TooltipProvider>
