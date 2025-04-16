37d5f5a115787939a71ff1f74ff66e64811c5152
tinomarques <tinoamarques@gmail.com>
2025-04-16 22:26:49 +0100

    temp

--- web/components/templates/sessions/sessionsPage.tsx
+++ web/components/templates/sessions/sessionsPage.tsx
@@ -16,15 +16,19 @@ import { SortDirection } from "@/services/lib/sorts/requests/sorts";
 import { TimeFilter } from "@/types/timeFilter";
 import { ChartPieIcon, ListBulletIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   getTimeIntervalAgo,
   TimeInterval,
 } from "../../../lib/timeCalculations/time";
 import { useDebounce } from "../../../services/hooks/debounce";
 import { useSessionNames, useSessions } from "../../../services/hooks/sessions";
-import { Row } from "../../layout/common/row";
-import ThemedTable from "../../shared/themed/themedTable";
+import {
+  columnDefsToDragColumnItems,
+  DragColumnItem,
+} from "../../shared/themed/table/columns/DragList";
+import ViewColumns from "../../shared/themed/table/columns/viewColumns";
+import ThemedTable from "../../shared/themed/table/themedTable";
 import ThemedTimeFilter from "../../shared/themed/themedTimeFilter";
 import { INITIAL_COLUMNS } from "./initialColumns";
 import SessionMetrics from "./SessionMetrics";
@@ -73,6 +77,12 @@ const TABS = [
 const SessionsPage = (props: SessionsPageProps) => {
   const { sort } = props;
   const router = useRouter();
+  const tableRef = useRef<any>(null);
+
+  // State for active columns
+  const [activeColumns, setActiveColumns] = useState<DragColumnItem[]>(
+    columnDefsToDragColumnItems(INITIAL_COLUMNS)
+  );
 
   const [timeFilter, setTimeFilter] = useState<{
     start: Date;
@@ -176,59 +186,55 @@ const SessionsPage = (props: SessionsPageProps) => {
     }
   };
 
-  return (
-    <Tabs
-      value={currentTab}
-      onValueChange={(value) => setCurrentTab(value)}
-      className="w-full"
-    >
-      <div>
-        {hasSessions || isSessionsLoading ? (
-          <>
-            <header className="h-16 px-4 w-full flex flex-row items-center justify-between bg-background border-b border-border">
-              <div className="flex flex-row items-center gap-2">
-                <Small className="font-semibold">Sessions /</Small>
+  return hasSessions || isSessionsLoading ? (
+    <main className="h-screen w-full">
+      <Tabs
+        value={currentTab}
+        onValueChange={(value) => setCurrentTab(value)}
+        className="w-full"
+      >
+        <header className="h-16 px-4 w-full flex flex-row items-center justify-between bg-background border-b border-border">
+          <div className="flex flex-row items-center gap-2">
+            <Small className="font-semibold">Sessions /</Small>
 
-                <Select
-                  value={
-                    selectedName === ""
-                      ? UNNAMED_SESSION_VALUE // Map empty string to placeholder
-                      : selectedName ?? "all"
-                  }
-                  onValueChange={handleSelectSessionName}
-                >
-                  <SelectTrigger className="w-[280px] h-8">
-                    <SelectValue placeholder="Select a session" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="all">All Sessions</SelectItem>
-                    {allNames.sessions.map((session) => (
-                      <SelectItem
-                        key={session.name}
-                        value={
-                          session.name === ""
-                            ? UNNAMED_SESSION_VALUE
-                            : session.name
-                        }
-                      >
-                        {session.name === "" ? "Unnamed" : session.name}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
+            <Select
+              value={
+                selectedName === ""
+                  ? UNNAMED_SESSION_VALUE // Map empty string to placeholder
+                  : selectedName ?? "all"
+              }
+              onValueChange={handleSelectSessionName}
+            >
+              <SelectTrigger className="w-[280px] h-8">
+                <SelectValue placeholder="Select a session" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All Sessions</SelectItem>
+                {allNames.sessions.map((session) => (
+                  <SelectItem
+                    key={session.name}
+                    value={
+                      session.name === "" ? UNNAMED_SESSION_VALUE : session.name
+                    }
+                  >
+                    {session.name === "" ? "Unnamed" : session.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
 
-                <ThemedTimeFilter
-                  currentTimeFilter={getTimeFilterObject(
-                    timeFilter.start,
-                    timeFilter.end
-                  )}
-                  timeFilterOptions={[]}
-                  onSelect={onTimeSelectHandler}
-                  isFetching={isSessionsLoading}
-                  defaultValue={"1m"}
-                  custom={true}
-                />
-                {/* <InputWithIcon
+            <ThemedTimeFilter
+              currentTimeFilter={getTimeFilterObject(
+                timeFilter.start,
+                timeFilter.end
+              )}
+              timeFilterOptions={[]}
+              onSelect={onTimeSelectHandler}
+              isFetching={isSessionsLoading}
+              defaultValue={"1m"}
+              custom={true}
+            />
+            {/* <InputWithIcon
                   icon={MagnifyingGlassIcon}
                   placeholder="Search session id or name..."
                   value={sessionIdSearch ?? ""}
@@ -237,87 +243,73 @@ const SessionsPage = (props: SessionsPageProps) => {
                   }
                   className="w-64"
                 /> */}
-              </div>
-
-              <div className=" flex flex-row items-center rounded-lg bg-white dark:bg-black border border-border gap-1 shadow-sm">
-                <label className="text-xs px-2 py-1 border-r border-border">
-                  Views
-                </label>
-                <TabsList size={"xs"} variant={"secondary"}>
-                  {TABS.map((tab) => (
-                    <TabsTrigger
-                      variant={"secondary"}
-                      key={tab.id}
-                      value={tab.id}
-                      className="flex items-center gap-2"
-                    >
-                      {tab.icon}
-                    </TabsTrigger>
-                  ))}
-                </TabsList>
-              </div>
-            </header>
-
-            {!canCreate && (
-              <FreeTierLimitBanner
-                feature="sessions"
-                itemCount={allNames.sessions.length}
-                freeLimit={freeLimit}
-                className="w-full"
-              />
-            )}
-
-            <Row>
-              <div className="w-full border-r border-slate-200 dark:border-slate-800 overflow-x-auto">
-                <TabsContent value="sessions" className="m-0">
-                  <ThemedTable
-                    rows={sessions || []}
-                    columns={INITIAL_COLUMNS.map((col: any) => {
-                      const key =
-                        col.accessorKey ?? col.id ?? (col.header as string);
-                      const renderFn =
-                        typeof col.cell === "function"
-                          ? (row: any) =>
-                              col.cell({ row, getValue: () => row[key] } as any)
-                          : (row: any) => row[key];
-                      const className = col.meta?.className;
-
-                      return {
-                        name: col.header as string,
-                        key: key,
-                        hidden: false,
-                        render: renderFn,
-                        className: className,
-                      };
-                    })}
-                    viewHandler={(row) =>
-                      router.push(
-                        `/sessions/${encodeURIComponent(row.session_id)}`
-                      )
-                    }
-                  />
-                </TabsContent>
-                <TabsContent value="metrics">
-                  <SessionMetrics
-                    selectedSession={
-                      allNames.sessions.find(
-                        (session) => session.name === selectedName
-                      ) ?? null
-                    }
-                  />
-                </TabsContent>
-              </div>
-            </Row>
-          </>
-        ) : (
-          <div className="flex flex-col w-full h-screen bg-background dark:bg-sidebar-background">
-            <div className="flex flex-1 h-full">
-              <EmptyStateCard feature="sessions" />
-            </div>
           </div>
+
+          <div className=" flex flex-row items-center rounded-lg bg-white dark:bg-black border border-border gap-1 shadow-sm">
+            <ViewColumns
+              columns={tableRef.current?.getAllColumns() || []}
+              activeColumns={activeColumns}
+              setActiveColumns={setActiveColumns}
+            />
+            <label className="text-xs px-2 py-1 border-l border-border">
+              Views
+            </label>
+            <TabsList size={"xs"} variant={"secondary"}>
+              {TABS.map((tab) => (
+                <TabsTrigger
+                  variant={"secondary"}
+                  key={tab.id}
+                  value={tab.id}
+                  className="flex items-center gap-2"
+                >
+                  {tab.icon}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+          </div>
+        </header>
+
+        {!canCreate && (
+          <FreeTierLimitBanner
+            feature="sessions"
+            itemCount={allNames.sessions.length}
+            freeLimit={freeLimit}
+            className="w-full"
+          />
         )}
+
+        <TabsContent value="sessions" className="w-full">
+          <ThemedTable
+            id="sessions-table"
+            tableRef={tableRef}
+            defaultData={sessions || []}
+            defaultColumns={INITIAL_COLUMNS}
+            skeletonLoading={isLoading}
+            dataLoading={isLoading}
+            activeColumns={activeColumns}
+            setActiveColumns={setActiveColumns}
+            rowLink={(row: TSessions) =>
+              `/sessions/${encodeURIComponent(row.session_id)}`
+            }
+          />
+        </TabsContent>
+        <TabsContent value="metrics">
+          <SessionMetrics
+            selectedSession={
+              allNames.sessions.find(
+                (session) => session.name === selectedName
+              ) ?? null
+            }
+          />
+        </TabsContent>
+      </Tabs>
+    </main>
+  ) : (
+    <div className="flex flex-col w-full h-screen bg-background dark:bg-sidebar-background">
+      <div className="flex flex-1 h-full">
+        <EmptyStateCard feature="sessions" />
       </div>
-    </Tabs>
+    </div>
   );
 };
 
