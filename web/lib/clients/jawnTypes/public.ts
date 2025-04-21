ace1f8cdd6918b51b5f9ca2af43deac1ff1e7c8b
tinomarques <tinoamarques@gmail.com>
2025-04-21 15:09:36 +0100

    temp

--- web/lib/clients/jawnTypes/public.ts
+++ web/lib/clients/jawnTypes/public.ts
@@ -1784,6 +1784,8 @@ Json: JsonObject;
       completion_tokens: number;
       /** Format: double */
       total_tokens: number;
+      /** Format: double */
+      avg_latency: number;
     };
     "ResultSuccess_SessionResult-Array_": {
       data: components["schemas"]["SessionResult"][];
@@ -1824,6 +1826,8 @@ Json: JsonObject;
       first_used: string;
       /** Format: double */
       session_count: number;
+      /** Format: double */
+      avg_latency: number;
     };
     "ResultSuccess_SessionNameResult-Array_": {
       data: components["schemas"]["SessionNameResult"][];
