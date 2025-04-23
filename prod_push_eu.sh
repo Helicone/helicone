e09d90dd32d0c8dc538ee44925c149187abd8d54
Justin <justintorre75@gmail.com>
2025-04-23 11:12:36 -0700

    moved back to supabase dir

--- prod_push_eu.sh
+++ prod_push_eu.sh
@@ -2,5 +2,6 @@ project_ref_input=$1
 default_project_ref="lmahfbbnchpworytrrqk"
 project_ref=${project_ref_input:-$default_project_ref}
 echo "Pushing to project ref: $project_ref"
+cd db
 supabase link --project-ref $project_ref
 supabase db push
