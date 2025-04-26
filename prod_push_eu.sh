6d0bad4d0eaf5c4a2eb77bf4aa8236eb41042e83
Justin <justintorre75@gmail.com>
2025-04-25 18:12:24 -0700

    remove to db

--- prod_push_eu.sh
+++ prod_push_eu.sh
@@ -2,6 +2,5 @@ project_ref_input=$1
 default_project_ref="lmahfbbnchpworytrrqk"
 project_ref=${project_ref_input:-$default_project_ref}
 echo "Pushing to project ref: $project_ref"
-cd db
 supabase link --project-ref $project_ref
 supabase db push
