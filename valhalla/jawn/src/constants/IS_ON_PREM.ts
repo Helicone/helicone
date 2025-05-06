bb1deead8104bfc82796c6bf07a093eecffe7200
Justin <justintorre75@gmail.com>
2025-05-06 14:26:39 -0700

    is helicone cloud better check now

--- valhalla/jawn/src/constants/IS_ON_PREM.ts
+++ valhalla/jawn/src/constants/IS_ON_PREM.ts
@@ -1,7 +1,1 @@
-export const IS_ON_PREM =
-  process.env.AZURE_BASE_URL &&
-  process.env.AZURE_API_VERSION &&
-  process.env.AZURE_DEPLOYMENT_NAME &&
-  process.env.OPENAI_API_KEY
-    ? true
-    : false;
+export const IS_ON_PREM = !(process.env.IS_HELICONE_CLOUD === "true");
