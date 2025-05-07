46fe5bfc004810e64aa833fb41617809a0d6678b
Juliette Chevalier <juliettech.1393@gmail.com>
2025-05-07 17:06:51 -0400

    Add blog on self-hosting engineering journey

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -226,6 +226,11 @@ export const BLOG_CONTENT: BlogStructure[] = [
   },
   {
     dynmaicEntry: {
+      folderName: "self-hosting-journey",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "implement-and-monitor-cag",
     },
   },
