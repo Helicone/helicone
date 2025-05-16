87cf7f3f7d0490b585dd214dbc867e1accb6649e
connortbot <loiconnor8@gmail.com>
2025-05-16 13:50:05 -0700

    correct app router styling, router-flexible navbarv2

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -87,7 +87,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
             <DndProvider backend={HTML5Backend}>
               <OrgContextProvider>
                 <FilterProvider>
-                  <ThemeProvider>
+                  <ThemeProvider attribute="class" defaultTheme="light">
                     {children}
                   </ThemeProvider>
                 </FilterProvider>
