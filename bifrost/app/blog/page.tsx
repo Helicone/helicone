46434634dbedf7ddd6cb1899adadd04201f1cbf1
colegottdank <colegottdank@gmail.com>
2025-05-20 15:52:54 -0700

    changed blog name

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -221,6 +221,11 @@ export type BlogStructure =
 export const BLOG_CONTENT: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "building-production-grade-ai-applications",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "the-complete-llm-model-comparison-guide",
     },
   },
