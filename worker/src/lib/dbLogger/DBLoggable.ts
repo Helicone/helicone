fec10cccd32054997af8628a2175f7b960cd3463
Justin Torre <justintorre75@gmail.com>
2025-05-23 09:49:07 -0700

    don't cache cancelled requests (#3681)

--- worker/src/lib/dbLogger/DBLoggable.ts
+++ worker/src/lib/dbLogger/DBLoggable.ts
@@ -385,6 +385,10 @@ export class DBLoggable {
     };
   }
 
+  async getStatus() {
+    return await this.response.status();
+  }
+
   async getResponse() {
     const { body: responseBody, endTime: responseEndTime } =
       await this.response.getResponseBody();
