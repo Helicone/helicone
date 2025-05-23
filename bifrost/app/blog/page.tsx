1b0507f5f08b9dc3dd840645224b249273c6ee58
Yusu-f <44467706+Yusu-f@users.noreply.github.com>
2025-05-23 21:28:25 +0100

    Add Claude 4 (#3814)

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -221,6 +221,11 @@ export type BlogStructure =
 export const BLOG_CONTENT: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "claude-opus-and-sonnet-4-full-developer-guide",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "building-production-grade-ai-applications",
     },
   },
