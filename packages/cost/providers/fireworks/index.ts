92d442141223cd79dbec4fe1aff80f11f51479df
kronk0s <bessaadkhalil@gmail.com>
2025-05-31 02:26:44 +0100

    deepseek v3 fireworks cost (#3821)

--- packages/cost/providers/fireworks/index.ts
+++ packages/cost/providers/fireworks/index.ts
@@ -205,4 +205,14 @@ export const costs: ModelRow[] = [
       completion_token: 0.000003,
     },
   },
+  {
+    model: {
+      operator: "equals",
+      value: "accounts/fireworks/models/deepseek-v3",
+    },
+    cost: {
+      prompt_token: 0.0000009,
+      completion_token: 0.0000009,
+    },
+  },
 ];
