5fca030efb047effe2c146f5aba1e4bdcf1eeb0c
Thomas Harmon <thomas.alan.harmon@gmail.com>
2025-06-18 17:17:37 -0700

    feat: Add sidecar yaml, instructions (#95)

--- llm-proxy/src/config/helicone.rs
+++ llm-proxy/src/config/helicone.rs
@@ -4,14 +4,17 @@ use url::Url;
 use crate::{types::secret::Secret, utils::default_true};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(default, rename_all = "kebab-case", deny_unknown_fields)]
+#[serde(rename_all = "kebab-case", deny_unknown_fields)]
 pub struct HeliconeConfig {
     /// The API key to authenticate the AI Gateway to the Helicone control
     /// plane.
+    #[serde(default = "default_api_key")]
     pub api_key: Secret<String>,
     /// The base URL of Helicone.
+    #[serde(default = "default_base_url")]
     pub base_url: Url,
     /// The websocket URL of the Helicone control plane.
+    #[serde(default = "default_websocket_url")]
     pub websocket_url: Url,
     /// Whether to enable authentication.
     ///
@@ -24,17 +27,29 @@ pub struct HeliconeConfig {
 impl Default for HeliconeConfig {
     fn default() -> Self {
         Self {
-            api_key: Secret::from(
-                std::env::var("HELICONE_API_KEY")
-                    .unwrap_or("sk-helicone-...".to_string()),
-            ),
-            base_url: "http://localhost:8585".parse().unwrap(),
-            websocket_url: "ws://localhost:8585/ws/v1/router/control-plane"
-                .parse()
-                .unwrap(),
+            api_key: default_api_key(),
+            base_url: default_base_url(),
+            websocket_url: default_websocket_url(),
             enable_auth: true,
         }
     }
+}
+
+fn default_api_key() -> Secret<String> {
+    Secret::from(
+        std::env::var("HELICONE_API_KEY")
+            .unwrap_or("sk-helicone-...".to_string()),
+    )
+}
+
+fn default_base_url() -> Url {
+    "http://localhost:8585".parse().unwrap()
+}
+
+fn default_websocket_url() -> Url {
+    "ws://localhost:8585/ws/v1/router/control-plane"
+        .parse()
+        .unwrap()
 }
 
 #[cfg(feature = "testing")]
--- llm-proxy/src/config/response_headers.rs
+++ llm-proxy/src/config/response_headers.rs
@@ -3,11 +3,20 @@ use serde::{Deserialize, Serialize};
 use crate::utils::default_true;
 
 /// Response headers useful for additional observability.
-#[derive(Default, Debug, Clone, Copy, Deserialize, Serialize)]
+#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
 #[serde(deny_unknown_fields, rename_all = "kebab-case")]
 pub struct ResponseHeadersConfig {
     #[serde(default = "default_true")]
     pub provider: bool,
     #[serde(default = "default_true")]
     pub provider_request_id: bool,
+}
+
+impl Default for ResponseHeadersConfig {
+    fn default() -> Self {
+        Self {
+            provider: true,
+            provider_request_id: true,
+        }
+    }
 }
