0d83faa4b649d168fe135341105832340708aa62
Justin Torre <justintorre75@gmail.com>
2025-06-20 15:54:47 -0700

    better error handling on auth (#3968)
    Co-authored-by: Justin Torre <justin@helicone.ai>

--- valhalla/jawn/src/controlPlane/controlPlane.ts
+++ valhalla/jawn/src/controlPlane/controlPlane.ts
@@ -4,6 +4,7 @@ import { RequestWrapper } from "../lib/requestWrapper/requestWrapper";
 import { authFromRequest } from "../middleware/auth";
 import { toExpressRequest } from "../utils/expressHelpers";
 import { HeliconeRouterSocket, WebsocketListeners } from "./WebsocketListeners";
+import { MessageTypeRX } from "@helicone-package/llm-mapper/router-bindings";
 // Create a WebSocket server to handle the upgrade
 const wss = new WebSocketServer({ noServer: true });
 
@@ -17,13 +18,20 @@ export async function webSocketControlPlaneServer(
   const req = requestWrapper.getRequest();
   const auth = await authFromRequest(toExpressRequest(req));
   if (auth.error || !auth.data) {
-    socket.write(
-      JSON.stringify({
-        type: "error",
-        error: auth.error,
-      })
-    );
+    wss.handleUpgrade(req, socket, head, async (clientWs) => {
+      clientWs.send(
+        JSON.stringify({
+          _type: "Error",
+          Unauthorized: {
+            message: auth.error ?? "Unauthorized",
+          },
+        } as MessageTypeRX)
+      );
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 2_000));
     socket.destroy();
+    return;
   } else {
     wss.handleUpgrade(req, socket, head, async (clientWs) => {
       websocketListeners.addListener(auth.data.organizationId, {
