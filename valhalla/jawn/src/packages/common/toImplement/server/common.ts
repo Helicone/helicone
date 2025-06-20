b013c6ceb1e319d4469aae29798ee694e980698e
Justin Torre <justin@helicone.ai>
2025-06-19 17:34:44 -0700

    fix: common select for auth in jawn

--- valhalla/jawn/src/packages/common/toImplement/server/common.ts
+++ valhalla/jawn/src/packages/common/toImplement/server/common.ts
@@ -1,6 +1,4 @@
 import { KeyPermissions } from "../../auth/types";
-
-import { Database } from "../../../../lib/db/database.types";
 import { dbExecute } from "../../../../lib/shared/db/dbExecute";
 import { hashAuth } from "../../../../utils/hash";
 import { AuthResult } from "../../auth/types";
@@ -8,18 +6,20 @@ import { err, ok } from "../../result";
 
 export async function authenticateBearer(bearer: string): Promise<AuthResult> {
   const hashedBearer = await hashAuth(bearer.replace("Bearer ", ""));
-  let apiKey = await dbExecute<
-    Database["public"]["Tables"]["helicone_api_keys"]["Row"] & {
-      tier: string;
-    }
-  >(
+  let apiKey = await dbExecute<{
+    id: number;
+    user_id: string;
+    organization_id: string;
+    tier: string;
+    key_permissions: string;
+  }>(
     `SELECT 
-      helicone_api_keys.*,
-      organization.tier
-    FROM helicone_api_keys
-    INNER JOIN organization
-    ON helicone_api_keys.organization_id = organization.id
-    WHERE helicone_api_keys.api_key_hash = $1 AND helicone_api_keys.soft_delete = false`,
+        helicone_api_keys.id, 
+        helicone_api_keys.user_id, 
+        helicone_api_keys.organization_id, 
+        organization.tier as tier,
+        helicone_api_keys.key_permissions
+    FROM helicone_api_keys INNER JOIN organization ON helicone_api_keys.organization_id = organization.id WHERE helicone_api_keys.api_key_hash = $1 AND helicone_api_keys.soft_delete = false`,
     [hashedBearer]
   );
 
@@ -30,12 +30,20 @@ export async function authenticateBearer(bearer: string): Promise<AuthResult> {
   // I dont know how we are getting in this case... but we are in some cases - Justin (2023)
   if (apiKey.data?.length === 0) {
     const hashedBearer2 = await hashAuth(bearer);
-    apiKey = await dbExecute<
-      Database["public"]["Tables"]["helicone_api_keys"]["Row"] & {
-        tier: string;
-      }
-    >(
-      `SELECT helicone_api_keys.*, organization.tier FROM helicone_api_keys INNER JOIN organization ON helicone_api_keys.organization_id = organization.id WHERE helicone_api_keys.api_key_hash = $1 AND helicone_api_keys.soft_delete = false`,
+    apiKey = await dbExecute<{
+      id: number;
+      user_id: string;
+      organization_id: string;
+      tier: string;
+      key_permissions: string;
+    }>(
+      `SELECT 
+        helicone_api_keys.id, 
+        helicone_api_keys.user_id, 
+        helicone_api_keys.organization_id, 
+        organization.tier as tier,
+        helicone_api_keys.key_permissions
+      FROM helicone_api_keys INNER JOIN organization ON helicone_api_keys.organization_id = organization.id WHERE helicone_api_keys.api_key_hash = $1 AND helicone_api_keys.soft_delete = false`,
       [hashedBearer2]
     );
   }
