d601fc1842dff12f2cbc618b163bd7008ccdca5c
connortbot <loiconnor8@gmail.com>
2025-06-23 15:35:18 -0700

    add organnization uuid to prompt tables

--- supabase/migrations/20250619011938_prompts_2025.sql
+++ supabase/migrations/20250619011938_prompts_2025.sql
@@ -1,5 +1,6 @@
 create table "public"."prompts_2025" (
     "id" character varying not null,
+    "organization" uuid not null,
     "created_at" timestamp with time zone not null default now(),
     "name" character varying,
     "tags" text[],
@@ -14,6 +15,7 @@ revoke all on table "public"."prompts_2025" from anon, authenticated, public;
 
 create table "public"."prompts_2025_versions" (
     "created_at" timestamp with time zone not null default now(),
+    "organization" uuid not null,
     "prompt_id" character varying not null,
     "major_version" integer not null,
     "minor_version" integer,
@@ -39,6 +41,10 @@ alter table "public"."prompts_2025" add constraint "prompts_2025_production_vers
 
 alter table "public"."prompts_2025" validate constraint "prompts_2025_production_version_fkey";
 
+alter table "public"."prompts_2025" add constraint "prompts_2025_organization_fkey" FOREIGN KEY (organization) REFERENCES organization(id) ON DELETE CASCADE not valid;
+
+alter table "public"."prompts_2025" validate constraint "prompts_2025_organization_fkey";
+
 alter table "public"."prompts_2025_versions" add constraint "prompts_2025_versions_created_by_fkey" FOREIGN KEY (created_by) REFERENCES auth.users(id) ON DELETE SET NULL not valid;
 
 alter table "public"."prompts_2025_versions" validate constraint "prompts_2025_versions_created_by_fkey";
@@ -46,3 +52,7 @@ alter table "public"."prompts_2025_versions" validate constraint "prompts_2025_v
 alter table "public"."prompts_2025_versions" add constraint "prompts_2025_versions_prompt_id_fkey" FOREIGN KEY (prompt_id) REFERENCES prompts_2025(id) ON DELETE CASCADE not valid;
 
 alter table "public"."prompts_2025_versions" validate constraint "prompts_2025_versions_prompt_id_fkey";
+
+alter table "public"."prompts_2025_versions" add constraint "prompts_2025_versions_organization_fkey" FOREIGN KEY (organization) REFERENCES organization(id) ON DELETE CASCADE not valid;
+
+alter table "public"."prompts_2025_versions" validate constraint "prompts_2025_versions_organization_fkey";
