ee58821d2f8400149480d46ed1c5f51429eff29f
connortbot <loiconnor8@gmail.com>
2025-06-23 18:29:00 -0700

    prompt hooks and new controller, add create prompt feat

---
+++ valhalla/jawn/src/controllers/public/prompt2025Controller.ts
@@ -0,0 +1,50 @@
+// src/users/usersController.ts
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Path,
+  Post,
+  Request,
+  Route,
+  Security,
+  Tags,
+} from "tsoa";
+import { Result } from "../../packages/common/result";
+import { Prompt2025Manager } from "../../managers/prompt/PromptManager";
+import type { JawnAuthenticatedRequest } from "../../types/request";
+import { type OpenAIChatRequest } from "@helicone-package/llm-mapper/mappers/openai/chat-v2";
+
+export interface PromptCreateResponse {
+  id: string;
+}
+
+
+// TODO: Delete old promptController and rename this to promptController
+@Route("v1/prompt-2025")
+@Tags("Prompt2025")
+@Security("api_key")
+export class Prompt2025Controller extends Controller {
+  @Post("create")
+  public async createPrompt2025(
+    @Body()
+    requestBody: {
+      name: string;
+      tags: string[];
+      promptBody: OpenAIChatRequest;
+    },
+    @Request() request: JawnAuthenticatedRequest
+  ): Promise<Result<PromptCreateResponse, string>> {
+    const promptManager = new Prompt2025Manager(request.authParams);
+
+    const result = await promptManager.createPrompt(requestBody);
+    if (result.error || !result.data) {
+      this.setStatus(500);
+    } else {
+      this.setStatus(201); // set return status 201
+    }
+    return result;
+  }
+}
