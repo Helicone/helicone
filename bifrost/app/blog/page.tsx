f59141914e231d6ff11edfa097307967c4bb9350
colegottdank <colegottdank@gmail.com>
2025-06-26 18:42:00 +0300

    Introducing the AI Gateway (#3996)

--- bifrost/app/blog/page.tsx
+++ bifrost/app/blog/page.tsx
@@ -221,6 +221,11 @@ export type BlogStructure =
 export const BLOG_CONTENT: BlogStructure[] = [
   {
     dynmaicEntry: {
+      folderName: "introducing-ai-gateway",
+    },
+  },
+  {
+    dynmaicEntry: {
       folderName: "claude-opus-and-sonnet-4-full-developer-guide",
     },
   },
