631d3688cdd0e912cbb3db71799db88f5b2bd0e5
Charlie W <hcharlie1201@gmail.com>
2025-06-30 15:04:24 -0700

    feat: check low priority queue SQS cron (#4024)
    * feat: check low priority queue
    
    * feat: dont spam

--- helicone-cron/src/client/SqsClient.ts
+++ helicone-cron/src/client/SqsClient.ts
@@ -4,13 +4,16 @@ import { SQSClient, GetQueueAttributesCommand } from "@aws-sdk/client-sqs";
 export class SqsClient {
   private sqs: SQSClient;
   private queueUrl: string;
+  private queueUrlLowPriority: string;
+  private sqsLowPriority: SQSClient;
 
   constructor(env: Env) {
     if (
       !env.AWS_REGION ||
       !env.AWS_ACCESS_KEY_ID ||
       !env.AWS_SECRET_ACCESS_KEY ||
-      !env.REQUEST_LOGS_QUEUE_URL
+      !env.REQUEST_LOGS_QUEUE_URL ||
+      !env.REQUEST_LOGS_QUEUE_URL_LOW_PRIORITY
     ) {
       throw new Error(
         "Required AWS SQS environment variables are not set, SQSProducer will not be initialized."
@@ -25,7 +28,16 @@ export class SqsClient {
       },
     });
 
+    this.sqsLowPriority = new SQSClient({
+      region: env.AWS_REGION,
+      credentials: {
+        accessKeyId: env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+
     this.queueUrl = env.REQUEST_LOGS_QUEUE_URL;
+    this.queueUrlLowPriority = env.REQUEST_LOGS_QUEUE_URL_LOW_PRIORITY;
   }
 
   async getQueueSize(): Promise<number | null> {
@@ -34,6 +46,17 @@ export class SqsClient {
       AttributeNames: ["ApproximateNumberOfMessages"],
     });
     const response = await this.sqs.send(command);
+    return response.Attributes?.ApproximateNumberOfMessages
+      ? Number(response.Attributes.ApproximateNumberOfMessages)
+      : null;
+  }
+
+  async getQueueSizeLowPriority(): Promise<number | null> {
+    const command = new GetQueueAttributesCommand({
+      QueueUrl: this.queueUrlLowPriority,
+      AttributeNames: ["ApproximateNumberOfMessages"],
+    });
+    const response = await this.sqsLowPriority.send(command);
     return response.Attributes?.ApproximateNumberOfMessages
       ? Number(response.Attributes.ApproximateNumberOfMessages)
       : null;
--- helicone-cron/src/heartbeats/alertSqsCongestion.ts
+++ helicone-cron/src/heartbeats/alertSqsCongestion.ts
@@ -8,31 +8,53 @@ const ALERT_BANNER_ID_DELAY_IN_QUEUE = 7;
 export async function alertSqsCongestion(env: Env, alertManager: AlertManager) {
   const sqsClient = new SqsClient(env);
   const queueSize = await sqsClient.getQueueSize();
+  const queueSizeLowPriority = await sqsClient.getQueueSizeLowPriority();
 
   console.log("Queue size: ", queueSize);
 
   // If we can't determine queue size, don't change alert state
-  if (queueSize === null) {
+  if (queueSize === null || queueSizeLowPriority === null) {
     console.error("Failed to determine SQS queue size");
     return;
   }
 
-  if (queueSize >= 100000) {
-    await callJawn(
-      "/v1/public/alert-banner",
-      "PATCH",
+  if (queueSize >= 100000 || queueSizeLowPriority >= 100000) {
+    const alertBanners = await callJawn<
+      null,
       {
-        id: ALERT_BANNER_ID_DELAY_IN_QUEUE,
-        active: true,
-      },
-      env
+        data: {
+          updated_at: string;
+          title: string;
+          message: string;
+          id: string;
+          created_at: string;
+          active: boolean;
+        }[];
+      }
+    >("/v1/public/alert-banner", "GET", null, env);
+
+    const banner = alertBanners?.data?.find(
+      (banner) => banner.id === String(ALERT_BANNER_ID_DELAY_IN_QUEUE)
     );
 
-    await alertManager.sendSlackMessageToChannel(
-      env.SLACK_ALERT_CHANNEL,
-      `SQS size is too high..queue size: {${queueSize}}. Setting Alert Banner to active. Also reminder to set tasks in ECS to at least 10`
-    );
-  } else if (queueSize > 10) {
+    // If the banner is not active, we need to activate it
+    if (banner && banner.active == false) {
+      await callJawn(
+        "/v1/public/alert-banner",
+        "PATCH",
+        {
+          id: ALERT_BANNER_ID_DELAY_IN_QUEUE,
+          active: true,
+        },
+        env
+      );
+
+      await alertManager.sendSlackMessageToChannel(
+        env.SLACK_ALERT_CHANNEL,
+        `SQS size is too high..queue size: {${queueSize}}. Setting Alert Banner to active. Also reminder to set tasks in ECS to at least 10`
+      );
+    }
+  } else if (queueSize >= 0 || queueSizeLowPriority >= 0) {
     const alertBanners = await callJawn<
       null,
       {
