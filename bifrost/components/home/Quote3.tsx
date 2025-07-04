e8effcd86bda43efe601c51900a810ba5ce8b671
colegottdank <colegottdank@gmail.com>
2025-07-04 14:45:51 -0700

    final quote

--- bifrost/components/home/Quote3.tsx
+++ bifrost/components/home/Quote3.tsx
@@ -8,34 +8,32 @@ const Quote3 = () => {
       <div className={cn(ISLAND_WIDTH)}>
         <div className="flex flex-col gap-y-9 lg:flex-row justify-between items-start lg:items-end">
           <h2 className="text-2xl md:text-[40px] tracking-tight leading-normal md:leading-[52px] font-semibold text-[#ACB3BA] w-full lg:max-w-[780px] text-wrap mr-20">
-            Finally have{" "}
+            Helicone is{" "}
             <span className="text-black">
-              clear visibility into AI costs and usage patterns
+              essential for debugging our complex agentic flows
             </span>{" "}
-            across all our legal features.
+            for AI code reviews. Can't imagine building without it.
           </h2>
           <div className="flex items-end gap-6">
             <Image
-              src="/static/home/logos/chris-smith.webp"
-              alt="Chris Smith"
+              src="/static/home/logos/soohoon.webp"
+              alt="Soohoon Choi"
               width={48}
               height={48}
               className="w-12 h-12 rounded-full"
             />
             <div className="flex flex-col gap-2">
               <Image
-                src="/static/filevine.webp"
-                alt="Filevine"
+                src="/static/greptile.webp"
+                alt="Greptile"
                 width={112}
                 height={28}
                 className="w-28 pb-2"
               />
               <h4 className="text-[17px] sm:text-xl font-medium whitespace-nowrap">
-                Chris Smith
+                Soohoon Choi
               </h4>
-              <p className="text-[15px] sm:text-lg w-auto">
-                Principal Software Engineer & Legal Futurist Lead
-              </p>
+              <p className="text-[15px] sm:text-lg w-auto">CTO</p>
             </div>
           </div>
         </div>
