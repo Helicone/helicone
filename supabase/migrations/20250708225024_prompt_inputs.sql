f72cc093ecc07691de16116d3a1082d932bc6372
connortbot <loiconnor8@gmail.com>
2025-07-10 14:22:49 -0700

    request id to prompt inputs table

--- supabase/migrations/20250708225024_prompt_inputs.sql
+++ supabase/migrations/20250708225024_prompt_inputs.sql
@@ -1,5 +1,6 @@
 create table "public"."prompts_2025_inputs" (
     "id" uuid not null default gen_random_uuid(),
+    "request_id" uuid not null,
     "version_id" uuid default gen_random_uuid(),
     "created_at" timestamp with time zone not null default now(),
     "inputs" jsonb not null
@@ -14,3 +15,7 @@ alter table "public"."prompts_2025_inputs" add constraint "prompts_2025_inputs_p
 alter table "public"."prompts_2025_inputs" add constraint "prompts_2025_inputs_version_id_fkey" FOREIGN KEY (version_id) REFERENCES prompts_2025_versions(id) ON DELETE CASCADE not valid;
 
 alter table "public"."prompts_2025_inputs" validate constraint "prompts_2025_inputs_version_id_fkey";
+
+alter table "public"."prompts_2025_inputs" add constraint "prompts_2025_inputs_request_id_fkey" FOREIGN KEY (request_id) REFERENCES request(id) ON DELETE CASCADE not valid;
+
+alter table "public"."prompts_2025_inputs" validate constraint "prompts_2025_inputs_request_id_fkey";
