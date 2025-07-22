65b516e355c59cc86ad242db55b9f788ade6118e
colegottdank <colegottdank@gmail.com>
2025-07-22 03:12:13 +0300

    chore: remove deprecated request gateway router ID migration files (#4144)

--- supabase/migrations/20250716011118_request_gateway_router_id.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "public"."request" ADD COLUMN gateway_router_id UUID;
-ALTER TABLE "public"."request" ADD COLUMN gateway_deployment_target VARCHAR(255);
-
-ALTER TABLE "public"."request" ADD CONSTRAINT "request_gateway_router_id_fkey" FOREIGN KEY (gateway_router_id) REFERENCES "public"."routers"("id") ON UPDATE CASCADE ON DELETE CASCADE;
-
--- supabase/migrations/20250718011118_request_gateway_drop_router_id.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "public"."request" DROP COLUMN IF EXISTS gateway_router_id;
-ALTER TABLE "public"."request" DROP COLUMN IF EXISTS gateway_deployment_target;
-
-ALTER TABLE "public"."request" DROP CONSTRAINT IF EXISTS "request_gateway_router_id_fkey";
