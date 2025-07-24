df96a5a51250f9eab6baec6bfc4637ccc914271e
colegottdank <colegottdank@gmail.com>
2025-07-24 11:22:47 -0700

    remove unused using

--- web/components/templates/prompts/experiments/table/ExperimentTable.tsx
+++ web/components/templates/prompts/experiments/table/ExperimentTable.tsx
@@ -59,7 +59,6 @@ import ExperimentDatasetSelector from "../experimentDatasetSelector";
 import ImportCSVDialog from "./ImportCSVDialog";
 import { useFeatureLimit } from "@/hooks/useFreeTierLimit";
 import { FreeTierLimitBanner } from "@/components/shared/FreeTierLimitBanner";
-import { Deprecated } from "@/components/shared/Deprecated";
 
 type TableDataType = {
   index: number;
