399e2b7a4784e538a216a34b2a29a965a2126e97
connortbot <loiconnor8@gmail.com>
2025-07-27 20:31:47 -0700

    fix

---
+++ sdk/typescript/helpers/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  external: ['openai'],
+  bundle: true,
+}) 
