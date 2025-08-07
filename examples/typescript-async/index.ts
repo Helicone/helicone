e42200ce7f412451f7e69fc6e598e85d3d6c62eb
Thomas Harmon <thomas.alan.harmon@gmail.com>
2025-08-06 17:49:30 -0700

    task: bump async package deps+async logger example
    - update the dependencies in the `@helicone/async` TypeScript package to
    use `peerDependencies` so that developers using Helicone can avoid
    dependency version conflicts.
    - Major version bump of the version of the async package since we also
      did a major version bump of our dependencies
    - Add example to examples/typescript-async to demonstrate the usage of
      the `@helicone/async` package. this package was also used in the
      `examples/together` example, but i figured more examples here wouldn't
      be problematic

---
+++ examples/typescript-async/index.ts
@@ -0,0 +1,33 @@
+require("dotenv").config({
+  path: ".env",
+});
+
+import OpenAI from "openai";
+import { HeliconeAsyncLogger } from "@helicone/async";
+
+export function createOpenAIClient() {
+  const logger = new HeliconeAsyncLogger({
+    apiKey: process.env.HELICONE_API_KEY ?? "",
+    providers: {
+      openAI: OpenAI,
+    },
+  });
+  logger.init();
+
+  return new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY ?? "",
+  });
+}
+
+const client = createOpenAIClient();
+
+async function main() {
+  const response = await client.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [{ role: "user", content: "Hello, world!" }]
+  });
+
+  console.log(response.choices[0].message.content);
+}
+
+main();
