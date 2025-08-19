8d011d427ae509cf7a5dc6578a06a10da912ce46
connortbot <loiconnor8@gmail.com>
2025-08-19 11:11:30 -0700

    fix

--- valhalla/jawn/src/index.ts
+++ valhalla/jawn/src/index.ts
@@ -223,7 +223,7 @@ function errorHandler(
     console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
     return res.status(422).json({
       message: "Validation Failed",
-      details: err?.fields,
+      details: err.fields,
     });
   }
   if (err instanceof Error) {
