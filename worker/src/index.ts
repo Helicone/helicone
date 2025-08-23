81d13ca86d4cf6757963acbabbcac77debc89bc6
Connor Loi <loiconnor8@gmail.com>
2025-08-22 17:22:40 -0700

    fix: add eu valhalla url (#4512)
    * add eu valhalla url
    
    * indent

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -51,6 +51,7 @@ async function modifyEnvBasedOnPath(
   if (request.isEU()) {
     env = {
       ...env,
+      VALHALLA_URL: env.EU_VALHALLA_URL,
       CLICKHOUSE_HOST: env.EU_CLICKHOUSE_HOST,
       CLICKHOUSE_USER: env.EU_CLICKHOUSE_USER,
       CLICKHOUSE_PASSWORD: env.EU_CLICKHOUSE_PASSWORD,
