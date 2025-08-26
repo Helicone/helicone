9bc81bc56e0081727cc3151d2dc22bc83e36970f
Justin Torre <justin@helicone.ai>
2025-08-26 10:03:19 -0700

    Revert "Revert "Justin/blue green secrets jawn to packages (#4536)""
    This reverts commit b3113c4b6a5a5df978aaa509dfe11b5b8367d05c.

--- web/lib/supabaseServer.ts
+++ web/lib/supabaseServer.ts
@@ -1,9 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../db/database.types";
+import { SecretManager } from "@helicone-package/secrets/SecretManager";
 
 export const supabaseUrl =
-  process.env.SUPABASE_URL ?? process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY ?? "n/a";
+  process.env.SUPABASE_URL ??
+  process.env.NEXT_PUBLIC_SUPABASE_URL ??
+  SecretManager.getSecret("SUPABASE_URL") ??
+  "";
+
+const supabaseServiceKey =
+  process.env.SUPABASE_SERVICE_KEY ??
+  SecretManager.getSecret("SUPABASE_SERVICE_KEY") ??
+  "n/a";
 
 export const getSupabaseServer = () =>
   createClient<Database>(supabaseUrl, supabaseServiceKey);
