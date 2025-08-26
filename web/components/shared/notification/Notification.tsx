3303ef1387da78574fd5a6d6b944d497132796a7
connortbot <loiconnor8@gmail.com>
2025-08-26 15:10:20 -0700

    lint

--- web/components/shared/notification/Notification.tsx
+++ web/components/shared/notification/Notification.tsx
@@ -66,7 +66,7 @@ const Notification = () => {
       case "info":
         return (
           <InformationCircleIcon
-            className="text-yellow-400 h-5 w-5"
+            className="h-5 w-5 text-yellow-400"
             aria-hidden="true"
           />
         );
@@ -79,7 +79,10 @@ const Notification = () => {
         );
       default:
         return (
-          <CheckCircleIcon className="h-5 w-5 text-green-500" aria-hidden="true" />
+          <CheckCircleIcon
+            className="h-5 w-5 text-green-500"
+            aria-hidden="true"
+          />
         );
     }
   };
