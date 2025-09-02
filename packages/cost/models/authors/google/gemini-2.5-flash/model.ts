e0b3c9005d306f87007a45c718094912bde57946
colegottdank <colegottdank@gmail.com>
2025-09-02 10:14:19 -0700

    update desc

--- packages/cost/models/authors/google/gemini-2.5-flash/model.ts
+++ packages/cost/models/authors/google/gemini-2.5-flash/model.ts
@@ -5,7 +5,7 @@ export const model = {
     name: "Google: Gemini 2.5 Flash",
     author: "google",
     description:
-      'Gemini 2.5 Flash is Google\'s state-of-the-art workhorse model, specifically designed for advanced reasoning, coding, mathematics, and scientific tasks. It includes built-in "thinking" capabilities, enabling it to provide responses with greater accuracy and nuanced context handling. \n\nAdditionally, Gemini 2.5 Flash is configurable through the "max tokens for reasoning" parameter, as described in the documentation (https://openrouter.ai/docs/use-cases/reasoning-tokens#max-tokens-for-reasoning).',
+      'Gemini 2.5 Flash is Google\'s state-of-the-art workhorse model, specifically designed for advanced reasoning, coding, mathematics, and scientific tasks. It includes built-in "thinking" capabilities, enabling it to provide responses with greater accuracy and nuanced context handling. \n\nAdditionally, Gemini 2.5 Flash is configurable through the "max tokens for reasoning" parameter, allowing fine-tuned control over its reasoning process.',
     contextLength: 1048576,
     maxOutputTokens: 65535,
     created: "2025-06-17T08:01:28",
