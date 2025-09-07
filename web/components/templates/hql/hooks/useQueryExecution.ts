7145b6c4c4ffa0aa5c4be4128d4680be0204d77e
Devin Ersoy <devinat11@gmail.com>
2025-09-07 14:16:02 -0700

    feat(hql): monaco setup, query execution, agent handlers

---
+++ web/components/templates/hql/hooks/useQueryExecution.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { components } from "@/lib/clients/jawnTypes/public";
+import { createExecuteQueryMutation } from "../constants";
+
+export interface QueryExecutionState {
+  result: components["schemas"]["ExecuteSqlResponse"];
+  loading: boolean;
+  error: string | null;
+}
+
+export function useQueryExecution() {
+  const [result, setResult] = useState<
+    components["schemas"]["ExecuteSqlResponse"]
+  >({
+    rows: [],
+    elapsedMilliseconds: 0,
+    size: 0,
+    rowCount: 0,
+  });
+  const [queryLoading, setQueryLoading] = useState(false);
+  const [queryError, setQueryError] = useState<string | null>(null);
+
+  const { mutate: handleExecuteQuery, mutateAsync: handleExecuteQueryAsync } =
+    useMutation<any, Error, string>(
+      createExecuteQueryMutation(setResult, setQueryError, setQueryLoading)
+    );
+
+  return {
+    result,
+    queryLoading,
+    queryError,
+    handleExecuteQuery,
+    handleExecuteQueryAsync,
+    setResult,
+    setQueryError,
+    setQueryLoading,
+  };
+}
+
+
