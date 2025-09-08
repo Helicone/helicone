80ebad67a4befa95fc6ef80694fa112dc5df7ab7
Thomas Harmon <thomas.alan.harmon@gmail.com>
2025-09-08 13:18:43 -0700

    fix: Override status for errors in anthropic streams
    context: anthropic will always return a 200 status code, even when there
    is actually an error sent in the event stream itself. this would confuse
    customers as when looking at the logs in the Helicone dashboard,
    they would expect to see a response since they see a 200 status code,
    but instead would see some random JSON (the error). to fix this, we are
    allowing the stream body parsers to override the status code.

--- valhalla/jawn/src/lib/shared/bodyProcessors/IBodyProcessor.ts
+++ valhalla/jawn/src/lib/shared/bodyProcessors/IBodyProcessor.ts
@@ -11,6 +11,12 @@ export interface ParseInput {
 export type ParseOutput = {
   processedBody: any;
   usage?: Usage;
+  // Enables us to override successful status codes if an error
+  // occurs mid stream. Eg, Anthropic will always return a 200 for streams,
+  // but may send an error in the `data` event stream, in which case we want
+  // to override this so that the logs show up as an "error" in our Helicone
+  // dashboard.
+  statusOverride?: number;
 };
 
 export interface IBodyProcessor {
