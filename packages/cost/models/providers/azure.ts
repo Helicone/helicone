6f5a6097952a6a7359b5427920eaa2e61a6b2114
colegottdank <colegottdank@gmail.com>
2025-09-17 14:54:04 -0700

    use model for deployment name (#4792)

--- packages/cost/models/providers/azure.ts
+++ packages/cost/models/providers/azure.ts
@@ -19,14 +19,17 @@ export class AzureOpenAIProvider extends BaseProvider {
   ];
 
   buildUrl(endpoint: ModelProviderConfig, config: UserEndpointConfig): string {
-    if (!config.baseUri || !config.deploymentName) {
+    if (
+      !config.baseUri ||
+      (!config.deploymentName && !endpoint.providerModelId)
+    ) {
       throw new Error("Azure OpenAI requires baseUri and deploymentName");
     }
     const apiVersion = config.apiVersion || "2025-01-01-preview";
     const baseUri = config.baseUri.endsWith("/")
       ? config.baseUri
       : `${config.baseUri}/`;
-    const builtUrl = `${baseUri}openai/deployments/${config.deploymentName}/chat/completions?api-version=${apiVersion}`;
+    const builtUrl = `${baseUri}openai/deployments/${config.deploymentName ?? endpoint.providerModelId}/chat/completions?api-version=${apiVersion}`;
     return builtUrl;
   }
 
