107e1f95cb0431c1452c115cb02a0d2dbf802261
Justin Torre <justintorre75@gmail.com>
2025-09-18 11:30:20 -0700

    larger tll (#4816)
    * larger tll
    
    * larger tll
    
    ---------
    
    Co-authored-by: Justin Torre <justin@helicone.ai>

--- worker/RequestBodyBufferContainer/src/config.ts
+++ worker/RequestBodyBufferContainer/src/config.ts
@@ -6,6 +6,8 @@ export type AppConfig = {
   enableUnsafeRead: boolean;
 };
 
+export const DEFAULT_TTL_SECONDS = 60 * 10; // 10 minutes
+
 export function getConfig(): AppConfig {
   return {
     port: parseInt(process.env.PORT ?? "8000", 10),
@@ -13,7 +15,10 @@ export function getConfig(): AppConfig {
       process.env.MAX_SIZE_BYTES ?? `${256 * 1024 * 1024}`,
       10
     ),
-    ttlSeconds: parseInt(process.env.TTL_SECONDS ?? "120", 10),
+    ttlSeconds: parseInt(
+      process.env.TTL_SECONDS ?? DEFAULT_TTL_SECONDS.toString(),
+      10
+    ),
     logLevel: process.env.LOG_LEVEL ?? "info",
     enableUnsafeRead: (process.env.ENABLE_UNSAFE_READ ?? "true").toLowerCase() === "true",
   };
