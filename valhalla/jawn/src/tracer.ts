67b64d3cab0775e707ba351603eb774fbf80c0d6
Justin Torre <justin@helicone.ai>
2025-09-24 23:16:30 -0700

    disable datadog for a minute

--- valhalla/jawn/src/tracer.ts
+++ valhalla/jawn/src/tracer.ts
@@ -1,3 +1,3 @@
 import tracer from "dd-trace";
-tracer.init(); // initialized in a different file to avoid hoisting.
+// tracer.init(); // DISABLED - was causing high egress costs to Datadog
 export default tracer;
