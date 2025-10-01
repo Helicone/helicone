59d2e54d4a082537b96df8b1378b0b8d6a39763f
Justin <justintorre75@gmail.com>
2025-10-01 16:59:26 -0700

    idompotent

--- clickhouse/migrations/schema_71_request_id_index.sql
+++ clickhouse/migrations/schema_71_request_id_index.sql
@@ -1,5 +1,5 @@
 ALTER TABLE default.request_response_rmt
-  ADD INDEX idx_request_id_bf request_id TYPE bloom_filter(0.01) GRANULARITY 1;
+  ADD INDEX IF NOT EXISTS idx_request_id_bf request_id TYPE bloom_filter(0.01) GRANULARITY 1;
 
 
 ALTER TABLE default.request_response_rmt
