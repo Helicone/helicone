9d499f2095796ec74ccf3a4a131e171a5e70cd8f
Justin Torre <justintorre75@gmail.com>
2025-10-01 03:23:41 +0000

    fix install

--- .devcontainer/.build.sh
+++ .devcontainer/.build.sh
@@ -6,4 +6,4 @@ sudo dockerd > /tmp/dockerd.log 2>&1 &
 sleep 5
 cd /workspaces/helicone/docker && docker compose up minio minio-setup clickhouse -d 
 echo 'y' | npx supabase start -x realtime,storage-api,imgproxy,mailpit,edge-runtime,logflare,vector,supavisor 
-yarn intall
+yarn install
