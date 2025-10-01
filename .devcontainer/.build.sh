31a15e843d0ad70ad144d30331ea7bccfe5bd9d1
Justin Torre <justintorre75@gmail.com>
2025-10-01 03:16:52 +0000

    credt

--- .devcontainer/.build.sh
+++ .devcontainer/.build.sh
@@ -1,2 +1,9 @@
-cd /workspaces/helicone/docker && docker compose up minio minio-setup clickhouse -d &
-echo 'y' | npx supabase start -x realtime,storage-api,imgproxy,mailpit,edge-runtime,logflare,vector,supavisor &
+sudo service docker start
+sleep 5
+sudo chmod 666 /var/run/docker.sock
+
+sudo dockerd > /tmp/dockerd.log 2>&1 &
+sleep 5
+cd /workspaces/helicone/docker && docker compose up minio minio-setup clickhouse -d 
+echo 'y' | npx supabase start -x realtime,storage-api,imgproxy,mailpit,edge-runtime,logflare,vector,supavisor 
+yarn intall
