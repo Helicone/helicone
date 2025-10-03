0966f55436c5a39bb3d965bd8e915e9d43b96c2b
Justin Torre <justintorre75@gmail.com>
2025-10-03 11:16:45 -0700

    Justin/tools playground (#4943)

--- web/components/templates/prompts/id/arrayDiffViewer.tsx
+++ web/components/templates/prompts/id/arrayDiffViewer.tsx
@@ -67,7 +67,7 @@ const ArrayDiffViewer = (props: ArrayDiffViewerProps) => {
                     <RoleButton
                       role={originItem.role}
                       onRoleChange={function (
-                        role: "function" | "assistant" | "user" | "system",
+                        role: "function" | "assistant" | "user" | "system" | "tool",
                       ): void {}}
                       disabled={true}
                     />
@@ -79,7 +79,7 @@ const ArrayDiffViewer = (props: ArrayDiffViewerProps) => {
                 <RoleButton
                   role={targetItem.role}
                   onRoleChange={function (
-                    role: "function" | "assistant" | "user" | "system",
+                    role: "function" | "assistant" | "user" | "system" | "tool",
                   ): void {}}
                   disabled={true}
                 />
