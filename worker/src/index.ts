a1dacc1fc79405b5a5073a985cd40f50eab3cf8b
Justin Torre <justintorre75@gmail.com>
2025-10-29 09:31:48 -0700

    Apply suggestion from @chitalian

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -20,7 +20,7 @@ import { APIKeysManager } from "./lib/managers/APIKeysManager";
 import { SecretManagerClass } from "@helicone-package/secrets/SecretManager";
 import { ModelProviderName } from "@helicone-package/cost/models/providers";
 
-// Needed for migrationsthis m
+// Needed for migrations
 export { RequestBodyBufferContainer } from "./RequestBodyBuffer/RequestBodyContainer";
 
 const FALLBACK_QUEUE = "fallback-queue";
