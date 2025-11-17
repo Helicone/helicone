5363c2f95d0eb2705dbc151d7fe34e60645b0809
Justin <justintorre75@gmail.com>
2025-11-17 11:32:23 -0800

    fix playground cache id and update vercel and clean up code

--- worker/src/lib/clients/S3Client.ts
+++ worker/src/lib/clients/S3Client.ts
@@ -32,16 +32,6 @@ export class S3Client {
     );
   };
 
-  getRequestResponseImageUrl = (
-    requestId: string,
-    orgId: string,
-    assetId: string
-  ) => {
-    return this.getBaseUrl(
-      `organizations/${orgId}/requests/${requestId}/assets/${assetId}`
-    );
-  };
-
   getBaseUrl = (key: string) => {
     return this.endpoint
       ? `${this.endpoint}/${this.bucketName}/${key}`
@@ -81,70 +71,6 @@ export class S3Client {
         return {
           data: null,
           error: `Failed to store data: ${response.statusText}, ${response.url}, ${signedRequest.url}`,
-        };
-      }
-
-      return { data: url, error: null };
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      return { data: null, error: error?.message };
-    }
-  }
-
-  async uploadBase64ToS3(
-    buffer: Buffer,
-    assetType: string,
-    requestId: string,
-    orgId: string,
-    assetId: string
-  ): Promise<Result<string, string>> {
-    const uploadUrl = this.getRequestResponseImageUrl(
-      requestId,
-      orgId,
-      assetId
-    );
-
-    return await this.uploadToS3(uploadUrl, buffer, assetType);
-  }
-
-  async uploadImageToS3(
-    image: Blob,
-    requestId: string,
-    orgId: string,
-    assetId: string
-  ): Promise<Result<string, string>> {
-    const uploadUrl = this.getRequestResponseImageUrl(
-      requestId,
-      orgId,
-      assetId
-    );
-
-    return await this.uploadToS3(
-      uploadUrl,
-      await image.arrayBuffer(),
-      image.type
-    );
-  }
-
-  private async uploadToS3(
-    url: string,
-    body: ArrayBuffer | Buffer,
-    contentType: string
-  ): Promise<Result<string, string>> {
-    try {
-      const signedRequest = await this.awsClient.sign(url, {
-        method: "PUT",
-        body: body,
-        headers: {
-          "Content-Type": contentType,
-        },
-      });
-
-      const response = await fetch(signedRequest.url, signedRequest);
-      if (!response.ok) {
-        return {
-          data: null,
-          error: `Failed to store data: ${response.statusText}`,
         };
       }
 
