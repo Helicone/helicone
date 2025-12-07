bdf0bb5e6ea3cf274ae22a704ec8d1c750325669
Justin <justintorre75@gmail.com>
2025-12-06 23:13:35 -0800

    start

--- replicas/start.sh
+++ replicas/start.sh
@@ -8,7 +8,6 @@ sudo service docker start
 sleep 5
 cd /home/ubuntu/workspaces/helicone/docker && docker compose up minio minio-setup clickhouse -d 
 echo 'y' | npx supabase start -x realtime,storage-api,imgproxy,mailpit,edge-runtime,logflare,vector,supavisor 
-npx supabase db reset
 yarn install
 
 cd /home/ubuntu/workspaces/helicone
