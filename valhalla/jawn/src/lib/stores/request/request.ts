ea15105239d07df22a056fa949b7fe3e0a66f7de
Justin <justintorre75@gmail.com>
2025-12-09 10:04:28 -0800

    added prefilter to reduce the number of selects on request page

--- valhalla/jawn/src/lib/stores/request/request.ts
+++ valhalla/jawn/src/lib/stores/request/request.ts
@@ -170,6 +170,16 @@ export async function getRequestsClickhouseNoSort(
 
   const sortSQL = createdAtSort === "asc" ? "ASC" : "DESC";
   const query = `
+    WITH top_requests AS (
+      SELECT 
+          request_id,
+          request_created_at
+      FROM request_response_rmt
+      WHERE (${builtFilter.filter})
+      ORDER BY request_created_at DESC
+      LIMIT ${limit}
+      OFFSET ${offset}
+    )
     SELECT response_id,
       if(notEmpty(response_body), response_body, '{"helicone_message": "fetching body from signed_url... contact engineering@helicone.ai for more information"}') as response_body,
       response_created_at,
@@ -204,11 +214,12 @@ export async function getRequestsClickhouseNoSort(
       storage_location
     FROM request_response_rmt
     WHERE (
-      (${builtFilter.filter})
+      request_created_at >= (SELECT min(request_created_at) FROM top_requests)
+      AND request_created_at <= (SELECT max(request_created_at) FROM top_requests)
+      AND request_id IN (SELECT request_id FROM top_requests)
+      and (${builtFilter.filter})
     )
     ORDER BY organization_id ${sortSQL}, toStartOfHour(request_created_at) ${sortSQL}, request_created_at ${sortSQL}
-    LIMIT ${limit}
-    OFFSET ${offset}
   `;
 
   const requests = await dbQueryClickhouse<HeliconeRequest>(
