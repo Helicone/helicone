0fdd47eaa045c7c097c9189fa6e739a12da84624
Justin <justintorre75@gmail.com>
2025-12-31 16:00:04 -0800

    fixes

--- web/components/templates/pricing/hooks.ts
+++ web/components/templates/pricing/hooks.ts
@@ -1,5 +1,5 @@
 import { useOrg } from "@/components/layout/org/organizationContext";
-import { getJawnClient } from "@/lib/clients/jawn";
+import { getJawnClient, $JAWN_API } from "@/lib/clients/jawn";
 import { useQuery } from "@tanstack/react-query";
 
 export const useCostForPrompts = () => {
@@ -42,14 +42,19 @@ export const useCostForExperiments = () => {
 
 export const useBillingUsage = () => {
   const org = useOrg();
-  return useQuery({
-    queryKey: ["billing-usage", org?.currentOrg?.id],
-    queryFn: async () => {
-      const jawn = getJawnClient(org?.currentOrg?.id);
-      const result = await jawn.GET("/v1/stripe/subscription/usage-stats");
-      return result.data;
-    },
-    staleTime: 5 * 60 * 1000, // 5 minutes
-    enabled: !!org?.currentOrg?.id,
-  });
+
+  const result = $JAWN_API.useQuery(
+    "get",
+    "/v1/stripe/subscription/usage-stats",
+    {},
+    {
+      enabled: !!org?.currentOrg?.id,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    }
+  );
+
+  return {
+    ...result,
+    data: result.data?.data,
+  };
 };
